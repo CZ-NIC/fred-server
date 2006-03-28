1edb6399a2928d6fe0c11b02480cc050fcf05080
Petr Blaha <petr.blaha@nic.cz>
2006-03-28 15:25:10 +0000

    procisteno
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@93 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- timestamp.cc
+++ timestamp.cc
@@ -11,34 +11,14 @@ sscanf(string , "%4d-%02d-%02d %02d:%02d:%lf" ,
                 &dt.tm_year ,  &dt.tm_mon , &dt.tm_mday ,
                 &dt.tm_hour,   &dt.tm_min , &sec);
 
-
-printf("sec %lf\n" , sec );
- dt.tm_sec = (int ) sec ;
-printf(" %d  %d %d %d %d %d\n" , dt.tm_year , dt.tm_mon , dt.tm_mday , dt.tm_hour , dt.tm_min , dt.tm_sec  );
+// prevod sekund
+dt.tm_sec = (int ) sec ;
 // mesic o jeden mene
 dt.tm_mon = dt.tm_mon -1;
 // rok - 1900
 dt.tm_year = dt.tm_year - 1900;
 
-printf(" %d  %d %d\n" , dt.tm_year , dt.tm_mon , dt.tm_mday );
 
 return mktime(&dt);
 }
 
-
-/*
-main()
-{
-time_t t;
-
-t = get_gmt_time("2006-03-22 16:45:30.847079");
-
-printf("%s\n" , ctime( &t ) );
-
-t = time(NULL );
-printf("aktualni cas %s" , ctime( &t ) );
-
-printf("gmt cas %s" , asctime( gmtime( &t)  ) );
-
-}
-*/
