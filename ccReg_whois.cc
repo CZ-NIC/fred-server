130fd12e81fdf7b5ae8d4379379cd900f88c3e0d
Petr Blaha <petr.blaha@nic.cz>
2006-03-30 08:48:43 +0000

    dotazuje se primo do SQL databaze
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@100 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- ccReg_whois.cc
+++ ccReg_whois.cc
@@ -9,6 +9,17 @@
 #include <ccReg.hh>
 #include <ccReg_whois.h>
 
+// funkce pro praci s postgres
+#include "pqsql.h"
+
+// konverze casu
+#include "timestamp.h"
+
+
+// definice pripojeno na databazi
+// #define DATABASE "dbname=ccreg user=ccreg password=Eeh5ahSi"
+#define DATABASE "dbname=ccReg user=pblaha"
+
 
 //
 // Example implementational code for IDL interface ccReg::Whois
@@ -19,48 +30,71 @@ ccReg_Whois_i::ccReg_Whois_i(){
 ccReg_Whois_i::~ccReg_Whois_i(){
   // add extra destructor code here
 }
-//   Methods corresponding to IDL attributes and operations
-// !!!!!!!! testovaci verze slouzi na propojeni s CORBOU
+
 ccReg::DomainWhois* ccReg_Whois_i::Domain(const char* domain_name)
 {
+PQ PQsql;
+char sqlString[1024];
 ccReg::DomainWhois *dm;
-struct tm dt;
-char buf[256] , ns[32];
+int clid , did ;
+char  ns[256];
 int i;
 
-// vytvor cas
-dt.tm_year = 2006 - 1900 ;
-dt.tm_mon = 2;
-dt.tm_mday = 7;
-dt.tm_hour = 18;
-dt.tm_min = 32;
-dt.tm_sec = 45;
 
 dm = new ccReg::DomainWhois;
 
-dm->name = CORBA::string_alloc( 64 );
-dm->name = CORBA::string_dup( domain_name );
-dm->description = CORBA::string_alloc( 64 );
-dm->description = CORBA::string_dup( "POPIS domeny" );
-dm->status = 1;
-dm->registered = mktime(&dt); // cas vytvoreni domeny
-dm->registrarName =  CORBA::string_alloc( 32 );
-dm->registrarName = CORBA::string_dup( "JMENO_REGISTRATORA" );
-dm->registrarUrl  = CORBA::string_alloc( 64 );
-dm->registrarUrl  = CORBA::string_dup("http://www.registrator.cz" );
+// dotaz na domenu
+sprintf( sqlString , "SELECT * FROM DOMAIN WHERE fqdn=\'%s\'" , domain_name );
 
-// vynuluj 
-memset( buf , 256 ,0 );
-for( i = 0 ; i < 4 ; i ++ )
+if( PQsql.OpenDatabase( DATABASE ) )
 {
-  sprintf( ns , "ns%d.%s.cz" , i + 1 ,  domain_name );
+  if( PQsql.ExecSelect( sqlString ) )
+  {
+   dm->name= CORBA::string_dup(  PQsql.GetFieldValueName("fqdn" , 0 ) )  ; // plnohodnotne jmeno domeny
 
-  if( i > 0 ) strcat( buf , "\t" );
+   dm->status = 0; // TODO
+   dm->created =  get_gmt_time( PQsql.GetFieldValueName("CrDate" , 0 ) )  ; // datum a cas  vytvoreni domeny
+   dm->expired =  get_gmt_time( PQsql.GetFieldValueName("ExDate" , 0 ) )  ; // datum expirace
 
-  strcat( buf , ns ); 
+   clid = atoi(  PQsql.GetFieldValueName("clid" , 0 ) ); // client registrator
+   did = atoi(  PQsql.GetFieldValueName("id" , 0 ) ); // id domeny
+
+
+
+     // free select
+    PQsql.FreeSelect();
+    // dotaz na registratora
+    sprintf( sqlString , "SELECT Name , Url FROM  REGISTRAR WHERE id=%d;" , clid ) ;
+
+    if( PQsql.ExecSelect( sqlString ) )
+      {
+        dm->registrarName = CORBA::string_dup( PQsql.GetFieldValueName("Name" , 0 ) );
+        dm->registrarUrl  = CORBA::string_dup( PQsql.GetFieldValueName("Url" , 0 ) );
+        PQsql.FreeSelect();
+      }
+
+    // dotaz na nameservry
+       sprintf( sqlString , "SELECT fqdn  FROM  HOST WHERE domainid=%d;" , did ) ;
+
+       memset( ns , 256 ,0 );
+
+    if( PQsql.ExecSelect( sqlString ) )
+      {
+
+        for( i = 0 ; i <  PQsql.GetSelectRows() ; i ++ )   
+           {
+               if( i > 0 ) strcat( ns , "\t" );
+               strcat( ns ,  PQsql.GetFieldValueName("fqdn" , i ) );
+           }
+
+         dm->NameServers  =  CORBA::string_dup( ns ); // uloz nameservry
+        PQsql.FreeSelect();
+      }
+ 
+   }
+
+ PQsql.Disconnect();
 }
-dm->NameServers  =  CORBA::string_alloc( 256 );
-dm->NameServers  =  CORBA::string_dup( buf );
 
 return dm;
 }
