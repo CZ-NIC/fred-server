77879b58ddf2959fee3eb99fdd48f47394cf17f8
Petr Blaha <petr.blaha@nic.cz>
2006-04-06 14:36:31 +0000

    vracene error code definice
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@118 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ response.h
@@ -0,0 +1,42 @@
+#define    COMMAND_OK     1000
+#define    COMMAND_FAILED 2400
+/*
+#define   EPP_MSG_1000    "Command completed successfully"
+           1001    "Command completed successfully; action pending"
+   
+         <enumeration value="1000"/>
+         <enumeration value="1001"/>
+         <enumeration value="1300"/>
+         <enumeration value="1301"/>
+         <enumeration value="1500"/>
+         <enumeration value="2000"/>
+         <enumeration value="2001"/>
+         <enumeration value="2002"/>
+         <enumeration value="2003"/>
+         <enumeration value="2004"/>
+         <enumeration value="2005"/>
+         <enumeration value="2100"/>
+         <enumeration value="2101"/>
+         <enumeration value="2102"/>
+         <enumeration value="2103"/>
+         <enumeration value="2104"/>
+         <enumeration value="2105"/>
+         <enumeration value="2106"/>
+         <enumeration value="2200"/>
+         <enumeration value="2201"/>
+         <enumeration value="2202"/>
+         <enumeration value="2300"/>
+         <enumeration value="2301"/>
+         <enumeration value="2302"/>
+         <enumeration value="2303"/>
+         <enumeration value="2304"/>
+         <enumeration value="2305"/>
+         <enumeration value="2306"/>
+         <enumeration value="2307"/>
+         <enumeration value="2308"/>
+         <enumeration value="2400"/>
+         <enumeration value="2500"/>
+         <enumeration value="2501"/>
+         <enumeration value="2502"/>
+
+*/
