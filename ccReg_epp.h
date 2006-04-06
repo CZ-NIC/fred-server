f30cdb656b3f6558bbcfa1c2f1ee375ada2919ec
Petr Blaha <petr.blaha@nic.cz>
2006-04-06 14:38:25 +0000

    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@121 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- ccReg_epp.h
+++ ccReg_epp.h
@@ -4,36 +4,33 @@
 //
 
 class ccReg_EPP_i: public POA_ccReg::EPP {
-private:
-  // Make sure all instances are built on the heap by making the
-  // destructor non-public
-  //virtual ~ccReg_EPP_i();
+
 public:
   // standard constructor
   ccReg_EPP_i();
   virtual ~ccReg_EPP_i();
 
-  // methods corresponding to defined IDL attributes and operations
-  ccReg::Response* ClientLogin(const char* ClID);
-  ccReg::Response* ClientLogout();
-  ccReg::Response* ContactCheck(const char* roid);
-  ccReg::Response* ContactInfo(const char* roid, ccReg::Contact_out c);
-  ccReg::Response* ContactDelete(const char* roid);
-  ccReg::Response* ContactUpdate(const ccReg::Contact& c);
-  ccReg::Response* ContactCreate(const ccReg::Contact& c);
-  ccReg::Response* HostCheck(const char* name);
-  ccReg::Response* HostInfo(const char* name, ccReg::Host_out h);
-  ccReg::Response* HostDelete(const char* name);
-  ccReg::Response* HostCreate(const ccReg::Host& h);
-  ccReg::Response* HostUpdate(const ccReg::Host& h);
-  ccReg::Response* DomainCheck(const char* name);
-  ccReg::Response* DomainInfo(const char* name, ccReg::Domain_out d);
-  ccReg::Response* DomainDelete(const char* name);
-  ccReg::Response* DomainUpdate(const ccReg::Domain& d);
-  ccReg::Response* DomainCreate(const ccReg::Domain& d);
 
+  // methods corresponding to defined IDL attributes and operations
+  ccReg::Response* ClientLogin(const char* ClID, const char* clTRID, CORBA::Short& clientID);
+  ccReg::Response* ClientLogout(CORBA::Short clientID , const char* clTRID);
+  ccReg::Response* ContactCheck(const char* roid, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* ContactInfo(const char* roid, ccReg::Contact_out c, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* ContactDelete(const char* roid, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* ContactUpdate(const ccReg::Contact& c, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* ContactCreate(const ccReg::Contact& c, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* HostCheck(const char* name, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* HostInfo(const char* name, ccReg::Host_out h, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* HostDelete(const char* name, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* HostCreate(const ccReg::Host& h, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* HostUpdate(const ccReg::Host& h, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* DomainCheck(const char* name, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* DomainInfo(const char* name, ccReg::Domain_out d, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* DomainDelete(const char* name, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* DomainUpdate(const ccReg::Domain& d, CORBA::Short clientID, const char* clTRID);
+  ccReg::Response* DomainCreate(const ccReg::Domain& d, CORBA::Short clientID, const char* clTRID);
 
 private:
-int clientID; // id prihlaseneho registratora
+
 };
 
