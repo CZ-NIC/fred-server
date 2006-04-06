d9ae31b1c7eb2289ddceff4ee8995dfa3d7aa69d
Petr Blaha <petr.blaha@nic.cz>
2006-04-06 14:35:57 +0000

    definice typu akci corba funkci
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@117 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ action.h
@@ -0,0 +1,26 @@
+// definice kodu EPP v ccReg
+// prihlasovani
+#define EPP_ClientLogin         100
+#define EPP_ClientLogout        101
+
+// funkce pro praci s kontakty
+#define EPP_ContactCheck        200
+#define EPP_ContactInfo         201
+#define EPP_ContactDelete       202  
+#define EPP_ContactUpdate       203 
+#define EPP_ContactCreate       204
+#define EPP_ContactTransfer     205
+// funkce pro hosty
+#define EPP_HostCheck           300
+#define EPP_HostInfo            301 
+#define EPP_HostDelete          302 
+#define EPP_HostUpdate          303
+#define EPP_HostCreate          304
+// funkce pro domeny
+#define EPP_DomainCheck         400
+#define EPP_DomainInfo          401
+#define EPP_DomainDelete        402
+#define EPP_DomainUpdate        403  
+#define EPP_DomainCreate        404  
+#define EPP_DomainTransfer      405 
+
