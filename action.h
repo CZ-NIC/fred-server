6a75681af853c20b3122e00b235f26613f584464
Petr Blaha <petr.blaha@nic.cz>
2006-04-18 13:51:39 +0000

    NSSET a UnknowAction  pro serverTRID
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@158 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- action.h
+++ action.h
@@ -16,11 +16,22 @@
 #define EPP_HostDelete          302 
 #define EPP_HostUpdate          303
 #define EPP_HostCreate          304
-// funkce pro domeny
-#define EPP_DomainCheck         400
-#define EPP_DomainInfo          401
-#define EPP_DomainDelete        402
-#define EPP_DomainUpdate        403  
-#define EPP_DomainCreate        404  
-#define EPP_DomainTransfer      405 
 
+// funkce pro NSSET
+#define EPP_NSsetCheck           400
+#define EPP_NSsetInfo            401
+#define EPP_NSsetDelete          402
+#define EPP_NSsetUpdate          403
+#define EPP_NSsetCreate          404
+
+
+// funkce pro domeny
+#define EPP_DomainCheck         500
+#define EPP_DomainInfo          501
+#define EPP_DomainDelete        502
+#define EPP_DomainUpdate        503  
+#define EPP_DomainCreate        504  
+#define EPP_DomainTransfer      505 
+
+// dalsi funkce
+#define EPP_UnknowAction       1000
