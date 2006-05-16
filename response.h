d229124eaa038363598ec4c113883068b170676a
Petr Blaha <petr.blaha@nic.cz>
2006-05-16 14:30:27 +0000

    nove navratove kody
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@249 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- response.h
+++ response.h
@@ -1,8 +1,13 @@
 #define    COMMAND_OK     1000
+#define    COMMAND_LOGOUT 1500
 #define    COMMAND_FAILED 2400
 #define COMMAND_OBJECT_NOT_EXIST 2303
 #define COMMAND_OBJECT_EXIST    2302
 #define COMMAND_AUTH_ERROR    2501
+
+#define COMMAND_NO_MESG  1300 // nejsou zadne zpravi vraci poll 
+#define COMMAND_ACK_MESG 1301 // zpravy jsou ve fronte vraci poll
+
 /*
 #define   EPP_MSG_1000    "Command completed successfully"
            1001    "Command completed successfully; action pending"
