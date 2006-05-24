8eebc34830771a851f40aa33a7c7ea056370c0ca
Petr Blaha <petr.blaha@nic.cz>
2006-05-24 14:36:41 +0000

    dalsi funkce
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@300 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- test.py
+++ test.py
@@ -4,6 +4,7 @@
 import sys 
 from omniORB import CORBA 
 import ccReg
+import time
 
 orb = CORBA.ORB_init(sys.argv, CORBA.ORB_ID) 
 file = open( "/tmp/ccReg.ref" );
@@ -29,26 +30,67 @@ loginID = result[1]
 
 print "Login  svrTRID '%s' err '%d' loginID '%d'" % ( result[0].svTRID ,result[0].errCode , loginID  );
 
+result = epp. ContactInfo(  'NECOCZ-PETR' , loginID , "pcontact-info" );
+contact =  result[1]
+print "Conact Info  '%s' err '%d' name  '%s' " % ( result[0].svTRID ,result[0].errCode , contact.Name  ) ;
 
+contact.Name = "David Pospisilik"
+contact.City = "Karlovy Vary"
+result = epp.ContactCreate( 'NECOCZ-DAVID'  , contact ,  loginID , "david-create" );
+
+print "ContactCreate  svrTRID '%s' err '%d' " % ( result.svTRID ,result.errCode );
+
+contact.Email = "david@neco.cz"
+result = epp.ContactUpdate( 'NECOCZ-DAVID'  , contact , [""] ,  [""] ,  loginID , "david-update" );
+print "ContactUpdate  svrTRID '%s' err '%d' " % ( result.svTRID ,result.errCode );
+
+
+result = epp.ContactDelete( 'NECOCZ-DAVID'  ,   loginID , "david-delete" );
+print "ContactDelete  svrTRID '%s' err '%d' " % ( result.svTRID ,result.errCode );
+ 
+
+ 
 result =epp.DomainInfo( 'neco.cz' , loginID , "python-dinfo" );
 domain = result[1]
 
-print "domain info: " , domain.name , domain.nsset,  domain.Registrant ,  domain.ClID 
+print "domain info: " , domain.name , domain.nsset,  domain.Registrant ,  domain.ClID,    domain.admin  , domain.stat
+print domain.admin
+
 print "DomainInfo  svrTRID '%s' err '%d' " % ( result[0].svTRID ,result[0].errCode  )
+
+
+result =epp.NSSetInfo( domain.nsset , loginID , "nsset-info" );
+nsset = result[1]
+
+print "NSSetInfo  svrTRID '%s' err '%d' " % ( result[0].svTRID  ,result[0].errCode  )
+print "nsset info: " , nsset.handle , nsset.dns ,  nsset.tech  
+
 
 result =  epp.DomainCreate( 'temp.cz', 'NECOCZ-PETR' , 'NECOCZ' , "heslo1234" , 12 , [   'NECOCZ-ADMIN' ,   'NECOCZ-ROBERT' ] , loginID , "python-create" );
 
 print "DomainCreate  svrTRID '%s' err '%d' " % ( result.svTRID ,result.errCode );
 
-result =  epp.DomainUpdate( 'temp.cz', "NECOCZ-ROBERT" ,  "heslo1234" , "" , "" , [   'NECOCZ-PETR' ] , [ 'NECOCZ-ADMIN'  ] , [ "clientTransferProhibited" ]  ,  [ ""  ] , loginID , "python-update" );
+
+result =  epp.DomainCheck( [  'temp.cz' , 'neco.cz' , 'xxx.cz' ], loginID , "python-domaincheck" )
+av =  result[1]
+print "DomainCheck  svrTRID '%s' err '%d' loginID '%d'" % ( result[0].svTRID ,result[0].errCode , loginID  );
+print "avial " , av
+
+
+result =  epp.DomainUpdate( 'temp.cz', "NECOCZ-ROBERT" ,  "heslo1234" , "" , "" , [   'NECOCZ-PETR' ] , [ 'NECOCZ-ADMIN'  ] , [ "clientTransferProhibited" ]  ,  [ "clientDeleteProhibited"  ] , loginID , "python-update" );
 print "DomainUpdate  svrTRID '%s' err '%d' " % ( result.svTRID ,result.errCode );
 
-result =epp.DomainInfo( 'temp.cz' , loginID , "python-dinfo" );
+result =epp.DomainInfo( 'temp.cz' , loginID , "python-info" );
 domain = result[1]
 
-print "domain info: " , domain.name , domain.nsset,  domain.Registrant ,  domain.ClID
+print "domain info: " 
+print "name: " ,  domain.name  ,   "NSSET: "  , domain.nsset ,  "reg:" ,  domain.Registrant , "client:"  ,  domain.ClID 
+print "status: " , domain.stat  , "ADMIN: " ,  domain.admin , "auth: "  , domain.AuthInfoPw
 print "DomainInfo  svrTRID '%s' err '%d' " % ( result[0].svTRID ,result[0].errCode  )
-  
+
+result =  epp.DomainRenew( 'temp.cz' , domain.ExDate , 24 , loginID , "python-renew" );  
+print "DomainReNew  svrTRID '%s' err '%d' " % ( result.svTRID ,result.errCode );
+
 result =  epp.DomainDelete( 'temp.cz',  loginID , "python-delete" );
 print "DomainDelete  svrTRID '%s' err '%d' " % ( result.svTRID ,result.errCode );
   
