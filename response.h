7effbcadfb8307fd49ba4a743a23634dbc160157
Jan Kryl <jan.kryl@nic.cz>
2006-05-29 17:20:56 +0000

    mod_eppd: Oprava warningu pri kompilaci
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@334 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- response.h
+++ response.h
@@ -4,11 +4,17 @@
 #define COMMAND_OBJECT_NOT_EXIST 2303
 #define COMMAND_OBJECT_EXIST    2302
 #define COMMAND_AUTH_ERROR    2501
+<<<<<<< .mine
+#define COMMAND_LOGOUT  1500
+#define COMMAND_NO_MESG 1301
+
+=======
 
 #define COMMAND_NO_MESG  1300 // nejsou zadne zpravi vraci poll 
 #define COMMAND_ACK_MESG 1301 // zpravy jsou ve fronte vraci poll
 
 #define COMMAND_PROHIBITS_OPERATION 2305
+>>>>>>> .r333
 /*
 #define   EPP_MSG_1000    "Command completed successfully"
            1001    "Command completed successfully; action pending"
