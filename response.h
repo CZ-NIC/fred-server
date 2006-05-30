bd4a82389f3a1e441b20407f416e646cf715a0fa
Petr Blaha <petr.blaha@nic.cz>
2006-05-30 12:07:08 +0000

    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@337 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- response.h
+++ response.h
@@ -4,54 +4,8 @@
 #define COMMAND_OBJECT_NOT_EXIST 2303
 #define COMMAND_OBJECT_EXIST    2302
 #define COMMAND_AUTH_ERROR    2501
-<<<<<<< .mine
-#define COMMAND_LOGOUT  1500
-#define COMMAND_NO_MESG 1301
-
-=======
 
 #define COMMAND_NO_MESG  1300 // nejsou zadne zpravi vraci poll 
 #define COMMAND_ACK_MESG 1301 // zpravy jsou ve fronte vraci poll
 
 #define COMMAND_PROHIBITS_OPERATION 2305
->>>>>>> .r333
-/*
-#define   EPP_MSG_1000    "Command completed successfully"
-           1001    "Command completed successfully; action pending"
-   
-         <enumeration value="1000"/>
-         <enumeration value="1001"/>
-         <enumeration value="1300"/>
-         <enumeration value="1301"/>
-         <enumeration value="1500"/>
-         <enumeration value="2000"/>
-         <enumeration value="2001"/>
-         <enumeration value="2002"/>
-         <enumeration value="2003"/>
-         <enumeration value="2004"/>
-         <enumeration value="2005"/>
-         <enumeration value="2100"/>
-         <enumeration value="2101"/>
-         <enumeration value="2102"/>
-         <enumeration value="2103"/>
-         <enumeration value="2104"/>
-         <enumeration value="2105"/>
-         <enumeration value="2106"/>
-         <enumeration value="2200"/>
-         <enumeration value="2201"/>
-         <enumeration value="2202"/>
-         <enumeration value="2300"/>
-         <enumeration value="2301"/>
-         <enumeration value="2302"/>
-         <enumeration value="2303"/>
-         <enumeration value="2304"/>
-         <enumeration value="2305"/>
-         <enumeration value="2306"/>
-         <enumeration value="2307"/>
-         <enumeration value="2308"/>
-         <enumeration value="2400"/>
-         <enumeration value="2500"/>
-         <enumeration value="2501"/>
-         <enumeration value="2502"/>
-
-*/
