bf3f2c0721952818089be5d4cefcb3b010fddf0d
Petr Blaha <petr.blaha@nic.cz>
2006-06-14 14:09:11 +0000

    definice logprintf pouziti LOG bez syslogu
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@468 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- log.h
+++ log.h
@@ -1,22 +1,32 @@
 #ifdef SYSLOG
 
 #include<syslog.h>
-  #define ERROR_LOG     LOG_ERR
-  #define WARNING_LOG   LOG_WARNING
-  #define SQL_LOG       LOG_DEBUG
-  #define NOTICE_LOG    LOG_NOTICE
-  #define EMERG_LOG     LOG_EMERG 
+#define EMERG_LOG     LOG_EMERG
+#define ALERT_LOG     LOG_ALERT
+#define ERROR_LOG     LOG_ERR
+#define CRIT_LOG      LOG_CRIT
+#define WARNING_LOG   LOG_WARNING
+#define SQL_LOG       LOG_DEBUG
+#define DEBUG_LOG     LOG_DEBUG
+#define NOTICE_LOG    LOG_NOTICE
+#define INFO_LOG      LOG_INFO
+#define EMERG_LOG     LOG_EMERG 
 #else
-  #define ERROR_LOG     stderr
-  #define WARNING_LOG   stderr
-  #define SQL_LOG       stdout
-  #define NOTICE_LOG    stdout
-  #define EMERG_LOG     stderr
+#define EMERG_LOG       0       /* system is unusable */
+#define ALERT_LOG       1       /* action must be taken immediately */
+#define CRIT_LOG        2       /* critical conditions */
+#define ERROR_LOG       3       /* error conditions */
+#define WARNING_LOG     4       /* warning conditions */
+#define NOTICE_LOG      5       /* normal but significant condition */
+#define INFO_LOG        6       /* informational */
+#define LOG_DEBUG       7       /* debug-level messages */
+#define SQL_LOG      LOG_DEBUG
 #endif
 
 #ifdef SYSLOG
   #define LOG syslog
 #else
-  #define LOG  fprintf
+  void logprintf(int level ,  char *fmt, ... );
+  #define LOG  logprintf
 #endif
 
