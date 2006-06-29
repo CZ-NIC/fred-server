29451b479435e6bb73f5140bb7a037d28f780be2
Jaromír Talíř <jaromir.talir@nic.cz>
2006-06-29 16:58:49 +0000

    testy na kontakty
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@619 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- cppunittests/cr.cc
+++ cppunittests/cr.cc
@@ -3,6 +3,7 @@
 #include "ccReg_epp.h"
 #include "conf.h"
 #include <iostream>
+#include <memory>
 
 class EPPServerTest : public CppUnit::TestFixture {
   CPPUNIT_TEST_SUITE( EPPServerTest );
@@ -25,25 +26,39 @@ class EPPServerTest : public CppUnit::TestFixture {
     epp->_remove_ref();
   }
 
+  // funkce na obecne prihlaseni
+  CORBA::Long loginRegistrar(const char *registrar, const char *passwd, 
+			     const char *newpasswd, unsigned errCode)
+  {
+    CORBA::Long clientID;
+    std::auto_ptr<ccReg::Response> rClientLogin(
+      epp->ClientLogin(registrar,passwd,newpasswd, 
+		       "cppunitklient", clientID , "" , 
+		       ccReg::EN)
+    );
+    CPPUNIT_ASSERT(rClientLogin.get() != NULL);
+    CPPUNIT_ASSERT_EQUAL(errCode,(unsigned)rClientLogin->errCode);
+    return clientID;
+  }
+
+  // funkce pro obecne odhlaseni 
+  void logoutRegistrar(CORBA::Long clientID)
+  {
+    std::auto_ptr<ccReg::Response> rClientLogout(
+     epp->ClientLogout(clientID,"cppunitklient")
+    );
+    CPPUNIT_ASSERT(rClientLogout.get() != NULL);
+    CPPUNIT_ASSERT_EQUAL(1500U,(unsigned)rClientLogout->errCode);
+  }
+
+  // funkce na prihlaseni a odhlaseni
   void loginSequence(const char *registrar, const char *passwd, 
 		     const char *newpasswd, unsigned errCode)
   {
-    CORBA::Long clientID;
-    ccReg::Response* rClientLogin = NULL;
-    rClientLogin = epp->ClientLogin(registrar,passwd,newpasswd, 
-				    "cppunitklient", clientID , "" , 
-				    ccReg::EN);
-    CPPUNIT_ASSERT(rClientLogin != NULL);
-    CPPUNIT_ASSERT_EQUAL(errCode,(unsigned)rClientLogin->errCode);
-    // pokud jsem testoval na chybu koncime
-    delete rClientLogin;
+    CORBA::Long clientID = loginRegistrar(registrar,passwd,newpasswd,errCode);
     if (errCode > 1999) return;
     CPPUNIT_ASSERT(clientID > 0);
-    ccReg::Response* rClientLogout = NULL;
-    rClientLogout = epp->ClientLogout(clientID,"cppunitklient");
-    CPPUNIT_ASSERT(rClientLogout != NULL);
-    CPPUNIT_ASSERT_EQUAL(1500U,(unsigned)rClientLogout->errCode);
-    delete rClientLogout;
+    logoutRegistrar(clientID);
   }
 
   // test na prihlaseni se spravnym heslem
@@ -65,14 +80,24 @@ class EPPServerTest : public CppUnit::TestFixture {
     loginSequence("REG-LRR","cppunit123456789","123456789",1000);
     loginSequence("REG-LRR","123456789","",1000);
   }
+
+  CORBA::Long login()
+  {
+    return loginRegistrar("REG-LRR","123456789","",1000);
+  }
+
   // test na kontrolu neexistujicich kontaktu
   void testContact()
   {
-    ccReg::Response* rClientLogin = NULL;
-    rClientLogin = epp->ClientLogin("REG_LRR,passwd,newpasswd, 
-				    "cppunitklient", clientID , "" , 
-				    ccReg::EN);
-    CPPUNIT_ASSERT(rClientLogin != NULL);
+    CORBA::Long clID = login();
+    const char *handle1 = "cppunitNEW";
+    const char *handle2 = "cppunitNEW2";
+    
+    std::auto_ptr<ccReg::Response> rClientLogout(
+     epp->ContactChange(clientID,"cppunitklient")
+    );
+    
+    logoutRegistrar(clID);
   }
   
 };
