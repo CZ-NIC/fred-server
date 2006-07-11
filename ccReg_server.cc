3fabd4efbd93cf47c11e133ba87a9eb26bbdd836
Petr Blaha <petr.blaha@nic.cz>
2006-07-11 13:20:55 +0000

    facility local1
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@693 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- ccReg_server.cc
+++ ccReg_server.cc
@@ -87,7 +87,9 @@ int main(int argc, char** argv)
          cout << "start syslog at level " <<  config.GetSYSLOGlevel()   << endl;
          cout << "start syslog facility local" <<  config.GetSYSLOGlocal()   << endl;
          setlogmask ( LOG_UPTO(  config.GetSYSLOGlevel()  )   );
-         openlog ("ccReg", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL0+  config.GetSYSLOGlocal()  );
+//         openlog ("ccReg", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL0+  config.GetSYSLOGlocal()  );
+// natvrdo local1
+         openlog ( "ccReg", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 #endif
 
 
