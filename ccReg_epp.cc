af1c11cd31162223f1d7e3153f3dfc19f1b51c3c
Petr Blaha <petr.blaha@nic.cz>
2006-08-02 14:33:50 +0000

    logovani disclose parametru a nastaveni pouze kdyz je Disclose ==  1
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@829 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- ccReg_epp.cc
+++ ccReg_epp.cc
@@ -1288,7 +1288,8 @@ crDate = 0;
 
 
 LOG( NOTICE_LOG, "ContactCreate: clientID -> %d clTRID [%s] handle [%s] %s ", clientID, clTRID, handle , HANDLE );
-
+LOG( NOTICE_LOG, "ContactCreate: Disclose Name %d Org %d Add %d Tel %d Fax %d Email %d" ,
+ c.DiscloseName  , c.DiscloseOrganization , c.DiscloseAddress , c.DiscloseTelephone , c.DiscloseFax , c.DiscloseEmail );
 
 
   if( DBsql.OpenDatabase( database ) )
@@ -1391,12 +1392,12 @@ LOG( NOTICE_LOG, "ContactCreate: clientID -> %d clTRID [%s] handle [%s] %s ", cl
                       DBsql.VAL( c.SSN );
 
 
-                      if( c.DiscloseName > 0 ) DBsql.VALUE( "t" );
-                      if( c.DiscloseOrganization > 0 ) DBsql.VALUE( "t" );
-                      if( c.DiscloseAddress > 0 ) DBsql.VALUE( "t" );
-                      if( c.DiscloseTelephone > 0 ) DBsql.VALUE( "t" );
-                      if( c.DiscloseFax > 0 ) DBsql.VALUE( "t" );
-                      if( c.DiscloseEmail > 0 ) DBsql.VALUE( "t" );
+                      if( c.DiscloseName == 1 ) DBsql.VALUE( "t" );
+                      if( c.DiscloseOrganization ==  1 ) DBsql.VALUE( "t" );
+                      if( c.DiscloseAddress ==  1 ) DBsql.VALUE( "t" );
+                      if( c.DiscloseTelephone ==  1 ) DBsql.VALUE( "t" );
+                      if( c.DiscloseFax ==  1 ) DBsql.VALUE( "t" );
+                      if( c.DiscloseEmail == 1 ) DBsql.VALUE( "t" );
 
 
 
