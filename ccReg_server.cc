fd866c4556d3ad2b1cce250339cea9c9550952d0
Jaromír Talíř <jaromir.talir@nic.cz>
2006-09-12 13:01:46 +0000

    perzistentni odkazy na vsechny interface
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@1214 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- ccReg_server.cc
+++ ccReg_server.cc
@@ -36,13 +36,20 @@ static void signalHandler(int signal)
 int main(int argc, char** argv)
 {
   try {
+    // nastaveni databaze
     char db[256];
-
+    Conf config; // READ CONFIG  file
+    // read config file
+    if (!config.ReadConfigFile(CONFIG_FILE)) {
+      std::cout << "Cannot read config file\n";
+      exit(-1);
+    }
+    strcpy( db , config.GetDBconninfo() );
+    std::cout << "DATABASE: "  << db << endl;
     // Initialise the ORB.
     CORBA::ORB_var orb = CORBA::ORB_init(argc, argv);
     orbToShutdown = orb;
     signal(SIGINT,signalHandler);
-
     // Obtain a reference to the root POA.
     CORBA::Object_var obj = orb->resolve_initial_references("RootPOA");
     PortableServer::POA_var rootPOA = PortableServer::POA::_narrow(obj);
@@ -53,81 +60,76 @@ int main(int argc, char** argv)
     pols.length(2);
     pols[0] = rootPOA->create_lifespan_policy(PortableServer::PERSISTENT);
     pols[1] = rootPOA->create_id_assignment_policy(PortableServer::USER_ID);
-    PortableServer::POA_var poa = rootPOA->create_POA("ccRegPOA",mgr.in(),pols);
-    mgr->activate();
+    PortableServer::POA_var poa = 
+      rootPOA->create_POA("ccRegPOA",mgr.in(),pols);
 
-    // nastaveni databaze
-    Conf config; // READ CONFIG  file
+    // prepare NameService object
+    std::string nameServiceIOR = "corbaname::";
+    nameServiceIOR += config.GetNameService();
+    NameService ns(orb,nameServiceIOR);
+
+    PortableServer::ObjectId_var adminObjectId = 
+      PortableServer::string_to_ObjectId("Admin");
+    ccReg_Admin_i* myccReg_Admin_i = new ccReg_Admin_i(db);
+    poa->activate_object_with_id(adminObjectId,myccReg_Admin_i);
+    CORBA::Object_var adminObj = myccReg_Admin_i->_this();          
+    myccReg_Admin_i->_remove_ref();
+    ns.bind("Admin",adminObj);
     
-    // read config file
-    if(  config.ReadConfigFile(  CONFIG_FILE ) )
-    {
-      strcpy( db , config.GetDBconninfo() );
-      std::cout << "DATABASE: "  << db << endl;
+    PortableServer::ObjectId_var whoisObjectId = 
+      PortableServer::string_to_ObjectId("Whois");
+    ccReg_Whois_i* myccReg_Whois_i = new ccReg_Whois_i(db);
+    poa->activate_object_with_id(whoisObjectId,myccReg_Whois_i);
+    CORBA::Object_var whoisObj = myccReg_Whois_i->_this();
+    myccReg_Whois_i->_remove_ref();
+    ns.bind("Whois",whoisObj);
 
-      // prepare NameService object
-      std::string nameServiceIOR = "corbaname::";
-      nameServiceIOR += config.GetNameService();
-      NameService ns(orb,nameServiceIOR);
-
-      ccReg_Admin_i* myccReg_Admin_i = new ccReg_Admin_i(db);
-      CORBA::Object_var adminObj = 
-        rootPOA->servant_to_reference(myccReg_Admin_i);    
-      myccReg_Admin_i->_remove_ref();
-      ccReg::Admin_var admin = ccReg::Admin::_narrow(adminObj);
-      ns.bind("Admin",adminObj);
-
-      ccReg_Whois_i* myccReg_Whois_i = new ccReg_Whois_i(db);
-      CORBA::Object_var whoisObj = 
-        rootPOA->servant_to_reference(myccReg_Whois_i);
-      myccReg_Whois_i->_remove_ref();
-      ccReg::Whois_var whois = ccReg::Whois::_narrow(whoisObj);
-      ns.bind("Whois",whoisObj);
-
-      ccReg_EPP_i* myccReg_EPP_i = new ccReg_EPP_i(admin, whois);
+    // pristup na admin a whois je mozna z EPP vyhodit
+    ccReg::Whois_var whois = ccReg::Whois::_narrow(whoisObj);
+    ccReg::Admin_var admin = ccReg::Admin::_narrow(adminObj);
+    ccReg_EPP_i* myccReg_EPP_i = new ccReg_EPP_i(admin, whois);
+    // musi byt zavolano pred loadZones
+    if (!myccReg_EPP_i->TestDatabaseConnect(db)) {
+      std::cout << "Database connection failed\n";
+      exit(-2);
+    } 
+    myccReg_EPP_i->loadZones();
      
-      // pokud projde uspesne test na pripojeni k databazi
-      if( myccReg_EPP_i->TestDatabaseConnect( db) ) 
-      {
-        // load zones
-        myccReg_EPP_i->loadZones();
-        // Activate the objects.  This tells the POA that the objects are
-        // ready to accept requests.
-        PortableServer::ObjectId_var myccReg_EPP_iid = 
-          PortableServer::string_to_ObjectId("ccReg");
-        poa->activate_object_with_id(myccReg_EPP_iid,myccReg_EPP_i);
-        // to delete implementaion on POA destruction
-        myccReg_EPP_i->_remove_ref();
+    PortableServer::ObjectId_var myccReg_EPP_iid = 
+      PortableServer::string_to_ObjectId("ccReg");
+    poa->activate_object_with_id(myccReg_EPP_iid,myccReg_EPP_i);
+    myccReg_EPP_i->_remove_ref();
+    // IDL interface: ccReg::EPP
+    // CORBA::Object_var ref = poa->id_to_reference(myccReg_EPP_iid);
+    CORBA::Object_var ref = myccReg_EPP_i->_this();
+    ns.bind("EPP",ref);
+//    CORBA::String_var sior(orb->object_to_string(ref));
+//    std::cout << "IDL object ccReg::EPP IOR = '" 
+//              << (char*)sior << "'" << std::endl;
+//    ofstream fout ("/tmp/ccReg.ref");
+//    fout << (char*)sior ; // orb->object_to_string(ref)  ; //  
+//    fout.close ();
+    
+    // pokud projde uspesne test na pripojeni k databazi
+    
 #ifdef SYSLOG
-        cout << "start syslog at level " 
-             <<  config.GetSYSLOGlevel()   << endl;
-        cout << "start syslog facility local" 
-             <<  config.GetSYSLOGlocal()   << endl;
-        setlogmask ( LOG_UPTO(  config.GetSYSLOGlevel()  )   );
-        openlog ( "ccReg", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+    cout << "start syslog at level " 
+         <<  config.GetSYSLOGlevel()   << endl;
+    cout << "start syslog facility local" 
+         <<  config.GetSYSLOGlocal()   << endl;
+    setlogmask ( LOG_UPTO(  config.GetSYSLOGlevel()  )   );
+    openlog ( "ccReg", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
 #endif
-        // IDL interface: ccReg::EPP
-        CORBA::Object_var ref = poa->id_to_reference(myccReg_EPP_iid);
-        ns.bind("EPP",ref);
-        CORBA::String_var sior(orb->object_to_string(ref));
-        std::cout << "IDL object ccReg::EPP IOR = '" 
-                  << (char*)sior << "'" << std::endl;
 
-        ofstream fout ("/tmp/ccReg.ref");
-        fout << (char*)sior ; // orb->object_to_string(ref)  ; //  
-        fout.close ();
-
-        // Obtain a POAManager, and tell the POA to start accepting
-        // requests on its objects.
-        PortableServer::POAManager_var pman = poa->the_POAManager();
-        pman->activate();
-        LOG(DEBUG_LOG , "Starting ccReg_server");
-        // odpoji se od terminalu a zalozi vlastni skupinu
-        setsid(); 
-        orb->run();
-        orb->destroy();
-      }
-    }
+    // Obtain a POAManager, and tell the POA to start accepting
+    // requests on its objects.
+    PortableServer::POAManager_var pman = poa->the_POAManager();
+    pman->activate();
+    LOG(DEBUG_LOG , "Starting ccReg_server");
+    // odpoji se od terminalu a zalozi vlastni skupinu
+    setsid(); 
+    orb->run();
+    orb->destroy();
   }
   catch (CORBA::TRANSIENT&) {
     LOG(
