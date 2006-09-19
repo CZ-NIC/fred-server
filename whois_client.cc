cdf7710fbb73cb092a92a6b9e562cc412b6da3be
Petr Blaha <petr.blaha@nic.cz>
2006-09-19 15:33:50 +0000

    nova verze whoisu a pouzivani omninames
    git-svn-id: file:///data/svn/enum/trunk/cr/ccReg@1315 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- whois_client.cc
+++ whois_client.cc
@@ -8,6 +8,8 @@
 
 #include "time.h"
 
+static CORBA::Object_ptr getObjectReference(CORBA::ORB_ptr orb);
+
 //////////////////////////////////////////////////////////////////////
 
 int main(int argc, char** argv)
@@ -21,10 +23,11 @@ int main(int argc, char** argv)
     CORBA::String_var errMsg , svTR , tim;
     int i , n;
     time_t t;
-    filebuf *pbuf;
-    char *buffer;
-    char name[64] , roid[32];
+//    filebuf *pbuf;
+  //  char *buffer;
+    char name[64];
     long size;
+/*
     ifstream fd ("/tmp/ccReg.ref");
     
     // OPRAVIT NA NAMESERVICE!!!!
@@ -51,23 +54,28 @@ int main(int argc, char** argv)
      // get CORBA reference
      obj = orb->string_to_object(  buffer );
 
+*/
+  obj =  getObjectReference( orb );
 
     if (CORBA::is_nil (obj)) 
       {
         return 1;
       }
 
-    ccReg::EPP_var EPP = ccReg::EPP::_narrow (obj);
-    cout <<"EPP" << endl;
+    ccReg::Whois_var Whois = ccReg::Whois::_narrow (obj);
     ccReg::DomainWhois *dm;
 
-    
-    //dm = EPP->getWhois()->getDomain("example.cz" , tim );
+    strcpy( name , "sracka.cz"  );
+    cout << "getDomain" << name << endl;
+
+    dm = Whois->getDomain( name  , tim );
+
+    cout << "WHOIS: " << name << endl;
 
     for( n = 0 ; n < dm->ns.length() ; n ++ )
     cout << "status" <<  dm->status <<   "NameServers: " << dm->ns[n] <<  endl;
 
-    cout <<"Registrator: " << dm->registrarName << "url: " << dm->registrarUrl <<  endl;
+    cout << "Registrator: " << dm->registrarName << "url: " << dm->registrarUrl <<  endl;
 
 
     cout << "registered: "  <<   dm->created  << endl;
@@ -77,15 +85,14 @@ int main(int argc, char** argv)
 
     cout << "timestamp: "  <<   tim << endl;
 
-    for( n = 0 ; n < dm->tech.length() ; n ++ )
-    cout <<  "Tech contact: " << dm->tech[n] <<  endl;
+//    for( n = 0 ; n < dm->tech.length() ; n ++ )
+//    cout <<  "Tech contact: " << dm->tech[n] <<  endl;
 
 
 //    for( n = 0 ; n < dm->admin.length() ; n ++ )
   //  cout <<  dm->name << "Admin contact: " << dm->admin[n] <<  endl;
    
-   delete dm ;
-    
+
     orb->destroy();
   }
   catch(CORBA::TRANSIENT&) {
@@ -96,8 +103,8 @@ int main(int argc, char** argv)
   }
 
 
-  catch(ccReg::Whois::DomainNotFound& ex) {
-    cerr << "Caught DomainNotFound::timestamp: " << ex.timestamp << endl;    
+  catch(ccReg::Whois::DomainError& ex) {
+    cerr << "Caught DomainError::timestamp: " << ex.timestamp << " type: "  << ex.type << endl;    
   }
 
   catch(CORBA::Exception& ex) {
@@ -111,4 +118,72 @@ int main(int argc, char** argv)
    cerr << "  mesg: " << fe.errmsg() << endl;
   }
   return 0;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+static CORBA::Object_ptr
+getObjectReference(CORBA::ORB_ptr orb)
+{
+  CosNaming::NamingContext_var rootContext;
+
+  try {
+    // Obtain a reference to the root context of the Name service:
+    CORBA::Object_var obj;
+    obj = orb->resolve_initial_references("NameService");
+
+    // Narrow the reference returned.
+    rootContext = CosNaming::NamingContext::_narrow(obj);
+    if( CORBA::is_nil(rootContext) ) {
+      cerr << "Failed to narrow the root naming context." << endl;
+      return CORBA::Object::_nil();
+    }
+  }
+  catch (CORBA::NO_RESOURCES&) {
+    cerr << "Caught NO_RESOURCES exception. You must configure omniORB "
+         << "with the location" << endl
+         << "of the naming service." << endl;
+    return 0;
+  }
+  catch(CORBA::ORB::InvalidName& ex) {
+    // This should not happen!
+    cerr << "Service required is invalid [does not exist]." << endl;
+    return CORBA::Object::_nil();
+  }
+
+  // Create a name object, containing the name test/context:
+  CosNaming::Name name;
+  name.length(2);
+
+  name[0].id   = (const char*) "ccReg";       // string copied
+  name[0].kind = (const char*) "context"; // string copied
+  name[1].id   = (const char*) "Whois";
+  name[1].kind = (const char*) "Object";
+  // Note on kind: The kind field is used to indicate the type
+  // of the object. This is to avoid conventions such as that used
+  // by files (name.type -- e.g. test.ps = postscript etc.)
+
+  try {
+    // Resolve the name to an object reference.
+    return rootContext->resolve(name);
+  }
+  catch(CosNaming::NamingContext::NotFound& ex) {
+    // This exception is thrown if any of the components of the
+    // path [contexts or the object] aren't found:
+    cerr << "Context not found." << endl;
+  }
+  catch(CORBA::TRANSIENT& ex) {
+    cerr << "Caught system exception TRANSIENT -- unable to contact the "
+         << "naming service." << endl
+         << "Make sure the naming server is running and that omniORB is "
+         << "configured correctly." << endl;
+
+  }
+  catch(CORBA::SystemException& ex) {
+    cerr << "Caught a CORBA::" << ex._name()
+         << " while using the naming service." << endl;
+    return 0;
+  }
+
+  return CORBA::Object::_nil();
 }
