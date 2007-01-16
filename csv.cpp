89342b13507d0802813aa07e07fe1d5b9b12684b
Petr Blaha <petr.blaha@nic.cz>
2007-01-16 08:30:28 +0000

    funkce pro nacitani obsahu CSV pro vypis z e-banky
    git-svn-id: file:///data/svn/enum/ccReg_server/trunk@2375 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ csv.cpp
@@ -0,0 +1,183 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<time.h>
+#include<ctype.h>
+#include<time.h>
+
+
+#include "csv.h"
+
+
+CSV::CSV()
+{
+separator=';' ;
+rows=0;
+cols=0;
+}
+
+CSV::~CSV()
+{
+}
+
+
+
+int CSV::get_sepnum()
+{
+int j , c ;
+int len ;
+
+len =  strlen(buf);
+
+  for( j = 0 , c =0  ; j < len ; j ++ )
+     {        
+        // ukonceni radku 
+        if( buf[j] == '\r' || buf[j] == '\n'  ) { buf[j] = 0 ; if( c > 0 )  c ++; break ; }
+        else   if( buf[j] == separator ) c ++ ;
+     }
+
+
+return c;
+}
+
+bool CSV::get_row()
+{
+
+  fgets( buf , MAX_LINE , fd );
+  if( feof(fd) ) {   fclose(fd) ; return false ; } 
+  else
+  {
+   if(  get_sepnum() >  0 )  return true;
+   else return false;
+  }
+
+}
+
+char * CSV::get_value(unsigned int col )
+{
+int i , start  , j ;
+unsigned int c;
+int len;
+
+
+len =   strlen(buf);
+
+
+if( col == 0 ) start=0;
+else
+{
+
+ for( i = 0 , c =0  , start=-1; i < len ; i ++ )
+    {    
+      if( buf[i] == separator )c++;
+
+      if( c == col ){ start =i+1 ;  break; }
+       
+    }
+
+}
+
+if( start >= 0 )
+{
+
+
+for( i = start , j =0; i < len ; i ++  , j ++ )
+   {
+   if( buf[i] == separator ) break;
+   else string[j] = buf[i];
+   }
+
+string[j] = 0; 
+
+
+return string;
+}
+else return "";
+
+}
+
+bool CSV::read_file(char *filename)
+{
+int i ,  numrec , cls  ,c ;
+
+if( ( fd = fopen( filename ,  "r" ) ) != NULL ) 
+{
+
+for(i=0 , numrec = 0 ;;i++)
+{
+  fgets( buf , MAX_LINE , fd );
+  if( feof(fd) ) break;
+
+  c = get_sepnum();  
+
+
+
+  if( c > 0 ) 
+   {
+     if( numrec > 0 )
+     {
+       if( c != cls  ) {  fclose(fd) ; return false ; } 
+     }
+
+     cls = c; numrec ++ ; 
+   } 
+  else  break;// prazdna radka na konci souboru
+
+}
+
+// fclose(fd);
+// reset 
+fseek( fd, 0, SEEK_SET );
+
+rows = numrec ; cols = cls;
+
+// debug("Rows in  files is %d cols %d\n" , rows , cols );
+return true;
+}
+else
+{
+   // debug("Error open file %s\n" , filename);
+   return false;
+ }
+
+
+}
+
+
+
+
+#ifdef TEST
+int main(int argc , char *argv[] )
+{
+CSV csv; 
+int c ,r =0;
+
+
+if( argc == 2 ) 
+{
+csv.read_file( argv[1]);
+
+while(csv.get_row() )
+{
+
+for(  c = 0  ; c < csv.get_cols() ; c ++ )
+  {
+       printf("[%d,%d] -> [%s]\n" , r ,c  , csv.get_value(c) );
+  }
+
+r++;
+}
+
+}
+
+if( argc == 1)
+  {
+   printf("pouziti:\n%s csvfile\n" ,  argv[0] );
+  }
+
+
+}
+
+#endif
+
---
+++ csv.h
@@ -0,0 +1,35 @@
+#define MAX_LINE 4096 
+#define MAX_STRING 1024
+// class for import from CSV file
+
+class CSV
+{
+public:
+CSV();
+~CSV();
+
+// precte radek ze souboru
+bool get_row();
+char * get_value(unsigned int col );
+
+bool read_file( char * filename );
+long get_numeric( int row , int col );
+unsigned char *get_string( int row ,  int col );
+
+char get_separator() { return  separator; };
+void set_separator(char s){ separator = s; } ;
+int get_rows(){ return  rows ; };
+int get_cols(){ return  cols ; };
+
+private:
+
+
+int get_sepnum(); // projede buffer radky
+
+FILE *fd;
+char separator;
+int rows , cols; // pocet radek a sloupcu CSV souboru
+char buf[MAX_LINE]; // buffer pro radek
+char string[MAX_STRING]; // 
+};
+
