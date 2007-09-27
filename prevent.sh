3ccc14828e7cfec0787b99dcdaa38460f2069c07
Ondřej Surý <ondrej.sury@nic.cz>
2007-09-27 09:27:38 +0000

    add or modify prevent.sh scripts
    git-svn-id: file:///data/svn/enum/ccReg_server/trunk@4157 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ prevent.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+PRODUCT=fred-server
+DIR=~/cov/${PRODUCT}/
+cov-build --dir ${DIR} --config /opt/prevent-linux64-3.6.1/config/coverity_config.xml make && \
+cov-analyze --all --dir ${DIR} --config /opt/prevent-linux64-3.6.1/config/coverity_config.xml && \
+cov-commit-defects --datadir ~/cov/gui/ --product ${PRODUCT} --user ondrej --dir ${DIR}
