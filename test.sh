9f7397a681f5587ed3fb5c1851accf7ae2d29db1
Jaromír Talíř <jaromir.talir@nic.cz>
2007-11-19 16:25:13 +0000

    no longer needed
    git-svn-id: file:///data/svn/enum/ccReg_server/trunk@4567 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-DATABASE="dbname=ccreg_test user=pblaha host=petr"
-echo DATABASE=$DATABASE
-time ./db_test  "$DATABASE" 1000 1000 > log1 &
-time ./db_test  "$DATABASE" 2000 1000 > log2 &
-time ./db_test  "$DATABASE" 3000 1000 > log3 &
-time ./db_test  "$DATABASE" 4000 1000 > log4 &
-time ./db_test  "$DATABASE" 5000 1000 > log5 &
