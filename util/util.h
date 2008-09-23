8f0ac17da42cc32033b007b612750c0e94687650
Aleš Doležal <ales.dolezal@nic.cz>
2008-09-23 12:43:17 +0000

    FIX: coverity warning NEGATIVE_RETURNS
    git-svn-id: file:///data/svn/enum/server/trunk@6425 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- util/util.h
+++ util/util.h
@@ -10,7 +10,7 @@ inline std::string escape(std::string _input,
                    const std::string _what = "'\\",
                    const std::string _esc_char = "'\\") {
 
-  std::string::size_type i = 0;
+  size_t i = 0;
   while ((i = _input.find_first_of(_what, i)) != _input.npos) {
     _input.replace(i, 1, _esc_char + _input[i]);
     i += _esc_char.length() + 1;
