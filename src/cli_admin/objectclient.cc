e8c51478b8549325f26e5791b396a51c32b799bf
Aleš Doležal <ales.dolezal@nic.cz>
2008-10-09 14:52:07 +0000

    Chages to resolve ticket #1642. -Better exception catching. -Check fucntions return values. -log messages
    git-svn-id: file:///data/svn/enum/server/trunk@6594 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/cli_admin/objectclient.cc
+++ src/cli_admin/objectclient.cc
@@ -16,12 +16,15 @@
  *  along with FRED.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define RESOLVE_TRY 3
+
 #include "simple.h"
 #include "commonclient.h"
 #include "objectclient.h"
 #include "register/register.h"
 #include "register/poll.h"
 #include "log/logger.h"
+#include "corba/nameservice.h"
 
 namespace Admin {
 
@@ -165,18 +168,21 @@ ObjectClient::deleteObjects(
         const std::string& typeList)
 {
     LOGGER("tracer").trace("ObjectClient::deleteObjects");
-    ccReg::EPP_var epp;
+    ccReg::EPP_var epp = NULL;
     // temporary done by using EPP corba interface
     // should be instead somewhere in register library (object.cc?)
     // get login information for first system registrar
     if (!m_db.ExecSelect(
                 "SELECT r.handle,ra.cert,ra.password "
                 "FROM registrar r, registraracl ra "
-                "WHERE r.id=ra.registrarid AND r.system='t' LIMIT 1 ")
-            )
+                "WHERE r.id=ra.registrarid AND r.system='t' LIMIT 1 ")) {
+        LOG(ERROR_LOG, "deleteObjects(): Error during ExecSelect");
         return -1;
-    if (!m_db.GetSelectRows())
+    }
+    if (!m_db.GetSelectRows()) {
+        LOG(ERROR_LOG, "deleteObjects(): No rows returned (1)");
         return -1;
+    }
     std::string handle = m_db.GetFieldValue(0, 0);
     std::string cert = m_db.GetFieldValue(0, 1);
     std::string password = m_db.GetFieldValue(0, 2);
@@ -198,10 +204,14 @@ ObjectClient::deleteObjects(
     unsigned int limit = m_conf.get<unsigned int>(OBJECT_DELETE_LIMIT_NAME);
     if (limit > 0)
         sql << "LIMIT " << limit;
-    if (!m_db.ExecSelect(sql.str().c_str()))
+    if (!m_db.ExecSelect(sql.str().c_str())) {
+        LOG(ERROR_LOG, "deleteObjects(): Error during ExecSelect");
         return -1;
-    if (!m_db.GetSelectRows())
+    }
+    if (!m_db.GetSelectRows()) {
+        LOG(NOTICE_LOG, "deleteObjects(): No rows returned (2)");
         return 0;
+    }
 
     std::ostream *debug;
     debug = m_conf.hasOpt(OBJECT_DEBUG_NAME) ? &std::cout : NULL;
@@ -217,13 +227,27 @@ ObjectClient::deleteObjects(
     }
     try {
         CORBA::Long clientId = 0;
-        CorbaClient cc(0, NULL, m_nsAddr, m_conf.get<std::string>("nameservice.context"));
-        CORBA::Object_var o = cc.getNS()->resolve("EPP");
-        epp = ccReg::EPP::_narrow(o);
+
+        for (int i = 0; i < RESOLVE_TRY; i++) {
+            try {
+                CorbaClient cc(0, NULL, m_nsAddr, m_conf.get<std::string>("nameservice.context"));
+                CORBA::Object_var o = cc.getNS()->resolve("EPP");
+                if ((epp = ccReg::EPP::_narrow(o)) != NULL) {
+                    break;
+                }
+            } catch (NameService::NOT_RUNNING) {
+                LOG(ERROR_LOG, "deleteObjects(): resolve attempt %d of %d catching NOT_RUNNING", i + 1, RESOLVE_TRY);
+            } catch (NameService::BAD_CONTEXT) {
+                LOG(ERROR_LOG, "deleteObjects(): resolve attempt %d of %d catching BAD_CONTEXT", i + 1, RESOLVE_TRY);
+            }
+        }
+
+        return 0;
         ccReg::Response_var r = epp->ClientLogin(
                 handle.c_str(),password.c_str(),"","system_delete_login",
                 "<system_delete_login/>",clientId,cert.c_str(),ccReg::EN);
         if (r->code != 1000 || !clientId) {
+            LOG(ERROR_LOG, "Cannot connect: %d", r->code);
             std::cerr << "Cannot connect: " << r->code << std::endl;
             throw -3;
         }
@@ -255,14 +279,18 @@ ObjectClient::deleteObjects(
                                 name.c_str(), clientId, cltrid.c_str(), xml.c_str());
                         break;
                 }
-                if (r->code != 1000)
+                if (r->code != 1000) {
+                    LOG(ERROR_LOG, "deleteObjects(): cannot %s: %s", cltrid.c_str(), name.c_str());
                     std::cerr << "Cannot " << cltrid << ": " << name << " code: " << r->code;
-                else
+                }
+                else {
                     std::cerr << cltrid << ": " << name;
+                }
                 std::cerr << std::endl;
             }
             catch (...) {
                 std::cerr << "Cannot " << cltrid << ": " << name << std::endl;
+                LOG(ERROR_LOG, "deleteObjects(): cannot %s: %s", cltrid.c_str(), name.c_str());
                 // proceed with next domain
             }
         }
@@ -271,10 +299,12 @@ ObjectClient::deleteObjects(
         return 0;
     }
     catch (int& i) {
+        LOG(ERROR_LOG, "deleteObjects(): Exception catched: %d", i);
         m_db.FreeSelect();
         return i;
     }
     catch (...) {
+        LOG(ERROR_LOG, "deleteObjects(): Unknown exception catched");
         m_db.FreeSelect();
         return -4;
     }
@@ -289,79 +319,116 @@ ObjectClient::delete_candidates()
 int
 ObjectClient::regular_procedure()
 {
-    //try {
-    std::auto_ptr<Register::Document::Manager> docMan(
-            Register::Document::Manager::create(
-                m_conf.get<std::string>(REG_DOCGEN_PATH_NAME),
-                m_conf.get<std::string>(REG_DOCGEN_TEMPLATE_PATH_NAME),
-                m_conf.get<std::string>(REG_FILECLIENT_PATH_NAME),
-                m_nsAddr)
-            );
-    CorbaClient *cc = new CorbaClient(0, NULL, m_nsAddr, m_conf.get<std::string>("nameservice.context"));
-    MailerManager mailMan(cc->getNS());
+    int i;
+    CorbaClient *cc = NULL;
+    LOG(NOTICE_LOG, "1");
+    try {
+        std::auto_ptr<Register::Document::Manager> docMan(
+                Register::Document::Manager::create(
+                    m_conf.get<std::string>(REG_DOCGEN_PATH_NAME),
+                    m_conf.get<std::string>(REG_DOCGEN_TEMPLATE_PATH_NAME),
+                    m_conf.get<std::string>(REG_FILECLIENT_PATH_NAME),
+                    m_nsAddr)
+                );
+        for (i = 0; i < RESOLVE_TRY; i++) {
+            try {
+                cc = new CorbaClient(0, NULL, m_nsAddr, m_conf.get<std::string>("nameservice.context"));
+                if (cc != NULL) {
+                    break;
+                }
+            } catch (NameService::NOT_RUNNING) {
+                LOG(ERROR_LOG, "regular_procedure(): resolve attempt %d of %d catching NOT_RUNNING", i + 1, RESOLVE_TRY);
+            } catch (NameService::BAD_CONTEXT) {
+                LOG(ERROR_LOG, "regular_procedure(): resolve attempt %d of %d catching BAD_CONTEXT", i + 1, RESOLVE_TRY);
+            }
+        }
+        MailerManager mailMan(cc->getNS());
 
-    std::auto_ptr<Register::Zone::Manager> zoneMan(
-            Register::Zone::Manager::create(&m_db));
-    std::auto_ptr<Register::Domain::Manager> domMan(
-            Register::Domain::Manager::create(&m_db, zoneMan.get()));
-    std::auto_ptr<Register::Contact::Manager> conMan(
-            Register::Contact::Manager::create(
-                &m_db,
-                m_conf.get<bool>(REG_RESTRICTED_HANDLES_NAME))
-            );
-    std::auto_ptr<Register::NSSet::Manager> nssMan(
-            Register::NSSet::Manager::create(
-                &m_db,
-                zoneMan.get(),
-                m_conf.get<bool>(REG_RESTRICTED_HANDLES_NAME))
-            );
-    std::auto_ptr<Register::KeySet::Manager> keyMan(
-            Register::KeySet::Manager::create(
-                &m_db,
-                m_conf.get<bool>(REG_RESTRICTED_HANDLES_NAME))
-            );
-    std::auto_ptr<Register::Poll::Manager> pollMan(
-            Register::Poll::Manager::create(
-                &m_db)
-            );
-    std::auto_ptr<Register::Manager> registerMan(
-            Register::Manager::create(
-                &m_db,
-                m_conf.get<bool>(REG_RESTRICTED_HANDLES_NAME))
-            );
-    std::auto_ptr<Register::Registrar::Manager> regMan(
-            Register::Registrar::Manager::create(&m_db));
-    std::auto_ptr<Register::Notify::Manager> notifyMan(
-            Register::Notify::Manager::create(
-                &m_db,
-                &mailMan,
-                conMan.get(),
-                nssMan.get(),
-                keyMan.get(),
-                domMan.get(),
-                docMan.get(),
-                regMan.get())
-            );
+        std::auto_ptr<Register::Zone::Manager> zoneMan(
+                Register::Zone::Manager::create(&m_db));
+        LOG(NOTICE_LOG, "5");
+        std::auto_ptr<Register::Domain::Manager> domMan(
+                Register::Domain::Manager::create(&m_db, zoneMan.get()));
 
+        LOG(NOTICE_LOG, "6");
+        std::auto_ptr<Register::Contact::Manager> conMan(
+                Register::Contact::Manager::create(
+                    &m_db,
+                    m_conf.get<bool>(REG_RESTRICTED_HANDLES_NAME))
+                );
+        LOG(NOTICE_LOG, "7");
+        std::auto_ptr<Register::NSSet::Manager> nssMan(
+                Register::NSSet::Manager::create(
+                    &m_db,
+                    zoneMan.get(),
+                    m_conf.get<bool>(REG_RESTRICTED_HANDLES_NAME))
+                );
+        LOG(NOTICE_LOG, "8");
+        std::auto_ptr<Register::KeySet::Manager> keyMan(
+                Register::KeySet::Manager::create(
+                    &m_db,
+                    m_conf.get<bool>(REG_RESTRICTED_HANDLES_NAME))
+                );
+        LOG(NOTICE_LOG, "9");
+        std::auto_ptr<Register::Poll::Manager> pollMan(
+                Register::Poll::Manager::create(
+                    &m_db)
+                );
+        LOG(NOTICE_LOG, "10");
+        std::auto_ptr<Register::Manager> registerMan(
+                Register::Manager::create(
+                    &m_db,
+                    m_conf.get<bool>(REG_RESTRICTED_HANDLES_NAME))
+                );
+        LOG(NOTICE_LOG, "11");
+        std::auto_ptr<Register::Registrar::Manager> regMan(
+                Register::Registrar::Manager::create(&m_db));
+        LOG(NOTICE_LOG, "12");
+        std::auto_ptr<Register::Notify::Manager> notifyMan(
+                Register::Notify::Manager::create(
+                    &m_db,
+                    &mailMan,
+                    conMan.get(),
+                    nssMan.get(),
+                    keyMan.get(),
+                    domMan.get(),
+                    docMan.get(),
+                    regMan.get())
+                );
 
-    registerMan->updateObjectStates();
-    registerMan->updateObjectStates();
-    notifyMan->notifyStateChanges(
-            m_conf.get<std::string>(OBJECT_NOTIFY_EXCEPT_TYPES_NAME),
-            0, NULL, false);
-    pollMan->createStateMessages(
-            m_conf.get<std::string>(OBJECT_POLL_EXCEPT_TYPES_NAME),
-            0, NULL);
-    deleteObjects(m_conf.get<std::string>(OBJECT_DELETE_TYPES_NAME));
+        LOG(NOTICE_LOG, "13");
+        registerMan->updateObjectStates();
+        LOG(NOTICE_LOG, "14");
+        registerMan->updateObjectStates();
+        notifyMan->notifyStateChanges(
+                m_conf.get<std::string>(OBJECT_NOTIFY_EXCEPT_TYPES_NAME),
+                0, NULL, false);
+        pollMan->createStateMessages(
+                m_conf.get<std::string>(OBJECT_POLL_EXCEPT_TYPES_NAME),
+                0, NULL);
+        if ((i = deleteObjects(m_conf.get<std::string>(OBJECT_DELETE_TYPES_NAME))) != 0) {
+            LOG(ERROR_LOG, "Admin::ObjectClient::regular_procedure(): Error has occured in deleteObject: %d", i);
+        }
 
-    pollMan->createLowCreditMessages();
-    notifyMan->generateLetters();
-try {
-    delete cc;
-} catch (CORBA::Exception &e) {
-    //std::cerr << "chyba:" << std::endl;
-    ;
-}
+        pollMan->createLowCreditMessages();
+        notifyMan->generateLetters();
+    } catch (ccReg::Admin::SQL_ERROR) {
+        LOG(ERROR_LOG, "Admin::ObjectClient::regular_procedure(): SQL_ERROR catched");
+    } catch (NameService::NOT_RUNNING) {
+        LOG(ERROR_LOG, "Admin::ObjectClient::regular_procedure(): NOT_RUNNING catched");
+    } catch (NameService::BAD_CONTEXT) {
+        LOG(ERROR_LOG, "Admin::ObjectClient::regular_procedure(): BAD_CONTEXT catched");
+    } catch (CORBA::Exception &e) {
+        LOG(ERROR_LOG, "Admin::ObjectClient::regular_procedure(): CORBA exception catched");
+    } catch (...) {
+        LOG(ERROR_LOG, "Admin::ObjectClient::regular_procedure(): unknown exception catched");
+    }
+
+    try {
+        delete cc;
+    } catch (CORBA::Exception &e) {
+        ;
+    }
     return 0;
 }
 
