fed3143dc26ed9ed46cbc5dfeb88f20cd05e68cd
Jiří Šádek <jiri.sadek@nic.cz>
2008-12-04 13:18:55 +0000

    FIX: fixed merge r6886 (util.h escape function) won't merge
    git-svn-id: file:///data/svn/enum/server/trunk@6913 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- util/util.h
+++ util/util.h
@@ -11,15 +11,13 @@ inline std::string escape(std::string _input,
                    const std::string _esc_char = "\\") {
 
   size_t i = 0;
-  size_t c = 0;
   while ((i = _input.find_first_of(_what, i)) != _input.npos) {
-    c = _what.find(_input[i], 0);
-    _input.replace(i, 1, _esc_char, c, 1);
-    _input.replace(i+1, 0, _input, i, 1);
-    i += 2;
+    _input.replace(i, 1, _esc_char + _input[i]);
+    i += _esc_char.length() + 1;
   }
   return _input;
 }
+
 
 template<class TpIn, class TpOut> 
 TpOut stream_cast(const TpIn& _value) {
