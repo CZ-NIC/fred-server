a4290d96cd55491948a9d5adfd3e922d05bec519
Juraj Viceník <juraj.vicenik@nic.cz>
2009-03-18 09:41:54 +0000

    ADD: pagetable for request logger
    git-svn-id: file:///data/svn/enum/server/trunk@7512 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ src/corba/admin/pagetable_logger.h
@@ -0,0 +1,24 @@
+#ifndef PAGETABLE_LOGGER_H_
+#define PAGETABLE_LOGGER_H_
+
+#include "pagetable_impl.h"
+
+class ccReg_Logger_i : public ccReg_PageTable_i,
+	public PortableServer::RefCountServantBase {
+private: 
+	std::auto_ptr<Register::Logger::List> m_lel;
+
+public:
+	//ccReg_Logger_i(Register::LogEntry::List *list, const Settings *_ptr);
+	ccReg_Logger_i(Register::Logger::List *list);
+	~ccReg_Logger_i();
+	DECL_PAGETABLE_I;
+
+	ccReg::Filters::Compound_ptr add();
+	Register::Logger::LogEntry *findId(ccReg::TID _id);
+
+public: 
+	const static int NUM_COLUMNS;
+};
+
+#endif // PAGETABLE_LOGGER_H_
