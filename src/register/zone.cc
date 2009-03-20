5e294f3c4f4da76d1e11d065c292776ed201cb51
Jaromír Talíř <jaromir.talir@nic.cz>
2009-03-20 17:26:01 +0000

    fixed zone check #1892
    git-svn-id: file:///data/svn/enum/server/trunk@7543 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/register/zone.cc
+++ src/register/zone.cc
@@ -37,7 +37,7 @@ namespace Register
   {
     struct ZoneImpl : public virtual Zone {
       ZoneImpl(
-        TID _id, const std::string& _fqdn, bool _isEnum, 
+        TID _id, const std::string& _fqdn, bool _isEnum,
         unsigned _maxLevel
       ) :
         id(_id), fqdn(_fqdn), isEnum(_isEnum), maxLevel(_maxLevel)
@@ -55,7 +55,7 @@ namespace Register
           copy[i] = tolower(copy[i]);
         unsigned l = fqdn.length();
         if (copy.length() < l) return false;
-        return copy.compare(copy.length()-l,l,fqdn) == 0;  
+        return copy.compare(copy.length()-l,l,fqdn) == 0;
       }
       /// interface implementation
       const TID getId() const
@@ -63,20 +63,20 @@ namespace Register
         return id;
       }
       /// interface implementation
-      const std::string& getFqdn() const 
+      const std::string& getFqdn() const
       {
-        return fqdn; 
+        return fqdn;
       }
       /// interface implementation
-      bool isEnumZone() const 
+      bool isEnumZone() const
       {
-        return isEnum; 
+        return isEnum;
       }
       /// interface implementation
       virtual unsigned getMaxLevel() const
       {
         return maxLevel;
-      }                 
+      }
     };
     typedef std::vector<ZoneImpl> ZoneList;
     class ManagerImpl : virtual public Manager
@@ -89,11 +89,11 @@ namespace Register
       bool loaded;
      public:
       ManagerImpl(DB *_db) :
-       db(_db), 
+       db(_db),
        defaultEnumSuffix("0.2.4"),
        enumZoneString("e164.arpa"),
        defaultDomainSuffix("cz"),
-       loaded(false) 
+       loaded(false)
       {
       }
       void load()
@@ -114,12 +114,12 @@ namespace Register
         db->FreeSelect();
         loaded = true;
       }
-      /// interface method implementation  
+      /// interface method implementation
       void parseDomainName(
         const std::string& fqdn, DomainName& domain, bool allowIDN
       ) const throw (INVALID_DOMAIN_NAME)
       {
-        std::string part; // one part(label) of fqdn  
+        std::string part; // one part(label) of fqdn
         for (unsigned i=0; i<fqdn.size(); i++) {
           if (part.empty()) {
             // first character of every label has to be letter or digit
@@ -139,14 +139,14 @@ namespace Register
               domain.push_back(part);
               part.clear();
               continue;
-            } 
+            }
             else {
               if (fqdn[i] == '-') {
                 // dash '-' is acceptable only if last character wasn't dash
-                if (part[part.length()-1] == '-' && 
-                    (!allowIDN || part != "xn-")) 
+                if (part[part.length()-1] == '-' &&
+                    (!allowIDN || part != "xn-"))
                   throw INVALID_DOMAIN_NAME();
-              } 
+              }
               else {
                 // other character could be only number or letter
                 if (!IS_NUMBER(fqdn[i]) && !IS_LETTER(fqdn[i]))
@@ -185,7 +185,7 @@ namespace Register
         std::string::size_type i = fqdn.rfind(esuf);
         return  i != std::string::npos && i + esuf.size() == fqdn.size();
       }
-      /// interface method implementation  
+      /// interface method implementation
       std::string makeEnumDomain(const std::string& number)
         const throw (NOT_A_NUMBER)
       {
@@ -211,9 +211,9 @@ namespace Register
         }
         // append enum domain zone
         result += '.';
-        result += getEnumZoneString(); 
+        result += getEnumZoneString();
         return result;
-      }      
+      }
       const std::string& getDefaultEnumSuffix() const
       {
         return defaultEnumSuffix;
@@ -224,7 +224,7 @@ namespace Register
       }
       const std::string& getEnumZoneString() const
       {
-        return enumZoneString; 
+        return enumZoneString;
       }
       const Zone* findZoneId(const std::string& fqdn) const
       {
@@ -239,7 +239,7 @@ namespace Register
         if (domain.size() < 1) return false;
         std::stringstream sql;
         sql << "SELECT COUNT(*) FROM enum_tlds "
-            << "WHERE LOWER(tld)='" << *domain.rbegin() << "'";
+            << "WHERE LOWER(tld)=LOWER('" << *domain.rbegin() << "')";
         if (!db->ExecSelect(sql.str().c_str())) return false;
         if (!db->GetSelectRows()) return false;
         bool ret = atoi(db->GetFieldValue(0,0)) > 0;
@@ -256,7 +256,7 @@ namespace Register
         if (!db->GetSelectRows()) throw SQL_ERROR();
         exists = atoi(db->GetFieldValue(0,0)) > 0;
         db->FreeSelect();
-        if (exists) throw ALREADY_EXISTS(); 
+        if (exists) throw ALREADY_EXISTS();
         unsigned dots = 1;
         for (unsigned i=0; i<fqdn.size(); i++)
           if (fqdn[i] == '.') dots++;
@@ -305,7 +305,7 @@ namespace Register
         idna_to_unicode_8z8z(fqdn.c_str(), &p, 0);
         result = p ? p : fqdn;
         if (p) free(p);
-        return result;        
+        return result;
       }
     };
     Manager* Manager::create(DB *db)
