275167f2a6179f55ae94bbb76116fd568aec0af7
Jan Zima <jan.zima@nic.cz>
2009-09-14 12:00:21 +0000

    added models registrarinvoice and enum_tlds, ticket #2100
    git-svn-id: file:///data/svn/enum/server/trunk@8960 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ src/register/model_enum_tlds.cc
@@ -0,0 +1,11 @@
+#include "model_enum_tlds.h"
+
+std::string ModelEnumTlds::table_name = "enum_tlds";
+
+DEFINE_PRIMARY_KEY(ModelEnumTlds, unsigned long long, tld, m_tld, table_name, "tld", .setDefault())
+
+
+ModelEnumTlds::field_list ModelEnumTlds::fields = list_of<ModelEnumTlds::field_list::value_type>
+    (&ModelEnumTlds::tld)
+;
+
---
+++ src/register/model_enum_tlds.h
@@ -0,0 +1,80 @@
+#ifndef _MODEL_ENUMTLDS_H_
+#define _MODEL_ENUMTLDS_H_
+
+#include "db_settings.h"
+#include "model.h"
+
+
+class ModelEnumTlds:
+    public Model::Base {
+public:
+    ModelEnumTlds()
+    { }
+    virtual ~ModelEnumTlds()
+    { }
+    const unsigned long long &getTld() const
+    {
+        return m_tld.get();
+    }
+    void setTld(const unsigned long long &tld)
+    {
+        m_tld = tld;
+    }
+
+    friend class Model::Base;
+
+    void insert()
+    {
+        Database::Connection conn = Database::Manager::acquire();
+        Database::Transaction tx(conn);
+        Model::Base::insert(this);
+        tx.commit();
+    }
+
+    void update()
+    {
+        Database::Connection conn = Database::Manager::acquire();
+        Database::Transaction tx(conn);
+        Model::Base::update(this);
+        tx.commit();
+    }
+
+    void reload()
+    {
+        Database::Connection conn = Database::Manager::acquire();
+        Database::Transaction tx(conn);
+        Model::Base::reload(this);
+        tx.commit();
+    }
+
+    void load(const Database::Row &_data)
+    {
+        Model::Base::load(this, _data);
+    }
+
+    std::string toString() const
+    {
+        return Model::Base::toString(this);
+    }
+
+    typedef Model::Field::List<ModelEnumTlds>  field_list;
+    static const field_list& getFields()
+    {
+        return fields;
+    }
+
+protected:
+    Field::Field<unsigned long long> m_tld;
+
+
+public:
+    static Model::Field::PrimaryKey<ModelEnumTlds, unsigned long long> tld;
+
+
+private:
+    static std::string table_name;  /** < model table name */
+    static field_list  fields;      /** < list of all model fields */
+}; // class ModelEnumTlds
+
+#endif // _MODEL_ENUMTLDS_H_
+
