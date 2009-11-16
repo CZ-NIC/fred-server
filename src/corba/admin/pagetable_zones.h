e1c6e28d5d98da19dcbcab95fd72dc48e07d1edc
Jan Zima <jan.zima@nic.cz>
2009-11-16 14:56:52 +0000

    Zone lists, manager, pagetable, makefiles, work in progress , Ticket #2095
    git-svn-id: file:///data/svn/enum/server/trunk@9471 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ src/corba/admin/pagetable_zones.h
@@ -0,0 +1,20 @@
+#ifndef _PAGETABLE_ZONES_H_
+#define _PAGETABLE_ZONES_H_
+
+#include "pagetable_impl.h"
+
+class ccReg_Zones_i:
+    public ccReg_PageTable_i,
+    public PortableServer::RefCountServantBase {
+private:
+    std::auto_ptr<Register::Zone::ZoneList> m_zoneList;
+public:
+    ccReg_Zones_i(Register::Zone::ZoneList *zoneList);
+    ~ccReg_Zones_i();
+    DECL_PAGETABLE_I;
+
+    ccReg::Filters::Compound_ptr add();
+    Register::Zone::Zone *findId(ccReg::TID id);
+}; // class ccReg_Zones_i
+
+#endif // _PAGETABLE_ZONES_H_
