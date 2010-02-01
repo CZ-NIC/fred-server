43a0dae9ebcce020c30b96dd33a6035e91cc80de
Jan Zima <jan.zima@nic.cz>
2010-02-01 15:42:16 +0000

    try {
    git-svn-id: file:///data/svn/enum/server/trunk@10410 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/corba/server.cc
+++ src/corba/server.cc
@@ -283,8 +283,10 @@ int main(int argc, char** argv) {
     NameService ns(orb, nameservice, cfg.get<std::string>("nameservice.context"));
 
     /* register specific object at nameservice */
-#ifdef ADIF
-    Database::Manager::init(new Database::ConnectionFactory(conn_info, 1, 10));
+
+    Database::Manager::init(new Database::ConnectionFactory(conn_info, 1, 100));
+
+    #ifdef ADIF
     PortableServer::ObjectId_var adminObjectId = PortableServer::string_to_ObjectId("Admin");
     ccReg_Admin_i* myccReg_Admin_i = new ccReg_Admin_i(conn_info, &ns, cfg);
     poa->activate_object_with_id(adminObjectId, myccReg_Admin_i);
@@ -294,7 +296,6 @@ int main(int argc, char** argv) {
 #endif
 
 #ifdef PIFD
-    Database::Manager::init(new Database::ConnectionFactory(conn_info, 1, 10));
     PortableServer::ObjectId_var webWhoisObjectId = PortableServer::string_to_ObjectId("WebWhois");
     ccReg_Admin_i* myccReg_Admin_i = new ccReg_Admin_i(conn_info, &ns, cfg, false);
     poa->activate_object_with_id(webWhoisObjectId, myccReg_Admin_i);
@@ -304,7 +305,6 @@ int main(int argc, char** argv) {
 #endif
 
 #ifdef RIFD
-    Database::Manager::init(new Database::ConnectionFactory(conn_info, 1, 10));
     MailerManager mm(&ns);
     ccReg_EPP_i* myccReg_EPP_i = new ccReg_EPP_i(conn_info, &mm, &ns, cfg);
 
@@ -349,7 +349,6 @@ int main(int argc, char** argv) {
 #endif
 
 #ifdef LOGD
-    // Database::Manager::init(new Database::ConnectionFactory(conn_info, 1, 10));
     PortableServer::ObjectId_var loggerObjectId =
     PortableServer::string_to_ObjectId("Logger");
     ccReg_Log_i* myccReg_Log_i;
