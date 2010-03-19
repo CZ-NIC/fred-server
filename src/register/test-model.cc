39d87fc04ff85520afa4e9fe9629d408978213d8
Jan Zima <jan.zima@nic.cz>
2010-03-19 12:53:19 +0000

    db model test - added serialized checking of result, Ticket #3535
    git-svn-id: file:///data/svn/enum/server/trunk@11024 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/register/test-model.cc
+++ src/register/test-model.cc
@@ -58,8 +58,8 @@ namespace std
      *  log_duration = off
      *  log_statement = 'none'
      *
-     *  lower shared_buffers
-     *  set max_connections > 300
+     *  lower shared_buffers ~2MB
+     *  set max_connections > 300 ~400
      *
      * postgres restart
      *
@@ -118,6 +118,12 @@ public:
         , gtime(rng, gen_time)
     {
         std::cout << "RandomDataGenerator using seed: " << seed_ << std::endl;
+    }
+
+    //used seed getter
+    unsigned get_seed() const
+    {
+        return seed_;
     }
 
     //generate some letter A-Z a-z
@@ -225,81 +231,110 @@ struct sync_barriers
     {}
 };
 
+struct Result
+{
+    unsigned number;//thread number
+    unsigned ret;//return code
+    std::string desc;//some closer description
+};
+
+typedef concurrent_queue<Result> ResultQueue;
+
 //thread functor
 class ModelBankPaymentThreadWorker
 {
 public:
 
-    ModelBankPaymentThreadWorker(unsigned number,unsigned sleep_time, sync_barriers* sb_ptr, unsigned seed = 0)
+    ModelBankPaymentThreadWorker(unsigned number,unsigned sleep_time, sync_barriers* sb_ptr, std::size_t thread_group_divisor, ResultQueue* result_queue_ptr = 0, unsigned seed = 0)
             : number_(number)
             , sleep_time_(sleep_time)
             , sb_ptr_(sb_ptr)
-            , rdg(seed)
+            , rdg_(seed)
+            , tgd_(thread_group_divisor)
+            , rsq_ptr (result_queue_ptr)
     {}
 
     void operator()()
     {
         ModelBankPayment mbp1, mbp2;
 
-        if(number_%2)//if odd number
+        if(number_%tgd_)//if synchronized thread
         {
             std::cout << "waiting: " << number_ << std::endl;
-            sb_ptr_->insert_barrier.wait();//wait for other odd threads
+            if(sb_ptr_)
+                sb_ptr_->insert_barrier.wait();//wait for other synced threads
         }
         else
-        {//even threads don't wait
+        {//non-synchronized thread
             std::cout << "NOwaiting: " << number_ << std::endl;
         }
 
         std::cout << "start: " << number_ << std::endl;
-
-
-        mbp_insert_data insert_data;
-
+        mbp_insert_data insert_data;//mostly randomly generated data
         insert_data.statement_id=0;//fk bank_statement (id) - none
-        insert_data.account_id=rdg.xnum1_6(); //fk bank_account (id) - num 1-6
+        insert_data.account_id=rdg_.xnum1_6(); //fk bank_account (id) - num 1-6
         insert_data.invoice_id=0; //fk invoice (id) - none
-        insert_data.account_number=rdg.xnumstring(17);//17 numletters
-        insert_data.bank_code=rdg.xnumstring(4);//4 numletters
-        insert_data.operation_code=rdg.xnum1_5(); // num 1-5
-        insert_data.transfer_type=rdg.xnum1_5(); // num 1-5
-        insert_data.payment_status=rdg.xnum1_6();// num 1-6
-        insert_data.konstsym=rdg.xnumstring(10);// 10 numletters
-        insert_data.varsymb=rdg.xnumstring(10);// 10 numletters
-        insert_data.specsymb=rdg.xnumstring(10);// 10 numletters
-        insert_data.price=rdg.xint();//int
-        insert_data.account_evid=rdg.xnumstring(20);//20 numletters
-        insert_data.account_date=rdg.xdate(); //some date
-        insert_data.account_memo=rdg.xstring(64); //64 chars
-        insert_data.account_name=rdg.xstring(64); //64 chars
-        insert_data.crtime=rdg.xptime();//timestamp
+        insert_data.account_number=rdg_.xnumstring(17);//17 numletters
+        insert_data.bank_code=rdg_.xnumstring(4);//4 numletters
+        insert_data.operation_code=rdg_.xnum1_5(); // num 1-5
+        insert_data.transfer_type=rdg_.xnum1_5(); // num 1-5
+        insert_data.payment_status=rdg_.xnum1_6();// num 1-6
+        insert_data.konstsym=rdg_.xnumstring(10);// 10 numletters
+        insert_data.varsymb=rdg_.xnumstring(10);// 10 numletters
+        insert_data.specsymb=rdg_.xnumstring(10);// 10 numletters
+        insert_data.price=rdg_.xint();//int
+        insert_data.account_evid=rdg_.xnumstring(20);//20 numletters
+        insert_data.account_date=rdg_.xdate(); //some date
+        insert_data.account_memo=rdg_.xstring(64); //64 chars
+        insert_data.account_name=rdg_.xstring(64); //64 chars
+        insert_data.crtime=rdg_.xptime();//timestamp
 
+        Result res;
+        res.ret = 0;
+        res.number = number_;
 
-        BOOST_REQUIRE_EQUAL(
-                mbp_insert_test(mbp1, insert_data)
-                , 0);
+        if((res.ret = mbp_insert_test(mbp1, insert_data))!=0)
+        {
+            res.desc = std::string("error in: mbp_insert_test using seed: ")
+                + boost::lexical_cast<std::string>(rdg_.get_seed());
+            if(rsq_ptr) rsq_ptr->push(res);
+            return;
+        }
 
-        if(number_%2) sb_ptr_->reload_barrier.wait();//wait for other odd threads
+        if(number_%tgd_)
+            if(sb_ptr_)
+                sb_ptr_->reload_barrier.wait();//wait for other synced threads
+        if((res.ret = mbp_reload_test(mbp1, mbp2))!=0)
+        {
+            res.desc = std::string("error in: mbp_reload_test using seed: ")
+                + boost::lexical_cast<std::string>(rdg_.get_seed());
+            if(rsq_ptr) rsq_ptr->push(res);
+            return;
+        }
 
-        BOOST_REQUIRE_EQUAL(
-                mbp_reload_test(mbp1, mbp2)
-                , 0);
-
-        if(number_%2) sb_ptr_->update_barrier.wait();//wait for other odd threads
-
-        BOOST_REQUIRE_EQUAL(
-                mbp_update_test(mbp1, mbp2)
-                , 0);
-
-
+        if(number_%tgd_)
+            if(sb_ptr_)
+                sb_ptr_->update_barrier.wait();//wait for other synced threads
+        if((res.ret = mbp_update_test(mbp1, mbp2))!=0)
+        {
+            res.desc = std::string("error in: mbp_update_test using seed: ")
+                + boost::lexical_cast<std::string>(rdg_.get_seed());
+            if(rsq_ptr) rsq_ptr->push(res);
+            return;
+        }
+        res.desc = std::string("ok");
+        if(rsq_ptr) rsq_ptr->push(res);
         std::cout << "end: " << number_ << std::endl;
     }
 
 private:
+    //need only defaultly constructible members here
     unsigned    number_;//thred identification
     unsigned    sleep_time_;//[s]
     sync_barriers* sb_ptr_;
-    RandomDataGenerator rdg;
+    RandomDataGenerator rdg_;
+    std::size_t tgd_;//thread group divisor
+    ResultQueue* rsq_ptr; //result queue non-owning pointer
 };
 
 BOOST_AUTO_TEST_CASE( test_model_files )
@@ -315,25 +350,43 @@ BOOST_AUTO_TEST_CASE( test_model_files )
 BOOST_AUTO_TEST_CASE( test_model_bank_payments_threaded )
 {
     std::size_t const thread_number = 300;//number of threads in test
+    // int(thread_number - thread_number / thread_group_divisor) is number of synced threads
+    std::size_t const thread_group_divisor = 4;
+
+    ResultQueue result_queue;
 
     //vector of thread functors
     std::vector<ModelBankPaymentThreadWorker> tw_vector;
     tw_vector.reserve(thread_number);
 
     //synchronization barriers instance
-    sync_barriers sb(thread_number/2);
+    sync_barriers sb(thread_number-thread_number/thread_group_divisor);
 
     //thread container
     boost::thread_group threads;
     for (unsigned i = 0; i < thread_number; ++i)
     {
-        tw_vector.push_back(ModelBankPaymentThreadWorker(i,3,&sb));
+        tw_vector.push_back(ModelBankPaymentThreadWorker(i,3,&sb, thread_group_divisor, &result_queue));
         threads.create_thread(tw_vector.at(i));
     }
 
     threads.join_all();
+    for(unsigned i = 0; i < thread_number; ++i)
+    {
+        Result thread_result;
+        result_queue.wait_and_pop(thread_result);
 
-    BOOST_CHECK( 1 + 1 == 2 );
+        BOOST_REQUIRE_EQUAL(thread_result.ret , 0);
+
+        if(thread_result.ret)
+        {
+            std::cerr << thread_result.desc
+                    << " thread number: " << thread_result.number
+                    << " return code: " << thread_result.ret
+                    << std::endl;
+        }
+    }//for i
+
 }
 
 
