CZ-NIC/fred-server

commit 0bd346189d61c719ed5f4051a1f17eabf40a02e7

Race condition when inserting properties - temporary inconsistency between std::map in memory and database caused breach of constraint in DB
git-svn-id: file:///data/svn/enum/server/trunk@11291 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

src/register/request.cc
@@ -590,56 +590,67 @@ bool ManagerImpl::record_check(ID id, Connection &conn)
 
 // find ID for the given name of a property
 // if the name is tool long, it's truncated to the maximal length allowed by the database
-ID ManagerImpl::find_property_name_id(const std::string &name, Connection &conn)
+ID ManagerImpl::find_property_name_id(const std::string &name, Connection &conn, boost::mutex::scoped_lock& prop_add2db)
 {
-    TRACE("[CALL] Register::Logger::ManagerImpl::find_property_name_id");
+        TRACE("[CALL] Register::Logger::ManagerImpl::find_property_name_id");
 	ID name_id;
 	std::map<std::string, ID>::iterator iter;
 
 	std::string name_trunc = name.substr(0, MAX_NAME_LENGTH);
+       
+        prop_add2db.lock();
 
-        // lock optimization
-        boost::mutex::scoped_lock properties_lock (properties_mutex);
+        bool db_insert = false;
+
 	iter = property_names.find(name_trunc);
 
 	if(iter != property_names.end()) {
 		name_id = iter->second;
-	} else {
-		// if the name isn't cached in the memory, try to find it in the database
-                properties_lock.unlock();
 
-		std::string s_name = conn.escape(name_trunc);
+                prop_add2db.unlock();
+                // unlock
+        } else {
+            // if the name isn't cached in the memory, try to find it in the database
 
-		boost::format query = boost::format("select id from request_property where name='%1%'") % s_name;
-		Result res = conn.exec(query.str());
+            std::string s_name = conn.escape(name_trunc);
 
-		if (res.size() > 0) {
-			// okay, it was found in the database
-			name_id = res[0][0];
-		} else if (res.size() == 0) {
-			// if not, add it to the database
-			ModelRequestProperty pn;
-			pn.setName(name_trunc);
+            boost::format query = boost::format("select id from request_property where name='%1%'") % s_name;
+            Result res = conn.exec(query.str());
+           
+            if (res.size() > 0) {
+                // okay, it was found in the database
+                name_id = res[0][0];
+            } else {
+                // not found, we're under lock, so we can add it now
+                // and let the lock release after commiting the transaction
+                ModelRequestProperty pn;
+                pn.setName(name_trunc);
 
-			try {
-				pn.insert();
-                                name_id = pn.getId();
-			} catch (Database::Exception &ex) {
-				logger_error(ex.what());
-			}
-		}
-
-		// now that we know the right database id of the name
-		// we can add it to the map
-                properties_lock.lock();
-		property_names[name_trunc] = name_id;
+                try {
+                    pn.insert();
+                    name_id = pn.getId();
+                } catch (Database::Exception &ex) {
+                    logger_error(ex.what());
+                    prop_add2db.unlock();
+                    return 0;
+                }
+                db_insert = true;
+            }
+        
+            // now that we know the right database id of the name
+            // we can add it to the map
+            property_names[name_trunc] = name_id;
+            
+            // if the name was inserted into database, we have to keep it locked
+            // until commit
+            if (!db_insert) prop_add2db.unlock();
 	}
-
+        
 	return name_id;
 }
 
 // insert properties for the given request record
-void ManagerImpl::insert_props(DateTime entry_time, RequestServiceType service, bool monitoring, ID entry_id,  const Register::Logger::RequestProperties& props, Connection conn)
+void ManagerImpl::insert_props(DateTime entry_time, RequestServiceType service, bool monitoring, ID entry_id,  const Register::Logger::RequestProperties& props, Connection conn, boost::mutex::scoped_lock &prop_lock)
 {
         TRACE("[CALL] Register::Logger::ManagerImpl::insert_props");
 	ID name_id, last_id = 0;
@@ -649,7 +660,7 @@ void ManagerImpl::insert_props(DateTime entry_time, RequestServiceType service, 
 	}
 
 	// process the first record
-	name_id = find_property_name_id(props[0].name, conn);
+	name_id = find_property_name_id(props[0].name, conn, prop_lock);
 
 	if (props[0].child) {
 		// the first property is set to child - this is an error
@@ -670,7 +681,7 @@ void ManagerImpl::insert_props(DateTime entry_time, RequestServiceType service, 
 
 	// process the rest of the sequence
 	for (unsigned i = 1; i < props.size(); i++) {
-		name_id = find_property_name_id(props[i].name, conn);
+		name_id = find_property_name_id(props[i].name, conn, prop_lock);
 
 		// create a new object for each iteration
 		// because ParentId must alternate between NULL and some value
@@ -708,6 +719,9 @@ ID ManagerImpl::i_CreateRequest(const char *sourceIP, RequestServiceType service
         std::auto_ptr<Logging::Context> ctx_entry;
 
         logd_auto_db db;
+
+        boost::mutex::scoped_lock prop_lock(properties_mutex);
+        prop_lock.unlock();
 
 	ID entry_id;
 
@@ -759,7 +773,7 @@ ID ManagerImpl::i_CreateRequest(const char *sourceIP, RequestServiceType service
 		return 0;
 	} catch (ConversionError &ex) {
 		std::cout << "Exception text: " << ex.what() << std::endl;
-		std::cout << "Here it is. Time: "  << std::endl;
+                return 0;
 	}
 
 	try {
@@ -778,10 +792,11 @@ ID ManagerImpl::i_CreateRequest(const char *sourceIP, RequestServiceType service
 		}
 
 		// inserting properties
-		insert_props(time, service, monitoring, entry_id, props, db);
+		insert_props(time, service, monitoring, entry_id, props, db, prop_lock);
 
 	} catch (Database::Exception &ex) {
 		logger_error(ex.what());
+                return 0;
 	}
 
         db.commit();
@@ -821,6 +836,9 @@ bool ManagerImpl::i_UpdateRequest(ID id, const Register::Logger::RequestProperti
 
         logd_auto_db db;
 
+        boost::mutex::scoped_lock prop_lock(properties_mutex);
+        prop_lock.unlock();
+
 	try {
 		// perform check
 #ifdef DEBUG_LOGD
@@ -840,7 +858,7 @@ bool ManagerImpl::i_UpdateRequest(ID id, const Register::Logger::RequestProperti
 		DateTime time = res[0][0].operator ptime();
 		RequestServiceType service = (RequestServiceType)(int)res[0][1];
 		bool monitoring        = (bool)res[0][2];
-		insert_props(time, service, monitoring, id, props, db);
+		insert_props(time, service, monitoring, id, props, db, prop_lock);
 	} catch (Database::Exception &ex) {
 		logger_error(ex.what());
 		return false;
@@ -859,6 +877,9 @@ bool ManagerImpl::close_request_worker(Connection &conn, ID id, const char *cont
 	bool monitoring;
 
 	time = boost::posix_time::to_iso_string(microsec_clock::universal_time());
+
+        boost::mutex::scoped_lock prop_lock(properties_mutex);
+        prop_lock.unlock();
 
 	try {
 		// first perform checks:
@@ -898,7 +919,7 @@ bool ManagerImpl::close_request_worker(Connection &conn, ID id, const char *cont
                     }
 
                     // inserting properties
-                    insert_props(res[0][0].operator ptime(), service, monitoring, id, props, conn);
+                    insert_props(res[0][0].operator ptime(), service, monitoring, id, props, conn, prop_lock);
 
                 }
 

src/register/request_impl.h
@@ -49,17 +49,18 @@ public:
 
  // for migration tool (util/logd_migration)
   void insert_props_pub(DateTime entry_time, RequestServiceType entry_service, bool monitoring, Database::ID entry_id, const Register::Logger::RequestProperties& props) {
-
-	insert_props(entry_time, entry_service, monitoring, entry_id, props, get_connection());
+        boost::mutex::scoped_lock props_lock(properties_mutex);
+	insert_props(entry_time, entry_service, monitoring, entry_id, props, get_connection(), props_lock);
   }
   
   List* createList() const;
 
 private:
   bool close_request_worker(Connection &conn, ID id, const char *content_out, const Register::Logger::RequestProperties &props);
-  void insert_props(DateTime entry_time, RequestServiceType service, bool monitoring, ID entry_id, const Register::Logger::RequestProperties& props, Connection conn);
+  
+  void insert_props(DateTime entry_time, RequestServiceType service, bool monitoring, ID entry_id, const Register::Logger::RequestProperties& props, Connection conn, boost::mutex::scoped_lock &prop_lock);
   bool record_check(Database::ID id, Connection &conn);
-  Database::ID find_property_name_id(const std::string &name, Connection &conn);
+  Database::ID find_property_name_id(const std::string &name, Connection &conn, boost::mutex::scoped_lock& prop_add2db);
   inline Database::ID find_last_property_value_id(Connection &conn);
   inline Database::ID find_last_request_id(Connection &conn);
   inline std::string getSessionUserName(Connection &conn, Database::ID session_id);

commit 2a72ee06fac36ab192674bb25422f8848e96d31f

Fixed problems introduced by r11291 - properties were not filled probably due to double lock call
git-svn-id: file:///data/svn/enum/server/trunk@11322 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

src/register/request.cc
@@ -598,7 +598,9 @@ ID ManagerImpl::find_property_name_id(const std::string &name, Connection &conn,
 
 	std::string name_trunc = name.substr(0, MAX_NAME_LENGTH);
        
-        prop_add2db.lock();
+        if(!prop_add2db.owns_lock()) {
+            prop_add2db.lock();
+        }
 
         bool db_insert = false;
 
@@ -718,10 +720,11 @@ ID ManagerImpl::i_CreateRequest(const char *sourceIP, RequestServiceType service
         TRACE("[CALL] Register::Logger::ManagerImpl::i_CreateRequest");
         std::auto_ptr<Logging::Context> ctx_entry;
 
-        logd_auto_db db;
 
         boost::mutex::scoped_lock prop_lock(properties_mutex);
         prop_lock.unlock();
+
+        logd_auto_db db;
 
 	ID entry_id;
 