049f57101830f4a9a68c5c47236a4c00ef6322a0
Jan Zima <jan.zima@nic.cz>
2010-06-01 12:59:03 +0000

    test-file-manager-client config, Ticket #4053
    git-svn-id: file:///data/svn/enum/server/trunk@11977 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ tests/test-file-manager-client.cc
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2007  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ // test-file-manager-client.cc
+
+
+#define BOOST_TEST_MODULE Test file manager client
+
+#include "test-file-manager-client.h"
+
+//args processing config for custom main
+HandlerPtrVector global_hpv =
+boost::assign::list_of
+(HandleArgsPtr(new HandleGeneralArgs))
+(HandleArgsPtr(new HandleCorbaNameServiceArgs))
+;
+
+#include "test_custom_main.h"
+
+ccReg::TID gid3 = 0;
+
+BOOST_AUTO_TEST_CASE( test_fmc_simple )
+{
+;
+}
---
+++ tests/test-file-manager-client.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2007  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// test-file-manager-client.h
+
+#ifndef TESTFILEMANAGERCLIENT_H_
+#define TESTFILEMANAGERCLIENT_H_
+
+#include <memory>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <map>
+#include <exception>
+#include <queue>
+#include <sys/time.h>
+#include <time.h>
+
+#include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/barrier.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time.hpp>
+#include <boost/assign/list_of.hpp>
+
+#include "corba_wrapper.h"
+#include "log/logger.h"
+#include "log/context.h"
+
+#include "random_data_generator.h"
+#include "concurrent_queue.h"
+#include "common.h"
+
+#include "handle_general_args.h"
+#include "handle_corbanameservice_args.h"
+
+
+#endif //TESTFILEMANAGERCLIENT_H_
