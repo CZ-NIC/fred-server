2006577346889df30a9635d5b68f73d70091c98b
Jan Zima <jan.zima@nic.cz>
2010-06-08 16:28:22 +0000

    hpmail testing curl command, #3895
    git-svn-id: file:///data/svn/enum/server/trunk@12119 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- util/hp/post_test.sh
+++ util/hp/post_test.sh
@@ -3,7 +3,7 @@
 echo
 echo "#login start#"
 echo
-curl -F "cc=us \n" -F "loginame=dreplech" -F "password=dreplech" \
+curl -F "cc=us  " -F "loginame=dreplech" -F "password=dreplech" \
 -F "standzak=hpcb_Jednorazova_zakazka" -F "poznamka=Testovaci prenos!!!" \
 -F "jobzak=2010" -F "verzeOS=Linux" -F "verzeProg=20100315001" -b ./tmpdir/phpcookie \
 -A '' --cacert ./cert/postsignum_qca_root.pem -i \
@@ -17,10 +17,12 @@ echo
 echo "#upload start#"
 echo
 
-curl -F "cc=us \n" -F "pocetupl=1\n" -F "crc=C74E16D9" \
+curl -F 'cc=us 
+' -F 'pocetupl=1
+' -F "crc=C74E16D9" \
 -F "upfile=@./tmpdir/test.txt" -b ./tmpdir/dump.txt \
 -H 'User-Agent: CommandLine klient HP' --cacert ./cert/postsignum_qca_root.pem \
--v https://online.postservis.cz/Command/command.php
+--trace-ascii -  https://online.postservis.cz/Command/command.php
 
 echo
 echo "#upload end#"
