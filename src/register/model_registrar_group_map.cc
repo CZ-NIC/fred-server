466011696552fc841b30ebc6a11aaa716a8669c4
Jan Zima <jan.zima@nic.cz>
2010-06-22 07:24:43 +0000

    fix #3862
    git-svn-id: file:///data/svn/enum/server/trunk@12374 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/register/model_registrar_group_map.cc
+++ src/register/model_registrar_group_map.cc
@@ -6,7 +6,7 @@ DEFINE_PRIMARY_KEY(ModelRegistrarGroupMap, unsigned long long, id, m_id, table_n
 DEFINE_BASIC_FIELD(ModelRegistrarGroupMap, unsigned long long, registrar_id, m_registrar_id, table_name, "registrar_id",.setForeignKey() )
 DEFINE_BASIC_FIELD(ModelRegistrarGroupMap, unsigned long long, registrar_group_id, m_registrar_group_id, table_name, "registrar_group_id",.setForeignKey() )
 DEFINE_BASIC_FIELD(ModelRegistrarGroupMap, Database::Date, member_from, m_member_from, table_name, "member_from", .setNotNull())
-DEFINE_BASIC_FIELD(ModelRegistrarGroupMap, Database::Date, member_until, m_member_until, table_name, "member_until", .setNotNull())
+DEFINE_BASIC_FIELD(ModelRegistrarGroupMap, Database::Date, member_until, m_member_until, table_name, "member_until", )
 
 
 ModelRegistrarGroupMap::field_list ModelRegistrarGroupMap::fields = list_of<ModelRegistrarGroupMap::field_list::value_type>
