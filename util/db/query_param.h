0c9d7a66467c8e9f81b600583e071355107107ee
Jan Zima <jan.zima@nic.cz>
2010-06-30 13:12:34 +0000

    binary query params #4164
    git-svn-id: file:///data/svn/enum/server/trunk@12581 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ util/db/query_param.h
@@ -0,0 +1,163 @@
+/*
+ * Copyright (C) 2010  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @file query_param.h
+ *  Implementation of param object.
+ */
+
+#ifndef QUERYPARAM_H_
+#define QUERYPARAM_H_
+
+#include <vector>
+#include <string>
+#include <ios>
+#include <iomanip>
+#include <sstream>
+#include <stdexcept>
+
+//buffer type
+typedef std::vector<char> QueryParamData;
+
+/**
+ * \class  QueryParam
+ * \brief  query parameter text or binary container
+ * endian conversion implementation is more easy than perfectly efficient
+ * but for database operations it's fast enough
+ */
+
+class QueryParam
+{
+    //text or binary data flag
+    bool binary_;
+    //text plain or binary in network byte order
+    QueryParamData buffer_;
+
+    //save binary type to buffer_ in network byte order
+    template <class T> void hton_impl(const T& t, std::size_t size_of_t)
+    {
+        buffer_ = QueryParamData(size_of_t);
+        QueryParamData etest(sizeof(long));
+        *(reinterpret_cast<long*>(&etest[0])) = 1l;
+
+        if(etest[0]) //host little endian
+        {
+            for (std::size_t i = 0; i < size_of_t; ++i)
+                buffer_[i] = reinterpret_cast
+                    <const char*>(&t)[size_of_t-i-1];
+        }
+        else //host big endian
+        {
+            for (std::size_t i = 0; i < size_of_t; ++i)
+                buffer_[i] = reinterpret_cast
+                    <const char*>(&t)[i];
+        }
+    }
+
+public:
+
+    //copy
+    QueryParam(const QueryParam& param )
+    : binary_(param.binary_)
+    , buffer_(param.buffer_)
+    {}
+
+    QueryParam& operator=(const QueryParam& param)
+    {
+        if (this != &param)
+        {
+            binary_=param.binary_;
+            buffer_=param.buffer_;
+        }
+        return *this;
+    }//operator=
+
+    //ctor
+    explicit QueryParam(const QueryParamData& data )
+    : binary_(false)
+    , buffer_(data)
+    {}
+    explicit QueryParam(const char* data_ptr, std::size_t data_size )
+    : binary_(false)
+    , buffer_(data_ptr, data_ptr + data_size)
+    {}
+
+    explicit QueryParam(const char* data_ptr )
+    : binary_(false)
+    , buffer_(data_ptr, data_ptr + strlen(data_ptr))
+    {}
+
+    explicit QueryParam(const std::string& text )
+    : binary_(false)
+    , buffer_(text.begin(), text.end())
+    {}
+
+    template <class T> QueryParam( T t )
+    : binary_(true)
+    {
+        std::size_t size_of_t = sizeof(t);
+
+        if(size_of_t==1)
+        {
+            buffer_.push_back(t);
+            return;
+        }
+
+        if((//(size_of_t!=1)||
+                (size_of_t%2)) || (size_of_t > 8))
+        {
+            std::stringstream msg;
+            msg << "QueryParam error: use only for basic types"
+                    " up to 8 bytes current size: "
+                    << sizeof(t);
+            throw std::runtime_error( msg.str());
+        }
+
+        hton_impl(t, size_of_t);
+    }
+
+    void print_buffer()
+    {
+        for (QueryParamData::const_iterator i = buffer_.begin()
+                ; i != buffer_.end(); ++i)
+        {
+            std::stringstream hexdump;
+            hexdump <<  std::setw( 2 ) << std::setfill( '0' )
+                << std::hex << std::uppercase
+                << static_cast<unsigned short>(static_cast<unsigned char>(*i));
+            std::cout << " " << hexdump.str();
+        }
+        std::cout << std::endl;
+
+        std::cout << std::string(buffer_.begin(),buffer_.end()) << std::endl;
+    }//print_buffer
+
+    const QueryParamData& get_data() const
+    {
+        return buffer_;
+    }
+
+    bool get_format() const
+    {
+        return binary_;
+    }
+};//class QueryParam
+
+typedef std::vector<QueryParam> QueryParams;
+
+#endif //QUERYPARAM_H_
