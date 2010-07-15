5665b0995e547fe9fb3843ea9f25567a8e2b6565
Jiří Šádek <jiri.sadek@nic.cz>
2010-07-15 13:56:18 +0000

    UNCAUGHT_EXCEPT fix
    git-svn-id: file:///data/svn/enum/server/trunk@12847 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- util/types/test_money.cc
+++ util/types/test_money.cc
@@ -64,68 +64,77 @@ main()
     int failures = 0;
     int num = 1;
 
-    test_money_from_string(num, "0", 0, IS_SAME, failures);
-    test_money_from_string(num, "0.", 0, IS_SAME, failures);
-    test_money_from_string(num, "0.0", 0, IS_SAME, failures);
-    test_money_from_string(num, "0.00", 0, IS_SAME, failures);
+    try {
 
-    test_money_from_string(num, "-0", 0, IS_SAME, failures);
-    test_money_from_string(num, "-0.", 0, IS_SAME, failures);
-    test_money_from_string(num, "-0.0", 0, IS_SAME, failures);
-    test_money_from_string(num, "-0.00", 0, IS_SAME, failures);
-    test_money_from_string(num, "-0.000", 0, IS_SAME, failures);
+        test_money_from_string(num, "0", 0, IS_SAME, failures);
+        test_money_from_string(num, "0.", 0, IS_SAME, failures);
+        test_money_from_string(num, "0.0", 0, IS_SAME, failures);
+        test_money_from_string(num, "0.00", 0, IS_SAME, failures);
 
-    test_money_from_string(num, ".1", 10, IS_SAME, failures);
-    test_money_from_string(num, ".01", 1, IS_SAME, failures);
-    test_money_from_string(num, ".011", 1, IS_SAME, failures);
-    test_money_from_string(num, ".111", 11, IS_SAME, failures);
-    test_money_from_string(num, "-.1", -10, IS_SAME, failures);
-    test_money_from_string(num, "-.01", -1, IS_SAME, failures);
-    test_money_from_string(num, "-.011", -1, IS_SAME, failures);
-    test_money_from_string(num, "-.111", -11, IS_SAME, failures);
+        test_money_from_string(num, "-0", 0, IS_SAME, failures);
+        test_money_from_string(num, "-0.", 0, IS_SAME, failures);
+        test_money_from_string(num, "-0.0", 0, IS_SAME, failures);
+        test_money_from_string(num, "-0.00", 0, IS_SAME, failures);
+        test_money_from_string(num, "-0.000", 0, IS_SAME, failures);
 
-    test_money_from_string(num, "0.1", 10, IS_SAME, failures);
-    test_money_from_string(num, "0.10", 10, IS_SAME, failures);
-    test_money_from_string(num, "0.01", 1, IS_SAME, failures);
-    test_money_from_string(num, "-0.1", -10, IS_SAME, failures);
-    test_money_from_string(num, "-0.10", -10, IS_SAME, failures);
-    test_money_from_string(num, "-0.01", -1, IS_SAME, failures);
+        test_money_from_string(num, ".1", 10, IS_SAME, failures);
+        test_money_from_string(num, ".01", 1, IS_SAME, failures);
+        test_money_from_string(num, ".011", 1, IS_SAME, failures);
+        test_money_from_string(num, ".111", 11, IS_SAME, failures);
+        test_money_from_string(num, "-.1", -10, IS_SAME, failures);
+        test_money_from_string(num, "-.01", -1, IS_SAME, failures);
+        test_money_from_string(num, "-.011", -1, IS_SAME, failures);
+        test_money_from_string(num, "-.111", -11, IS_SAME, failures);
 
-    test_money_from_string(num, "123", 12300, IS_SAME, failures);
-    test_money_from_string(num, "123.", 12300, IS_SAME, failures);
-    test_money_from_string(num, "123.0", 12300, IS_SAME, failures);
-    test_money_from_string(num, "123.4", 12340, IS_SAME, failures);
-    test_money_from_string(num, "123.45", 12345, IS_SAME, failures);
-    test_money_from_string(num, "123.455", 12345, IS_SAME, failures);
+        test_money_from_string(num, "0.1", 10, IS_SAME, failures);
+        test_money_from_string(num, "0.10", 10, IS_SAME, failures);
+        test_money_from_string(num, "0.01", 1, IS_SAME, failures);
+        test_money_from_string(num, "-0.1", -10, IS_SAME, failures);
+        test_money_from_string(num, "-0.10", -10, IS_SAME, failures);
+        test_money_from_string(num, "-0.01", -1, IS_SAME, failures);
 
-    test_money_from_string(num, "-123", -12300, IS_SAME, failures);
-    test_money_from_string(num, "-123.", -12300, IS_SAME, failures);
-    test_money_from_string(num, "-123.0", -12300, IS_SAME, failures);
-    test_money_from_string(num, "-123.4", -12340, IS_SAME, failures);
-    test_money_from_string(num, "-123.45", -12345, IS_SAME, failures);
-    test_money_from_string(num, "-123.455", -12345, IS_SAME, failures);
+        test_money_from_string(num, "123", 12300, IS_SAME, failures);
+        test_money_from_string(num, "123.", 12300, IS_SAME, failures);
+        test_money_from_string(num, "123.0", 12300, IS_SAME, failures);
+        test_money_from_string(num, "123.4", 12340, IS_SAME, failures);
+        test_money_from_string(num, "123.45", 12345, IS_SAME, failures);
+        test_money_from_string(num, "123.455", 12345, IS_SAME, failures);
 
-    test_money_from_string(num, "21474836.47", 2147483647, IS_SAME, failures);
-    test_money_from_string(num, "-21474836.47", -2147483647, IS_SAME, failures);
+        test_money_from_string(num, "-123", -12300, IS_SAME, failures);
+        test_money_from_string(num, "-123.", -12300, IS_SAME, failures);
+        test_money_from_string(num, "-123.0", -12300, IS_SAME, failures);
+        test_money_from_string(num, "-123.4", -12340, IS_SAME, failures);
+        test_money_from_string(num, "-123.45", -12345, IS_SAME, failures);
+        test_money_from_string(num, "-123.455", -12345, IS_SAME, failures);
 
-    test_string_from_money(num, Database::Money(12300), "123.00", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(12340), "123.40", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(12345), "123.45", IS_SAME, failures);
+        test_money_from_string(num, "21474836.47", 2147483647, IS_SAME, failures);
+        test_money_from_string(num, "-21474836.47", -2147483647, IS_SAME, failures);
 
-    test_string_from_money(num, Database::Money(-12300), "-123.00", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(-12340), "-123.40", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(-12345), "-123.45", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(12300), "123.00", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(12340), "123.40", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(12345), "123.45", IS_SAME, failures);
 
-    test_string_from_money(num, Database::Money(0), "0.00", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(1), "0.01", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(10), "0.10", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(100), "1.00", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(-12300), "-123.00", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(-12340), "-123.40", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(-12345), "-123.45", IS_SAME, failures);
 
-    test_string_from_money(num, Database::Money(-1), "-0.01", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(-10), "-0.10", IS_SAME, failures);
-    test_string_from_money(num, Database::Money(-100), "-1.00", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(0), "0.00", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(1), "0.01", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(10), "0.10", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(100), "1.00", IS_SAME, failures);
+
+        test_string_from_money(num, Database::Money(-1), "-0.01", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(-10), "-0.10", IS_SAME, failures);
+        test_string_from_money(num, Database::Money(-100), "-1.00", IS_SAME, failures);
 
 
-    std::cout << "Number of failures: " << failures << std::endl;
-    exit(failures);
+        std::cout << "Number of failures: " << failures << std::endl;
+        exit(failures);
+    }
+    catch (std::exception &_e) {
+        std::cerr << "Test no. " << num << " failed - conversion error (" << _e.what() << ")" << std::endl;
+    }
+    catch (...) {
+        std::cerr << "Test no. " << num << " failed" << std::endl;
+    }
 }
