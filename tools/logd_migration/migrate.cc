0f51e32886d0485ff85cab75493c2439f37f45d2
Jiří Šádek <jiri.sadek@nic.cz>
2010-07-15 14:18:45 +0000

    UNCAUGHT_EXCEPT fix
    git-svn-id: file:///data/svn/enum/server/trunk@12853 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- tools/logd_migration/migrate.cc
+++ tools/logd_migration/migrate.cc
@@ -48,21 +48,21 @@ const int LC_EPP = 3;
 void logger(boost::format &fmt)
 {
 #ifdef HAVE_LOGGER
-	LOGGER("fred-server").error(fmt);
+    LOGGER("fred-server").error(fmt);
 #endif
 }
 
 void logger(std::string &str)
 {
 #ifdef HAVE_LOGGER
-	LOGGER("fred-server").error(str);
+    LOGGER("fred-server").error(str);
 #endif
 }
 
 void logger(const char *str)
 {
 #ifdef HAVE_LOGGER
-	LOGGER("fred-server").error(str);
+    LOGGER("fred-server").error(str);
 #endif
 }
 
@@ -70,145 +70,149 @@ void signal_handler(int signum);
 
 void signal_handler(int signum)
 {
-	std::cout << "Received signal " << signum << ", exiting. " << std::endl;	
-	exit(0);
+    std::cout << "Received signal " << signum << ", exiting. " << std::endl;
+    exit(0);
 }
 
 int main()
 {
-	int trans_count;
-	epp_red_command_type cmd_type;
-	epp_command_data *cdata; /* command data structure */
-	parser_status pstat;
-	std::string line, rawline;
-        void *schema;
-        clock_t time1, time2, time3, t_parser=0, t_logcomm=0, t_backend=0;
+    int trans_count;
+    epp_red_command_type cmd_type;
+    epp_command_data *cdata; /* command data structure */
+    parser_status pstat;
+    std::string line, rawline;
+    void *schema;
+    clock_t time1, time2, time3, t_parser=0, t_logcomm=0, t_backend=0;
 
-
+    try {
         Database::Manager::init(new Database::ConnectionFactory(CONNECTION_STRING));
 
-	Register::Logger::ManagerImpl serv;
-		
-	cdata = NULL;
+        Register::Logger::ManagerImpl serv;
 
-	// TODO -  check if the file EPP_SCHEMA exists (or better, create some config file for this thing with all the variables which are used
-	schema = epp_parser_init(EPP_SCHEMA);
+        cdata = NULL;
 
-	signal(15, signal_handler);
-	signal(11, signal_handler);
-	signal(6, signal_handler);
+        // TODO -  check if the file EPP_SCHEMA exists (or better, create some config file for this thing with all the variables which are used
+        schema = epp_parser_init(EPP_SCHEMA);
 
-	// setup loggin via LOGGER
-	Logging::Manager::instance_ref().get(PACKAGE).addHandler(Logging::Log::LT_FILE, std::string(LOG_FILENAME));
-	Logging::Manager::instance_ref().get(PACKAGE).setLevel(Logging::Log::LL_TRACE);
-	LOGGER(PACKAGE).info("Logging initialized for migration");
+        signal(15, signal_handler);
+        signal(11, signal_handler);
+        signal(6, signal_handler);
 
-	// TODO is this safe?
-	Connection serv_conn = serv.get_connection();
-        
-	Transaction serv_transaction(serv_conn);
+        // setup loggin via LOGGER
+        Logging::Manager::instance_ref().get(PACKAGE).addHandler(Logging::Log::LT_FILE, std::string(LOG_FILENAME));
+        Logging::Manager::instance_ref().get(PACKAGE).setLevel(Logging::Log::LL_TRACE);
+        LOGGER(PACKAGE).info("Logging initialized for migration");
 
-	//here we're changing the approach:
-	//	accept action table id , | separator and xml like before
-	//	use only the method insert_properties
-	//
+        // TODO is this safe?
+        Connection serv_conn = serv.get_connection();
+        Transaction serv_transaction(serv_conn);
 
-	mem_pool = new pool_subst();
+        //here we're changing the approach:
+        //    accept action table id , | separator and xml like before
+        //    use only the method insert_properties
+        //
 
-	trans_count = 0;
-	while(std::getline(std::cin, line)) {
-		//size_t i;
-		char *end = NULL;
-		pool_manager man(mem_pool);
+        mem_pool = new pool_subst();
 
-		if (line.empty()) continue;
-	
-		if (line[INPUT_ID_LENGTH] != '|') {
-			logger("Error in input line: ID at the beginning doesn't have proper length");	
+        trans_count = 0;
+        while (std::getline(std::cin, line)) {
+            //size_t i;
+            char *end = NULL;
+            pool_manager man(mem_pool);
 
-			std::cout << "Error in input line: ID at the beginning doesn't have proper length: " << line << std::endl;	
-			continue;
-		}
+            if (line.empty()) continue;
 
-                std::string id_str = line.substr(0, INPUT_ID_LENGTH);
-		line = line.substr(INPUT_ID_LENGTH + 1);
-		TID entry_id = strtoull(id_str.c_str(), &end, 10);
+            if (line[INPUT_ID_LENGTH] != '|') {
+                logger("Error in input line: ID at the beginning doesn't have proper length");
 
-		if(line[INPUT_DATE_LENGTH] != '|') {
-			logger("Error in input line: Date at the beginning doesn't have proper length");	
-			std::cout << "Error in input line: Date at the beginning doesn't have proper length" << std::endl;	
-			continue;
-		}
+                std::cout << "Error in input line: ID at the beginning doesn't have proper length: " << line << std::endl;
+                continue;
+            }
 
-                std::string date_str = line.substr(0, INPUT_DATE_LENGTH);
-		line = line.substr(INPUT_DATE_LENGTH + 1);
+            std::string id_str = line.substr(0, INPUT_ID_LENGTH);
+            line = line.substr(INPUT_ID_LENGTH + 1);
+            TID entry_id = strtoull(id_str.c_str(), &end, 10);
 
-                std::string monitoring_str = line.substr(0, INPUT_MON_LENGTH);
-                line = line.substr(INPUT_MON_LENGTH + 1);
-                
-                bool mon_flag = (monitoring_str.at(0) == 't');
+            if(line[INPUT_DATE_LENGTH] != '|') {
+                logger("Error in input line: Date at the beginning doesn't have proper length");
+                std::cout << "Error in input line: Date at the beginning doesn't have proper length" << std::endl;
+                continue;
+            }
 
-		// OK, both values found
-		
-		time1 = clock();
-		pstat = epp_parse_command(line.c_str(), line.length(), &cdata, &cmd_type);
+            std::string date_str = line.substr(0, INPUT_DATE_LENGTH);
+            line = line.substr(INPUT_DATE_LENGTH + 1);
 
-		time2 = clock();
-		t_parser += time2 - time1;
-		// count << (int)pstat << endl;
+            std::string monitoring_str = line.substr(0, INPUT_MON_LENGTH);
+            line = line.substr(INPUT_MON_LENGTH + 1);
 
-		// test if the failure is serious enough to close connection 
-		if (pstat > PARSER_HELLO) {
-			switch (pstat) {
-				case PARSER_NOT_XML:
-					logger("Request is not XML");
-					continue;
-				case PARSER_NOT_COMMAND:
-					logger("Request is neither a command nor hello");
-					continue;
-				case PARSER_ESCHEMA:
-					logger("Schema's parser error - check correctness of schema");
-					continue;
-				case PARSER_EINTERNAL:
-					logger("Internal parser error occured when processing request");
-					continue;
-				default:
-					logger("Unknown error occured during parsing stage");
-					continue;
-			}
-		}
-		time2 = clock();
+            bool mon_flag = (monitoring_str.at(0) == 't');
+            // OK, both values found
 
-		// we don't care about the action_type - it's already in the request table
-		epp_action_type action_type = UnknownAction;
-		std::auto_ptr<Register::Logger::RequestProperties> props = log_epp_command(cdata, cmd_type, -1, &action_type);
+            time1 = clock();
+            pstat = epp_parse_command(line.c_str(), line.length(), &cdata, &cmd_type);
 
-		time3 = clock();
-		t_logcomm += time3 - time2;
-		epp_parser_request_cleanup(cdata);	
+            time2 = clock();
+            t_parser += time2 - time1;
+            // count << (int)pstat << endl;
 
-		serv.insert_props_pub(date_str, LC_EPP, mon_flag, entry_id, *props);
-		trans_count++;
+            // test if the failure is serious enough to close connection
+            if (pstat > PARSER_HELLO) {
+                switch (pstat) {
+                    case PARSER_NOT_XML:
+                        logger("Request is not XML");
+                        continue;
+                    case PARSER_NOT_COMMAND:
+                        logger("Request is neither a command nor hello");
+                        continue;
+                    case PARSER_ESCHEMA:
+                        logger("Schema's parser error - check correctness of schema");
+                        continue;
+                    case PARSER_EINTERNAL:
+                        logger("Internal parser error occured when processing request");
+                        continue;
+                    default:
+                        logger("Unknown error occured during parsing stage");
+                        continue;
+                }
+            }
+            time2 = clock();
 
-		if((trans_count % COMMIT_INTERVAL) == 0) {
-			serv_transaction.commit();
-		}
+            // we don't care about the action_type - it's already in the request table
+            epp_action_type action_type = UnknownAction;
+            std::auto_ptr<Register::Logger::RequestProperties> props = log_epp_command(cdata, cmd_type, -1, &action_type);
 
-		t_backend += clock() - time3;
-	}
-	
-	serv_transaction.commit();
+            time3 = clock();
+            t_logcomm += time3 - time2;
+            epp_parser_request_cleanup(cdata);
 
-	printf(" --------- REPORT: \n"
-		" Parser: 	   %12i \n"
-		" log_epp_command: %12i \n"
-		" Register::Logger::ManagerImpl: 	   %12i \n",
-		(int)(t_parser/ 1000),
-		(int)(t_logcomm / 1000),
-		(int)(t_backend / 1000));
+            serv.insert_props_pub(date_str, LC_EPP, mon_flag, entry_id, *props);
+            trans_count++;
 
-	free(schema);
+            if((trans_count % COMMIT_INTERVAL) == 0) {
+                serv_transaction.commit();
+            }
 
-	// if the transaction wasn't commited in the previous command
+            t_backend += clock() - time3;
+        }
+
+        serv_transaction.commit();
+
+        printf(" --------- REPORT: \n"
+            " Parser:        %12i \n"
+            " log_epp_command: %12i \n"
+            " Register::Logger::ManagerImpl:        %12i \n",
+            (int)(t_parser/ 1000),
+            (int)(t_logcomm / 1000),
+            (int)(t_backend / 1000));
+
+        free(schema);
+        // if the transaction wasn't commited in the previous command
+    }
+    catch (std::exception &_e) {
+        std::cerr << "error occured (" << _e.what() << ")" << std::endl;
+    }
+    catch (...) {
+        std::cerr << "error occured" << std::endl;
+    }
 }
 
