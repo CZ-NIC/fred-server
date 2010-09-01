1a62b5b7d0706ee8c9b309123dae475510547f47
Jan Zima <jan.zima@nic.cz>
2010-09-01 12:32:10 +0000

    messages headers #4427
    git-svn-id: file:///data/svn/enum/server/trunk@13550 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/corba/messages/messages_corba_impl.h
+++ src/corba/messages/messages_corba_impl.h
@@ -1,3 +1,28 @@
+/*
+ * Copyright (C) 2010  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @messages_corba_impl.cc
+ *  corba client header for registry messages
+ */
+
+
+
 #ifndef MESSAGES_CORBA_IMPL_H_
 #define MESSAGES_CORBA_IMPL_H_
 
@@ -6,6 +31,6 @@
 
 unsigned long long save_file(std::vector<char> & file_buffer
         , const std::string file_name, const std::string file_mime_type
-        , const unsigned int file_type );//"application/pdf",6
+        , const unsigned int file_type );
 
 #endif // MESSAGES_CORBA_IMPL_H_
--- src/register/messages/messages_impl.cc
+++ src/register/messages/messages_impl.cc
@@ -1,4 +1,26 @@
-//messages_impl.cc
+/*
+ * Copyright (C) 2010  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @messages_impl.cc
+ *  implementation of registry messages
+ */
+
 #include "messages_impl.h"
 #include "messages/messages_corba_impl.h"
 #include "db_settings.h"
