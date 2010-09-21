c54b605f46ad217628c53d2d1065c02769e6def9
Jiří Šádek <jiri.sadek@nic.cz>
2010-09-21 09:32:17 +0000

    Ticket #4508 - minor fixes, contactCreate impl. draft
    git-svn-id: file:///data/svn/enum/server/trunk@13942 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/corba/mojeid/corba_wrap.h
+++ src/corba/mojeid/corba_wrap.h
@@ -41,5 +41,68 @@ Registry::NullableBoolean* corba_wrap_nullable_boolean(const Database::Value &_v
 }
 
 
+Registry::NullableDate* corba_wrap_nullable_date(const Database::Value &_v)
+{
+    if (_v.isnull()) {
+        return 0;
+    }
+    else {
+        boost::gregorian::date tmp = from_string(static_cast<std::string>(_v));
+        if (tmp.is_special()) {
+            return 0;
+        }
+        else {
+            Registry::Date d;
+            d.year  = static_cast<int>(tmp.year());
+            d.month = static_cast<int>(tmp.month());
+            d.day   = static_cast<int>(tmp.day());
+            return new Registry::NullableDate(d);
+        }
+    }
+}
+
+
+std::string corba_unwrap_string(const CORBA::String_member &_s)
+{
+    return static_cast<std::string>(_s);
+}
+
+
+Database::QueryParam corba_unwrap_nullable_string(const Registry::NullableString *_v)
+{
+    if (_v) {
+        return std::string(_v->_value());
+    }
+    else {
+        return Database::QueryParam();
+    }
+}
+
+
+Database::QueryParam corba_unwrap_nullable_boolean(const Registry::NullableBoolean *_v)
+{
+    if (_v) {
+        return _v->_value();
+    }
+    else {
+        return Database::QueryParam();
+    }
+}
+
+
+Database::QueryParam corba_unwrap_nullable_date(const Registry::NullableDate *_v)
+{
+    if (_v) {
+        return boost::gregorian::date(_v->_value().year,
+                                      _v->_value().month,
+                                      _v->_value().day);
+    }
+    else {
+        return Database::QueryParam();
+    }
+}
+
+
+
 #endif
 
--- src/corba/mojeid/mojeid_impl.cc
+++ src/corba/mojeid/mojeid_impl.cc
@@ -51,18 +51,190 @@ CORBA::ULongLong MojeIDImpl::contactCreate(const Contact &_contact,
     ConnectionReleaser releaser;
 
     try {
+        if (_contact.addresses.length() == 0) {
+            throw std::runtime_error("contact has no address");
+        }
+        if (_contact.emails.length() == 0) {
+            throw std::runtime_error("contact has no email");
+        }
+
+        /* TODO: get mojeid registrar id from database (may be in server init phase) */
+        unsigned long long mojeid_registrar_id = 1;
+
+        std::string handle = static_cast<std::string>(_contact.username);
+        try {
+            /* TODO: 2nd parameter should be from configuration */
+            Register::Contact::ManagerPtr contact_mgr(Register::Contact::Manager::create(0, false));
+            LOGGER(PACKAGE).debug(boost::format("handle '%1%' availability check")
+                    % handle);
+
+            Register::NameIdPair cinfo;
+            Register::Contact::Manager::CheckAvailType check_result;
+            check_result = contact_mgr->checkAvail(handle, cinfo);
+
+            if (check_result == Register::Contact::Manager::CA_INVALID_HANDLE) {
+                throw std::runtime_error(str(boost::format("handle '%1%' is not valid")
+                            % handle));
+            }
+            else if (check_result == Register::Contact::Manager::CA_REGISTRED) {
+                throw std::runtime_error(str(boost::format("handle '%1%' is already registered")
+                            % handle));
+            }
+            else if (check_result == Register::Contact::Manager::CA_PROTECTED) {
+                throw std::runtime_error(str(boost::format("handle '%1%' is in protection period")
+                        % handle));
+            }
+            LOGGER(PACKAGE).debug(boost::format("handle '%1%' check passed")
+                    % handle);
+        }
+        catch (std::exception &_ex) {
+            LOGGER(PACKAGE).debug(_ex.what());
+            throw;
+        }
+        catch (...) {
+            throw std::runtime_error("contact handle availability check failed");
+        }
+
         Database::Connection conn = Database::Manager::acquire();
         Database::Transaction tx(conn);
 
-        Register::Contact::ManagerPtr contact_mgr(Register::Contact::Manager::create(0, false));
+        /* object_registry record */
+        Database::Result roreg = conn.exec_params(
+                "SELECT create_object($1::integer, $2::text, $3::integer)",
+                Database::query_param_list(mojeid_registrar_id)(handle)(1));
+        if (roreg.size() != 1) {
+            throw std::runtime_error("create object failed");
+        }
+        unsigned long long id = static_cast<unsigned long long>(roreg[0][0]);
 
+        /* object record */
+        Database::Result robject = conn.exec_params(
+                "INSERT INTO object (id, clid, authinfopw) VALUES ($1::integer, $2::integer,"
+                    " $3::text)",
+                Database::query_param_list(id)(mojeid_registrar_id)("TODO: random string"));
+
+        std::string qcontact = "INSERT INTO contact ("
+                                    "id, name, organization, street1, street2, street3,"
+                                    " city, stateorprovince, postalcode, country, telephone,"
+                                    " fax, email, notifyemail, vat, ssn, ssntype,"
+                                    " disclosename, discloseorganization, discloseaddress,"
+                                    " disclosetelephone, disclosefax, discloseemail,"
+                                    " disclosenotifyemail, disclosevat, discloseident"
+                               ") VALUES ("
+                                    "$1::integer, $2::text, $3::text, $4::text, $5::text,"
+                                    " $6::text, $7::text, $8::text, $9::text, $10::text,"
+                                    " $11::text, $12::text, $13::text, $14::text, $15::text,"
+                                    " $16::text, $17::integer, $18::boolean, $19::boolean,"
+                                    " $20::boolean, $21::boolean, $22::boolean, $23::boolean,"
+                                    " $24::boolean, $25::boolean, $26::boolean)";
+
+        Database::QueryParam telephone = Database::QueryParam();
+        Database::QueryParam fax = Database::QueryParam();
+        for (unsigned int i = 0; i <  _contact.phones.length(); ++i) {
+            std::string type = corba_unwrap_string(_contact.phones[i].type);
+            if (type == "DEFAULT") {
+                telephone = corba_unwrap_string(_contact.phones[i].number);
+            }
+            else if (type == "FAX") {
+                fax = corba_unwrap_string(_contact.phones[i].number);
+            }
+        }
+
+        Database::QueryParam email = Database::QueryParam();
+        Database::QueryParam notify_email = Database::QueryParam();
+        for (unsigned int i = 0; i < _contact.emails.length(); ++i) {
+            std::string type = corba_unwrap_string(_contact.emails[i].type);
+            if (type == "DEFAULT") {
+                email = corba_unwrap_string(_contact.emails[i].email_address);
+            }
+            else if (type == "NOTIFY") {
+                notify_email = corba_unwrap_string(_contact.emails[i].email_address);
+            }
+        }
+
+        Database::QueryParam ssn = Database::QueryParam();
+        Database::QueryParam ssn_type = Database::QueryParam();
+        if (_contact.ssn_type) {
+            int type = boost::lexical_cast<int>(_contact.ssn_type->_value());
+            ssn_type = type;
+            if (type == 2) ssn = corba_unwrap_nullable_string(_contact.id_card_num);
+            if (type == 3) ssn = corba_unwrap_nullable_string(_contact.passport_num);
+            if (type == 4) ssn = corba_unwrap_nullable_string(_contact.vat_id_num);
+            if (type == 5) ssn = corba_unwrap_nullable_string(_contact.ssn_id_num);
+            if (type == 6) ssn = corba_unwrap_nullable_date(_contact.birth_date);
+        }
+
+        Database::QueryParams pcontact = Database::query_param_list
+                                    (id)
+                                    (corba_unwrap_string(_contact.first_name)
+                                        + " " + corba_unwrap_string(_contact.last_name))
+                                    (corba_unwrap_nullable_string(_contact.organization))
+                                    (corba_unwrap_string(_contact.addresses[0].street1))
+                                    (corba_unwrap_nullable_string(_contact.addresses[0].street2))
+                                    (corba_unwrap_nullable_string(_contact.addresses[0].street3))
+                                    (corba_unwrap_string(_contact.addresses[0].city))
+                                    (corba_unwrap_nullable_string(_contact.addresses[0].state))
+                                    (corba_unwrap_string(_contact.addresses[0].postal_code))
+                                    (corba_unwrap_string(_contact.addresses[0].country_code))
+                                    (telephone)
+                                    (fax)
+                                    (email)
+                                    (notify_email)
+                                    (corba_unwrap_nullable_string(_contact.vat_reg_num))
+                                    (ssn)
+                                    (ssn_type)
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_name))
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_organization))
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_address))
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_phone))
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_fax))
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_email))
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_notify_email))
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_vat))
+                                    (corba_unwrap_nullable_boolean(_contact.disclose_ident));
+        /* contact record */
+        Database::Result rcontact = conn.exec_params(qcontact, pcontact);
+
+        /* save history */
+        conn.exec("INSERT INTO history (id) VALUES (DEFAULT)");
+        Database::Result rhistory = conn.exec("SELECT currval('history_id_seq')");
+        unsigned long long history_id = 0;
+        history_id = rhistory[0][0];
+        if (!history_id) {
+            throw std::runtime_error("cannot save new history");
+        }
+
+        conn.exec_params("UPDATE object_registry SET crhistoryid = $1::integer,"
+                " historyid = $2::integer WHERE id = $3::integer",
+                Database::query_param_list(history_id)(history_id)(id));
+
+        Database::Result robject_history = conn.exec_params(
+                "INSERT INTO object_history (historyid, id, clid, upid, trdate, update, authinfopw)"
+                " SELECT $1::integer, o.id, o.clid, o.upid, o.trdate, o.update, o.authinfopw FROM object o"
+                " WHERE o.id = $2::integer",
+                Database::query_param_list(history_id)(id));
+
+        Database::Result rcontact_history = conn.exec_params(
+                "INSERT INTO contact_history (historyid, id, name, organization, street1, street2, street3,"
+                " city, stateorprovince, postalcode, country, telephone, fax, email, disclosename,"
+                " discloseorganization, discloseaddress, disclosetelephone, disclosefax, discloseemail,"
+                " notifyemail, vat, ssn, ssntype, disclosevat, discloseident, disclosenotifyemail)"
+                " SELECT $1::integer, c.id, c.name, c.organization, c.street1, c.street2, c.street3,"
+                " c.city, c.stateorprovince, c.postalcode, c.country, c.telephone, c.fax, c.email,"
+                " c.disclosename, c.discloseorganization, c.discloseaddress, c.disclosetelephone, c.disclosefax,"
+                " c.discloseemail, c.notifyemail, c.vat, c.ssn, c.ssntype, c.disclosevat, c.discloseident,"
+                " c.disclosenotifyemail FROM contact c WHERE c.id = $2::integer",
+                Database::query_param_list(history_id)(id));
+
+        tx.commit();
+        return id;
     }
     catch (std::exception &_ex) {
+        throw Registry::MojeID::ErrorReport(_ex.what());
     }
     catch (...) {
+        throw Registry::MojeID::ErrorReport();
     }
-
-    return 0;
 }
 
 
@@ -128,18 +300,7 @@ Contact* MojeIDImpl::contactInfo(const char* _handle)
         data->passport_num = ident_type == 3 ? corba_wrap_nullable_string(rinfo[0][7]) : 0;
         data->vat_id_num   = ident_type == 4 ? corba_wrap_nullable_string(rinfo[0][7]) : 0;
         data->ssn_id_num   = ident_type == 5 ? corba_wrap_nullable_string(rinfo[0][7]) : 0;
-        data->birth_date   = 0;
-        if (ident_type == 6) {
-            boost::gregorian::date tmp = from_string(rinfo[0][7]);
-            if (!tmp.is_special()) {
-                Date birth;
-                birth.day   = static_cast<int>(tmp.day());
-                birth.month = static_cast<int>(tmp.month());
-                birth.year  = static_cast<int>(tmp.year());
-
-                data->birth_date = new NullableDate(birth);
-            }
-        }
+        data->birth_date   = ident_type == 6 ? corba_wrap_nullable_date(rinfo[0][7]) : 0;
 
         data->disclose_name         = corba_wrap_nullable_boolean(rinfo[0][8]);
         data->disclose_organization = corba_wrap_nullable_boolean(rinfo[0][9]);
@@ -168,7 +329,7 @@ Contact* MojeIDImpl::contactInfo(const char* _handle)
         if (notify_email.size()) {
             data->emails.length(2);
             data->emails[1].type = "NOTIFY";
-            data->emails[2].email_address = corba_wrap_string(notify_email);
+            data->emails[1].email_address = corba_wrap_string(notify_email);
         }
 
         std::string telephone = static_cast<std::string>(rinfo[0][26]);
