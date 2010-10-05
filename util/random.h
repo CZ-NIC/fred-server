0abd7513bc0fd1c797d547936a9efe1c5c68b9d6
Jiří Šádek <jiri.sadek@nic.cz>
2010-10-05 08:00:54 +0000

    forgotten commit
    git-svn-id: file:///data/svn/enum/server/trunk@14225 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- util/random.h
+++ util/random.h
@@ -1,37 +1,23 @@
-#ifndef RANDOM_H_
-#define RANDOM_H_
+#ifndef RANDOM_UTIL_H_
+#define RANDOM_UTIL_H_
 
-#include <iostream>
-#include <boost/random/mersenne_twister.hpp>
-#include <boost/random/uniform_int.hpp>
-#include <boost/random/variate_generator.hpp>
 #include <string>
-#include <vector>
-#include <time.h>
-#include <sys/time.h>
 
 
 namespace Random {
 
 
-unsigned long msseed()
-{
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    return tv.tv_usec + tv.tv_sec;
-}
+int integer(const int &_min, const int &_max);
 
-boost::mt19937 rng(msseed());
+std::string string_from(const unsigned int &_len, const std::string &_allowed);
 
+std::string string_lower_alpha(const unsigned int &_len);
 
+std::string string_upper_alpha(const unsigned int &_len);
 
-int integer(const int &_min, const int &_max)
-{
-    boost::uniform_int<> range(_min, _max);
-    boost::variate_generator<boost::mt19937&, boost::uniform_int<> > gen(rng, range);
-    return gen();
-}
+std::string string_alpha(const unsigned int &_len);
 
+std::string string_alphanum(const unsigned int &_len);
 
 template<typename T1, typename T2>
 T2 choice(const T1 &_from)
@@ -40,44 +26,7 @@ T2 choice(const T1 &_from)
 }
 
 
-std::string string_from(const unsigned int &_len, const std::string &_allowed)
-{
-    std::string out;
-    out.reserve(_len);
-    for (unsigned int i = 0; i < _len; i++) {
-        out += choice<std::string, char>(_allowed);
-    }
-    return out;
 }
 
-
-std::string string_lower_alpha(const unsigned int &_len)
-{
-    return string_from(_len, "abcdefghijklmnopqrstuvwxyz");
-}
-
-
-std::string string_upper_alpha(const unsigned int &_len)
-{
-    return string_from(_len, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-}
-
-
-std::string string_alpha(const unsigned int &_len)
-{
-    return string_from(_len, "abcdefghijklmnopqrstuvwxyz"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-}
-
-
-std::string string_alphanum(const unsigned int &_len)
-{
-    return string_from(_len, "abcdefghijklmnopqrstuvwxyz"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
-}
-
-
-}
-
-#endif /*RANDOM_H_*/
+#endif /*RANDOM_UTIL_H_*/
 
