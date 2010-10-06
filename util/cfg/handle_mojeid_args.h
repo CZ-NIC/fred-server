eef34158882208f790a48a4cf364ea6b49726429
Jiří Šádek <jiri.sadek@nic.cz>
2010-10-06 09:55:04 +0000

    mojeid config handler
    git-svn-id: file:///data/svn/enum/server/trunk@14266 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ util/cfg/handle_mojeid_args.h
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2010  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @handle_mojeid_args.h
+ *  mojeid backend config
+ */
+
+#ifndef HANDLE_MOJEID_ARGS_H_
+#define HANDLE_MOJEID_ARGS_H_
+
+#include <iostream>
+#include <exception>
+#include <string>
+#include <vector>
+
+#include <boost/program_options.hpp>
+
+#include "faked_args.h"
+#include "handle_args.h"
+
+namespace po = boost::program_options;
+
+/**
+ * \class HandleMojeIDArgs
+ * \brief mojeid backend config
+ */
+class HandleMojeIDArgs : public HandleArgs
+{
+public:
+    std::string registrar_handle;
+    std::string svtrid_prefix;
+    std::string redirect_url;
+
+    boost::shared_ptr<boost::program_options::options_description>
+    get_options_description()
+    {
+        boost::shared_ptr<boost::program_options::options_description> cfg_opts(
+                new boost::program_options::options_description(
+                        std::string("MojeID server options")));
+
+        cfg_opts->add_options()
+                ("mojeid.registrar_handle",
+                 po::value<std::string>()->default_value("REG-MOJEID"),
+                 "registrar used for mojeid operations")
+                ("mojeid.svtrid_prefix",
+                 po::value<std::string>()->default_value("MojeID"),
+                 "prefix for action servertrid (it will looks like svtrid_prefix-0000000001)")
+                ("mojeid.redirect_url",
+                 po::value<std::string>()->default_value("https://%1%/identification/%2%/"),
+                 "email password redirect url");
+
+        return cfg_opts;
+    }//get_options_description
+    void handle( int argc, char* argv[],  FakedArgs &fa)
+    {
+        boost::program_options::variables_map vm;
+        handler_parse_args(get_options_description(), vm, argc, argv, fa);
+
+        redirect_url = (vm.count("redirect_url") == 0
+                ? std::string("") : vm["redirect_url"].as<std::string>());
+    }//handle
+};
+
+#endif //HANDLE_MOJEID_ARGS_H_
