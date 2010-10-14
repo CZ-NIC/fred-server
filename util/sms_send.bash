ceb00617d0df3b0246252411c974fc217055a8bc
Jaromír Talíř <jaromir.talir@nic.cz>
2010-10-14 20:56:48 +0000

    working version
    git-svn-id: file:///data/svn/enum/server/trunk@14412 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- util/sms_send.bash
+++ util/sms_send.bash
@@ -1,30 +1,33 @@
 #!/bin/bash
-#sms sender parameters
+# sms sender parameters
 # $LOGIN $PASSWORD
 # $1 - phone number $2 - text message content
+#
+# dependancies : openssl, python, curl
 ########################################################
-#echo $1 $2
+# echo $1 $2
 
 PHONE_NUMBER=`echo $1 |  tr -d ' .'` #remove spaces and dots
 TEXTMESSAGE=$2
+#URL=http://api.mobilem.cz/xmlapi2.xp
+URL=https://mobilem.cz/api/xmlapi2.xp
 
-#adjust logfile name
+# adjust logfile name
 LOGTEMPFILE=$(mktemp /tmp/smslog.XXXXXXXXXX) || { echo "Failed to create temp logfile"; exit 1; }
 
-TEXTMESSAGE=$(python -c "import urllib; print urllib.quote('$TEXTMESSAGE');")
+# prepare request data
 TEXTMESSAGEAUTHSUBSTR=${TEXTMESSAGE:0:31}
-PASSWORDMD5=`echo $PASSWORD | openssl dgst -md5`
+PASSWORDMD5=`echo -ne $PASSWORD | openssl dgst -md5 | cut -d' ' -f2`
 ACTION=send
 AUTHTEXT=$PASSWORDMD5$LOGIN$ACTION$TEXTMESSAGEAUTHSUBSTR
-AUTHTEXTMD5=`echo $AUTHTEXT | openssl dgst -md5`
-REQUEST_DATA=' -d login='$LOGIN' -d auth='$AUTHTEXTMD5' --data-urlencode msisdn='$PHONE_NUMBER' -d msg='$TEXTMESSAGE' -d recack=1'
-echo curl -k $REQUEST_DATA  https://mobilem.cz/api/xmlapi2.xp > $LOGTEMPFILE
+AUTHTEXTMD5=`echo -ne $AUTHTEXT | openssl dgst -md5 | cut -d' ' -f2`
+TEXTMESSAGE2=$(python -c "import urllib; print urllib.quote('$TEXTMESSAGE');")
+REQUEST_DATA=" -d action=$ACTION -d login=$LOGIN -d auth=$AUTHTEXTMD5 --data-urlencode msisdn=$PHONE_NUMBER -d msg=$TEXTMESSAGE2 -d recack=1"
 
-#debug
-#CURL_RESULT=`echo status error`
-
-CURL_RESULT=`curl -k $REQUEST_DATA https://mobilem.cz/api/xmlapi2.xp`
+# log and process request
+echo "curl -k $REQUEST_DATA  $URL" > $LOGTEMPFILE
+CURL_RESULT=`curl $REQUEST_DATA $URL 2>/dev/null`
 
 echo $CURL_RESULT >> $LOGTEMPFILE
-echo $CURL_RESULT | grep status | grep ok && exit 0
+echo $CURL_RESULT | grep "status='ok'" >/dev/null && exit 0
 exit  1
