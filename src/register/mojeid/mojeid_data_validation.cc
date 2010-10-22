916a5d485d4fc44c23afc15eba120abd59d6789e
Jiří Šádek <jiri.sadek@nic.cz>
2010-10-22 17:03:43 +0000

    Ticket #4575 - validation field names renamed, added some other required data
    git-svn-id: file:///data/svn/enum/server/trunk@14697 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/register/mojeid/mojeid_data_validation.cc
+++ src/register/mojeid/mojeid_data_validation.cc
@@ -13,22 +13,51 @@ const boost::regex EMAIL_PATTERN("^[-!#$%&'*+/=?^_`{}|~0-9A-Za-z]+(\\.[-!#$%&'*+
                                  "@(?:[A-Za-z0-9](?:[A-Za-z0-9-]{0,61}[A-Za-z0-9])?\\.)+[A-Za-z]{2,6}\\.?$");
 
 
+const std::string field_username    = "contact.username";
+const std::string field_phone       = "auth_sms.phone_number";
+const std::string field_first_name  = "contact.first_name";
+const std::string field_last_name   = "contact.last_name";
+const std::string field_street1     = "address.street1";
+const std::string field_country     = "address.country";
+const std::string field_city        = "address.city";
+const std::string field_postal_code = "address.postal_code";
+const std::string field_email       = "email.email";
+
+
+
 void validate_contact_data(const ::MojeID::Contact &_data)
 {
     FieldErrorMap errors;
+
+    /* required */
+    if (static_cast<std::string>(_data.street1).empty()) {
+        errors[field_street1] = REQUIRED;
+    }
+    if (static_cast<std::string>(_data.city).empty()) {
+        errors[field_city] = REQUIRED;
+    }
+    if (static_cast<std::string>(_data.postalcode).empty()) {
+        errors[field_postal_code] = REQUIRED;
+    }
+
+    /* hmmm?! */
+    if (static_cast<std::string>(_data.name).empty()) {
+        errors[field_first_name] = REQUIRED;
+    }
+
     /* contact handle has to be in domain-token
      * pattern ^[a-z0-9](-?[a-z0-9])*$ and max length 30
      */
     if (_data.handle.length() == 0) {
-        errors["username"] = REQUIRED;
+        errors[field_username] = REQUIRED;
     }
     else if (_data.handle.length() > 30) {
-        errors["username"] = INVALID;
+        errors[field_username] = INVALID;
     }
     else if (!boost::regex_search(
                 boost::to_lower_copy(_data.handle),
                 USERNAME_PATTERN)) {
-        errors["username"] = INVALID;
+        errors[field_username] = INVALID;
     }
 
     /* main phone has to be in format (czech mobile operators):
@@ -36,13 +65,13 @@ void validate_contact_data(const ::MojeID::Contact &_data)
      * and 14 characters long
      */
     if (static_cast<std::string>(_data.telephone).length() > 14) {
-        errors["phone"] = INVALID;
+        errors[field_phone] = INVALID;
     }
     else if (static_cast<std::string>(_data.telephone).length() > 0
                 && !boost::regex_search(
                         static_cast<std::string>(_data.telephone),
                         PHONE_PATTERN)) {
-        errors["phone"] = INVALID;
+        errors[field_phone] = INVALID;
     }
     else {
         /* main phone has to be unique among all mojeid contacts */
@@ -59,23 +88,26 @@ void validate_contact_data(const ::MojeID::Contact &_data)
                     (static_cast<std::string>(_data.telephone))
                     (_data.handle));
         if (unique_phone.size() > 0) {
-            errors["phone"] = NOT_AVAILABLE;
+            errors[field_phone] = NOT_AVAILABLE;
         }
     }
 
     /* main address has to be from Czech Republic */
-    if (static_cast<std::string>(_data.country) != "CZ") {
-        errors["country"] = INVALID;
+    if (static_cast<std::string>(_data.country).empty()) {
+        errors[field_country] = REQUIRED;
+    }
+    else if (static_cast<std::string>(_data.country) != "CZ") {
+        errors[field_country] = INVALID;
     }
 
     /* email should match following pattern */
     if (static_cast<std::string>(_data.email).length() == 0) {
-        errors["email"] = REQUIRED;
+        errors[field_email] = REQUIRED;
     }
     else if (!boost::regex_search(
                 static_cast<std::string>(_data.email),
                 EMAIL_PATTERN)) {
-        errors["email"] = INVALID;
+        errors[field_email] = INVALID;
     }
     else {
         /* main email has to be unique among all mojeid contacts */
@@ -92,7 +124,7 @@ void validate_contact_data(const ::MojeID::Contact &_data)
                     (static_cast<std::string>(_data.email))
                     (_data.handle));
         if (unique_email.size() > 0) {
-            errors["email"] = NOT_AVAILABLE;
+            errors[field_email] = NOT_AVAILABLE;
         }
     }
 
