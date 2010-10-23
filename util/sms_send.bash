7709e8bed3801b867347798ea347300c13e454f3
Jaromír Talíř <jaromir.talir@nic.cz>
2010-10-23 11:36:44 +0000

    updated sms sender
    git-svn-id: file:///data/svn/enum/server/trunk@14718 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- util/sms_send.bash
+++ util/sms_send.bash
@@ -9,12 +9,16 @@
 
 PHONE_NUMBER=`echo $1 |  tr -d ' .'` #remove spaces and dots
 TEXTMESSAGE=$2
-#URL=http://api.mobilem.cz/xmlapi2.xp
-URL=https://mobilem.cz/api/xmlapi2.xp
+URL=http://api.mobilem.cz/xmlapi2.xp
+#URL=https://api.mobilem.cz/xmlapi2.xp
 
 # adjust logfile name
-LOGTEMPFILE=$(mktemp /tmp/smslog.XXXXXXXXXX) || { echo "Failed to create temp logfile"; exit 1; }
+#LOGTEMPFILE=$(mktemp /tmp/smslog.XXXXXXXXXX) || { echo "Failed to create temp logfile"; exit 1; }
+if [ -z "$LOGFILE" ]; then
+    LOGFILE=/tmp/fred_sms_tomato.log
+fi
 
+echo "-=-=-=-=-=-=-=-= $(date) =-=-=-=-=-=-=-=-" >> $LOGFILE
 # prepare request data
 TEXTMESSAGEAUTHSUBSTR=${TEXTMESSAGE:0:31}
 PASSWORDMD5=`echo -ne $PASSWORD | openssl dgst -md5 | cut -d' ' -f2`
@@ -22,12 +26,15 @@ ACTION=send
 AUTHTEXT=$PASSWORDMD5$LOGIN$ACTION$TEXTMESSAGEAUTHSUBSTR
 AUTHTEXTMD5=`echo -ne $AUTHTEXT | openssl dgst -md5 | cut -d' ' -f2`
 TEXTMESSAGE2=$(python -c "import urllib; print urllib.quote('$TEXTMESSAGE');")
-REQUEST_DATA=" -d action=$ACTION -d login=$LOGIN -d auth=$AUTHTEXTMD5 --data-urlencode msisdn=$PHONE_NUMBER -d msg=$TEXTMESSAGE2 -d recack=1"
+REQUEST_DATA=" -d action=$ACTION -d login=$LOGIN -d auth=$AUTHTEXTMD5 --data-urlencode msisdn=$PHONE_NUMBER -d msg=$TEXTMESSAGE2 -d recack=1 -d sendermsisdn=mojeID"
 
 # log and process request
-echo "curl -k $REQUEST_DATA  $URL" > $LOGTEMPFILE
+echo "QUERY" >> $LOGFILE
+echo "curl -k $REQUEST_DATA  $URL" >> $LOGFILE
 CURL_RESULT=`curl $REQUEST_DATA $URL 2>/dev/null`
 
-echo $CURL_RESULT >> $LOGTEMPFILE
+echo "RESPONSE" >> $LOGFILE
+echo $CURL_RESULT >> $LOGFILE
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-" >> $LOGFILE
 echo $CURL_RESULT | grep "status='ok'" >/dev/null && exit 0
 exit  1
