d5546db1b07e8aa8bab0748d513e76e77b259b91
Jan Zima <jan.zima@nic.cz>
2010-11-18 15:59:54 +0000

    conversion test #4798
    git-svn-id: file:///data/svn/enum/server/trunk@15152 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

---
+++ tests/test-conv.cc
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2010  CZ.NIC, z.s.p.o.
+ *
+ *  This file is part of FRED.
+ *
+ *  FRED is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, version 2 of the License.
+ *  (at your option) any later version.
+ *
+ *  FRED is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define BOOST_TEST_MODULE Test Conversions
+
+#include "test-conv.h"
+
+
+
+//args processing config for custom main
+HandlerPtrVector global_hpv =
+boost::assign::list_of
+(HandleArgsPtr(new HandleGeneralArgs))
+(HandleArgsPtr(new HandleDatabaseArgs))
+;
+
+#include "cfg/test_custom_main.h"
+
+
+
+BOOST_AUTO_TEST_CASE( test_exec )
+{
+    int ret = 0;
+    try
+    {
+        //current utc ptime
+        boost::posix_time::ptime act_time
+            = boost::posix_time::second_clock::universal_time();
+
+         boost::posix_time::ptime lact_time;
+
+        Database::Connection conn = Database::Manager::acquire();
+        Database::Result res = conn.exec_params(
+                "SELECT $1 at time zone 'Europe/Prague' "
+                , Database::query_param_list (act_time));
+        if (res.size() == 1)
+        {
+            lact_time = res[0][0];
+        }
+
+        std::ostringstream buf;
+        buf.imbue(std::locale(std::locale(""),new date_facet("%x")));
+        //buf.imbue(std::locale("cs_CZ.utf8"));
+        buf << lact_time.date();
+
+
+        std::cout
+        << "\nact_time: " << act_time
+        << "\ndate: " << act_time.date()
+        << "\nldate: " << buf.str()
+        << "\nltime: " << lact_time
+        << std::endl;
+
+
+    }
+    catch(...)
+    {
+        ret = 1;//error
+    }
+    BOOST_REQUIRE_EQUAL(ret , 0);
+}
---
+++ tests/test-conv.h
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2010  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TESTCONV_H_
+#define TESTCONV_H_
+
+#include <memory>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <queue>
+#include <sys/time.h>
+#include <time.h>
+
+
+#include <boost/format.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/barrier.hpp>
+#include <boost/date_time.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/assign/list_of.hpp>
+
+#include "db_settings.h"
+#include "log/logger.h"
+#include "log/context.h"
+#include "random_data_generator.h"
+#include "concurrent_queue.h"
+
+#include "cfg/handle_general_args.h"
+#include "cfg/handle_database_args.h"
+
+
+#ifdef BOOST_NO_STDC_NAMESPACE
+namespace std
+{
+  using ::time;
+}
+#endif
+
+#endif // TESTCONV_H_
