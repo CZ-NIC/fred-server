8fc93eafd3ad62de0b706d2dae3af4f5c33b7fad
Jan Zima <jan.zima@nic.cz>
2011-05-23 14:33:30 +0000

    insertInvoicePrefix_nozone test #5355
    git-svn-id: file:///data/svn/enum/server/trunk@17139 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- tests/test-invoice.cc
+++ tests/test-invoice.cc
@@ -84,6 +84,37 @@ bool check_std_exception_invoice_prefix(std::exception const & ex)
     return (ex_msg.find(std::string("Missing invoice prefix")) != std::string::npos);
 }
 
+BOOST_AUTO_TEST_CASE( insertInvoicePrefix_nozone )
+{
+    // setting up logger
+    setup_logging(CfgArgs::instance());
+    //db
+    Database::Connection conn = Database::Manager::acquire();
+
+    //manager
+    std::auto_ptr<Fred::Invoicing::Manager> invMan(Fred::Invoicing::Manager::create());
+
+    int year = boost::gregorian::day_clock::universal_day().year()+20;
+
+    conn.exec_params("delete from invoice_prefix where year = $1::bigint", Database::query_param_list(year));
+
+    BOOST_CHECK(
+    (invMan->insertInvoicePrefix(
+            0//no zoneId
+            , 0//type
+            , year//year
+            , year*10000//prefix
+            )));
+
+    BOOST_CHECK(
+    (invMan->insertInvoicePrefix(
+            "com"//zone
+            , 1//type
+            , year//year
+            , year*10000 + 1000//prefix
+            )));
+
+}//insertInvoicePrefix
 
 
 BOOST_AUTO_TEST_CASE( insertInvoicePrefix )
