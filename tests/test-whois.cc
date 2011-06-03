1fea25e0aee1963365ed4a6ba870eed1eb403e15
Jan Zima <jan.zima@nic.cz>
2011-06-03 09:07:31 +0000

    test_whois #5394
    git-svn-id: file:///data/svn/enum/server/trunk@17343 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- tests/test-whois.cc
+++ tests/test-whois.cc
@@ -37,6 +37,8 @@
 #include <boost/assign/list_of.hpp>
 
 #include "setup_server_decl.h"
+#include "time_clock.h"
+#include "fredlib/registrar.h"
 
 #include "cfg/handle_general_args.h"
 #include "cfg/handle_server_args.h"
@@ -85,25 +87,37 @@ BOOST_AUTO_TEST_CASE( test_whois )
     //db
     Database::Connection conn = Database::Manager::acquire();
 
+    // registrar
+    std::string time_string(TimeStamp::microsec());
+    std::string registrar_handle(std::string("REG-FRED_WHOIS")+time_string);
+    Fred::Registrar::Manager::AutoPtr regMan
+             = Fred::Registrar::Manager::create(DBSharedPtr());
+    Fred::Registrar::Registrar::AutoPtr registrar_ = regMan->createRegistrar();
+    registrar_->setHandle(registrar_handle);//REGISTRAR_ADD_HANDLE_NAME
+    registrar_->setCountry("CZ");//REGISTRAR_COUNTRY_NAME
+    registrar_->setName("Whois Pepa");
+    registrar_->setCity("WhoisPraha");
+    registrar_->setVat(true);
+    registrar_->save();
+
     Database::Result res_contact =
-    conn.exec("select obr.name from contact c "
+    conn.exec("select obr.name, c.name from contact c "
             " join object_registry obr on c.id = obr.id "
-            " where c.name like 'Pepa Zdepa'");
-    std::string contact_city;
+            );
+    std::string contact_name;
     if(res_contact.size() > 0)
     {
-        std::string contact_name =
-        std::string(res_contact[0][0]);
+        std::string contact_handle =std::string(res_contact[0][0]);
         ccReg::ContactDetail_var test_contact
-            = whois1_ref->getContactByHandle(contact_name.c_str());
-        contact_city = static_cast<std::string>(test_contact->city);
+            = whois1_ref->getContactByHandle(contact_handle.c_str());
+        contact_name = static_cast<std::string>(test_contact->name);
     }
-    BOOST_CHECK_EQUAL(contact_city.compare("Praha"),0);
+    BOOST_CHECK_EQUAL(contact_name.compare(std::string(res_contact[0][1])),0);
 
     //test call
         ccReg::WhoisRegistrar_var registrar
-            = whois1_ref->getRegistrarByHandle("REG-FRED_A");
-        BOOST_CHECK_EQUAL(static_cast<std::string>(registrar->country).compare("CZ"), 0);
+            = whois1_ref->getRegistrarByHandle(registrar_handle.c_str());
+        BOOST_CHECK_EQUAL(static_cast<std::string>(registrar->handle).compare(registrar_handle), 0);
 
         ccReg::WhoisRegistrarList_var registrar_list
             = whois1_ref->getRegistrarsByZone("cz");
