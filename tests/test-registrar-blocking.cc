fa4a13962ec5b207f0a0302723d53630376d5383
jvicenik <jvicenik@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-09-05 17:10:40 +0000

    Ticket #5666 - test registrar blocking
    git-svn-id: file:///data/svn/enum/server/trunk@18431 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- tests/test-registrar-blocking.cc
+++ tests/test-registrar-blocking.cc
@@ -1,5 +1,6 @@
 #include <boost/test/unit_test.hpp>
 #include <iostream>
+#include "time_clock.h"
 
 #include "fredlib/db_settings.h"
 #include "old_utils/dbsql.h"
@@ -12,6 +13,53 @@
 
 #include "epp_corba_client_impl.h"
 
+
+void  init_corba_container()
+{
+ //corba config
+    FakedArgs orb_fa = CfgArgs::instance()->fa;
+
+    //conf pointers
+    HandleCorbaNameServiceArgs* ns_args_ptr=CfgArgs::instance()->
+                get_handler_ptr_by_type<HandleCorbaNameServiceArgs>();
+
+    CorbaContainer::set_instance(orb_fa.get_argc(), orb_fa.get_argv()
+            , ns_args_ptr->nameservice_host
+            , ns_args_ptr->nameservice_port
+            , ns_args_ptr->nameservice_context);
+}
+
+// TODO: this should also be used in invoice test
+Database::ID create_registrar(Fred::Registrar::Manager *regMan)
+{
+    Fred::Registrar::Registrar::AutoPtr registrar = regMan->createRegistrar();
+
+    //current time into char string
+    std::string time_string(TimeStamp::microsec());
+    std::string registrar_novat_handle(std::string("REG-FRED_NOVAT_INV")+time_string);
+
+    registrar->setHandle(registrar_novat_handle);//REGISTRAR_ADD_HANDLE_NAME
+    registrar->setCountry("CZ");//REGISTRAR_COUNTRY_NAME
+    registrar->setVat(true);
+    registrar->save();
+
+    return registrar->getId();
+}
+
+Database::ID block_reg_and_test(Fred::Registrar::Manager *regman)
+{
+    Database::ID reg_id = create_registrar(regman);
+
+    std::auto_ptr<EppCorbaClient> epp_cli(new EppCorbaClientImpl());
+
+    BOOST_REQUIRE(regman->blockRegistrar(reg_id, epp_cli.get()) == true);
+
+    BOOST_REQUIRE(Fred::Registrar::isRegistrarBlocked(reg_id));
+    // BOOST_REQUIRE(regman->isRegistrarBlocked(reg_id));
+    // this is gonna be correct
+    return reg_id;
+}
+
 BOOST_AUTO_TEST_SUITE(TestRegistrarBlocking)
 
 BOOST_AUTO_TEST_CASE( test_block_registrar )
@@ -23,17 +71,7 @@ BOOST_AUTO_TEST_CASE( test_block_registrar )
     std::auto_ptr<Fred::Registrar::Manager> regMan(
             Fred::Registrar::Manager::create(m_db));
 
-    //corba config
-    FakedArgs orb_fa = CfgArgs::instance()->fa;
-
-    //conf pointers
-    HandleCorbaNameServiceArgs* ns_args_ptr=CfgArgs::instance()->
-                get_handler_ptr_by_type<HandleCorbaNameServiceArgs>();
-
-    CorbaContainer::set_instance(orb_fa.get_argc(), orb_fa.get_argv()
-            , ns_args_ptr->nameservice_host
-            , ns_args_ptr->nameservice_port
-            , ns_args_ptr->nameservice_context);
+    init_corba_container();
 
     std::auto_ptr<EppCorbaClientImpl> epp_cli (new EppCorbaClientImpl());
 
@@ -41,8 +79,104 @@ BOOST_AUTO_TEST_CASE( test_block_registrar )
     // registrar_disconnect must be cleaned after this call or it could use own registrar
     // regMan->blockRegistrar(1, epp_cli.get());
 
-    // regMan->unblockRegistrar(1, 0);
+    regMan->blockRegistrar(1, epp_cli.get() );
 
 }
+
+BOOST_AUTO_TEST_CASE( test_is_registrar_blocked )
+{
+    init_corba_container();
+    std::auto_ptr<Fred::Registrar::Manager> regman(
+                   Fred::Registrar::Manager::create(DBDisconnectPtr(NULL)));
+
+    Database::ID reg_id = block_reg_and_test(regman.get());
+}
+
+BOOST_AUTO_TEST_CASE(test_unblock_registrar)
+{
+    init_corba_container();
+    std::auto_ptr<Fred::Registrar::Manager> regman(
+                   Fred::Registrar::Manager::create(DBDisconnectPtr(NULL)));
+
+    Database::ID reg_id = block_reg_and_test(regman.get());
+
+    regman->unblockRegistrar(reg_id, 767);
+
+    BOOST_REQUIRE(!Fred::Registrar::isRegistrarBlocked(reg_id));
+
+}
+
+BOOST_AUTO_TEST_CASE( test_double_unlock_registrar)
+{
+    init_corba_container();
+    std::auto_ptr<Fred::Registrar::Manager> regman(
+                       Fred::Registrar::Manager::create(DBDisconnectPtr(NULL)));
+
+    Database::ID reg_id = block_reg_and_test(regman.get());
+
+    regman->unblockRegistrar(reg_id, 777);
+
+    BOOST_REQUIRE_THROW(regman->unblockRegistrar(reg_id, 787),  Fred::NOT_BLOCKED);
+
+}
+
+BOOST_AUTO_TEST_CASE( test_block_again)
+{
+    init_corba_container();
+    std::auto_ptr<Fred::Registrar::Manager> regman(
+                       Fred::Registrar::Manager::create(DBDisconnectPtr(NULL)));
+    std::auto_ptr<EppCorbaClient> epp_cli(new EppCorbaClientImpl());
+
+    Database::ID reg_id = block_reg_and_test(regman.get());
+
+    regman->unblockRegistrar(reg_id, 777);
+
+    BOOST_REQUIRE(regman->blockRegistrar(reg_id, epp_cli.get()) == false);
+
+}
+
+BOOST_AUTO_TEST_CASE( test_block_again_no_special_values_used_in_interval_definition )
+{
+    init_corba_container();
+    std::auto_ptr<Fred::Registrar::Manager> regman(
+                         Fred::Registrar::Manager::create(DBDisconnectPtr(NULL)));
+    std::auto_ptr<EppCorbaClient> epp_cli(new EppCorbaClientImpl());
+
+    Database::Connection conn = Database::Manager::acquire();
+
+    Database::ID registrar_id = create_registrar(regman.get());
+
+// pretend that registrar was blocked 'till the end of last month:
+    conn.exec_params ("INSERT INTO registrar_disconnect (registrarid, blocked_from, blocked_to) VALUES "
+            "($1::integer, now() - interval '1 month', date_trunc('month', now()) ) ",
+            Database::query_param_list(registrar_id));
+
+    BOOST_REQUIRE(regman->blockRegistrar(registrar_id, epp_cli.get()));
+
+}
+
+/// might not work shortly (couple of minutes) after begining of a month
+BOOST_AUTO_TEST_CASE( test_block_last_year_interference )
+{
+    init_corba_container();
+    std::auto_ptr<Fred::Registrar::Manager> regman(
+                         Fred::Registrar::Manager::create(DBDisconnectPtr(NULL)));
+    std::auto_ptr<EppCorbaClient> epp_cli(new EppCorbaClientImpl());
+
+    Database::Connection conn = Database::Manager::acquire();
+
+    Database::ID registrar_id = create_registrar(regman.get());
+
+// pretend that registrar was blocked 'till the end of last month:
+    conn.exec_params ("INSERT INTO registrar_disconnect (registrarid, blocked_from, blocked_to) VALUES "
+            "($1::integer, "
+            " date_trunc('month', now()) - interval '1 year' + interval '1 minute', "
+            " now() - interval '1 year' - interval '1 minute') ",
+            Database::query_param_list(registrar_id));
+
+    BOOST_REQUIRE(regman->blockRegistrar(registrar_id, epp_cli.get()));
+
+}
+
 
 BOOST_AUTO_TEST_SUITE_END();
