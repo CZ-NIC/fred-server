afd42e5e0267e5d52b20a2cd0eba6a10fbcf14ef
jzima <jzima@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-09-21 14:16:27 +0000

    test fix
    git-svn-id: file:///data/svn/enum/server/trunk@18629 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- tests/test-invoice.cc
+++ tests/test-invoice.cc
@@ -72,7 +72,7 @@
 
 BOOST_AUTO_TEST_SUITE(TestInvoice)
 
-const unsigned DEFAULT_REGISTRATION_PERIOD = 24;
+const unsigned DEFAULT_REGISTRATION_PERIOD = 2;
 
 using namespace Fred::Invoicing;
 
@@ -1888,7 +1888,8 @@ BOOST_AUTO_TEST_CASE( archiveInvoices )
             Fred::Banking::XMLnode entry = vat_rates.getChild("entry");
             if (entry.getName().compare("entry") != 0) throw std::runtime_error("xml element name is not \"entry\"");
 
-            if (invoice_prefix_typ == 1) std::cout << "acc invoice" << std::endl;
+            if (invoice_prefix_typ == 1) //std::cout << "acc invoice" << std::endl
+                ;
 
             /* not working this way
             if(vat_rates.hasChild("entry"))
