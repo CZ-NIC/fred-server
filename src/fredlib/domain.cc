7006ce3644e6e78e7da78ed50619cb7fc7fbe6cc
jsadek <jsadek@b60c293d-5e0d-0410-87b2-fb9704ccd4b5>
2011-10-17 16:46:38 +0000

    merged fix r18984 from branche 2.8 (whois deletecandidate state handling - re-register of delete domain in actual day)
    git-svn-id: file:///data/svn/enum/server/trunk@18994 b60c293d-5e0d-0410-87b2-fb9704ccd4b5

--- src/fredlib/domain.cc
+++ src/fredlib/domain.cc
@@ -1355,7 +1355,7 @@ public:
   {
       Database::Connection conn = Database::Manager::acquire();
       Database::Result result = conn.exec_params(
-              "SELECT oreg.name FROM object_registry oreg"
+              "SELECT oreg.name, oreg.id, oreg.crdate FROM object_registry oreg"
               " JOIN object_state os ON os.object_id = oreg.id"
               " JOIN enum_object_states eos ON  eos.id = os.state_id"
               " WHERE oreg.type = 3"
@@ -1369,7 +1369,28 @@ public:
               " < date_trunc('day', (current_timestamp::timestamp AT TIME ZONE 'UTC') AT TIME ZONE 'Europe/Prague' +  interval '1 day')))"
               " AND oreg.name = $1::text", Database::query_param_list(_fqdn));
       if (result.size() > 0) {
-          return true;
+          LOGGER(PACKAGE).debug(boost::format("delete pending check for fqdn %1% selected id=%2%")
+                                      % static_cast<std::string>(result[0][0])
+                                      % static_cast<std::string>(result[0][1]));
+          Database::Result check = conn.exec_params(
+                  "SELECT oreg.name, oreg.id FROM object_registry oreg"
+                  " WHERE oreg.type = 3"
+                  " AND oreg.name = $1::text"
+                  " AND oreg.id != $2::bigint"
+                  " AND oreg.crdate > $3::timestamp",
+                  Database::query_param_list
+                    (_fqdn)
+                    (static_cast<unsigned long long>(result[0][1]))
+                    (static_cast<std::string>(result[0][2])));
+          if (check.size() > 0) {
+              LOGGER(PACKAGE).debug(boost::format("delete pending check found newer domain %1% with id=%2%")
+                      % static_cast<std::string>(check[0][0])
+                      % static_cast<std::string>(check[0][1]));
+              return false;
+          }
+          else {
+              return true;
+          }
       }
       else {
           return false;
