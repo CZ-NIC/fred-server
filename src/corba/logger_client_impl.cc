acd8f8cc6c7a0cc53bd199329fa35c1c8c1f4e91
Jiri Sadek <jiri.sadek@nic.cz>
2013-01-31 00:30:40 +0100

    Ticket #7669 - logger client interface extended (create, close request)

--- src/corba/logger_client_impl.cc
+++ src/corba/logger_client_impl.cc
@@ -4,6 +4,37 @@
 namespace Fred {
 namespace Logger {
 
+ccReg::RequestProperties_var corba_wrap_request_properties(
+        const RequestProperties &_properties)
+{
+    ccReg::RequestProperties_var props(new ccReg::RequestProperties());
+    props->length(_properties.size());
+    for (RequestProperties::size_type i = 0; i < _properties.size(); ++i)
+    {
+        props[i].name = _properties[i].name.c_str();
+        props[i].value = _properties[i].value.c_str();
+        props[i].child = _properties[i].child;
+    }
+    return props;
+}
+
+
+ccReg::ObjectReferences_var corba_wrap_object_references(
+        const ObjectReferences &_references)
+{
+    ccReg::ObjectReferences_var refs(new ccReg::ObjectReferences());
+    refs->length(_references.size());
+    for (ObjectReferences::size_type i = 0; i < _references.size(); ++i)
+    {
+        refs[i].type = _references[i].type.c_str();
+        refs[i].id = _references[i].id;
+    }
+    return refs;
+}
+
+
+
+
 LoggerCorbaClientImpl::LoggerCorbaClientImpl()
 {
     boost::mutex::scoped_lock lock (ref_mutex);
@@ -44,6 +75,117 @@ std::auto_ptr<RequestCountInfo> LoggerCorbaClientImpl::getRequestCountUsers(
     return ret;
 
 }
+
+
+unsigned long long LoggerCorbaClientImpl::createRequest(
+            const std::string &_src_ip,
+            const std::string &_service,
+            const std::string &_content,
+            const RequestProperties &_properties,
+            const ObjectReferences &_references,
+            const std::string &_type,
+            const unsigned long long _session_id)
+{
+    try
+    {
+        ccReg::RequestProperties_var props = corba_wrap_request_properties(_properties);
+        ccReg::ObjectReferences_var refs = corba_wrap_object_references(_references);
+
+        ccReg::RequestServiceList_var service_list = logger_ref->getServices();
+        unsigned long long service_id = 0;
+        for (unsigned long long i = 0; i < service_list->length(); ++i) {
+            if (std::string(service_list[i].name) == _service) {
+                service_id = service_list[i].id;
+                break;
+            }
+        }
+        if (service_id == 0) {
+            throw std::runtime_error("unknown service");
+        }
+        ccReg::RequestTypeList_var request_type_list = logger_ref->getRequestTypesByService(service_id);
+        unsigned long long request_type_id = 0;
+        for (unsigned long long i = 0; i < request_type_list->length(); ++i) {
+            if (std::string(request_type_list[i].name) == _type) {
+                request_type_id = request_type_list[i].id;
+                break;
+            }
+        }
+        if (request_type_id == 0) {
+            throw std::runtime_error("unknown request type");
+        }
+
+        CORBA::ULongLong rid = logger_ref->createRequest(
+                _src_ip.c_str(),
+                service_id,
+                _content.c_str(),
+                props,
+                refs,
+                request_type_id,
+                _session_id);
+
+        return static_cast<unsigned long long>(rid);
+    }
+    catch (...)
+    {
+        throw std::runtime_error("create request failed");
+    }
+}
+
+
+void LoggerCorbaClientImpl::closeRequest(
+            const unsigned long long _request_id,
+            const std::string &_service,
+            const std::string &_content,
+            const RequestProperties &_properties,
+            const ObjectReferences &_references,
+            const std::string &_result,
+            const unsigned long long _session_id)
+{
+    try
+    {
+        ccReg::RequestProperties_var props = corba_wrap_request_properties(_properties);
+        ccReg::ObjectReferences_var refs = corba_wrap_object_references(_references);
+
+        ccReg::RequestServiceList_var service_list = logger_ref->getServices();
+        unsigned long long service_id = 0;
+        for (unsigned long long i = 0; i < service_list->length(); ++i) {
+            if (std::string(service_list[i].name) == _service) {
+                service_id = service_list[i].id;
+                break;
+            }
+        }
+        if (service_id == 0) {
+            throw std::runtime_error("unknown service");
+        }
+
+        ccReg::ResultCodeList_var result_code_list = logger_ref->getResultCodesByService(service_id);
+        unsigned long long result_code_id = 0;
+        for (unsigned long long i = 0; i < result_code_list->length(); ++i) {
+            if (std::string(result_code_list[i].name) == _result) {
+                result_code_id = result_code_list[i].result_code;
+                break;
+            }
+        }
+        if (result_code_id == 0) {
+            throw std::runtime_error("unknown result code");
+        }
+
+        logger_ref->closeRequest(
+                _request_id,
+                _content.c_str(),
+                props,
+                refs,
+                result_code_id,
+                _session_id);
+
+
+    }
+    catch (...)
+    {
+        throw std::runtime_error("close request failed");
+    }
+}
+
 
 }
 }
--- src/corba/logger_client_impl.h
+++ src/corba/logger_client_impl.h
@@ -28,10 +28,31 @@ public:
                const boost::posix_time::ptime &to,
                const std::string &service);
 
+    unsigned long long createRequest(
+            const std::string &_src_ip,
+            const std::string &_service,
+            const std::string &_content,
+            const RequestProperties &_properties,
+            const ObjectReferences &_references,
+            const std::string &_type,
+            const unsigned long long _session_id);
+
+    void closeRequest(
+            const unsigned long long _request_id,
+            const std::string &_service,
+            const std::string &_content,
+            const RequestProperties &_properties,
+            const ObjectReferences &_references,
+            const std::string &_result,
+            const unsigned long long _session_id); 
+
+
 private:
     ccReg::Logger_var logger_ref;
 
     boost::mutex ref_mutex;
+
+
 };
 
 }
