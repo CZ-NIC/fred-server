7bcd49643af04602ab14e1f8c8975294676f3863
Michal Strnad <michal.strnad@nic.cz>
2013-02-04 13:58:00 +0100

    Ticket #8130 - class GetObjectStateIdMap moved

--- src/fredlib/domain/cancel_object_state_request.cc
+++ src/fredlib/domain/cancel_object_state_request.cc
@@ -23,6 +23,7 @@
 
 #include "fredlib/domain/cancel_object_state_request.h"
 #include "fredlib/domain/get_blocking_status_desc_list.h"
+#include "fredlib/domain/get_object_state_id_map.h"
 #include "fredlib/opcontext.h"
 #include "fredlib/db_settings.h"
 #include "util/optional_value.h"
