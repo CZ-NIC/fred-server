e3885625893dfdc49984f78ab8e317abf0a2ac2e
Jan Zima <jan.zima@nic.cz>
2013-02-22 18:01:32 +0100

    Ticket #7875 - create keyset, create nsset history fix

--- src/fredlib/keyset/create_keyset.cc
+++ src/fredlib/keyset/create_keyset.cc
@@ -17,8 +17,8 @@
  */
 
 /**
- *  @file create_nsset.h
- *  create nsset
+ *  @file create_keyset.h
+ *  create keyset
  */
 
 
@@ -27,7 +27,7 @@
 
 #include <boost/algorithm/string.hpp>
 
-#include "fredlib/nsset/create_nsset.h"
+#include "fredlib/keyset/create_keyset.h"
 #include "fredlib/object/object.h"
 
 #include "fredlib/opcontext.h"
@@ -39,132 +39,89 @@
 namespace Fred
 {
 
-    CreateNsset::CreateNsset(const std::string& handle
+    CreateKeyset::CreateKeyset(const std::string& handle
                 , const std::string& registrar)
     : handle_(handle)
     , registrar_(registrar)
     {}
 
-    CreateNsset::CreateNsset(const std::string& handle
+    CreateKeyset::CreateKeyset(const std::string& handle
             , const std::string& registrar
             , const Optional<std::string>& authinfo
-            , const Optional<short>& tech_check_level
-            , const std::vector<DnsHost>& dns_hosts
+            , const std::vector<DnsKey>& dns_keys
             , const std::vector<std::string>& tech_contacts
             , const Optional<unsigned long long> logd_request_id
             )
     : handle_(handle)
     , registrar_(registrar)
     , authinfo_(authinfo)
-    , tech_check_level_(tech_check_level)
-    , dns_hosts_(dns_hosts)
+    , dns_keys_(dns_keys)
     , tech_contacts_(tech_contacts)
     , logd_request_id_(logd_request_id.isset()
             ? Nullable<unsigned long long>(logd_request_id.get_value())
             : Nullable<unsigned long long>())//is NULL if not set
     {}
 
-    CreateNsset& CreateNsset::set_authinfo(const std::string& authinfo)
+    CreateKeyset& CreateKeyset::set_authinfo(const std::string& authinfo)
     {
         authinfo_ = authinfo;
         return *this;
     }
 
-    CreateNsset& CreateNsset::set_tech_check_level(short tech_check_level)
+    CreateKeyset& CreateKeyset::set_dns_keys(const std::vector<DnsKey>& dns_keys)
     {
-        tech_check_level_ = tech_check_level;
+        dns_keys_ = dns_keys;
         return *this;
     }
 
-    CreateNsset& CreateNsset::set_dns_hosts(const std::vector<DnsHost>& dns_hosts)
-    {
-        dns_hosts_ = dns_hosts;
-        return *this;
-    }
-
-    CreateNsset& CreateNsset::set_tech_contacts(const std::vector<std::string>& tech_contacts)
+    CreateKeyset& CreateKeyset::set_tech_contacts(const std::vector<std::string>& tech_contacts)
     {
         tech_contacts_ = tech_contacts;
         return *this;
     }
 
-    CreateNsset& CreateNsset::set_logd_request_id(unsigned long long logd_request_id)
+    CreateKeyset& CreateKeyset::set_logd_request_id(unsigned long long logd_request_id)
     {
         logd_request_id_ = logd_request_id;
         return *this;
     }
 
-    boost::posix_time::ptime CreateNsset::exec(OperationContext& ctx, const std::string& returned_timestamp_pg_time_zone_name)
+    boost::posix_time::ptime CreateKeyset::exec(OperationContext& ctx, const std::string& returned_timestamp_pg_time_zone_name)
     {
         boost::posix_time::ptime timestamp;
 
         try
         {
-            unsigned long long object_id = CreateObject("nsset", handle_, registrar_, authinfo_).exec(ctx);
-            //create nsset
+            unsigned long long object_id = CreateObject("keyset", handle_, registrar_, authinfo_).exec(ctx);
+            //create keyset
             {
-                Database::QueryParams params;//query params
-                std::stringstream col_sql, val_sql;
-                Util::HeadSeparator col_separator("",", "), val_separator("",", ");
-
-                col_sql <<"INSERT INTO nsset (";
-                val_sql << " VALUES (";
-
-                //id
-                params.push_back(object_id);
-                col_sql << col_separator.get() << "id";
-                val_sql << val_separator.get() << "$" << params.size() <<"::integer";
-
-                if(tech_check_level_.isset())
-                {
-                    params.push_back(tech_check_level_.get_value());
-                    col_sql << col_separator.get() << "checklevel";
-                    val_sql << val_separator.get() << "$" << params.size() <<"::smallint";
-                }
-
-                col_sql <<")";
-                val_sql << ")";
                 //insert
-                ctx.get_conn().exec_params(col_sql.str() + val_sql.str(), params);
+                ctx.get_conn().exec_params("INSERT INTO keyset (id) VALUES ($1::integer)"
+                        , Database::query_param_list(object_id));
 
-                //set dns hosts
-                if(!dns_hosts_.empty())
+                //set dns keys
+                if(!dns_keys_.empty())
                 {
-                    for(std::vector<DnsHost>::iterator i = dns_hosts_.begin(); i != dns_hosts_.end(); ++i)
+                    for(std::vector<DnsKey>::iterator i = dns_keys_.begin(); i != dns_keys_.end(); ++i)
                     {
-                        Database::Result add_host_id_res = ctx.get_conn().exec_params(
-                            "INSERT INTO host (nssetid, fqdn) VALUES( "
-                            " $1::integer, LOWER($2::text)) RETURNING id"
-                            , Database::query_param_list(object_id)(i->get_fqdn()));
-                        if(add_host_id_res.size() != 1)
+                        std::string nwkey = i->get_key();
+
+                        //erase spaces in key
+                        nwkey.erase(std::remove_if(nwkey.begin(), nwkey.end(), isspace), nwkey.end());
+
+                        Database::Result dns_keys_res = ctx.get_conn().exec_params(
+                            "INSERT INTO dnskey (keysetid, flags, protocol, alg, key) VALUES($1::integer "
+                            ", $2::integer, $3::integer, $4::integer, $5::text) RETURNING id"
+                            , Database::query_param_list(object_id)(i->get_flags())(i->get_protocol())(i->get_alg())(nwkey));
+                        if (dns_keys_res.size() != 1)
                         {
-                            std::string errmsg("set dns hosts || invalid:dns fqdn: ");
-                            errmsg += boost::replace_all_copy(i->get_fqdn(),"|", "[pipe]");//quote pipes
+                            std::string errmsg("dns keys || invalid:dns key: ");
+                            errmsg += boost::replace_all_copy(nwkey,"|", "[pipe]");//quote pipes
                             errmsg += " |";
-                            throw CNEX(errmsg.c_str());
+                            throw CKEX(errmsg.c_str());
                         }
-
-                        unsigned long long add_host_id = add_host_id_res[0][0];
-
-                        std::vector<std::string> dns_host_ip = i->get_inet_addr();
-
-                        for(std::vector<std::string>::iterator j = dns_host_ip.begin(); j != dns_host_ip.end(); ++j)
-                        {
-                            Database::Result add_host_ipaddr_res = ctx.get_conn().exec_params(
-                                "INSERT INTO host_ipaddr_map (hostid, nssetid, ipaddr) "
-                                " VALUES($1::integer, $2::integer, $3::inet) RETURNING hostid"
-                                , Database::query_param_list(add_host_id)(object_id)(*j));
-                            if(add_host_ipaddr_res.size() != 1)
-                            {
-                                std::string errmsg("set dns hosts || invalid:ipaddr: ");
-                                errmsg += boost::replace_all_copy(*j,"|", "[pipe]");//quote pipes
-                                errmsg += " |";
-                                throw CNEX(errmsg.c_str());
-                            }
-
-                        }//for j
                     }//for i
-                }//if set dns hosts
+                }//if set dns keys
 
 
                 //set tech contacts
@@ -174,7 +131,7 @@ namespace Fred
                     std::stringstream sql;
 
                     params.push_back(object_id);
-                    sql << "INSERT INTO nsset_contact_map(nssetid, contactid) "
+                    sql << "INSERT INTO keyset_contact_map(keysetid, contactid) "
                             " VALUES ($" << params.size() << "::integer, ";
 
                     for(std::vector<std::string>::iterator i = tech_contacts_.begin(); i != tech_contacts_.end(); ++i)
@@ -193,7 +150,7 @@ namespace Fred
                                 std::string errmsg("unable to lock || not found:tech contact: ");
                                 errmsg += boost::replace_all_copy(*i,"|", "[pipe]");//quote pipes
                                 errmsg += " |";
-                                throw CNEX(errmsg.c_str());
+                                throw CKEX(errmsg.c_str());
                             }
                         }
 
@@ -204,9 +161,9 @@ namespace Fred
                         params_i.push_back(*i);
 
                         {//precheck uniqueness
-                            Database::Result nsset_res = ctx.get_conn().exec_params(
-                            "SELECT nssetid, contactid FROM nsset_contact_map "
-                            " WHERE nssetid = $1::bigint "
+                            Database::Result keyset_res = ctx.get_conn().exec_params(
+                            "SELECT keysetid, contactid FROM keyset_contact_map "
+                            " WHERE keysetid = $1::bigint "
                             "  AND contactid = raise_exception_ifnull("
                             "    (SELECT oreg.id FROM object_registry oreg "
                             "       JOIN contact c ON oreg.id = c.id "
@@ -214,12 +171,12 @@ namespace Fred
                             "     ,'|| not found:tech contact: '||ex_data($2::text)||' |')"
                             , params_i);
 
-                            if (nsset_res.size() == 1)
+                            if (keyset_res.size() == 1)
                             {
-                                std::string errmsg("set tech contact precheck uniqueness failed || already set:tech contact: ");
+                                std::string errmsg("tech contact already set || already set:tech contact: ");
                                 errmsg += boost::replace_all_copy(*i,"|", "[pipe]");//quote pipes
                                 errmsg += " |";
-                                throw CNEX(errmsg.c_str());
+                                throw CKEX(errmsg.c_str());
                             }
                         }
 
@@ -227,16 +184,17 @@ namespace Fred
                             " (SELECT oreg.id FROM object_registry oreg JOIN contact c ON oreg.id = c.id "
                             " WHERE oreg.name = UPPER($"<< params_i.size() << "::text) AND oreg.erdate IS NULL) "
                             " ,'|| not found:tech contact: '||ex_data($"<< params.size() << "::text)||' |')) "
-                            " RETURNING nssetid";
-                        Database::Result nsset_add_check_res = ctx.get_conn().exec_params(sql_i.str(), params_i);
-                        if (nsset_add_check_res.size() != 1)
+                            " RETURNING keysetid";
+
+                        Database::Result keyset_add_check_res = ctx.get_conn().exec_params(sql_i.str(), params_i);
+                        if (keyset_add_check_res.size() != 1)
                         {
-                            std::string errmsg("set tech contact failed || invalid:handle: ");
+                            std::string errmsg("add tech contact failed || invalid:handle: ");
                             errmsg += boost::replace_all_copy(handle_,"|", "[pipe]");//quote pipes
                             errmsg += " | invalid:tech contact: ";
                             errmsg += boost::replace_all_copy(*i,"|", "[pipe]");//quote pipes
                             errmsg += " |";
-                            throw CNEX(errmsg.c_str());
+                            throw CKEX(errmsg.c_str());
                         }
                     }//for i
                 }//if set tech contacts
@@ -255,7 +213,7 @@ namespace Fred
                         std::string errmsg("|| not found crdate:handle: ");
                         errmsg += boost::replace_all_copy(handle_,"|", "[pipe]");//quote pipes
                         errmsg += " |";
-                        throw CNEX(errmsg.c_str());
+                        throw CKEX(errmsg.c_str());
                     }
 
                     timestamp = boost::posix_time::time_from_string(std::string(crdate_res[0][0]));
@@ -279,40 +237,41 @@ namespace Fred
                         " WHERE id = $2::integer"
                         , Database::query_param_list(history_id)(object_id));
 
-                //nsset_history
+                //keyset_history
                 ctx.get_conn().exec_params(
-                    "INSERT INTO nsset_history(historyid,id,checklevel) "
-                    " SELECT $1::bigint, id, checklevel FROM nsset "
-                        " WHERE id = $2::integer"
+                    "INSERT INTO keyset_history(historyid,id) "
+                    " SELECT $1::bigint, id FROM keyset "
+                    " WHERE id = $2::integer"
+                    , Database::query_param_list(history_id)(object_id));
+
+                //dsrecord_history
+                ctx.get_conn().exec_params(
+                    "INSERT INTO dsrecord_history(historyid, id, keysetid, keytag, alg, digesttype, digest, maxsiglife) "
+                    " SELECT $1::bigint, id, keysetid, keytag, alg, digesttype, digest, maxsiglife FROM dsrecord "
+                    " WHERE keysetid = $2::integer"
+                    , Database::query_param_list(history_id)(object_id));
+
+                //dnskey_history
+                ctx.get_conn().exec_params(
+                    "INSERT INTO dnskey_history(historyid, id, keysetid, flags, protocol, alg, key) "
+                    " SELECT $1::bigint, id, keysetid, flags, protocol, alg, key FROM dnskey "
+                    " WHERE keysetid = $2::integer"
+                    , Database::query_param_list(history_id)(object_id));
+
+                //keyset_contact_map_history
+                ctx.get_conn().exec_params(
+                    "INSERT INTO keyset_contact_map_history(historyid,keysetid, contactid) "
+                    " SELECT $1::bigint, keysetid, contactid FROM keyset_contact_map "
+                        " WHERE keysetid = $2::integer"
                         , Database::query_param_list(history_id)(object_id));
 
-                //host_history
-                ctx.get_conn().exec_params(
-                    "INSERT INTO host_history(historyid, id, nssetid, fqdn) "
-                    " SELECT $1::bigint, id, nssetid, fqdn FROM host "
-                        " WHERE nssetid = $2::integer"
-                    , Database::query_param_list(history_id)(object_id));
-
-                //host_ipaddr_map_history
-                ctx.get_conn().exec_params(
-                    "INSERT INTO host_ipaddr_map_history(historyid, id, hostid, nssetid, ipaddr) "
-                    " SELECT $1::bigint, id, hostid, nssetid, ipaddr FROM host_ipaddr_map "
-                        " WHERE nssetid = $2::integer"
-                    , Database::query_param_list(history_id)(object_id));
-
-                //nsset_contact_map_history
-                ctx.get_conn().exec_params(
-                    "INSERT INTO nsset_contact_map_history(historyid, nssetid, contactid) "
-                    " SELECT $1::bigint, nssetid, contactid FROM nsset_contact_map "
-                        " WHERE nssetid = $2::integer"
-                    , Database::query_param_list(history_id)(object_id));
             }//save history
 
 
         }//try
         catch(...)//common exception processing
         {
-            handleOperationExceptions<CreateNssetException>(__FILE__, __LINE__, __ASSERT_FUNCTION);
+            handleOperationExceptions<CreateKeysetException>(__FILE__, __LINE__, __ASSERT_FUNCTION);
         }
 
         return timestamp;
