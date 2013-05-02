229a41d18fc6e0cee8f7c1a898ac2e35d2cc2765
Jan Zima <jan.zima@nic.cz>
2013-05-02 14:28:37 +0200

    Ticket #8135 - object_states_request lock test

--- tests/deprecated/test_locking_trigger.cc
+++ tests/deprecated/test_locking_trigger.cc
@@ -42,6 +42,8 @@
 #include "random_data_generator.h"
 #include "concurrent_queue.h"
 
+#include "src/fredlib/contact/create_contact.h"
+#include "src/fredlib/contact/info_contact_diff.h"
 #include "src/contact_verification/public_request_contact_verification_impl.h"
 
 #include "cfg/handle_general_args.h"
@@ -308,5 +310,237 @@ BOOST_FIXTURE_TEST_CASE( test_locking_trigger_threaded, Case_locking_trigger_thr
 
     BOOST_CHECK(pass_counter == 1);
 }
+
+
+struct Locking_object_state_request_fixture
+{
+    Fred::OperationContext ctx;
+    std::string registrar_handle;
+    std::string xmark;
+    std::string contact_handle;
+    unsigned long long info_contact_id;
+    Fred::InfoContactOutput info_contact;
+    std::vector<Fred::InfoContactOutput> info_contact_history;
+
+    //init
+    Locking_object_state_request_fixture()
+    : registrar_handle (static_cast<std::string>(ctx.get_conn().exec("SELECT handle FROM registrar WHERE system = TRUE ORDER BY id LIMIT 1")[0][0]))
+    , xmark(RandomDataGenerator().xnumstring(6))
+    , contact_handle(std::string("TEST-OBJECT-STATE-REQUEST-CONTACT-HANDLE")+xmark)
+    , info_contact_id(0)
+    {
+        //corba config
+        FakedArgs fa = CfgArgs::instance()->fa;
+        //conf pointers
+        HandleCorbaNameServiceArgs* ns_args_ptr=CfgArgs::instance()->
+                    get_handler_ptr_by_type<HandleCorbaNameServiceArgs>();
+        CorbaContainer::set_instance(fa.get_argc(), fa.get_argv()
+                , ns_args_ptr->nameservice_host
+                , ns_args_ptr->nameservice_port
+                , ns_args_ptr->nameservice_context);
+
+        BOOST_CHECK(!registrar_handle.empty());//expecting existing system registrar
+
+        Fred::Contact::PlaceAddress place;
+        place.street1 = std::string("STR1") + xmark;
+        place.city = "Praha";
+        place.postalcode = "11150";
+        place.country = "CZ";
+
+        Fred::CreateContact(contact_handle,registrar_handle)
+            .set_name(std::string("TEST-OBJECT-STATE-REQUEST-CONTACT NAME")+xmark)
+            .set_disclosename(true)
+            .set_place(place)
+            .set_discloseaddress(true)
+            .exec(ctx);
+
+        info_contact_id = static_cast<unsigned long long>(ctx.get_conn().exec_params(
+                "SELECT id FROM object_registry WHERE name=$1::text AND erdate IS NULL"
+                , Database::query_param_list(contact_handle))[0][0]);
+
+        BOOST_CHECK(info_contact_id);//expecting existing object
+        info_contact = Fred::InfoContact(contact_handle, registrar_handle).exec(ctx);
+        info_contact_history = Fred::InfoContactHistory(info_contact.info_contact_data.roid, registrar_handle).exec(ctx);
+        ctx.commit_transaction();
+    }
+
+    //destroy
+    virtual ~Locking_object_state_request_fixture()
+    {
+        BOOST_TEST_MESSAGE(contact_handle);
+
+    }
+
+
+};
+
+//thread functor
+class LockingObjectStateRequestThreadWorker
+{
+public:
+
+    LockingObjectStateRequestThreadWorker(unsigned number,unsigned sleep_time
+            , sync_barriers* sb_ptr
+            , Locking_object_state_request_fixture* fixture_ptr
+            , ThreadResultQueue* result_queue_ptr = 0, unsigned seed = 0)
+            : number_(number)
+            , sleep_time_(sleep_time)
+            , sb_ptr_(sb_ptr)
+            , fixture_ptr_(fixture_ptr)
+            , rdg_(seed)
+            , rsq_ptr (result_queue_ptr)
+    {}
+
+    void operator()()
+    {
+        ThreadResult res;
+        res.number = number_;
+        res.ret = 0;
+        res.desc = std::string("ok");
+
+        try
+        {
+            //db connection with transaction
+            Database::Connection conn = Database::Manager::acquire();
+            Database::Transaction tx(conn);
+
+            //std::cout << "waiting: " << number_ << std::endl;
+            if(sb_ptr_) sb_ptr_->barrier1.wait();//wait for other synced threads
+            //std::cout << "start: " << number_ << std::endl;
+
+            //call some impl
+            Fred::lock_object_state_request_lock(fixture_ptr_->info_contact_id);
+            //if(sb_ptr_) sb_ptr_->barrier2.wait();//wait for other synced threads
+
+            BOOST_TEST_MESSAGE( "states thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+            bool cic = Fred::object_has_state(fixture_ptr_->info_contact_id
+                , Fred::ObjectState::CONDITIONALLY_IDENTIFIED_CONTACT);
+            bool ic = Fred::object_has_state(fixture_ptr_->info_contact_id
+                , Fred::ObjectState::IDENTIFIED_CONTACT);
+            bool vc = Fred::object_has_state(fixture_ptr_->info_contact_id
+                , Fred::ObjectState::VALIDATED_CONTACT);
+
+            BOOST_TEST_MESSAGE( "cic: " << cic << " thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+            BOOST_TEST_MESSAGE( "ic: " << ic << " thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+            BOOST_TEST_MESSAGE( "vc: " << vc << " thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+
+            if(!cic && !ic && !vc)
+            {
+                Fred::insert_object_state(fixture_ptr_->info_contact_id, Fred::ObjectState::CONDITIONALLY_IDENTIFIED_CONTACT );
+                BOOST_TEST_MESSAGE( "set CONDITIONALLY_IDENTIFIED_CONTACT thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+                res.ret = 1;
+            }
+            else if (cic && !ic && !vc)
+            {
+                Fred::insert_object_state(fixture_ptr_->info_contact_id, Fred::ObjectState::IDENTIFIED_CONTACT );
+                BOOST_TEST_MESSAGE( "set IDENTIFIED_CONTACT thread: "<< number_ << " object_id: " << fixture_ptr_->info_contact_id);
+                res.ret = 1;
+            }
+            else if (cic && ic && !vc)
+            {
+                Fred::insert_object_state(fixture_ptr_->info_contact_id, Fred::ObjectState::VALIDATED_CONTACT );
+                BOOST_TEST_MESSAGE( "set VALIDATED_CONTACT thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+                res.ret = 1;
+            }
+
+            BOOST_TEST_MESSAGE( "sleep thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+            boost::this_thread::sleep( boost::posix_time::milliseconds(2000));
+            BOOST_TEST_MESSAGE( "update states thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+            Fred::update_object_states(fixture_ptr_->info_contact_id);
+            BOOST_TEST_MESSAGE( "commit thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+            boost::this_thread::sleep( boost::posix_time::milliseconds(2000));
+            tx.commit();
+            BOOST_TEST_MESSAGE( "end thread: " << number_ << " object_id: " << fixture_ptr_->info_contact_id);
+        }
+        catch(const std::exception& ex)
+        {
+            BOOST_TEST_MESSAGE("exception 1 in operator() thread number: " << number_
+                    << " reason: " << ex.what() );
+            res.ret = 134217728;
+            res.desc = std::string(ex.what());
+            return;
+        }
+        catch(...)
+        {
+            BOOST_TEST_MESSAGE("exception 2 in operator() thread number: " << number_ );
+            res.ret = 268435456;
+            res.desc = std::string("unknown exception");
+            return;
+        }
+
+        if(rsq_ptr) rsq_ptr->push(res);
+        //std::cout << "end: " << number_ << std::endl;
+    }
+
+private:
+    //need only defaultly constructible members here
+    unsigned    number_;//thred identification
+    unsigned    sleep_time_;//[s]
+    sync_barriers* sb_ptr_;
+    Locking_object_state_request_fixture* fixture_ptr_;
+    RandomDataGenerator rdg_;
+    ThreadResultQueue* rsq_ptr; //result queue non-owning pointer
+};//class LockingObjectStateRequestThreadWorker
+
+
+BOOST_FIXTURE_TEST_CASE( test_locking_object_state_request_threaded, Locking_object_state_request_fixture )
+{
+    HandleThreadGroupArgs* thread_args_ptr=CfgArgs::instance()->
+                   get_handler_ptr_by_type<HandleThreadGroupArgs>();
+
+    std::size_t const thread_number = thread_args_ptr->thread_number;
+    ThreadResultQueue result_queue;
+
+    //vector of thread functors
+    std::vector<LockingObjectStateRequestThreadWorker> tw_vector;
+    tw_vector.reserve(thread_number);
+
+    //synchronization barriers instance
+    sync_barriers sb(thread_number);
+
+    //thread container
+    boost::thread_group threads;
+    for (unsigned i = 0; i < thread_number; ++i)
+    {
+        tw_vector.push_back(LockingObjectStateRequestThreadWorker(i,3,&sb
+            , dynamic_cast<Locking_object_state_request_fixture*>(this)
+            , &result_queue));
+        threads.create_thread(tw_vector.at(i));
+    }
+
+    threads.join_all();
+
+    BOOST_TEST_MESSAGE( "threads end result_queue.size(): " << result_queue.size() );
+
+    unsigned long pass_counter = 0;
+    for(unsigned i = 0; i < thread_number; ++i)
+    {
+        ThreadResult thread_result;
+        if(!result_queue.try_pop(thread_result)) {
+            continue;
+        }
+
+        BOOST_TEST_MESSAGE( "result.ret: " << thread_result.ret );
+
+        if(thread_result.ret != 0)
+        {
+            if(thread_result.ret == 1)
+            {
+                ++pass_counter;
+            }
+            else
+            {
+                BOOST_FAIL( thread_result.desc
+                    << " thread number: " << thread_result.number
+                    << " return code: " << thread_result.ret
+                    << " description: " << thread_result.desc);
+            }
+        }
+    }//for i
+
+    BOOST_TEST_MESSAGE( "pass_counter: " << pass_counter );
+    BOOST_CHECK(pass_counter == 3);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END();//TestLockingTrigger
