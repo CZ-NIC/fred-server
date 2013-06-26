d285023f781ba59b34381c08f0c7dd2ca984f1ab
Jan Zima <jan.zima@nic.cz>
2013-06-26 18:28:01 +0200

    Ticket #8964 - CreateDomain ENUM support for DeleteDomain tests

--- tests/test-delete-domain.cc
+++ tests/test-delete-domain.cc
@@ -87,6 +87,57 @@ BOOST_AUTO_TEST_SUITE(TestDeleteDomain)
 
 const std::string server_name = "test-delete-domain";
 
+struct delete_enum_domain_fixture
+{
+    Fred::OperationContext fixture_ctx;
+    Fred::OperationContext ctx;
+    std::string registrar_handle;
+    std::string xmark;
+    std::string admin_contact2_handle;
+    std::string registrant_contact_handle;
+    std::string test_domain_fqdn;
+
+    delete_enum_domain_fixture()
+    :registrar_handle (static_cast<std::string>(fixture_ctx.get_conn().exec("SELECT handle FROM registrar WHERE system = TRUE ORDER BY id LIMIT 1")[0][0]))
+    , xmark(RandomDataGenerator().xnumstring(9))
+    , admin_contact2_handle(std::string("TEST-ADMIN-CONTACT3-HANDLE")+xmark)
+    , registrant_contact_handle(std::string("TEST-REGISTRANT-CONTACT-HANDLE") + xmark)
+    , test_domain_fqdn ( std::string()+xmark.at(0)+'.'+xmark.at(1)+'.'+xmark.at(2)+'.'
+                                    +xmark.at(3)+'.'+xmark.at(4)+'.'+xmark.at(5)+'.'
+                                    +xmark.at(6)+'.'+xmark.at(7)+'.'+xmark.at(8)+".0.2.4.e164.arpa")
+    {
+        BOOST_CHECK(!registrar_handle.empty());//expecting existing system registrar
+
+        Fred::CreateContact(admin_contact2_handle,registrar_handle)
+            .set_name(std::string("TEST-ADMIN-CONTACT3 NAME")+xmark)
+            .set_disclosename(true)
+            .set_street1(std::string("STR1")+xmark)
+            .set_city("Praha").set_postalcode("11150").set_country("CZ")
+            .set_discloseaddress(true)
+            .exec(fixture_ctx);
+
+        Fred::CreateContact(registrant_contact_handle,registrar_handle)
+                .set_name(std::string("TEST-REGISTRANT-CONTACT NAME")+xmark)
+                .set_disclosename(true)
+                .set_street1(std::string("STR1")+xmark)
+                .set_city("Praha").set_postalcode("11150").set_country("CZ")
+                .set_discloseaddress(true)
+                .exec(fixture_ctx);
+
+        Fred::CreateDomain(
+                test_domain_fqdn //const std::string& fqdn
+                , registrar_handle //const std::string& registrar
+                , registrant_contact_handle //registrant
+                )
+        .set_admin_contacts(Util::vector_of<std::string>(admin_contact2_handle))
+        .set_enum_validation_expiration(boost::gregorian::day_clock::day_clock::universal_day()+boost::gregorian::days(5))
+        .set_enum_publish_flag(false)
+        .exec(fixture_ctx);
+        fixture_ctx.commit_transaction();
+    }
+    ~delete_enum_domain_fixture()
+    {}
+};
 
 struct delete_domain_fixture
 {
@@ -142,6 +193,59 @@ struct delete_domain_fixture
  * calls in test shouldn't throw
  */
 BOOST_FIXTURE_TEST_CASE(delete_domain, delete_domain_fixture )
+{
+
+    Fred::InfoDomainOutput domain_info1 = Fred::InfoDomain(test_domain_fqdn, registrar_handle).exec(ctx);
+    BOOST_CHECK(domain_info1.info_domain_data.delete_time.isnull());
+
+    Fred::DeleteDomain(test_domain_fqdn).exec(ctx);
+    ctx.commit_transaction();
+
+    std::vector<Fred::InfoDomainHistoryOutput> domain_history_info1 = Fred::InfoDomainHistory(
+    domain_info1.info_domain_data.roid, registrar_handle).exec(ctx);
+
+    BOOST_CHECK(!domain_history_info1.at(0).info_domain_data.delete_time.isnull());
+
+    Fred::InfoDomainOutput domain_info1_with_change = domain_info1;
+    domain_info1_with_change.info_domain_data.delete_time = domain_history_info1.at(0).info_domain_data.delete_time;
+
+    BOOST_CHECK(domain_info1_with_change == domain_history_info1.at(0));
+
+    BOOST_CHECK(!domain_history_info1.at(0).info_domain_data.delete_time.isnull());
+
+    BOOST_CHECK(domain_history_info1.at(0).next_historyid.isnull());
+    BOOST_CHECK(!domain_history_info1.at(0).history_valid_from.is_not_a_date_time());
+    BOOST_CHECK(!domain_history_info1.at(0).history_valid_to.isnull());
+    BOOST_CHECK(domain_history_info1.at(0).history_valid_from <= domain_history_info1.at(0).history_valid_to);
+
+    BOOST_CHECK(static_cast<bool>(ctx.get_conn().exec_params(
+        "select erdate is not null from object_registry where name = $1::text"
+        , Database::query_param_list(test_domain_fqdn))[0][0]));
+
+    BOOST_CHECK(ctx.get_conn().exec_params(
+        "SELECT o.id FROM object o JOIN object_registry oreg ON o.id = oreg.id WHERE oreg.name = $1::text"
+        , Database::query_param_list(test_domain_fqdn)).size() == 0);
+
+    BOOST_CHECK(ctx.get_conn().exec_params(
+        "SELECT d.id FROM domain d JOIN object_registry oreg ON d.id = oreg.id WHERE oreg.name = $1::text"
+        , Database::query_param_list(test_domain_fqdn)).size() == 0);
+
+    BOOST_CHECK(ctx.get_conn().exec_params(
+        "SELECT dcm.contactid FROM domain_contact_map dcm JOIN object_registry oreg ON dcm.domainid = oreg.id WHERE oreg.name = $1::text"
+        , Database::query_param_list(test_domain_fqdn)).size() == 0);
+
+    BOOST_CHECK(ctx.get_conn().exec_params(
+        "SELECT ev.domainid FROM enumval ev JOIN object_registry oreg ON ev.domainid = oreg.id WHERE oreg.name = $1::text"
+        , Database::query_param_list(test_domain_fqdn)).size() == 0);
+
+}//delete_domain
+
+/**
+ * test DeleteDomain
+ * create test ENUM domain, delete test ENUM domain, check erdate of test ENUM domain is null
+ * calls in test shouldn't throw
+ */
+BOOST_FIXTURE_TEST_CASE(delete_enum_domain, delete_enum_domain_fixture )
 {
 
     Fred::InfoDomainOutput domain_info1 = Fred::InfoDomain(test_domain_fqdn, registrar_handle).exec(ctx);
