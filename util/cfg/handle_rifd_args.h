b51fd6fc9771f17ab0916335c9cef30489bd54e9
Jan Korous <jan.korous@nic.cz>
2013-07-09 17:18:15 +0200

    Ticket #1494 - Added allow_idn to rifd configuration handler

--- util/cfg/handle_rifd_args.h
+++ util/cfg/handle_rifd_args.h
@@ -47,6 +47,7 @@ public:
     unsigned rifd_session_timeout;
     unsigned rifd_session_registrar_max;
     bool rifd_epp_update_domain_keyset_clear;
+    bool allow_idn;
 
     boost::shared_ptr<po::options_description>
     get_options_description()
@@ -65,7 +66,10 @@ public:
                  "RIFD maximum number active sessions per registrar")
                 ("rifd.epp_update_domain_keyset_clear",
                  po::value<bool>()->default_value(false),
-                 "EPP command update domain will also clear keyset when changing NSSET");
+                 "EPP command update domain will also clear keyset when changing NSSET")
+                 ("rifd.allow_idn",
+                 po::value<bool>()->default_value(false),
+                 "allow IDN");
 
         return opts_descs;
     }//get_options_description
@@ -78,6 +82,7 @@ public:
         rifd_session_timeout = vm["rifd.session_timeout"].as<unsigned>();
         rifd_session_registrar_max = vm["rifd.session_registrar_max"].as<unsigned>();
         rifd_epp_update_domain_keyset_clear = vm["rifd.epp_update_domain_keyset_clear"].as<bool>();
+        allow_idn = vm["rifd.allow_idn"].as<bool>();
     }//handle
 };
 
