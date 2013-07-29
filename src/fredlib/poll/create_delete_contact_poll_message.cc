1695cb96b590f3333dfd77c752de64cbfa27eb76
Jan Zima <jan.zima@nic.cz>
2013-07-29 09:59:17 +0200

    Ticket #8703 - CreateDeleteContactPollMessage exception replaced by boost exception based exception impl

--- src/fredlib/poll/create_delete_contact_poll_message.cc
+++ src/fredlib/poll/create_delete_contact_poll_message.cc
@@ -4,9 +4,6 @@
 #include <boost/lexical_cast.hpp>
 
 
-#define MY_EXCEPTION_CLASS(DATA) CreateDeleteContactPollMessageException(__FILE__, __LINE__, __ASSERT_FUNCTION, (DATA))
-#define MY_ERROR_CLASS(DATA)     CreateDeleteContactPollMessageError(__FILE__, __LINE__, __ASSERT_FUNCTION, (DATA))
-
 namespace Fred {
 namespace Poll {
 
@@ -20,8 +17,6 @@ CreateDeleteContactPollMessage::CreateDeleteContactPollMessage(
 
 void CreateDeleteContactPollMessage::exec(Fred::OperationContext &_ctx)
 {
-    try
-    {
         Database::Result r = _ctx.get_conn().exec_params(
                 "SELECT eot.name AS object_type, r.handle as registrar_handle"
                 " FROM object_registry oreg JOIN object_history oh ON oh.id = oreg.id"
@@ -31,17 +26,11 @@ void CreateDeleteContactPollMessage::exec(Fred::OperationContext &_ctx)
                 Database::query_param_list(history_id_));
 
         if (r.size() != 1) {
-            std::string errmsg("|| not found:object history:");
-            errmsg += boost::lexical_cast<std::string>(history_id_);
-            errmsg += " |";
-            throw MY_EXCEPTION_CLASS(errmsg.c_str());
+            BOOST_THROW_EXCEPTION(Exception().set_object_history_not_found(history_id_));
         }
 
         if (static_cast<std::string>(r[0][0]) != "contact") {
-            std::string errmsg("|| not found:contact:");
-            errmsg += boost::lexical_cast<std::string>(history_id_);
-            errmsg += " |";
-            throw MY_EXCEPTION_CLASS(errmsg.c_str());
+            BOOST_THROW_EXCEPTION(Exception().set_contact_not_found(history_id_));
         }
 
         std::string sponsoring_registrar = static_cast<std::string>(r[0][1]);
@@ -50,12 +39,7 @@ void CreateDeleteContactPollMessage::exec(Fred::OperationContext &_ctx)
         _ctx.get_conn().exec_params(
                 "INSERT INTO poll_eppaction (msgid, objid) VALUES ($1::bigint, $2::bigint)",
                 Database::query_param_list(poll_msg_id)(history_id_));
-    }
-    catch (...)
-    {
-        handleOperationExceptions<CreateDeleteContactPollMessageException>(
-                __FILE__, __LINE__, __ASSERT_FUNCTION);
-    }
+
 }
 
 
