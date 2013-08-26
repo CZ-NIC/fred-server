120a6aa60eca972ffdb6f228ba5338d2e25accc4
Jan Korous <jan.korous@nic.cz>
2013-08-26 17:47:11 +0200

    Ticket #9199 - Upgraded CreateContactCheck - generating uuid handle by FAKE boost::uuid

--- src/fredlib/contact/verification/create_check.cc
+++ src/fredlib/contact/verification/create_check.cc
@@ -23,6 +23,8 @@
 
 #include <boost/assign/list_of.hpp>
 #include <boost/algorithm/string/join.hpp>
+/* TODO - FIXME - only temporary for uuid mockup */
+#include  <cstdlib>
 
 #include "fredlib/contact/verification/create_check.h"
 #include "fredlib/contact/verification/enum_check_status.h"
@@ -64,29 +66,79 @@ namespace Fred
     }
 
     std::string CreateContactCheck::exec(OperationContext& _ctx) {
-        std::string handle;
+        /* TODO - FIXME
+           temporary ugliness - waiting for boost 1.42 to use boost::uuid
+           refer to ticket #9148 for complete saga of UUID handles
+
+           after 1.42 is available just
+            - change handle generating command to lowercase
+            - change function generate to operator()
+            - include these:
+              #include <boost/uuid/uuid.hpp>            // uuid class
+              #include <boost/uuid/uuid_generators.hpp> // generators
+              #include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
+         */
+
+        struct BOOST { struct UUIDS { struct RANDOM_GENERATOR {
+            static std::string generate() {
+                std::string bytes;
+
+                // generate random 128bits = 16 bytes
+                for (int i = 0; i < 16; ++i) {
+                    bytes += static_cast<char>(rand()%256);
+                }
+                /* some specific uuid rules
+                 * http://www.cryptosys.net/pki/Uuid.c.html
+                 */
+                bytes.at(6) = static_cast<char>(0x40 | (bytes.at(6) & 0xf));
+                bytes.at(8) = static_cast<char>(0x80 | (bytes.at(8) & 0x3f));
+
+                // buffer for hex representation of one byte + terminating zero
+                char hex_rep[3];
+
+                // converting raw bytes to hex string representation
+                std::string result;
+                for (std::string::iterator it = bytes.begin(); it != bytes.end(); ++it) {
+                    sprintf(hex_rep,"%x",*it);
+                    result += hex_rep;
+                }
+
+                // hyphens for canonical form
+                result.insert(8, "-");
+                result.insert(13, "-");
+                result.insert(18, "-");
+                result.insert(23, "-");
+                return result;
+            }
+        }; }; };
+
+        /* end of temporary ugliness - please cut and replace between ASAP*/
+
+        std::string handle = boost::lexical_cast<std::string>(BOOST::UUIDS::RANDOM_GENERATOR::generate());
 
         try {
             Database::Result insert_contact_check_res = _ctx.get_conn().exec_params(
                 "INSERT INTO contact_check ( "
+                "   handle,"
                 "   contact_history_id,"
                 "   enum_contact_testsuite_id,"
                 "   enum_contact_check_status_id,"
                 "   logd_request_id"
                 ")"
                 "VALUES ("
+                "   $1::uuid,"
                 "   (SELECT o_h.historyid"
                 "       FROM object_registry AS o_r"
                 "           LEFT JOIN object_history AS o_h USING(id)"
                 "           LEFT JOIN h AS ON o_h.historyid = h.id"
-                "       WHERE o_r.name=$1::varchar"
+                "       WHERE o_r.name=$2::varchar"
                 "       AND h.next IS NULL),"
-                "   (SELECT id FROM enum_contact_testsuite WHERE name=$2::varchar),"
-                "   (SELECT id FROM enum_contact_check_status WHERE name=$3::varchar),"
-                "   $4::bigint"
-                ")"
-                "RETURNING handle;",
+                "   (SELECT id FROM enum_contact_testsuite WHERE name=$3::varchar),"
+                "   (SELECT id FROM enum_contact_check_status WHERE name=$4::varchar),"
+                "   $5::bigint"
+                ");",
                 Database::query_param_list
+                    (handle)
                     (contact_handle_)
                     (testsuite_name_)
                     (Fred::ContactCheckStatus::ENQUEUED)
@@ -96,9 +148,6 @@ namespace Fred
             if (insert_contact_check_res.size() != 1) {
                 BOOST_THROW_EXCEPTION(Fred::InternalError("contact_check creation failed"));
             }
-
-            handle = static_cast<std::string>( ( *insert_contact_check_res.begin() )["handle"] );
-
         } catch(ExceptionStack& ex) {
             ex.add_exception_stack_info( to_string() );
             throw;
