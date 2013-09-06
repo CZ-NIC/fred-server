d6ee1187ee5d2c0666b6eaf2998d09fc1c7bf66f
Jan Korous <jan.korous@nic.cz>
2013-09-06 15:30:22 +0200

    Ticket #9447 - Created Optional<T> == Nullable<T> comparison

---
+++ util/optional_nullable_equal.h
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2012  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @file
+ *  optional nullable comparison
+ */
+
+#ifndef OPTIONAL_NULLABLE_EQUAL_H_454434343413
+#define OPTIONAL_NULLABLE_EQUAL_H_454434343413
+
+#include "util/optional_value.h"
+#include "db/nullable.h"
+#include <boost/function.hpp>
+#include <boost/lambda/lambda.hpp>
+#include <boost/lambda/if.hpp>
+#include <boost/lambda/bind.hpp>
+
+template<class T>
+bool equal( const Nullable<T>& lhs,
+            const Optional<T>& rhs,
+            boost::function<bool(const T&, const T&) > comparator
+                = boost::function<bool(const T&, const T&) >(boost::lambda::if_then_else_return(boost::lambda::_1 == boost::lambda::_2, true, false)) ) {
+    if( lhs.isnull() && !rhs.isset() ) {
+        return true;
+    }
+    if( !lhs.isnull() && rhs.isset() ) {
+        return comparator(static_cast<T>(lhs), rhs.get_value());
+    }
+    return false;
+}
+
+template<class T>
+bool equal( const Optional<T>& lhs,
+            const Nullable<T>& rhs,
+            boost::function<bool(const T&, const T&) > comparator
+                = boost::function<bool(const T&, const T&) >(boost::lambda::if_then_else_return(boost::lambda::_1 == boost::lambda::_2, true, false)) ) {
+    if( rhs.isnull() && !lhs.isset() ) {
+        return true;
+    }
+    if( !rhs.isnull() && lhs.isset() ) {
+        return comparator(static_cast<T>(rhs), lhs.get_value());
+    }
+    return false;
+}
+
+#endif // #include guard end
+
