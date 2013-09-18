9ae420f91433f73dac910efe0e007998b7cfa648
Jan Zima <jan.zima@nic.cz>
2013-09-18 16:49:48 +0200

    Ticket #9487 - InfoContactData::to_string

--- src/fredlib/contact/info_contact_data.cc
+++ src/fredlib/contact/info_contact_data.cc
@@ -32,6 +32,7 @@
 #include <boost/date_time/posix_time/ptime.hpp>
 #include <boost/date_time/posix_time/time_period.hpp>
 #include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include "util/optional_value.h"
 #include "util/db/nullable.h"
@@ -328,6 +329,51 @@ namespace Fred
     {
         print_diff_ = print_diff;
     }
+
+    std::string InfoContactData::to_string() const
+    {
+        return Util::format_data_structure("InfoContactData",
+        Util::vector_of<std::pair<std::string,std::string> >
+        (std::make_pair("crhistoryid",boost::lexical_cast<std::string>(crhistoryid)))
+        (std::make_pair("historyid",boost::lexical_cast<std::string>(historyid)))
+        (std::make_pair("delete_time",delete_time.print_quoted()))
+        (std::make_pair("handle",handle))
+        (std::make_pair("roid",roid))
+        (std::make_pair("sponsoring_registrar_handle",sponsoring_registrar_handle))
+        (std::make_pair("create_registrar_handle",create_registrar_handle))
+        (std::make_pair("update_registrar_handle",update_registrar_handle.print_quoted()))
+        (std::make_pair("creation_time",boost::lexical_cast<std::string>(creation_time)))
+        (std::make_pair("update_time",update_time.print_quoted()))
+        (std::make_pair("transfer_time",transfer_time.print_quoted()))
+        (std::make_pair("authinfopw",authinfopw))
+        (std::make_pair("name",name.print_quoted()))
+        (std::make_pair("organization",organization.print_quoted()))
+        (std::make_pair("street1",street1.print_quoted()))
+        (std::make_pair("street2",street2.print_quoted()))
+        (std::make_pair("street3",street3.print_quoted()))
+        (std::make_pair("city",city.print_quoted()))
+        (std::make_pair("stateorprovince",stateorprovince.print_quoted()))
+        (std::make_pair("postalcode",postalcode.print_quoted()))
+        (std::make_pair("country",country.print_quoted()))
+        (std::make_pair("telephone",telephone.print_quoted()))
+        (std::make_pair("fax",fax.print_quoted()))
+        (std::make_pair("email",email.print_quoted()))
+        (std::make_pair("notifyemail_",notifyemail.print_quoted()))
+        (std::make_pair("vat",vat.print_quoted()))
+        (std::make_pair("ssntype",ssntype.print_quoted()))
+        (std::make_pair("ssn",ssn.print_quoted()))
+        (std::make_pair("disclosename",disclosename.print_quoted()))
+        (std::make_pair("discloseorganization",discloseorganization.print_quoted()))
+        (std::make_pair("discloseaddress",discloseaddress.print_quoted()))
+        (std::make_pair("disclosetelephone",disclosetelephone.print_quoted()))
+        (std::make_pair("disclosefax",disclosefax.print_quoted()))
+        (std::make_pair("discloseemail",discloseemail.print_quoted()))
+        (std::make_pair("disclosevat",disclosevat.print_quoted()))
+        (std::make_pair("discloseident",discloseident.print_quoted()))
+        (std::make_pair("disclosenotifyemail",disclosenotifyemail.print_quoted()))
+        );
+    }
+
 
 }//namespace Fred
 
