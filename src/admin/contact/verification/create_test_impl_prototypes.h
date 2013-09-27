e2c30f8b0c7053c743d3c7c26034379d0af0d6c6
Jan Korous <jan.korous@nic.cz>
2013-09-27 14:19:52 +0200

    Ticket #9256 - Test implementation prototypes creation

---
+++ src/admin/contact/verification/create_test_impl_prototypes.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2013  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @file
+ *  push back contact verification checks to queue up to it's at maximal length
+ */
+
+#ifndef ADMIN_CONTACT_VERIFICATION_CREATE_TEST_IMPL_PROTOTYPES_H_224556435
+#define ADMIN_CONTACT_VERIFICATION_CREATE_TEST_IMPL_PROTOTYPES_H_224556435
+
+#include <map>
+#include <string>
+#include <boost/shared_ptr.hpp>
+
+#include "admin/contact/verification/test_impl/test_interface.h"
+
+namespace Admin {
+
+    /**
+     * @return mapping of testnames to test implementation instances
+     */
+    std::map<std::string, boost::shared_ptr<Admin::ContactVerificationTest> > create_test_impl_prototypes(void);
+
+}
+
+
+#endif // #include guard end
