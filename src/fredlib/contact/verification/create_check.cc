dcebc5e06018576bacb9bc0dd6aff59d946ffc0a
Jan Korous <jan.korous@nic.cz>
2014-01-31 11:19:14 +0100

    Ticket #9469 - Replaced temporary UUID generator by boost::uuid

--- src/fredlib/contact/verification/create_check.cc
+++ src/fredlib/contact/verification/create_check.cc
@@ -29,8 +29,9 @@
 
 #include <boost/assign/list_of.hpp>
 #include <boost/algorithm/string/join.hpp>
-/* TODO - FIXME - only temporary for uuid mockup */
-#include  <cstdlib>
+#include <boost/uuid/uuid.hpp>            // uuid class
+#include <boost/uuid/uuid_generators.hpp> // generators
+#include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
 
 namespace Fred
 {
@@ -64,59 +65,7 @@ namespace Fred
     }
 
     std::string CreateContactCheck::exec(OperationContext& _ctx) {
-        /* TODO - FIXME
-           temporary ugliness - waiting for boost 1.42 to use boost::uuid
-           refer to ticket #9148 for complete saga of UUID handles
-
-           after 1.42 is available just
-            - change handle generating command to lowercase
-            - change function generate to operator()
-            - include these:
-              #include <boost/uuid/uuid.hpp>            // uuid class
-              #include <boost/uuid/uuid_generators.hpp> // generators
-              #include <boost/uuid/uuid_io.hpp>         // streaming operators etc.
-         */
-
-        struct BOOST { struct UUIDS { struct RANDOM_GENERATOR {
-            static std::string generate() {
-                srand(time(NULL));
-                std::vector<unsigned char> bytes;
-
-                // generate random 128bits = 16 bytes
-                for (int i = 0; i < 16; ++i) {
-                    bytes.push_back( RandomDataGenerator().xletter()%256 );
-                }
-                /* some specific uuid rules
-                 * http://www.cryptosys.net/pki/Uuid.c.html
-                 */
-                bytes.at(6) = static_cast<char>(0x40 | (bytes.at(6) & 0xf));
-                bytes.at(8) = static_cast<char>(0x80 | (bytes.at(8) & 0x3f));
-
-                // buffer for hex representation of one byte + terminating zero
-                char hex_rep[3];
-
-                // converting raw bytes to hex string representation
-                std::string result;
-                for (std::vector<unsigned char>::iterator it = bytes.begin(); it != bytes.end(); ++it) {
-                    sprintf(hex_rep,"%02x",*it);
-                    // conversion target is hhhh - so in case it gets wrong just cut off the tail
-                    hex_rep[2] = 0;
-                    result += hex_rep;
-                }
-
-                // hyphens for canonical form
-                result.insert(8, "-");
-                result.insert(13, "-");
-                result.insert(18, "-");
-                result.insert(23, "-");
-
-                return result;
-            }
-        }; }; };
-
-        /* end of temporary ugliness - please cut and replace between ASAP*/
-
-        Fred::OperationContext ctx_unique;
+         Fred::OperationContext ctx_unique;
         std::string unique_test_query =
                 "SELECT handle "
                 "   FROM contact_check "
@@ -124,7 +73,7 @@ namespace Fred
         // generate handle over and over until it is unique
         std::string handle;
         do {
-            handle = boost::lexical_cast<std::string>(BOOST::UUIDS::RANDOM_GENERATOR::generate());
+            handle = boost::lexical_cast<std::string>(boost::uuids::random_generator()());
         } while(
             ctx_unique.get_conn().exec_params(
                 unique_test_query,
