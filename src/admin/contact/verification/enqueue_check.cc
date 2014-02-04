d80fc2f89b93db7c8a1cec3be422f81124c5c3af
Jan Korous <jan.korous@nic.cz>
2014-02-04 14:17:17 +0100

    Ticket #9256 - Implemented enqueue

---
+++ src/admin/contact/verification/enqueue_check.cc
@@ -0,0 +1,57 @@
+#include "src/admin/contact/verification/enqueue_check.h"
+#include "src/admin/contact/verification/resolve_check.h"
+#include "src/fredlib/contact/verification/create_check.h"
+#include "src/fredlib/contact/verification/info_check.h"
+#include "src/fredlib/contact/verification/enum_check_status.h"
+
+#include "src/fredlib/db_settings.h"
+
+namespace  Admin {
+    std::string enqueue_check(
+        Fred::OperationContext& _ctx,
+        long long               _contact_id,
+        const std::string&      _testsuite_handle,
+        Optional<long long>     _logd_request_id
+    ) {
+        std::string created_handle = Fred::CreateContactCheck(
+            _contact_id,
+            _testsuite_handle,
+            _logd_request_id
+        ).exec(_ctx);
+
+        Fred::InfoContactCheckOutput info = Fred::InfoContactCheck(
+            created_handle
+        ).exec(_ctx);
+
+        Database::Result obsolete_handles_res = _ctx.get_conn().exec_params(
+            "SELECT c_ch.handle as handle_ "
+            "   FROM contact_check AS c_ch "
+            "       JOIN enum_contact_check_status AS enum_status ON c_ch.enum_contact_check_status_id = enum_status.id "
+            "       JOIN enum_contact_testsuite AS enum_testsuite ON c_ch.enum_contact_testsuite_id = enum_testsuite.id "
+            "       JOIN contact_history AS c_h1 ON c_ch.contact_history_id = c_h1.historyid "
+            "       JOIN contact_history AS c_h2 ON c_h1.id = c_h2.id "
+            "   WHERE enum_status.handle = $1::varchar "
+            "       AND enum_testsuite.handle = $2::varchar "
+            "       AND c_h2.historyid = $3::integer "
+            "       AND c_ch.handle != $4::uuid ",
+            Database::query_param_list
+                (Fred::ContactCheckStatus::ENQUEUED)
+                (_testsuite_handle)
+                (info.contact_history_id)
+                (created_handle)
+        );
+
+        for(Database::Result::Iterator it = obsolete_handles_res.begin();
+            it != obsolete_handles_res.end();
+            ++it
+        ) {
+            resolve_check(
+                static_cast<std::string>( (*it)["handle_"]),
+                Fred::ContactCheckStatus::INVALIDATED,
+                _logd_request_id
+            ).exec(_ctx);
+        }
+
+        return created_handle;
+    }
+}
---
+++ src/admin/contact/verification/enqueue_check.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2013  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @file
+ *  enqueue specified check (and invalidates checks obsoleted by it)
+ */
+
+#ifndef ADMIN_CONTACT_VERIFICATION_ENQUEUE_CHECK_H_32421015464
+#define ADMIN_CONTACT_VERIFICATION_ENQUEUE_CHECK_H_32421015464
+
+#include "src/fredlib/contact/verification/create_check.h"
+
+#include "src/fredlib/opcontext.h"
+#include "util/optional_value.h"
+
+namespace Admin {
+
+    /**
+     * @throw Fred::CreateContactCheck::ExceptionUnknownContactId
+     * @throw Fred::CreateContactCheck::ExceptionUnknownTestsuiteHandle
+     */
+
+    std::string enqueue_check(
+        Fred::OperationContext& _ctx,
+        long long               _contact_id,
+        const std::string&      _testsuite_handle,
+        Optional<long long>     _logd_request_id = Optional<long long>());
+}
+
+
+#endif // #include guard end
