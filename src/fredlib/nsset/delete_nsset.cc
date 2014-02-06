d10335ef872b1cba9c48b67775cd24914fd1dee9
Jan Korous <jan.korous@nic.cz>
2014-02-06 14:00:48 +0100

    Ticket #10501 - Implemented DeleteNssetBy...

--- src/fredlib/nsset/delete_nsset.cc
+++ src/fredlib/nsset/delete_nsset.cc
@@ -22,6 +22,8 @@
  */
 
 #include <string>
+#include <boost/assign.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include "src/fredlib/nsset/delete_nsset.h"
 #include "src/fredlib/object/object.h"
@@ -32,69 +34,104 @@
 
 namespace Fred
 {
-    DeleteNsset::DeleteNsset(const std::string& handle)
+    static void delete_nsset_impl(OperationContext& ctx, unsigned long long id) {
+        ctx.get_conn().exec_params(
+            "DELETE FROM nsset_contact_map "
+            "   WHERE nssetid = $1::integer",
+            Database::query_param_list(id));
+
+        ctx.get_conn().exec_params(
+            "DELETE FROM host_ipaddr_map "
+            "   WHERE nssetid = $1::integer",
+            Database::query_param_list(id));
+
+        ctx.get_conn().exec_params(
+            "DELETE FROM host "
+            "   WHERE nssetid = $1::integer",
+            Database::query_param_list(id));
+
+        Database::Result delete_nsset_res = ctx.get_conn().exec_params(
+            "DELETE FROM nsset "
+            "   WHERE id = $1::integer RETURNING id",
+            Database::query_param_list(id));
+        if (delete_nsset_res.size() != 1) {
+            BOOST_THROW_EXCEPTION(Fred::InternalError("delete nsset failed"));
+        }
+    }
+
+    DeleteNssetByHandle::DeleteNssetByHandle(const std::string& handle)
     : handle_(handle)
     {}
 
-    void DeleteNsset::exec(OperationContext& ctx)
+    void DeleteNssetByHandle::exec(OperationContext& ctx)
     {
         try
         {
-            //lock object_registry row for update and get nsset_id
-            unsigned long long nsset_id = get_object_id_by_handle_and_type_with_lock(
-                    ctx,handle_,"nsset",static_cast<Exception*>(0),
-                    &Exception::set_unknown_nsset_handle);
+            unsigned long long nsset_id = lock_object_by_handle_and_type(
+                ctx,
+                handle_,
+                "nsset",
+                static_cast<Exception*>(NULL),
+                &Exception::set_unknown_nsset_handle);
 
-            //check if object is linked
-            Database::Result linked_result = ctx.get_conn().exec_params(
-                "SELECT * FROM object_state os "
-                " JOIN enum_object_states eos ON eos.id = os.state_id "
-                " WHERE os.object_id = $1::integer AND eos.name = $2::text "
-                " AND valid_to IS NULL",
-                Database::query_param_list
-                (nsset_id)
-                (Fred::ObjectState::LINKED));
-
-            if (linked_result.size() > 0)
-            {
+            if (is_object_linked(ctx, nsset_id)) {
                 BOOST_THROW_EXCEPTION(Exception().set_object_linked_to_nsset_handle(handle_));
             }
 
-            ctx.get_conn().exec_params("DELETE FROM nsset_contact_map WHERE nssetid = $1::integer"
-                , Database::query_param_list(nsset_id));
-
-            ctx.get_conn().exec_params("DELETE FROM host_ipaddr_map WHERE nssetid = $1::integer"
-                , Database::query_param_list(nsset_id));
-
-
-            ctx.get_conn().exec_params("DELETE FROM host WHERE nssetid = $1::integer"
-                , Database::query_param_list(nsset_id));
-
-            Database::Result delete_nsset_res = ctx.get_conn().exec_params(
-                "DELETE FROM nsset WHERE id = $1::integer RETURNING id"
-                    , Database::query_param_list(nsset_id));
-            if (delete_nsset_res.size() != 1)
-            {
-                BOOST_THROW_EXCEPTION(Fred::InternalError("delete nsset failed"));
-            }
+            delete_nsset_impl(ctx, nsset_id);
 
             Fred::DeleteObjectByHandle(handle_,"nsset").exec(ctx);
-        }//try
-        catch(ExceptionStack& ex)
-        {
+
+        } catch(ExceptionStack& ex) {
             ex.add_exception_stack_info(to_string());
             throw;
         }
-
-    }//DeleteNsset::exec
-
-    std::string DeleteNsset::to_string() const
-    {
-        return Util::format_operation_state("DeleteNsset",
-        Util::vector_of<std::pair<std::string,std::string> >
-        (std::make_pair("handle",handle_)));
     }
 
+    std::string DeleteNssetByHandle::to_string() const
+    {
+        return Util::format_operation_state(
+            "DeleteNssetByHandle",
+            boost::assign::list_of
+              (std::make_pair("handle", handle_ ))
+        );
+    }
 
+    DeleteNssetById::DeleteNssetById(unsigned long long id)
+    : id_(id)
+    {}
+
+    void DeleteNssetById::exec(OperationContext& ctx)
+    {
+        try
+        {
+            unsigned long long nsset_id = lock_object_by_id(
+                ctx,
+                id_,
+                static_cast<Exception*>(NULL),
+                &Exception::set_unknown_nsset_id);
+
+            if (is_object_linked(ctx, id_)) {
+                BOOST_THROW_EXCEPTION(Exception().set_object_linked_to_nsset_id(id_));
+            }
+
+            delete_nsset_impl(ctx, id_);
+
+            Fred::DeleteObjectById(id_).exec(ctx);
+
+        } catch(ExceptionStack& ex) {
+            ex.add_exception_stack_info(to_string());
+            throw;
+        }
+    }
+
+    std::string DeleteNssetById::to_string() const
+    {
+        return Util::format_operation_state(
+            "DeleteNssetById",
+            boost::assign::list_of
+              (std::make_pair("id", boost::lexical_cast<std::string>(id_) ))
+        );
+    }
 }//namespace Fred
 
