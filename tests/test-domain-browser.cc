bae2c70ca575ca1fa83fe530d54054f6aa1bf658
Jan Zima <jan.zima@nic.cz>
2014-02-11 14:38:33 +0100

    Ticket #10487 - exception tests

--- tests/test-domain-browser.cc
+++ tests/test-domain-browser.cc
@@ -182,7 +182,7 @@ BOOST_FIXTURE_TEST_CASE(get_registrar_detail_no_user, get_registrar_detail_no_us
         Fred::InfoRegistrarOutput registrar_info = Fred::InfoRegistrarByHandle(test_registrar_handle).exec(ctx);
         Registry::DomainBrowserImpl::DomainBrowser impl(server_name);
         Registry::DomainBrowserImpl::RegistrarDetail rd = impl.getRegistrarDetail(0, test_registrar_handle);
-        BOOST_ERROR("unreported mojeidContact state");
+        BOOST_ERROR("unreported missing user contact");
     }
     catch( const Registry::DomainBrowserImpl::UserNotExists& ex)
     {
@@ -213,6 +213,30 @@ BOOST_FIXTURE_TEST_CASE(get_registrar_detail_not_mojeid_user, get_registrar_deta
         BOOST_ERROR("unreported mojeidContact state");
     }
     catch( const Registry::DomainBrowserImpl::UserNotExists& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+struct get_registrar_detail_no_registrar_fixture
+: mojeid_user_contact_fixture
+{};
+
+/**
+ * test getRegistrarDetail no registrar
+*/
+BOOST_FIXTURE_TEST_CASE(get_registrar_detail_no_registrar, get_registrar_detail_no_registrar_fixture )
+{
+    try
+    {
+        Fred::OperationContext ctx;
+        Registry::DomainBrowserImpl::DomainBrowser impl(server_name);
+        Registry::DomainBrowserImpl::RegistrarDetail rd = impl.getRegistrarDetail(user_contact_info.info_contact_data.id,
+                "NO-NO-REGISTRAR-HANDLE");
+        BOOST_ERROR("unreported missing registrar");
+    }
+    catch( const Registry::DomainBrowserImpl::ObjectNotExists& ex)
     {
         BOOST_CHECK(true);
         BOOST_MESSAGE(boost::diagnostic_information(ex));
@@ -370,6 +394,81 @@ BOOST_FIXTURE_TEST_CASE(get_contact_detail, get_contact_fixture )
     BOOST_MESSAGE(cd.states);
     BOOST_MESSAGE(cd.state_codes);
 }
+
+struct get_contact_detail_no_user_fixture
+: test_contact_fixture
+{};
+
+/**
+ * test getContactDetail no user contact
+*/
+BOOST_FIXTURE_TEST_CASE(get_contact_detail_no_user, get_contact_detail_no_user_fixture )
+{
+    try
+    {
+        Fred::OperationContext ctx;
+        Fred::InfoContactOutput test_contact_info = Fred::InfoContactByHandle(test_contact_handle).exec(ctx);
+        Registry::DomainBrowserImpl::DomainBrowser impl(server_name);
+        Registry::DomainBrowserImpl::ContactDetail cd = impl.getContactDetail(0,test_contact_info.info_contact_data.id, "CS");
+
+        BOOST_ERROR("unreported missing user contact");
+    }
+    catch( const Registry::DomainBrowserImpl::UserNotExists& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+struct get_contact_detail_not_mojeid_user_fixture
+: user_contact_fixture
+  , test_contact_fixture
+{};
+
+/**
+ * test getContactDetail not mojeid user contact
+ */
+BOOST_FIXTURE_TEST_CASE(get_contact_detail_not_mojeid_user, get_contact_detail_not_mojeid_user_fixture )
+{
+    try
+    {
+        Fred::OperationContext ctx;
+        Fred::InfoContactOutput test_contact_info = Fred::InfoContactByHandle(test_contact_handle).exec(ctx);
+        Registry::DomainBrowserImpl::DomainBrowser impl(server_name);
+        Registry::DomainBrowserImpl::ContactDetail cd = impl.getContactDetail(user_contact_info.info_contact_data.id,
+                test_contact_info.info_contact_data.id, "CS");
+        BOOST_ERROR("unreported mojeidContact state");
+    }
+    catch( const Registry::DomainBrowserImpl::UserNotExists& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+struct get_contact_detail_no_test_contact_fixture
+: mojeid_user_contact_fixture
+{};
+
+/**
+ * test getContactDetail not mojeid user contact
+ */
+BOOST_FIXTURE_TEST_CASE(get_contact_detail_no_test_contact, get_contact_detail_no_test_contact_fixture )
+{
+    try
+    {
+        Fred::OperationContext ctx;
+        Registry::DomainBrowserImpl::DomainBrowser impl(server_name);
+        Registry::DomainBrowserImpl::ContactDetail cd = impl.getContactDetail(user_contact_info.info_contact_data.id,0, "CS");
+        BOOST_ERROR("unreported missing test contact");
+    }
+    catch( const Registry::DomainBrowserImpl::ObjectNotExists& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
 
 BOOST_AUTO_TEST_SUITE_END();//getContactDetail
 
