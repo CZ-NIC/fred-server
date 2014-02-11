e3fba984f4b95b599eda3ab2e16c4844f37b7a88
Jan Korous <jan.korous@nic.cz>
2014-02-11 11:33:34 +0100

    Ticket #10535 - Refactored creation of poll messages in fredlib

--- src/fredlib/poll/create_delete_contact_poll_message.h
+++ src/fredlib/poll/create_delete_contact_poll_message.h
@@ -15,26 +15,25 @@ class CreateDeleteContactPollMessage : public Util::Printable
 public:
     typedef unsigned long long ObjectHistoryId;
 
-    CreateDeleteContactPollMessage(const ObjectHistoryId &_history_id);
-
-    void exec(Fred::OperationContext &_ctx);
-
     DECLARE_EXCEPTION_DATA(contact_not_found, unsigned long long);
     DECLARE_EXCEPTION_DATA(object_history_not_found, unsigned long long);
     struct Exception
-    : virtual Fred::OperationException
-    , ExceptionData_contact_not_found<Exception>
-    , ExceptionData_object_history_not_found<Exception>
-    {};
+    :
+        virtual Fred::OperationException,
+        ExceptionData_contact_not_found<Exception>,
+        ExceptionData_object_history_not_found<Exception>
+    { };
 
-    /**
-    * Dumps state of the instance into the string
-    * @return string with description of the instance state
-    */
+    CreateDeleteContactPollMessage(const ObjectHistoryId &_history_id);
+
+    unsigned long long exec(Fred::OperationContext &_ctx);
+
     std::string to_string() const;
 
 private:
     ObjectHistoryId history_id_;
+
+    static std::string message_type_handle() { return "delete_contact"; }
 };
 
 }
