8a8d21f02e101a8b13f7be6aa570c1f6a598655d
Jan Korous <jan.korous@nic.cz>
2014-02-14 11:05:13 +0100

    Ticket #9583 - Implemented test_Invalidating_old_checks

--- tests/admin/contact/verification/test-enqueue_check.cc
+++ tests/admin/contact/verification/test-enqueue_check.cc
@@ -71,5 +71,61 @@ BOOST_AUTO_TEST_CASE(test_Enqueued_check_data)
 
 }
 
+/**
+testing whether old enqueud checks are correctly invalidated
+@pre existing contact with enqueued checks
+@post correct data set to previous check
+*/
+BOOST_AUTO_TEST_CASE(test_Invalidating_old_checks)
+{
+    setup_testsuite testsuite;
+    setup_contact contact;
+
+    Fred::OperationContext ctx;
+
+    std::string invalidated_check_handle_1 = Fred::CreateContactCheck(
+        contact.contact_id_,
+        testsuite.testsuite_handle
+    ).exec(ctx);
+
+    std::string invalidated_check_handle_2 = Fred::CreateContactCheck(
+        contact.contact_id_,
+        testsuite.testsuite_handle
+    ).exec(ctx);
+
+    std::string new_check_handle = Admin::enqueue_check(
+        ctx,
+        contact.contact_id_,
+        testsuite.testsuite_handle,
+        RandomDataGenerator().xuint()
+    );
+
+    Fred::InfoContactCheckOutput invalidated_check_info_1 = Fred::InfoContactCheck(invalidated_check_handle_1)
+        .exec(ctx);
+
+    Fred::InfoContactCheckOutput invalidated_check_info_2 = Fred::InfoContactCheck(invalidated_check_handle_2)
+        .exec(ctx);
+
+    Fred::InfoContactCheckOutput new_check_info = Fred::InfoContactCheck(new_check_handle)
+        .exec(ctx);
+
+    BOOST_CHECK_EQUAL(
+        invalidated_check_info_1.check_state_history.rbegin()->status_handle,
+        Fred::ContactCheckStatus::INVALIDATED);
+
+    BOOST_CHECK_EQUAL(
+        invalidated_check_info_2.check_state_history.rbegin()->status_handle,
+        Fred::ContactCheckStatus::INVALIDATED);
+
+    BOOST_CHECK_EQUAL(
+        invalidated_check_info_1.check_state_history.rbegin()->logd_request_id,
+        new_check_info.check_state_history.rbegin()->logd_request_id);
+
+    BOOST_CHECK_EQUAL(
+        invalidated_check_info_2.check_state_history.rbegin()->logd_request_id,
+        new_check_info.check_state_history.rbegin()->logd_request_id);
+
+}
+
 BOOST_AUTO_TEST_SUITE_END();
 BOOST_AUTO_TEST_SUITE_END();
