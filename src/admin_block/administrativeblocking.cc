b3876b8e14885ef04548facee35fcab92b0462a9
Jiri Sadek <jiri.sadek@nic.cz>
2014-05-22 14:42:09 +0200

    Ticket #11126 - create poll message when updating domain during blocking (registrant contact copy)

--- src/admin_block/administrativeblocking.cc
+++ src/admin_block/administrativeblocking.cc
@@ -440,7 +440,9 @@ namespace Registry
                                 if (0 < _log_req_id) {
                                     update_domain.set_logd_request_id(_log_req_id);
                                 }
-                                update_domain.exec(ctx);
+                                unsigned long long new_hid = update_domain.exec(ctx);
+                                /* in case of error when creating poll message we fail with internal server error */
+                                Fred::Poll::CreateUpdateObjectPollMessage(new_hid).exec(ctx);
                             }
                             result.push_back(result_item);
                         }
