7cf78bbbee343ef9c038a314af8b245e0a93c034
Jan Zima <jan.zima@nic.cz>
2014-06-03 14:44:20 +0200

    Ticket #10104 - setObjectBlockStatus doc comment

--- src/domain_browser/domain_browser.h
+++ src/domain_browser/domain_browser.h
@@ -529,8 +529,8 @@ namespace Registry
              * @param objtype type of the objects to be set
              * @param object_id list of database ids of the objects to be set
              * @param block_type is type of blocking to be applied (value from enum Registry::DomainBrowser::ObjectBlockType)
-             * @param blocked_objects is list of blocked object handles
-             * @return false if blocking were set, true if not or exception in case of failure
+             * @param blocked_objects is list of object handles of objects with SERVER_BLOCKED state
+             * @return false if all objects have SERVER_BLOCKED state (no block_type applied), true if not or exception in case of failure
              */
             bool setObjectBlockStatus(unsigned long long user_contact_id,
                 const std::string& objtype,
