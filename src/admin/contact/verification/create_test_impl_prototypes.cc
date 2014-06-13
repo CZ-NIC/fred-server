55e615b073fa5efed71f25c3f6fae6811cbec508
Jan Korous <jan.korous@nic.cz>
2014-06-13 11:03:57 +0200

    Ticket #9147 - Safely handling factory created test implementation prototypes

--- src/admin/contact/verification/create_test_impl_prototypes.cc
+++ src/admin/contact/verification/create_test_impl_prototypes.cc
@@ -6,6 +6,7 @@
 #include "src/admin/contact/verification/test_impl/test_contactability.h"
 #include "src/admin/contact/verification/test_impl/test_email_exists.h"
 #include "src/admin/contact/verification/test_impl/test_send_letter.h"
+#include "src/admin/contact/verification/exceptions.h"
 
 #include "util/log/context.h"
 
@@ -23,51 +24,73 @@ namespace  Admin {
 
         std::map< std::string, boost::shared_ptr<ContactVerification::Test> > result;
 
-        result[ContactVerification::TestNameSyntax::registration_name()] =
-            ContactVerification::test_factory::instance_ref()
-                .create_sh_ptr(ContactVerification::TestNameSyntax::registration_name());
-
-        result[ContactVerification::TestEmailSyntax::registration_name()] =
-            ContactVerification::test_factory::instance_ref()
-                .create_sh_ptr(ContactVerification::TestEmailSyntax::registration_name());
-
-        result[ContactVerification::TestPhoneSyntax::registration_name()] =
-            ContactVerification::test_factory::instance_ref()
-                .create_sh_ptr(ContactVerification::TestPhoneSyntax::registration_name());
-
-        result[ContactVerification::TestCzAddress::registration_name()] =
+        try {
+            result[ContactVerification::TestNameSyntax::registration_name()] =
                 ContactVerification::test_factory::instance_ref()
-                    .create_sh_ptr(ContactVerification::TestCzAddress::registration_name());
+                    .create_sh_ptr(ContactVerification::TestNameSyntax::registration_name());
 
-        // no check of cast result is needed - same type is used for cast as for object selection
-        dynamic_cast<ContactVerification::TestCzAddress *>(
-            result[ContactVerification::TestCzAddress::registration_name()].get()
-        )->set_mvcr_address_xml_filename(_cz_address_dataset_path);
+            result[ContactVerification::TestEmailSyntax::registration_name()] =
+                ContactVerification::test_factory::instance_ref()
+                    .create_sh_ptr(ContactVerification::TestEmailSyntax::registration_name());
 
-        result[ContactVerification::TestContactability::registration_name()] =
-            ContactVerification::test_factory::instance_ref()
-                .create_sh_ptr(ContactVerification::TestContactability::registration_name());
+            result[ContactVerification::TestPhoneSyntax::registration_name()] =
+                ContactVerification::test_factory::instance_ref()
+                    .create_sh_ptr(ContactVerification::TestPhoneSyntax::registration_name());
 
-        // no check of cast result is needed - same type is used for cast as for object selection
-        dynamic_cast<ContactVerification::TestContactability *>(
-            result[ContactVerification::TestContactability::registration_name()].get()
-        )->set_document_file_manager(_document_manager)
-        .set_email_manager(_mailer_manager)
-        .set_letter_manager(_message_manager );
+            {
+                result[ContactVerification::TestCzAddress::registration_name()] =
+                    ContactVerification::test_factory::instance_ref()
+                        .create_sh_ptr(ContactVerification::TestCzAddress::registration_name());
 
-        result[ContactVerification::TestEmailExists::registration_name()] =
-            ContactVerification::test_factory::instance_ref()
-                .create_sh_ptr(ContactVerification::TestEmailExists::registration_name());
+                ContactVerification::TestCzAddress * instance =
+                    dynamic_cast<ContactVerification::TestCzAddress *>(
+                        result[ContactVerification::TestCzAddress::registration_name()].get() );
 
-        result[ContactVerification::TestSendLetter::registration_name()] =
+                if(instance == NULL) {
+                    throw Admin::ExceptionTestImplementationError();
+                }
+                instance->set_mvcr_address_xml_filename(_cz_address_dataset_path);
+            }
+
+            {
+                result[ContactVerification::TestContactability::registration_name()] =
+                    ContactVerification::test_factory::instance_ref()
+                        .create_sh_ptr(ContactVerification::TestContactability::registration_name());
+
+                ContactVerification::TestContactability * instance =
+                    dynamic_cast<ContactVerification::TestContactability *>(
+                        result[ContactVerification::TestContactability::registration_name()].get() );
+
+                if(instance == NULL) {
+                    throw Admin::ExceptionTestImplementationError();
+                }
+                instance->set_document_file_manager(_document_manager)
+                    .set_email_manager(_mailer_manager)
+                    .set_letter_manager(_message_manager );
+            }
+
+            result[ContactVerification::TestEmailExists::registration_name()] =
+                ContactVerification::test_factory::instance_ref()
+                    .create_sh_ptr(ContactVerification::TestEmailExists::registration_name());
+
+            {
+                result[ContactVerification::TestSendLetter::registration_name()] =
                     ContactVerification::test_factory::instance_ref()
                         .create_sh_ptr(ContactVerification::TestSendLetter::registration_name());
 
-        // no check of cast result is needed - same type is used for cast as for object selection
-        dynamic_cast<ContactVerification::TestSendLetter *>(
-            result[ContactVerification::TestSendLetter::registration_name()].get()
-        )->set_document_file_manager(_document_manager)
-        .set_letter_manager(_message_manager );
+                ContactVerification::TestSendLetter * instance =
+                    dynamic_cast<ContactVerification::TestSendLetter *>(
+                        result[ContactVerification::TestSendLetter::registration_name()].get() );
+
+                if(instance == NULL) {
+                    throw Admin::ExceptionTestImplementationError();
+                }
+                instance->set_document_file_manager(_document_manager)
+                    .set_letter_manager(_message_manager );
+            }
+        } catch (const std::bad_cast& ) {
+            throw Admin::ExceptionTestImplementationError();
+        }
 
         return result;
     }
