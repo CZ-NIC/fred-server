5b2950f4ca0b69cdba9aa03e6b700481761663cc
Jiri Sadek <jiri.sadek@nic.cz>
2014-06-30 17:13:00 +0200

    Ticket #10710 - test for saving request_id in disclose flags change method

--- tests/test-domain-browser.cc
+++ tests/test-domain-browser.cc
@@ -958,7 +958,7 @@ BOOST_FIXTURE_TEST_CASE(set_contact_disclose_flags, set_contact_disclose_flags_f
     set_flags.notify_email = true;
     set_flags.telephone = true;
     set_flags.vat = true;
-    impl.setContactDiscloseFlags(user_contact_info.info_contact_data.id,set_flags, 0);
+    impl.setContactDiscloseFlags(user_contact_info.info_contact_data.id,set_flags, 42);
 
     Fred::InfoContactOutput my_contact_info = Fred::InfoContactByHandle(user_contact_handle).exec(ctx);
     BOOST_CHECK(!my_contact_info.info_contact_data.disclosename.get_value_or_default());
@@ -970,6 +970,7 @@ BOOST_FIXTURE_TEST_CASE(set_contact_disclose_flags, set_contact_disclose_flags_f
     BOOST_CHECK(my_contact_info.info_contact_data.discloseident.get_value_or_default());
     BOOST_CHECK(my_contact_info.info_contact_data.disclosevat.get_value_or_default());
     BOOST_CHECK(my_contact_info.info_contact_data.disclosenotifyemail.get_value_or_default());
+    BOOST_CHECK(!my_contact_info.logd_request_id.isnull() && my_contact_info.logd_request_id.get_value() == 42);
 }
 
 
