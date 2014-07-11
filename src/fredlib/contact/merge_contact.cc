fbcf900aba161a559473ed545053d19a5442c46c
Jan Zima <jan.zima@nic.cz>
2014-07-11 16:51:06 +0200

    Ticket #11395 - abort merge if any object have state serverUpdateProhibited or domain have serverBlocked state

--- src/fredlib/contact/merge_contact.cc
+++ src/fredlib/contact/merge_contact.cc
@@ -168,7 +168,8 @@ namespace Fred
                 tmp.set_registrant = dst_contact_handle_;
 
                 //check if object blocked
-                if(Fred::ObjectHasState(tmp.domain_id,Fred::ObjectState::SERVER_BLOCKED).exec(ctx))
+                if(Fred::ObjectHasState(tmp.domain_id,Fred::ObjectState::SERVER_UPDATE_PROHIBITED).exec(ctx)
+                    || Fred::ObjectHasState(tmp.domain_id,Fred::ObjectState::SERVER_BLOCKED).exec(ctx))
                 {
                     BOOST_THROW_EXCEPTION(Fred::MergeContact::Exception().set_object_blocked(tmp.fqdn));
                 }
@@ -210,7 +211,8 @@ namespace Fred
                 tmp.add_admin_contact = dst_contact_handle_;
 
                 //check if object blocked
-                if(Fred::ObjectHasState(tmp.domain_id,Fred::ObjectState::SERVER_BLOCKED).exec(ctx))
+                if(Fred::ObjectHasState(tmp.domain_id,Fred::ObjectState::SERVER_UPDATE_PROHIBITED).exec(ctx)
+                    || Fred::ObjectHasState(tmp.domain_id,Fred::ObjectState::SERVER_BLOCKED).exec(ctx))
                 {
                     BOOST_THROW_EXCEPTION(Fred::MergeContact::Exception().set_object_blocked(tmp.fqdn));
                 }
@@ -275,6 +277,12 @@ namespace Fred
                 tmp.rem_tech_contact = src_contact_handle_;
                 tmp.add_tech_contact = dst_contact_handle_;
 
+                //check if object blocked
+                if(Fred::ObjectHasState(tmp.nsset_id,Fred::ObjectState::SERVER_UPDATE_PROHIBITED).exec(ctx))
+                {
+                    BOOST_THROW_EXCEPTION(Fred::MergeContact::Exception().set_object_blocked(tmp.handle));
+                }
+
                 if(!dry_run)
                 {
                     try
@@ -334,6 +342,12 @@ namespace Fred
                 tmp.sponsoring_registrar = std::string(result[i][1]);
                 tmp.rem_tech_contact = src_contact_handle_;
                 tmp.add_tech_contact = dst_contact_handle_;
+
+                //check if object blocked
+                if(Fred::ObjectHasState(tmp.keyset_id,Fred::ObjectState::SERVER_UPDATE_PROHIBITED).exec(ctx))
+                {
+                    BOOST_THROW_EXCEPTION(Fred::MergeContact::Exception().set_object_blocked(tmp.handle));
+                }
 
                 if(!dry_run)
                 {
