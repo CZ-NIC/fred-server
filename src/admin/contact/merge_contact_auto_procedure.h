b698e5b14875897716eef5fd9e6ef533214ab825
Jan Zima <jan.zima@nic.cz>
2014-11-10 15:57:05 +0100

    Ticket #11573 - minimal doc comments for automatic merge procedure

--- src/admin/contact/merge_contact_auto_procedure.h
+++ src/admin/contact/merge_contact_auto_procedure.h
@@ -1,6 +1,11 @@
 #ifndef MERGE_CONTACT_AUTO_PROC_H__
 #define MERGE_CONTACT_AUTO_PROC_H__
 
+/**
+ *  @file
+ *  contact merge auto procedure
+ */
+
 #include "util/optional_value.h"
 #include "src/fredlib/logger_client.h"
 #include "src/fredlib/mailer.h"
@@ -11,12 +16,29 @@
 
 namespace Admin {
 
-
+/**
+ * Contact duplicates merge procedure.
+ */
 class MergeContactAutoProcedure
 {
 public:
+    /**
+     * Minimal constructor.
+     * @param mm legacy mailer interface
+     * @param _logger_client legacy logger interface
+     * @param _registrar merged contacts registrar handle
+     */
     MergeContactAutoProcedure(Fred::Mailer::Manager& mm, Fred::Logger::LoggerClient &_logger_client, const std::string& _registrar);
 
+    /**
+     * Constructor.
+     * @param mm legacy mailer interface
+     * @param _logger_client legacy logger interface
+     * @param _registrar merged contacts registrar handle
+     * @param _limit is not used
+     * @param _dry_run just write what could be done; don't actually touch data
+     * @param _verbose output verbosity level: 0,1,2,3 if _dry_run is set then 3
+     */
     MergeContactAutoProcedure(
             Fred::Mailer::Manager& mm,
             Fred::Logger::LoggerClient &_logger_client,
@@ -25,17 +47,33 @@ public:
             const Optional<bool> &_dry_run,
             const Optional<unsigned short> &_verbose);
 
+    /**
+     * Not implemented, to be removed in #12190
+     */
     MergeContactAutoProcedure& set_limit(const Optional<unsigned long long> &_limit);
 
+    /**
+     * Just print not do actions
+     */
     MergeContactAutoProcedure& set_dry_run(const Optional<bool> &_dry_run);
 
+    /**
+     * Specify custom order of filters for selection of destination contact.
+     * If not set, there is default in @ref get_default_selection_filter_order()
+     */
     MergeContactAutoProcedure& set_selection_filter_order(
             const std::vector<Fred::ContactSelectionFilterType> &_selection_filter_order);
 
+    /**
+     * Output verbosity level: 0,1,2,3 if dry_run is set then 3
+     */
     MergeContactAutoProcedure& set_verbose(const Optional<unsigned short> &_verbose);
 
+    /**
+     * Execute automatic merge of duplicate contacts.
+     * @return email notification data about object changes for testing purposes only
+     */
     std::vector<Fred::MergeContactNotificationEmailWithAddr> exec();
-
 
 private:
     bool is_set_dry_run() const;
