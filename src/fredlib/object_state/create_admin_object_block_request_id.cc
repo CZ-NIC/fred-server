14ceb44a4d445c46816c6697abfbfa7fbba59f9a
Jan Zima <jan.zima@nic.cz>
2015-01-16 11:02:57 +0100

    Ticket #12555 - unnecessary prefix Fred removed

--- src/fredlib/object_state/create_admin_object_block_request_id.cc
+++ src/fredlib/object_state/create_admin_object_block_request_id.cc
@@ -172,7 +172,7 @@ namespace Fred
             _ctx.get_log().debug("serverBlockedId = " + boost::lexical_cast< std::string >(serverBlockedId));
         }
         _ctx.get_log().debug("LockObjectStateRequestLock call");
-        Fred::LockObjectStateRequestLock(object_id_).exec(_ctx);
+        LockObjectStateRequestLock(object_id_).exec(_ctx);
         _ctx.get_log().debug("LockObjectStateRequestLock success");
         Database::Result rcheck = _ctx.get_conn().exec_params(
             "SELECT 1 "
