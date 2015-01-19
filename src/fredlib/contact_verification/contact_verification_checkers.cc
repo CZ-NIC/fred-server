ba70ba15d16c08fbcb3423eb9271d41cf2d0635a
Jan Zima <jan.zima@nic.cz>
2015-01-19 15:06:11 +0100

    Ticket #12341 - mojeid email length named constant

--- src/fredlib/contact_verification/contact_verification_checkers.cc
+++ src/fredlib/contact_verification/contact_verification_checkers.cc
@@ -135,8 +135,9 @@ bool contact_checker_phone_unique(const Contact &_data, FieldErrorMap &_errors)
 
 bool contact_checker_email_format(const Contact &_data, FieldErrorMap &_errors)
 {
+    static const std::string::size_type MAX_MOJEID_EMAIL_LENGTH = 200; //max mojeid email length
     bool result = boost::algorithm::trim_copy(_data.email.get_value_or_default()).length() > 0
-        ? ((_data.email.get_value_or_default().length() <= 200) //max mojeid email length
+        ? ((_data.email.get_value_or_default().length() <= MAX_MOJEID_EMAIL_LENGTH)
             && DjangoEmailFormat().check(_data.email.get_value_or_default()))
         : true;//no email to check
     if (result == false) {
