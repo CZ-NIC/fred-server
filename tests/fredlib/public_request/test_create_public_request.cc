907472aef7c3d4194235c39549455a756a32f4b4
Michal Strnad <michal.strnad@nic.cz>
2015-05-13 09:09:43 +0200

    Ticket #13341 - Test suite TestCreatePublicRequest done.

--- tests/fredlib/public_request/test_create_public_request.cc
+++ tests/fredlib/public_request/test_create_public_request.cc
@@ -84,6 +84,37 @@ BOOST_FIXTURE_TEST_SUITE(TestCreatePublicRequest, create_public_request_fixture)
 DECLARE_EXCEPTION_DATA(unknown_registrar_handle, std::string);
 
 /**
+ * test PublicRequestObjectLockGuard with wrong object_id
+ */
+BOOST_AUTO_TEST_CASE(public_request_object_lock_guard_wrong_id)
+{
+    Fred::OperationContext ctx;
+
+    const Fred::ObjectId bad_object_id = static_cast< Fred::RegistrarId >(ctx.get_conn().exec(
+        "SELECT 100+2*MAX(id) FROM object")[0][0]);
+    BOOST_CHECK_EXCEPTION(
+    try {
+        Fred::PublicRequestObjectLockGuard(ctx, bad_object_id);
+    }
+    catch(const Fred::PublicRequestObjectLockGuard::Exception &e) {
+        BOOST_CHECK(e.is_set_object_doesnt_exist());
+        BOOST_CHECK(e.get_object_doesnt_exist() == bad_object_id);
+        BOOST_TEST_MESSAGE(boost::diagnostic_information(e));
+        throw;
+    }
+    catch(const std::exception &e) {
+        BOOST_ERROR(boost::diagnostic_information(e));
+        throw;
+    }
+    catch(...) {
+        BOOST_ERROR("unexpected exception occurs");
+        throw;
+    },
+    std::exception,
+    check_std_exception);
+}
+
+/**
  * test CreatePublicRequest with wrong registrar
  */
 BOOST_AUTO_TEST_CASE(create_public_request_wrong_registrar)
@@ -93,31 +124,158 @@ BOOST_AUTO_TEST_CASE(create_public_request_wrong_registrar)
     const Fred::RegistrarId bad_registrar_id = static_cast< Fred::RegistrarId >(ctx.get_conn().exec(
         "SELECT 100+2*MAX(id) FROM registrar")[0][0]);
     BOOST_CHECK_EXCEPTION(
-    try
-    {
+    try {
         Fred::CreatePublicRequest(PublicRequestTypeFake("mojeid_contact_identification"))
             .set_registrar_id(bad_registrar_id)
             .exec(ctx, Fred::PublicRequestObjectLockGuard(ctx, contact_id));
     }
-    catch(const Fred::CreatePublicRequest::Exception &e)
-    {
+    catch(const Fred::CreatePublicRequest::Exception &e) {
         BOOST_CHECK(e.is_set_unknown_registrar());
         BOOST_CHECK(e.get_unknown_registrar() == bad_registrar_id);
         BOOST_TEST_MESSAGE(boost::diagnostic_information(e));
         throw;
     }
-    catch(const std::exception &e)
-    {
+    catch(const std::exception &e) {
         BOOST_ERROR(boost::diagnostic_information(e));
         throw;
     }
-    catch(...)
-    {
+    catch(...) {
         BOOST_ERROR("unexpected exception occurs");
         throw;
     },
     std::exception,
     check_std_exception);
+}
+
+/**
+ * test CreatePublicRequest with wrong public request type
+ */
+BOOST_AUTO_TEST_CASE(create_public_request_wrong_type)
+{
+    Fred::OperationContext ctx;
+    const std::string bad_type = "absolutely_wrong_prt";
+
+    BOOST_CHECK_EXCEPTION(
+    try {
+        Fred::CreatePublicRequest(PublicRequestTypeFake(bad_type))
+            .exec(ctx, Fred::PublicRequestObjectLockGuard(ctx, contact_id));
+    }
+    catch(const Fred::CreatePublicRequest::Exception &e) {
+        BOOST_CHECK(e.is_set_unknown_type());
+        BOOST_CHECK(e.get_unknown_type() == bad_type);
+        BOOST_TEST_MESSAGE(boost::diagnostic_information(e));
+        throw;
+    }
+    catch(const std::exception &e) {
+        BOOST_ERROR(boost::diagnostic_information(e));
+        throw;
+    }
+    catch(...) {
+        BOOST_ERROR("unexpected exception occurs");
+        throw;
+    },
+    std::exception,
+    check_std_exception);
+}
+
+/**
+ * test CreatePublicRequest
+ */
+BOOST_AUTO_TEST_CASE(create_public_request_ok)
+{
+    Fred::OperationContext ctx;
+    typedef std::vector< std::string > TypeName;
+    TypeName type_names;
+    {
+        const Database::Result res = ctx.get_conn().exec("SELECT name FROM enum_public_request_type ORDER BY id");
+        BOOST_CHECK(0 < res.size());
+        for (::size_t idx = 0; idx < res.size(); ++idx) {
+            type_names.push_back(static_cast< std::string >(res[idx][0]));
+        }
+        BOOST_CHECK(type_names.size() == res.size());
+    }
+
+    Fred::PublicRequestObjectLockGuard locked_contact(ctx, contact_id);
+    for (TypeName::const_iterator name_ptr = type_names.begin(); name_ptr != type_names.end(); ++name_ptr) {
+        const PublicRequestTypeFake public_request_type(*name_ptr);
+        const Fred::PublicRequestId public_request_id = Fred::CreatePublicRequest(public_request_type)
+            .exec(ctx, locked_contact);
+        const Database::Result res = ctx.get_conn().exec_params(
+            "SELECT "
+                "id,"
+                "(SELECT name=$2::TEXT FROM enum_public_request_type WHERE id=pr.request_type),"
+                "create_time=NOW(),"
+                "(SELECT name='new' FROM enum_public_request_status WHERE id=pr.status),"
+                "resolve_time IS NULL,"
+                "reason IS NULL,"
+                "email_to_answer IS NULL,"
+                "answer_email_id IS NULL,"
+                "registrar_id IS NULL,"
+                "create_request_id IS NULL,"
+                "resolve_request_id IS NULL "
+            "FROM public_request pr "
+            "WHERE id=$1::BIGINT",
+            Database::query_param_list(public_request_id)(*name_ptr));
+        BOOST_CHECK(res.size() == 1);
+        BOOST_CHECK(static_cast< Fred::PublicRequestId >(res[0][0]) == public_request_id);
+        BOOST_CHECK(!res[0][1].isnull() && static_cast< bool >(res[0][1]));
+        BOOST_CHECK(!res[0][2].isnull() && static_cast< bool >(res[0][2]));
+        BOOST_CHECK(!res[0][3].isnull() && static_cast< bool >(res[0][3]));
+        BOOST_CHECK(static_cast< bool >(res[0][4]));
+        BOOST_CHECK(static_cast< bool >(res[0][5]));
+        BOOST_CHECK(static_cast< bool >(res[0][6]));
+        BOOST_CHECK(static_cast< bool >(res[0][7]));
+        BOOST_CHECK(static_cast< bool >(res[0][8]));
+        BOOST_CHECK(static_cast< bool >(res[0][9]));
+        BOOST_CHECK(static_cast< bool >(res[0][10]));
+    }
+
+    const std::string reason = "Naprosto bezdůvodně!";
+    const std::string email_to_answer = "noreply@nic.cz";
+    BOOST_CHECK(reason != email_to_answer);
+    const PublicRequestTypeFake public_request_type(*type_names.begin());
+    Fred::PublicRequestId public_request_id[2];
+    public_request_id[0] = Fred::CreatePublicRequest(
+        public_request_type, reason, email_to_answer, registrar_id)
+        .exec(ctx, locked_contact);
+    public_request_id[1] = Fred::CreatePublicRequest(public_request_type)
+        .set_reason(reason)
+        .set_email_to_answer(email_to_answer)
+        .set_registrar_id(registrar_id)
+        .exec(ctx, locked_contact);
+    const Database::Result res = ctx.get_conn().exec_params(
+        "SELECT "
+            "id,"
+            "(SELECT name=$3::TEXT FROM enum_public_request_type WHERE id=pr.request_type),"
+            "create_time=NOW(),"
+            "(SELECT name='new' FROM enum_public_request_status WHERE id=pr.status),"
+            "resolve_time IS NULL,"
+            "reason=$4::TEXT,"
+            "email_to_answer=$5::TEXT,"
+            "answer_email_id IS NULL,"
+            "registrar_id=$6::BIGINT,"
+            "create_request_id IS NULL,"
+            "resolve_request_id IS NULL "
+        "FROM public_request pr "
+        "WHERE id IN ($1::BIGINT,$2::BIGINT) ORDER BY id",
+        Database::query_param_list(public_request_id[0])(public_request_id[1])
+                                  (*type_names.begin())(reason)(email_to_answer)(registrar_id));
+    BOOST_CHECK(res.size() == 2);
+    for (int idx = 0; idx < 2; ++idx) {
+        BOOST_CHECK(static_cast< Fred::PublicRequestId >(res[idx][0]) == public_request_id[idx]);
+        BOOST_CHECK(!res[idx][1].isnull() && static_cast< bool >(res[idx][1]));
+        BOOST_CHECK(!res[idx][2].isnull() && static_cast< bool >(res[idx][2]));
+        BOOST_CHECK(!res[idx][3].isnull() && static_cast< bool >(res[idx][3]));
+        BOOST_CHECK(static_cast< bool >(res[idx][4]));
+        BOOST_CHECK(!res[idx][5].isnull() && static_cast< bool >(res[idx][5]));
+        BOOST_CHECK(!res[idx][6].isnull() && static_cast< bool >(res[idx][6]));
+        BOOST_CHECK(static_cast< bool >(res[idx][7]));
+        BOOST_CHECK(!res[idx][8].isnull() && static_cast< bool >(res[idx][8]));
+        BOOST_CHECK(static_cast< bool >(res[idx][9]));
+        BOOST_CHECK(static_cast< bool >(res[idx][10]));
+    }
+
+    ctx.commit_transaction();
 }
 
 BOOST_AUTO_TEST_SUITE_END();//TestCreateContact
