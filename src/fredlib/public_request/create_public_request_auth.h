065305fd3ac053f738f6dc9b943c037a4f7a148a
Michal Strnad <michal.strnad@nic.cz>
2015-05-14 12:21:40 +0200

    Ticket #13341 - CreatePublicRequestAuth operation doxygened.

--- src/fredlib/public_request/create_public_request_auth.h
+++ src/fredlib/public_request/create_public_request_auth.h
@@ -1,3 +1,26 @@
+/*
+ * Copyright (C) 2015  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @file
+ *  declaration of CreatePublicRequestAuth class
+ */
+
 #ifndef CREATE_PUBLIC_REQUEST_AUTH_H_D034201C95EC2E9FF787F4CBF17E7802//date "+%s"|md5sum|tr "[a-f]" "[A-F]"
 #define CREATE_PUBLIC_REQUEST_AUTH_H_D034201C95EC2E9FF787F4CBF17E7802
 
@@ -7,8 +30,11 @@
 
 namespace Fred {
 
-enum { PUBLIC_REQUEST_AUTH_IDENTIFICATION_LENGTH = 32 };
+enum { PUBLIC_REQUEST_AUTH_IDENTIFICATION_LENGTH = 32 };/**< length of string for identification of public request with authentication*/
 
+/**
+ * Operation for creation of public request with authentication.
+ */
 class CreatePublicRequestAuth
 {
 public:
@@ -19,26 +45,71 @@ public:
         ExceptionData_unknown_type< Exception >,
         ExceptionData_unknown_registrar_id< Exception >
     {};
+
+    /**
+     * Constructor with mandatory parameters.
+     * @param _type type of public request
+     * @param _password authentication string
+     */
     CreatePublicRequestAuth(const PublicRequestTypeIface &_type,
                             const std::string &_password);
+
+    /**
+     * Constructor with all parameters.
+     * @param _type type of public request
+     * @param _password authentication string
+     * @param _reason reason of public request creation
+     * @param _email_to_answer the answer recipient's email address
+     * @param _registrar_id I don't know relationship between this registrar and public request!
+     */
     CreatePublicRequestAuth(const PublicRequestTypeIface &_type,
                             const std::string &_password,
                             const Optional< std::string > &_reason,
                             const Optional< std::string > &_email_to_answer,
                             const Optional< RegistrarId > &_registrar_id);
     ~CreatePublicRequestAuth() { }
+
+    /**
+     * Sets reason of public request creation.
+     * @param _reason sets reason of public request creation
+     * @return operation instance reference to allow method chaining
+     */
     CreatePublicRequestAuth& set_reason(const std::string &_reason);
+
+    /**
+     * Sets reason of public request creation.
+     * @param _email sets email address of answer recipient's
+     * @return operation instance reference to allow method chaining
+     */
     CreatePublicRequestAuth& set_email_to_answer(const std::string &_email);
+
+    /**
+     * Sets reason of public request creation.
+     * @param _id sets registrar id
+     * @return operation instance reference to allow method chaining
+     */
     CreatePublicRequestAuth& set_registrar_id(RegistrarId _id);
+
+    /**
+     * Result of creation operation.
+     */
     struct Result
     {
         Result() { }
         Result(const Result &_src);
         Result& operator=(const Result &_src);
-        PublicRequestId public_request_id;
-        std::string identification;
-        std::string password;
+        PublicRequestId public_request_id;/**< unique numeric identification of just created public request*/
+        std::string identification;/**< unique string identification of just created public request*/
+        std::string password;/**< authentication string of just created public request*/
     };
+
+    /**
+     * Executes creation.
+     * @param _ctx contains reference to database and logging interface
+     * @param _locked_object guarantees exclusive access to all public requests of given object
+     * @return @ref Result object corresponding with performed operation
+     * @throw Exception if something wrong happened
+     */
     Result exec(OperationContext &_ctx, const PublicRequestObjectLockGuard &_locked_object)const;
 private:
     const std::string type_;
