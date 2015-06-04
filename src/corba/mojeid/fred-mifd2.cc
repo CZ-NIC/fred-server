d2d10bf05e8aaf4ed1ce923e1adecd8393237e09
Michal Strnad <michal.strnad@nic.cz>
2015-06-04 10:56:38 +0200

    Ticket #13440 - Minimal version, skeleton only.

---
+++ src/corba/mojeid/fred-mifd2.cc
@@ -0,0 +1,94 @@
+#include "server2_i.h"
+
+#include <iostream>
+
+#include <boost/format.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time.hpp>
+#include <boost/assign/list_of.hpp>
+
+#include "src/fredlib/db_settings.h"
+#include "util/corba_wrapper.h"
+#include "log/logger.h"
+#include "log/context.h"
+#include "src/corba/connection_releaser.h"
+
+#include "setup_server.h"
+
+#include "cfg/config_handler.h"
+#include "cfg/handle_general_args.h"
+#include "cfg/handle_server_args.h"
+#include "cfg/handle_logging_args.h"
+#include "cfg/handle_database_args.h"
+#include "cfg/handle_registry_args.h"
+#include "cfg/handle_corbanameservice_args.h"
+#include "cfg/handle_mojeid_args.h"
+
+
+const std::string server_name = "fred-mifd2";
+
+//config args processing
+HandlerPtrVector global_hpv =
+boost::assign::list_of
+    (HandleArgsPtr(new HandleHelpArg("\nUsage: " + server_name + " <switches>\n")))
+    (HandleArgsPtr(new HandleConfigFileArgs(CONFIG_FILE) ))
+    (HandleArgsPtr(new HandleServerArgs))
+    (HandleArgsPtr(new HandleLoggingArgs))
+    (HandleArgsPtr(new HandleDatabaseArgs))
+    (HandleArgsPtr(new HandleCorbaNameServiceArgs))
+    (HandleArgsPtr(new HandleRegistryArgs))
+    (HandleArgsPtr(new HandleMojeIDArgs));
+
+
+
+int main(int argc, char *argv[])
+{
+    FakedArgs fa; //producing faked args with unrecognized ones
+    try {   //config
+        fa = CfgArgs::init<HandleHelpArg>(global_hpv)->handle(argc, argv);
+
+        // setting up logger
+        setup_logging(CfgArgs::instance());
+
+        //CORBA init
+        corba_init();
+
+        //create server object with poa and nameservice registration
+        CorbaContainer::get_instance()
+            ->register_server(new Registry::MojeID::Server_i(server_name)
+            , "MojeID");
+        run_server(CfgArgs::instance(), CorbaContainer::get_instance());
+        return EXIT_SUCCESS;
+    }//try
+    catch (const CORBA::TRANSIENT&) {
+        std::cerr << "Caught system exception TRANSIENT -- unable to contact the server." << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (const CORBA::SystemException &e) {
+        std::cerr << "Caught a CORBA::" << e._name() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (const CORBA::Exception &e) {
+        std::cerr << "Caught CORBA::Exception: " << e._name() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (const omniORB::fatalException &e) {
+        std::cerr << "Caught omniORB::fatalException:" << std::endl
+                  << "  file: " << e.file() << std::endl
+                  << "  line: " << e.line() << std::endl
+                  << "  mesg: " << e.errmsg() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (const ReturnFromMain&) {
+        return EXIT_SUCCESS;
+    }
+    catch (const std::exception &e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (...) {
+        std::cerr << "Unknown Error" << std::endl;
+        return EXIT_FAILURE;
+    }
+}
