6d0abea228b95db9be531726cbff8cb460d20cc7
Michal Strnad <michal.strnad@nic.cz>
2015-06-16 10:07:13 +0200

    Ticket #13521 - Add two-phase commit ability and hide commit_transaction() method in OperationContext class.

--- src/fredlib/opcontext.cc
+++ src/fredlib/opcontext.cc
@@ -23,53 +23,132 @@
 
 #include "src/fredlib/opcontext.h"
 
-#include <string>
-
-#include "src/fredlib/db_settings.h"
-#include "util/log/log.h"
+#include <stdexcept>
 
 namespace Fred
 {
-    OperationContext::~OperationContext()
-    {
-        if (in_transaction_)
-        {
-            try
-            {
-                conn_->exec("ROLLBACK TRANSACTION");
-            }
-            catch(...)
-            {
-                try
-                {
-                    log_.error("OperationContext::~OperationContext: rollback failed");
-                }
-                catch(...){}
-            }
+
+namespace
+{
+
+const std::string& check_transaction_id(const std::string &_value)
+{
+    if (!_value.empty()) {
+        return _value;
+    }
+    throw std::runtime_error("prepared transaction id mustn't be empty");
+}
+
+using namespace Database;
+
+std::auto_ptr< StandaloneConnection > get_database_conn()
+{
+    StandaloneConnectionFactory *const factory =
+        new StandaloneConnectionFactory(Manager::getConnectionString());
+    StandaloneConnection *const conn_ptr = StandaloneManager(factory).acquire();
+    return std::auto_ptr< StandaloneConnection >(conn_ptr);
+}
+
+void start_transaction(StandaloneConnection &_conn)
+{
+    _conn.exec("START TRANSACTION ISOLATION LEVEL READ COMMITTED");
+}
+
+}//Fred::{anonymous}
+
+OperationContextDatabasePart::OperationContextDatabasePart()
+:   transaction_id_(),
+    conn_(get_database_conn())
+{
+    start_transaction(this->get_conn());
+}
+
+OperationContextDatabasePart::OperationContextDatabasePart(const std::string &_transaction_id)
+:   transaction_id_(check_transaction_id(_transaction_id)),
+    conn_(get_database_conn())
+{
+    start_transaction(this->get_conn());
+}
+
+Database::StandaloneConnection& OperationContextDatabasePart::get_conn()const
+{
+    Database::StandaloneConnection *const conn_ptr = conn_.get();
+    if (conn_ptr != NULL) {
+        return *conn_ptr;
+    }
+    throw std::runtime_error("database connection doesn't exist");
+}
+
+std::string OperationContextDatabasePart::get_transaction_id()const
+{
+    const bool process_two_phase_commit = !transaction_id_.empty();
+    if (process_two_phase_commit) {
+        return transaction_id_;
+    }
+    throw std::runtime_error("prepared transaction not in progress");
+}
+
+OperationContextLoggingPart::OperationContextLoggingPart()
+:   log_(LOGGER(PACKAGE))
+{
+}
+
+OperationContextCreator::~OperationContextCreator()
+{
+    Database::StandaloneConnection *const conn_ptr = conn_.get();
+    if (conn_ptr == NULL) {
+        return;
+    }
+    try {
+        conn_ptr->exec("ROLLBACK");
+    }
+    catch(...) {
+        try {
+            this->get_log().error("OperationContext::~OperationContext: rollback failed");
+        }
+        catch(...) {
         }
     }
-
-    OperationContext::OperationContext()
-    : conn_(Database::StandaloneManager(new Database::StandaloneConnectionFactory(Database::Manager::getConnectionString())).acquire())
-    , in_transaction_(true)
-    , log_(LOGGER(PACKAGE))
-    {
-        conn_->exec("START TRANSACTION  ISOLATION LEVEL READ COMMITTED");
+    try {
+        conn_.reset();
     }
-
-    Database::StandaloneConnection& OperationContext::get_conn()
-    {
-        return *conn_.get();
-    }
-
-    Logging::Log& OperationContext::get_log()
-    {
-        return log_;
-    }
-
-    void OperationContext::commit_transaction()
-    {
-        conn_->exec("COMMIT TRANSACTION");
-        in_transaction_ = false;
+    catch(...) {
+        try {
+            this->get_log().error("OperationContext::~OperationContext: database connection destroying failed");
+        }
+        catch(...) {
+        }
     }
 }
+
+void OperationContextCreator::commit_transaction()
+{
+    Database::StandaloneConnection *const conn_ptr = conn_.get();
+    if (conn_ptr == NULL) {
+        throw std::runtime_error("no transaction in progress");
+    }
+    const bool process_two_phase_commit = !transaction_id_.empty();
+    if (process_two_phase_commit) {
+        conn_ptr->exec_params("PREPARE TRANSACTION $1::TEXT", Database::query_param_list(transaction_id_));
+    }
+    else {
+        conn_ptr->exec("COMMIT");
+    }
+    conn_.reset();
+}
+
+void commit_transaction(const std::string &_transaction_id)
+{
+    check_transaction_id(_transaction_id);
+    std::auto_ptr< Database::StandaloneConnection > conn_ptr = get_database_conn();
+    conn_ptr->exec_params("COMMIT PREPARED $1::TEXT", Database::query_param_list(_transaction_id));
+}
+
+void rollback_transaction(const std::string &_transaction_id)
+{
+    check_transaction_id(_transaction_id);
+    std::auto_ptr< Database::StandaloneConnection > conn_ptr = get_database_conn();
+    conn_ptr->exec_params("ROLLBACK PREPARED $1::TEXT", Database::query_param_list(_transaction_id));
+}
+
+}//Fred
--- src/fredlib/opcontext.h
+++ src/fredlib/opcontext.h
@@ -17,35 +17,131 @@
  */
 
 /**
- *  @file opcontext.h
+ *  @file
  *  operation context
  */
 
-#ifndef OPCONTEXT_H_
-#define OPCONTEXT_H_
+#ifndef OPCONTEXT_H_514FF01C2C974C899314DD0B8DE0E372//date "+%s"|md5sum|tr "[a-f]" "[A-F]"
+#define OPCONTEXT_H_514FF01C2C974C899314DD0B8DE0E372
+
+#include "util/log/log.h"
+#include "src/fredlib/db_settings.h"
 
 #include <string>
 #include <boost/utility.hpp>
 
-#include "src/fredlib/db_settings.h"
-#include "util/log/log.h"
-
+/**
+ * What's need in Fred.
+ */
 namespace Fred
 {
-    class OperationContext
-    : boost::noncopyable
-    {
-        const std::auto_ptr<Database::StandaloneConnection> conn_;
-        bool in_transaction_;
-        Logging::Log& log_;
 
-    public:
-        virtual ~OperationContext();
-        OperationContext();
-        Database::StandaloneConnection& get_conn();
-        Logging::Log& get_log();
-        void commit_transaction();
-    };//class OperationContext
+class OperationContext;
+class OperationContextCreator;
+
+/**
+ * Database connection used in Fred operations.
+ */
+class OperationContextDatabasePart
+{
+public:
+    typedef Database::StandaloneConnection Connection;///< typename alias
+    /**
+     * Obtain database connection with running transaction.
+     * @return database connection object reference
+     * @throw std::runtime_error if no transaction in progress
+     */
+    Connection& get_conn()const;
+    /**
+     * Database transaction string identification which will use in first phase of two-phase commit.
+     * @throw std::runtime_error if no transaction_id was set in the past
+     */
+    std::string get_transaction_id()const;
+private:
+    OperationContextDatabasePart();
+    OperationContextDatabasePart(const std::string &_transaction_id);
+    const std::string transaction_id_;
+    typedef std::auto_ptr< Connection > ConnectionPtr;
+    ConnectionPtr conn_;
+    friend class OperationContext;
+    friend class OperationContextCreator;
+};
+
+/**
+ * Logging object used in Fred operations.
+ */
+class OperationContextLoggingPart
+{
+public:
+    /**
+     * Obtain logging object.
+     * @return logging object reference
+     */
+    Logging::Log& get_log() { return log_; }
+private:
+    OperationContextLoggingPart();
+    Logging::Log &log_;
+    friend class OperationContext;
+};
+
+/**
+ * Common objects needed in Fred operations. It consists of two parts, database and logging.
+ */
+class OperationContext
+:   public OperationContextDatabasePart,
+    public OperationContextLoggingPart
+{
+private:
+    OperationContext() { }
+    OperationContext(const std::string &_transaction_id):OperationContextDatabasePart(_transaction_id) { }
+    friend class OperationContextCreator;
+};
+
+/**
+ * Makes accessible OperationContext instance and offers commit_transaction() method.
+ */
+class OperationContextCreator
+:   private boost::noncopyable,
+    public OperationContext
+{
+public:
+    /**
+     * Creates logging object and starts database transaction without two-phase commit ability.
+     */
+    OperationContextCreator() { }
+    /**
+     * Creates logging object and starts database transaction prepared for two-phase commit.
+     * @param _transaction_id database transaction string identification usable in second phase commit/rollback
+     * @throw std::runtime_error in case of empty _transaction_id
+     * @note Calling commit_transaction() will process first phase of two-phase commit.
+     */
+    OperationContextCreator(const std::string &_transaction_id)
+    :   OperationContext(_transaction_id) { }
+    /**
+     * Processes database transaction rollback if transaction wasn't commited.
+     */
+    ~OperationContextCreator();
+    /**
+     * Commits database transaction.
+     * @throw std::runtime_error if no transaction in progress
+     * @note If transaction_id was set it will process first phase of two-phase commit.
+     */
+    void commit_transaction();
+};
+
+/**
+ * Processes second phase of two-phase database transaction commit on standalone database connection.
+ * @param _transaction_id database transaction string identification used in first phase of two-phase commit
+ * @throw std::runtime_error if _transaction_id empty
+ */
+void commit_transaction(const std::string &_transaction_id);
+/**
+ * Drops two-phase database transaction on standalone database connection.
+ * @param _transaction_id database transaction string identification used in first phase of two-phase commit
+ * @throw std::runtime_error if _transaction_id empty
+ */
+void rollback_transaction(const std::string &_transaction_id);
 
 }//namespace Fred
-#endif //OPCONTEXT_H_
+
+#endif //OPCONTEXT_H_514FF01C2C974C899314DD0B8DE0E372
