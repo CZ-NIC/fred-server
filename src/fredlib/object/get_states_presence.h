5e09ee31f41f224528233c1b0b9f5ca0f59ceaa6
Michal Strnad <michal.strnad@nic.cz>
2015-07-08 14:15:21 +0200

    Ticket #13589 - Add Fred::Object::Get<>::States<>::Presence class.

--- src/fredlib/object/get_states_presence.h
+++ src/fredlib/object/get_states_presence.h
@@ -28,6 +28,8 @@
 #include "src/fredlib/opcontext.h"
 #include "src/fredlib/opexception.h"
 
+#include <vector>
+
 /// Fred matters
 namespace Fred {
 /// Fred objects matters
@@ -39,31 +41,219 @@ namespace Object {
  * typedef State::set< State::DELETE_CANDIDATE,
  *                     State::DELETE_WARNING,
  *                     State::VALIDATED_CONTACT > states_for_check;
- * Get< Type::CONTACT >(contact_id).states< states_for_check >().presence().exec(ctx);
+ * const Get< Type::CONTACT >::States< SetOfStates >::Presence states_presence =
+ *     Get< Type::CONTACT >(contact_id).states< states_for_check >().presence(ctx);
  * @endcode
  */
 
+/**
+ * Template class for getting information about fred object of given type.
+ * @tparam OBJECT_TYPE type of fred object
+ */
 template < Type::Value OBJECT_TYPE >
 class Get
 {
 public:
-    Get(Id _object_id);
+    /**
+     * Object identified by numeric id.
+     * @param _object_id object numeric id
+     */
+    Get(Id _object_id):id_(_object_id), handle_() { }
+    /**
+     * Object identified by string handle.
+     * @param _object_handle object string handle
+     */
+    Get(const std::string &_object_handle):id_(INVALID_ID), handle_(_object_handle) { }
+    /**
+     * Exception class used when given object doesn't exist.
+     */
+    class object_doesnt_exist:public std::runtime_error
+    {
+    public:
+        object_doesnt_exist()
+        :   std::runtime_error(Type(OBJECT_TYPE).into< std::string >() + " doesn't exist")
+        { }
+    };
+    /**
+     * Template class for getting information about given set of states.
+     */
     template < typename STATES >
     class States
     {
     public:
+        /**
+         * Copy constructor.
+         * @param _src source
+         */
+        States(const States &_src):object_(_src.object_) { }
+        /**
+         * Collects information about presence of given states.
+         */
         class Presence
         {
-            struct Result;
-            Result exec(OperationContext &_ctx)const;
+        public:
+            /**
+             * Copy constructor.
+             * @param _src source
+             */
+            Presence(const Presence &_src):state_presents_(_src.state_presents_) { }
+            typedef bool Presents;///< true if state presents
+            /**
+             * Get information about given state.
+             * @tparam STATE inspected state
+             * @return true if given state presents
+             * @note STATE must be member of STATES set
+             */
+            template < State::Value STATE >
+            Presents get()const
+            {
+                return state_presents_[STATES::template at< STATE >::position];
+            }
+        private:
+            Presence(OperationContext &_ctx, const Get &_object);
+            template < int IDX = 0, int REST = STATES::count >
+            class process_db_result;
+            template < typename SET_OF_STATES = STATES, typename X = bool >
+            class query;
+            std::vector< Presents > state_presents_;
+            friend class States;
         };
-        Presence presence()const;
+        /**
+         * Obtains information about given states presence.
+         * @param _ctx operation context
+         * @return information about given states presence
+         */
+        Presence presence(OperationContext &_ctx)const { return Presence(_ctx, object_); }
+    private:
+        States(const Get *_src_ptr):object_(*_src_ptr) { }
+        friend class Get;
+        const Get &object_;
     };
+    /**
+     * Creates instance of States< STATES > class used for obtaining information about given set of states.
+     * @tparam STATES set of fred object states
+     */
     template < typename STATES >
-    States< STATES > states()const;
+    States< STATES > states()const
+    {
+        return States< STATES >(this);
+    }
 private:
-    const Id object_id_;
+    enum { INVALID_ID = 0 };
+    const Id id_;
+    const std::string handle_;
+    bool identified_by_handle()const { return !handle_.empty(); }
 };
+
+template < Type::Value OBJECT_TYPE >
+    template < typename STATES >
+        template < typename X >
+        class Get< OBJECT_TYPE >::States< STATES >::Presence::query< typename State::set< >::type, X >
+        {
+        private:
+            static std::string& append_column(std::string &_columns) { return _columns; }
+            template < typename FRIEND_STATES, typename FRIEND_X >
+            friend class query;
+        };
+
+template < Type::Value OBJECT_TYPE >
+    template < typename STATES >
+        template < typename SET_OF_STATES, typename X >
+        class Get< OBJECT_TYPE >::States< STATES >::Presence::query
+        {
+        public:
+            static std::string by_id()
+            {
+                static std::string sql;
+                if (sql.empty()) {
+                    std::string columns;
+                    query::add_column(columns);
+                    sql = "SELECT " + columns + " "
+                          "FROM object_registry obr "
+                          "LEFT JOIN object_state os ON os.object_id=obr.id AND os.valid_to IS NULL "
+                          "LEFT JOIN enum_object_states eos ON eos.id=os.state_id "
+                          "WHERE obr.id=$1::BIGINT AND "
+                                "obr.erdate IS NULL AND "
+                                "obr.type=(SELECT id FROM enum_object_type "
+                                          "WHERE name='" + Type(OBJECT_TYPE).into< std::string >() + "') "
+                          "GROUP BY obr.id";
+                }
+                return sql;
+            }
+            static std::string by_handle()
+            {
+                static std::string sql;
+                if (sql.empty()) {
+                    std::string columns;
+                    query::add_column(columns);
+                    sql = "SELECT " + columns + " "
+                          "FROM object_registry obr "
+                          "LEFT JOIN object_state os ON os.object_id=obr.id AND os.valid_to IS NULL "
+                          "LEFT JOIN enum_object_states eos ON eos.id=os.state_id "
+                          "WHERE obr.name=$1::TEXT AND "
+                                "obr.erdate IS NULL AND "
+                                "obr.type=(SELECT id FROM enum_object_type "
+                                          "WHERE name='" + Type(OBJECT_TYPE).into< std::string >() + "') "
+                          "GROUP BY obr.id";
+                }
+                return sql;
+            }
+        private:
+            static std::string& add_column(std::string &columns)
+            {
+                static const std::string state_name = State(SET_OF_STATES::value).into< std::string >();
+                columns.append("BOOL_OR(eos.name='" + state_name + "')");
+                typedef query< typename SET_OF_STATES::tail, X > tail;
+                return tail::append_column(columns);
+            }
+            static std::string& append_column(std::string &columns)
+            {
+                columns.append(",");
+                return query::add_column(columns);
+            }
+            template < typename FRIEND_STATES, typename FRIEND_X >
+            friend class query;
+        };
+
+template < Type::Value OBJECT_TYPE >
+    template < typename STATES >
+        template < int IDX >
+        class Get< OBJECT_TYPE >::States< STATES >::Presence::process_db_result< IDX, 0 >
+        {
+        public:
+            process_db_result(const Database::Row&) { }
+            void into(std::vector< Presents >&)const { }
+        };
+
+template < Type::Value OBJECT_TYPE >
+    template < typename STATES >
+        template < int IDX, int REST >
+        class Get< OBJECT_TYPE >::States< STATES >::Presence::process_db_result
+        {
+        public:
+            process_db_result(const Database::Row &_row):columns_(_row) { }
+            void into(std::vector< Presents > &_state_presents)const
+            {
+                _state_presents.push_back(static_cast< Presents >(columns_[IDX]));
+                process_db_result< IDX + 1, REST - 1 >(columns_).into(_state_presents);
+            }
+        private:
+            const Database::Row &columns_;
+        };
+
+template < Type::Value OBJECT_TYPE >
+    template < typename STATES >
+    Get< OBJECT_TYPE >::States< STATES >::Presence::Presence(OperationContext &_ctx, const Get &_object)
+    {
+        const Database::Result dbres = _object.identified_by_handle()
+            ? _ctx.get_conn().exec_params(query<>::by_handle(), Database::query_param_list(_object.handle_))
+            : _ctx.get_conn().exec_params(query<>::by_id(),     Database::query_param_list(_object.id_));
+        if (dbres.size() <= 0) {
+            throw object_doesnt_exist();
+        }
+        state_presents_.reserve(STATES::count);
+        process_db_result<>(dbres[0]).into(state_presents_);
+    }
 
 }//Fred::Object
 }//Fred
