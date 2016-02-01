bf6deff2b8d44bdc9020833820f1aeac1a679440
Michal Strnad <michal.strnad@nic.cz>
2016-02-01 11:55:29 +0100

    Ticket #13440 - Comment changed.

--- src/mojeid/messages/generate.cc
+++ src/mojeid/messages/generate.cc
@@ -730,58 +730,12 @@ std::string collect_address(const Nullable< Fred::Contact::PlaceAddress > &_addr
     return std::string();
 }
 
-/********************************************************************************************************
- * mail_type.name='mojeid_identification'                                                               *
- ********************************************************************************************************
-Vážený uživateli,
-
-před tím, než Vám aktivujeme účet mojeID, musíme ověřit správnost Vašich
-kontaktních údajů, a to prostřednictvím kódů PIN1 a PIN2.
-
-PIN1: <?cs var:passwd ?>
-PIN2: Vám byl zaslán pomocí SMS.
-
-Do formuláře pro zadání těchto kódů budete přesměrováni po kliknutí na
-následující odkaz:
-
-https://<?cs var:hostname ?>/identify/email-sms/<?cs var:identification ?>/?password1=<?cs var:passwd ?>
-
-Po úspěšném odeslání formuláře budete moci začít Váš účet mojeID používat.
-Zároveň Vám pošleme poštou dopis s kódem PIN3, po jehož zadání bude Váš
-účet plně aktivní.
-
-Základní údaje o Vašem účtu:
-
-účet mojeID: <?cs var:handle ?>
-jméno:       <?cs var:firstname ?>
-příjmení:    <?cs var:lastname ?>
-e-mail:      <?cs var:email ?>
-
-Váš tým <?cs var:defaults.company ?>
-
- ********************************************************************************************************
- * mail_type.name='mojeid_verified_contact_transfer'                                                    *
- ********************************************************************************************************
-Vážený uživateli,
-
-tento e-mail potvrzuje úspěšné založení účtu mojeID s těmito údaji:
-
-účet mojeID: <?cs var:handle ?>
-jméno:       <?cs var:firstname ?>
-příjmení:    <?cs var:lastname ?>
-e-mail:      <?cs var:email ?>
-
-Pro aktivaci Vašeho účtu je nutné vložit kód PIN1.
-
-PIN1: <?cs var:passwd ?>
-
-Aktivaci účtu proveďte kliknutím na následující odkaz:
-
-https://<?cs var:hostname ?>/identify/email/<?cs var:identification ?>/?password1=<?cs var:passwd ?>
-
-Váš tým <?cs var:defaults.company ?>
-
-*/
+/* Mail templates:
+ *     SELECT mt.name,mtempl.template
+ *     FROM mail_type mt
+ *     JOIN mail_templates mtempl ON mtempl.id=mt.id
+ *     WHERE mt.name IN ('mojeid_identification','mojeid_verified_contact_transfer');
+ */
 Generate::MessageId send_email(
     const std::string &_mail_template,
     Fred::OperationContext &_ctx,
