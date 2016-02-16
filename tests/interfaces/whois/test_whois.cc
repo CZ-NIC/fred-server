26090e97373b61bfc970e7de00e9dd4c4bfc335a
Mykyta Viazovskyi <mykyta.viazovskyi@nic.cz>
2016-02-16 12:26:57 +0100

    Ticket #14894 - added get_registrars test case

--- tests/interfaces/whois/test_whois.cc
+++ tests/interfaces/whois/test_whois.cc
@@ -32,7 +32,9 @@ struct test_registrar_fixture
         Fred::CreateRegistrar(test_registrar_handle)
             .set_name(std::string("TEST-REGISTRAR NAME")+xmark)
             .set_street1(std::string("STR1")+xmark)
-            .set_city("Praha").set_postalcode("11150").set_country("CZ")
+            .set_city("Praha")
+            .set_postalcode("11150")
+            .set_country("CZ")
             .exec(ctx);
 
         ctx.commit_transaction();//commit fixture
@@ -43,7 +45,9 @@ struct test_registrar_fixture
 
 BOOST_AUTO_TEST_SUITE(get_registrar_by_handle)
 
-struct get_registrar_fixture : whois_impl_instance_fixture, test_registrar_fixture
+struct get_registrar_fixture
+        : whois_impl_instance_fixture,
+          test_registrar_fixture
 {};
 
 BOOST_FIXTURE_TEST_CASE(get_registrar_by_handle, get_registrar_fixture)
@@ -67,15 +71,17 @@ BOOST_FIXTURE_TEST_CASE(get_registrar_by_handle, get_registrar_fixture)
     BOOST_CHECK(reg.url == ird.url.get_value_or_default());
 }
 
-struct get_registar_no_registrar_fixuture : whois_impl_instance_fixture, test_registrar_fixture
+struct get_registar_no_registrar_fixuture
+        : whois_impl_instance_fixture,
+          test_registrar_fixture
 {};
 
 BOOST_FIXTURE_TEST_CASE(get_registar_no_registrar, get_registar_no_registrar_fixuture)
 {
     try
     {
-        Fred::OperationContext ctx; //needed?
-        Fred::InfoRegistrarOutput ird = Fred::InfoRegistrarByHandle(test_registrar_handle).exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone);
+//        Fred::OperationContext ctx; //needed?
+//        Fred::InfoRegistrarOutput ird = Fred::InfoRegistrarByHandle(test_registrar_handle).exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone);
         Registry::WhoisImpl::Registrar reg = impl.get_registrar_by_handle("reg-");
         BOOST_ERROR("unreported missing registrar");
     }
@@ -102,5 +108,65 @@ BOOST_FIXTURE_TEST_CASE(get_registrar_wrong_handle, get_registar_no_registrar_fi
 }
 
 BOOST_AUTO_TEST_SUITE_END();//get_registrar_by_handle
+
+struct get_my_registrar_list_fixture
+        : test_registrar_fixture
+{
+    std::map<std::string,Fred::InfoRegistrarOutput> registrar_info;
+    get_my_registrar_list_fixture()
+    {
+        Fred::OperationContext ctx;
+        for(int i=0; i<10; ++i)
+        {
+            std::ostringstream test_handles;
+            test_handles << "n" << i << test_registrar_handle;
+            Fred::CreateRegistrar& cr = Fred::CreateRegistrar(test_handles.str())
+                .set_name(std::string("TEST-REGISTRAR NAME")+xmark)
+                .set_street1(std::string("STR1")+xmark)
+                .set_city("Praha")
+                .set_postalcode("11150")
+                .set_country("CZ");
+            if(i>5) //4 of them to be system
+            {
+                cr = cr.set_system(true);
+            }
+            cr.exec(ctx);
+            registrar_info[test_handles.str()] =
+                    Fred::InfoRegistrarByHandle(test_handles.str())
+                    .exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone);
+        }
+
+        ctx.commit_transaction();
+    }
+    ~get_my_registrar_list_fixture()
+    {}
+};
+
+BOOST_AUTO_TEST_SUITE(get_registrars)
+
+BOOST_FIXTURE_TEST_CASE(get_my_registrars, get_my_registrar_list_fixture)
+{
+    Fred::OperationContext ctx;
+    std::vector<Fred::InfoRegistrarOutput> reg_list_out =
+            Fred::InfoRegistrarAllExceptSystem()
+            .exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone);
+    for(int i=0; i < reg_list_out.size(); ++i)
+    {
+        //refactor
+        BOOST_CHECK(reg_list_out.at(i).info_registrar_data.handle == map_at(registrar_info, reg_list_out.at(i).info_registrar_data.handle).info_registrar_data.handle);
+        BOOST_CHECK(reg_list_out.at(i).info_registrar_data.id == map_at(registrar_info, reg_list_out.at(i).info_registrar_data.handle).info_registrar_data.id);
+        BOOST_CHECK(reg_list_out.at(i).info_registrar_data.street1.get_value() == map_at(registrar_info, reg_list_out.at(i).info_registrar_data.handle).info_registrar_data.street1.get_value());
+        BOOST_CHECK(reg_list_out.at(i).info_registrar_data.city.get_value() == map_at(registrar_info, reg_list_out.at(i).info_registrar_data.handle).info_registrar_data.city.get_value());
+        BOOST_CHECK(reg_list_out.at(i).info_registrar_data.postalcode.get_value() == map_at(registrar_info, reg_list_out.at(i).info_registrar_data.handle).info_registrar_data.postalcode.get_value());
+        BOOST_CHECK(reg_list_out.at(i).info_registrar_data.country.get_value() == map_at(registrar_info, reg_list_out.at(i).info_registrar_data.handle).info_registrar_data.country.get_value());
+        BOOST_CHECK(!reg_list_out.at(i).info_registrar_data.system.get_value());//neither of them to be system
+    }
+
+}
+
+
+BOOST_AUTO_TEST_SUITE_END();//get_registrars
+
+
 
 BOOST_AUTO_TEST_SUITE_END();//TestWhois
