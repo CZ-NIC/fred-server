14883f10419ecc2c0eedb0c676f923f6200f0e32
Mykyta Viazovskyi <mykyta.viazovskyi@nic.cz>
2016-02-17 14:31:34 +0100

    Ticket #14894 - added get_nsset_by_handle test case

--- tests/interfaces/whois/test_whois.cc
+++ tests/interfaces/whois/test_whois.cc
@@ -8,15 +8,17 @@
 
 #define BOOST_TEST_NO_MAIN
 
+#include <sstream>
 #include <boost/test/unit_test.hpp>
 #include <boost/exception/diagnostic_information.hpp>
 
-BOOST_AUTO_TEST_SUITE(TestWhois)
 
 struct whois_impl_instance_fixture
 {
     Registry::WhoisImpl::Server_impl impl;
 };
+
+BOOST_AUTO_TEST_SUITE(TestWhois)
 
 struct test_registrar_fixture
 {
@@ -72,7 +74,7 @@ BOOST_FIXTURE_TEST_CASE(get_registrar_by_handle, get_registrar_fixture)
 }
 
 struct get_registar_no_registrar_fixuture
-        : whois_impl_instance_fixture,
+        : whois_impl_instance_fixture,//need?
           test_registrar_fixture
 {};
 
@@ -83,7 +85,7 @@ BOOST_FIXTURE_TEST_CASE(get_registar_no_registrar, get_registar_no_registrar_fix
 //        Fred::OperationContext ctx; //needed?
 //        Fred::InfoRegistrarOutput ird = Fred::InfoRegistrarByHandle(test_registrar_handle).exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone);
         Registry::WhoisImpl::Registrar reg = impl.get_registrar_by_handle("reg-");
-        BOOST_ERROR("unreported missing registrar");
+        BOOST_ERROR("unreported dangling registrar");
     }
     catch(const Registry::WhoisImpl::ObjectNotExists& ex)
     {
@@ -161,12 +163,211 @@ BOOST_FIXTURE_TEST_CASE(get_my_registrars, get_my_registrar_list_fixture)
         BOOST_CHECK(reg_list_out.at(i).info_registrar_data.country.get_value() == map_at(registrar_info, reg_list_out.at(i).info_registrar_data.handle).info_registrar_data.country.get_value());
         BOOST_CHECK(!reg_list_out.at(i).info_registrar_data.system.get_value());//neither of them to be system
     }
+}
 
+BOOST_AUTO_TEST_SUITE_END();//get_registrars
+/*
+ * SKIPPED AS NO PROPER INTERFACE PROVIDED
+struct get_registrar_groups_fixture
+{
+    //unsigned long long
+    std::map<unsigned long long, std::string> groups_info;
+    //vector for groups
+    get_registrar_groups_fixture()
+    {
+        for(int i=0; i < 10; ++i)
+        {
+            std::ostringstream test_groups;
+            test_groups << "testgroup" << i;
+//            groups_info[Fred::Registrar::ManagerImpl::createRegistrarGroup(test_groups.str())] = test_groups.str();
+            groups_info[Fred::Registrar::Manager::create(0)->createRegistrarGroup(test_groups.str())] = test_groups.str();
+        }
+    }
+
+};
+
+BOOST_AUTO_TEST_SUITE(get_registrar_groups);
+
+BOOST_AUTO_TEST_SUITE_END();//get_registrar_groups
+
+struct get_registrar_certification_list_fixture
+{
+
+};
+
+BOOST_AUTO_TEST_SUITE(get_registrar_certification_list);
+
+BOOST_AUTO_TEST_SUITE_END();//get_registrar_certification_list
+
+struct get_managed_zone_list_fixture
+{
+
+};
+
+BOOST_AUTO_TEST_SUITE(get_managed_zone_list);
+
+BOOST_AUTO_TEST_SUITE_END();//get_managed_zone_list
+*/
+
+BOOST_AUTO_TEST_SUITE(get_contact_by_handle);
+
+struct get_contact_by_handle_fixture
+        : whois_impl_instance_fixture,
+          test_registrar_fixture
+{
+    std::string test_contact_handle;
+    Fred::Contact::PlaceAddress contact_place;
+
+    get_contact_by_handle_fixture()
+    : test_registrar_fixture(),
+      test_contact_handle(std::string("TEST-CONTACT-HANDLE")+xmark)
+    {
+        Fred::OperationContext ctx;
+        contact_place.city = "Praha";
+        contact_place.country = "CZ";
+        contact_place.postalcode = "11150";
+        contact_place.street1 = "STR1";
+        Fred::CreateContact(test_contact_handle, test_registrar_handle)
+            .set_place(contact_place).exec(ctx);
+
+        ctx.commit_transaction();//commit fixture
+        BOOST_MESSAGE(test_contact_handle);
+    }
+};
+
+
+BOOST_FIXTURE_TEST_CASE(get_contact_by_handle, get_contact_by_handle_fixture)
+{
+    Fred::OperationContext ctx;
+    Fred::InfoContactData icd = Fred::InfoContactByHandle(test_contact_handle).exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone).info_contact_data;
+    Registry::WhoisImpl::Contact con = impl.get_contact_by_handle(test_registrar_handle);
+    BOOST_CHECK(con.address.city  == icd.place.get_value_or_default().city);
+    BOOST_CHECK(con.address.country_code == icd.place.get_value_or_default().country);
+    BOOST_CHECK(con.address.postal_code == icd.place.get_value_or_default().postalcode);
+    BOOST_CHECK(con.address.street1 == icd.place.get_value_or_default().street1);
+}
+
+struct get_no_contact_by_handle_fixture
+        : whois_impl_instance_fixture
+{};
+
+BOOST_FIXTURE_TEST_CASE(get_contact_by_handle_no_contact, get_no_contact_by_handle_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::Contact con = impl.get_contact_by_handle("abc");
+        BOOST_ERROR("unreported dangling contact");
+    }
+    catch(const Registry::WhoisImpl::ObjectNotExists& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(get_contact_by_handle_wrong_contact, get_no_contact_by_handle_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::Contact con = impl.get_contact_by_handle("");//[a-zA-Z0-9_:.-]{1,63}
+        BOOST_ERROR("contact handle rule is wrong");
+    }
+    catch(const Registry::WhoisImpl::InvalidHandle& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END();//get_contact_by_handle
+
+BOOST_AUTO_TEST_SUITE(get_nsset_by_handle)
+
+struct get_nsset_by_handle_fixture
+        : whois_impl_instance_fixture,
+          test_registrar_fixture
+{
+    std::string test_nsset_handle;
+
+    get_nsset_by_handle_fixture()
+    : test_registrar_fixture(),
+      test_nsset_handle(std::string("TEST-NSSET-HANDLE")+xmark)
+    {
+        Fred::OperationContext ctx;
+        std::vector<Fred::DnsHost> v_dns;
+        v_dns.push_back(Fred::DnsHost(std::string("TEST-FQDN")+xmark,
+                        boost::asio::ip::address()));
+        std::vector<std::string> tech_contacts;
+        tech_contacts.push_back("TEST-TECH-CONTACT");
+
+        Fred::CreateNsset(test_nsset_handle, test_registrar_handle,
+                          Optional<std::string>(), Optional<short>(), v_dns,
+                          tech_contacts, Optional<unsigned long long>())
+            .exec(ctx);
+
+        ctx.commit_transaction();//commit fixture
+        BOOST_MESSAGE(test_nsset_handle);
+    }
+};
+
+    BOOST_FIXTURE_TEST_CASE(get_nsset_by_handle, get_nsset_by_handle_fixture)
+    {
+        Fred::OperationContext ctx;
+        Fred::InfoNssetData ind = Fred::InfoNssetByHandle(test_nsset_handle).exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone).info_nsset_data;
+        Registry::WhoisImpl::NSSet nss = impl.get_nsset_by_handle(test_nsset_handle);
+        /*
+handle
+registrar
+authinfo
+tech_check_level
+dns_hosts
+tech_contacts
+logd_request_id
+         */
+        //new nsset has to be unchanged and not being transferred
+        BOOST_CHECK(nss.changed.isnull());
+        BOOST_CHECK(nss.last_transfer.isnull());
+        BOOST_CHECK(nss.created == ind.creation_time);//as that or greater than __
+        BOOST_CHECK(nss.handle == ind.handle);
+        BOOST_CHECK(nss.nservers.at(0).fqdn == ind.dns_hosts.at(0).get_fqdn());
+        BOOST_CHECK(nss.nservers.at(0).ip_addresses.at(0) == ind.dns_hosts.at(0).get_inet_addr().at(0)); //comparing two boost::address'es
+        BOOST_CHECK(nss.registrar_handle == ind.create_registrar_handle);
+        BOOST_CHECK(nss.tech_contact_handles.at(0) == ind.tech_contacts.at(0).handle);
+    }
+
+struct get_no_nsset_by_handle_fixture
+        : whois_impl_instance_fixture
+{};
+
+BOOST_FIXTURE_TEST_CASE(get_nsset_by_handle_no_nsset, get_no_nsset_by_handle_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::NSSet nss = impl.get_nsset_by_handle("abc");
+        BOOST_ERROR("unreported dangling nsset");
+    }
+    catch(const Registry::WhoisImpl::ObjectNotExists& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(get_nsset_by_handle_wrong_nsset, get_no_nsset_by_handle_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::NSSet nss = impl.get_nsset_by_handle("");//[a-zA-Z0-9_:.-]{1,63}
+        BOOST_ERROR("nsset handle rule is wrong");
+    }
+    catch(const Registry::WhoisImpl::InvalidHandle& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
 }
 
 
-BOOST_AUTO_TEST_SUITE_END();//get_registrars
-
-
+BOOST_AUTO_TEST_SUITE_END()//get_nsset_by_handle
 
 BOOST_AUTO_TEST_SUITE_END();//TestWhois
