316af509b18f2b040851ab9342491d7f7b65bd61
Mykyta Viazovskyi <mykyta.viazovskyi@nic.cz>
2016-02-26 18:34:33 +0100

    Ticket #14894 - get_domain_by_handle done partly

--- tests/interfaces/whois/test_whois.cc
+++ tests/interfaces/whois/test_whois.cc
@@ -443,7 +443,6 @@ BOOST_FIXTURE_TEST_CASE(get_nssets_by_ns_wrong_ns, get_nssets_by_ns_fixture)
 {
     try
     {
-        Fred::OperationContext ctx;
         Registry::WhoisImpl::NSSetSeq nss_s = impl.get_nssets_by_ns(test_wrong_fqdn, test_limit);
         BOOST_ERROR("domain handle rule is wrong");
     }
@@ -458,7 +457,6 @@ BOOST_FIXTURE_TEST_CASE(get_nssets_by_ns_no_ns, get_nssets_by_ns_fixture)
 {
     try
     {
-        Fred::OperationContext ctx;
         Registry::WhoisImpl::NSSetSeq nss_s = impl.get_nssets_by_ns(test_no_fqdn, test_limit);
         BOOST_ERROR("unreported dangling NSSets");
     }
@@ -542,7 +540,6 @@ BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_wrong_ns, get_nssets_by_tech_c_fixt
 {
     try
     {
-        Fred::OperationContext ctx;
         Registry::WhoisImpl::NSSetSeq nss_s = impl.get_nssets_by_tech_c(test_wrong_handle, test_limit);
         BOOST_ERROR("domain handle rule is wrong");
     }
@@ -557,7 +554,6 @@ BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_no_ns, get_nssets_by_tech_c_fixture
 {
     try
     {
-        Fred::OperationContext ctx;
         Registry::WhoisImpl::NSSetSeq nss_s = impl.get_nssets_by_tech_c(test_no_handle, test_limit);
         BOOST_ERROR("unreported dangling NSSets");
     }
@@ -611,7 +607,6 @@ BOOST_FIXTURE_TEST_CASE(get_nameserver_by_fqdn_wrong_ns, get_nameserver_by_fqdn_
 {
     try
     {
-        Fred::OperationContext ctx;
         Registry::WhoisImpl::NameServer ns = impl.get_nameserver_by_fqdn(test_wrong_handle);
         BOOST_ERROR("domain handle rule is wrong");
     }
@@ -626,7 +621,6 @@ BOOST_FIXTURE_TEST_CASE(get_nameserver_by_fqdn_no_ns, get_nameserver_by_fqdn_fix
 {
     try
     {
-        Fred::OperationContext ctx;
         Registry::WhoisImpl::NameServer ns = impl.get_nameserver_by_fqdn(test_no_handle);
         BOOST_ERROR("unreported dangling nameserver");
     }
@@ -777,7 +771,6 @@ BOOST_FIXTURE_TEST_CASE(get_keysets_by_tech_c_wrong_contact, get_keysets_by_tech
 {
     try
     {
-        Fred::OperationContext ctx;
         Registry::WhoisImpl::KeySetSeq ks_s = impl.get_nssets_by_tech_c(test_wrong_handle, test_limit);
         BOOST_ERROR("tech contact handle rule is wrong");
     }
@@ -788,22 +781,321 @@ BOOST_FIXTURE_TEST_CASE(get_keysets_by_tech_c_wrong_contact, get_keysets_by_tech
     }
 }
 
-    BOOST_FIXTURE_TEST_CASE(get_keysets_by_tech_c_no_contact, get_keysets_by_tech_c_fixture)
+BOOST_FIXTURE_TEST_CASE(get_keysets_by_tech_c_no_contact, get_keysets_by_tech_c_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::KeySetSeq ks_s = impl.get_keysets_by_tech_c(test_no_handle, test_limit);
+        BOOST_ERROR("unreported dangling KeySets");
+    }
+    catch(const Registry::WhoisImpl::ObjectNotExists& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()//get_keysets_by_tech_c
+
+
+BOOST_AUTO_TEST_SUITE(get_domain_by_handle)
+
+struct test_registrant_fixture
+{
+    std::string test_registrant_handle;
+
+    test_registrant_fixture()
+    : test_registrant_handle("registrant-fixture")
+    {}
+};
+
+struct test_domain_fixture
+        : test_registrar_fixture, test_registrant_fixture, test_contact_fixture
+{
+    std::string test_fqdn;
+    std::string no_fqdn;
+    std::string wrong_fqdn;
+
+    test_domain_fixture()
+    : test_registrar_fixture(),
+      test_registrant_fixture(),
+      test_contact_fixture(),
+      test_fqdn(std::string("test") + xmark + ".cz"),
+      no_fqdn("fine-handle.cz"),
+      wrong_fqdn("")
+    {
+        Fred::OperationContext ctx;
+        Fred::CreateDomain(test_fqdn, test_registrar_handle, test_registrant_handle)
+            .set_admin_contacts(Util::vector_of<std::string>(test_admin))
+            .exec(ctx);
+        ctx.commit_transaction();
+        BOOST_MESSAGE(test_fqdn);
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(get_domain_by_handle, test_domain_fixture)
+{
+    Fred::OperationContext ctx;
+    Fred::InfoDomainData idd = Fred::InfoDomainByHandle(test_fqdn).exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone).info_domain_data;
+    Registry::WhoisImpl::Domain dom = impl.get_domain_by_handle(test_fqdn);
+    BOOST_CHECK(dom.admin_contact_handles.at(0) == idd.admin_contacts.at(0));
+    BOOST_CHECK(dom.changed.isnull());
+    BOOST_CHECK(dom.fqdn == idd.fqdn);
+    BOOST_CHECK(dom.last_transfer.isnull());
+    BOOST_CHECK(dom.registered == idd.creation_time);
+    BOOST_CHECK(dom.registrant_handle == idd.registrant);
+    BOOST_CHECK(dom.registrar_handle == idd.create_registrar_handle);
+    //Jiri: others?
+}
+
+BOOST_FIXTURE_TEST_CASE(get_domain_by_handle_wrong_handle, test_domain_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::Domain dom = impl.get_domain_by_handle(wrong_fqdn);
+        BOOST_ERROR("domain handle rule is wrong");
+    }
+    catch(const Registry::WhoisImpl::InvalidLabel& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+struct wrong_zone_fixture
+        : test_registrar_fixture, test_registrant_fixture, test_contact_fixture
+{
+    std::string test_fqdn_bad_zone;
+
+    wrong_zone_fixture()
+    : test_registrar_fixture(),
+      test_contact_fixture(),
+      test_fqdn_bad_zone("a.")
+    {
+        std::ostringstream bad_zone;
+        std::vector<std::string> list = impl.get_managed_zone_list();
+        bad_zone << *list.rbegin();
+        while(list.end() != std::find(list.begin(), list.end(), bad_zone.str()))
+        {
+            bad_zone << "a";
+        }
+        test_fqdn_bad_zone += bad_zone.str();
+        BOOST_MESSAGE(test_fqdn_bad_zone);
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(get_domain_by_handle_wrong_zone, wrong_zone_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::Domain dom = impl.get_domain_by_handle(test_fqdn_bad_zone);
+        BOOST_ERROR("unreported managed zone");
+    }
+    catch(const Registry::WhoisImpl::UnmanagedZone& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+struct many_labels_fixture
+        : test_registrar_fixture, test_registrant_fixture, test_contact_fixture
+{
+    std::vector<std::string> domain_list;
+    std::string prepare_zone(Fred::OperationContext& ctx, const std::string& zone)
+    {
+        Fred::Zone::Data zone_data;
+        try
+        {
+            zone_data = Fred::Zone::find_zone_in_fqdn(ctx, zone);
+        }
+        catch(const Fred::Zone::Exception& ex)
+        {
+            BOOST_ERROR("test zone was not created properly");
+        }
+        std::ostringstream labeled_zone;
+        for(int i=0; i < zone_data.dots_max + 1; ++i)
+        {
+            labeled_zone << "1.";
+        }
+        labeled_zone << zone_data.name;
+        return labeled_zone.str();
+    }
+    many_labels_fixture()
+    {
+        Fred::OperationContext ctx;
+        std::vector<std::string> zone_seq = ::Whois::get_managed_zone_list(ctx);
+        for(std::vector<std::string>::iterator it = zone_seq.begin(); it != zone_seq.end(); ++it)
+        {
+            domain_list.push_back(prepare_zone(ctx, *it));
+        }
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(get_domain_by_handle_many_labels, many_labels_fixture)
+{
+    for(std::vector<std::string>::iterator it = domain_list.begin(); it != domain_list.end(); ++it)
     {
         try
         {
-            Fred::OperationContext ctx;
-            Registry::WhoisImpl::KeySetSeq ks_s = impl.get_keysets_by_tech_c(test_no_handle, test_limit);
-            BOOST_ERROR("unreported dangling KeySets");
+            Registry::WhoisImpl::Domain dom = impl.get_domain_by_handle(*it);
+            BOOST_ERROR("permitted label number is wrong");
         }
-        catch(const Registry::WhoisImpl::ObjectNotExists& ex)
+        catch(const Registry::WhoisImpl::TooManyLabels& ex)
         {
             BOOST_CHECK(true);
             BOOST_MESSAGE(boost::diagnostic_information(ex));
         }
     }
+}
+
+BOOST_FIXTURE_TEST_CASE(get_domain_by_handle_no_handle, test_domain_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::Domain dom = impl.get_domain_by_handle(no_fqdn);
+        BOOST_ERROR("unreported dangling domain");
+    }
+    catch(const Registry::WhoisImpl::ObjectNotExists& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(get_domain_by_handle_invalid_handle, test_domain_fixture)
+{
+
+}
+
+struct invalid_unmanaged_fixture
+        : wrong_zone_fixture
+{
+    std::string invalid_unmanaged_fqdn;
+    invalid_unmanaged_fixture()
+    : wrong_zone_fixture()//do not create bad_zone domain for it
+    {
+        std::ostringstream prefix;
+        for(int i=0; i < 256 - test_fqdn_bad_zone.size(); ++i)//exceed the size of valid label
+        {
+            prefix << "1";
+        }
+        prefix << test_fqdn_bad_zone;
+        invalid_unmanaged_fqdn = prefix.str();
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(get_domain_by_handle_invalid_unmanaged, invalid_unmanaged_fixture)
+{
+    try
+    {
+        Registry::WhoisImpl::Domain dom = impl.get_domain_by_handle(invalid_unmanaged_fqdn);
+        BOOST_ERROR("domain must have invalid label and unmanaged zone");
+    }
+    catch(const Registry::WhoisImpl::InvalidLabel& ex)
+    {
+        BOOST_CHECK(true);
+        BOOST_MESSAGE(boost::diagnostic_information(ex));
+    }
+    catch(const Registry::WhoisImpl::UnmanagedZone& ex)
+    {
+        BOOST_ERROR("domain must check name validity first");
+    }
+}
+
+struct unmanaged_toomany_fixture
+        : wrong_zone_fixture
+{
+    std::vector<std::string> domain_list;
+    std::string unmanaged_toomany_fqdn;
+
+    std::string prepare_zone(Fred::OperationContext& ctx, const std::string& zone)
+    {
+        Fred::Zone::Data zone_data;
+        try
+        {
+            zone_data = Fred::Zone::find_zone_in_fqdn(ctx, zone);
+        }
+        catch(const Fred::Zone::Exception& ex)
+        {
+            BOOST_ERROR("test zone was not created properly");
+        }
+        std::ostringstream labeled_zone;
+        for(int i=0; i < zone_data.dots_max + 1; ++i)
+        {
+            labeled_zone << "1.";
+        }
+        labeled_zone << test_fqdn_bad_zone; //unmanaged zone appended
+        return labeled_zone.str();
+    }
+    unmanaged_toomany_fixture()
+    : wrong_zone_fixture()
+    {
+        Fred::OperationContext ctx;
+        std::vector<std::string> zone_seq = ::Whois::get_managed_zone_list(ctx);
+        for(std::vector<std::string>::iterator it = zone_seq.begin(); it != zone_seq.end(); ++it)
+        {
+            domain_list.push_back(prepare_zone(ctx, *it));
+        }
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(get_domain_by_handle_unmanaged_toomany, unmanaged_toomany_fixture)
+{
+    for(std::vector<std::string>::iterator it = domain_list.begin(); it != domain_list.end(); ++it)
+    {
+        try
+        {
+            Registry::WhoisImpl::Domain dom = impl.get_domain_by_handle(*it);
+            BOOST_ERROR("domain must have unmanaged zone and exceeded number of labels");
+        }
+        catch(const Registry::WhoisImpl::UnmanagedZone& ex)
+        {
+            BOOST_CHECK(true);
+            BOOST_MESSAGE(boost::diagnostic_information(ex));
+        }
+        catch(const Registry::WhoisImpl::TooManyLabels& ex)
+        {
+            BOOST_ERROR("domain must check managed zone first");
+        }
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()//get_domain_by_handle
 
 
-BOOST_AUTO_TEST_SUITE_END()//get_keysets_by_tech_c
+BOOST_AUTO_TEST_SUITE(get_domains_by_registrant)
+
+struct test_domain_fixture
+        : test_registrar_fixture, test_registrant_fixture, test_contact_fixture
+{
+    std::string test_fqdn;
+    std::string no_fqdn;
+    std::string wrong_fqdn;
+
+    test_domain_fixture()
+    : test_registrar_fixture(),
+      test_registrant_fixture(),
+      test_contact_fixture(),
+      test_fqdn(std::string("test") + xmark + ".cz"),
+      no_fqdn("fine-handle.cz"),
+      wrong_fqdn("")
+    {
+        Fred::OperationContext ctx;
+        Fred::CreateDomain(test_fqdn, test_registrar_handle, test_registrant_handle)
+            .set_admin_contacts(Util::vector_of<std::string>(test_admin))
+            .exec(ctx);
+        ctx.commit_transaction();
+        BOOST_MESSAGE(test_fqdn);
+    }
+};
+
+BOOST_FIXTURE_TEST_CASE(get_domains_by_registrant, test_domain_fixture)
+{
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()//get_domains_by_registrant
+
 
 BOOST_AUTO_TEST_SUITE_END();//TestWhois
