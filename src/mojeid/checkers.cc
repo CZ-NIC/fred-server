97757981d668d088d945db5313b584bd62e67d14
Michal Strnad <michal.strnad@nic.cz>
2016-03-11 08:35:17 +0100

    Ticket #14242 - Move checks into mojeID.

--- src/mojeid/checkers.cc
+++ src/mojeid/checkers.cc
@@ -16,7 +16,7 @@
  * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "src/fredlib/contact/checkers.h"
+#include "src/mojeid/checkers.h"
 #include "src/fredlib/contact/ssntype.h"
 #include "src/fredlib/object/object_type.h"
 #include "src/fredlib/contact_verification/django_email_format.h"
