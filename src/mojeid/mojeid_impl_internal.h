07903cf017084425fa981c272e6db975e85774a0
Michal Strnad <michal.strnad@nic.cz>
2016-03-18 11:13:57 +0100

    Ticket #14242 - Check ssn instead of birth date.

--- src/mojeid/mojeid_impl_internal.h
+++ src/mojeid/mojeid_impl_internal.h
@@ -55,7 +55,7 @@ struct check_contact_optional_addresses
 
 typedef boost::mpl::list< Fred::MojeID::check_contact_username,
                           Fred::check_contact_name,
-                          Fred::MojeID::check_contact_birthday,
+                          Fred::MojeID::check_contact_ssn,
                           Fred::check_contact_email_presence,
                           Fred::check_contact_email_validity,
                           Fred::check_contact_notifyemail_validity,
