fe30623df2876b932562a6369e7dd637e1c8daac
Mykyta Viazovskyi <mykyta.viazovskyi@nic.cz>
2016-04-12 18:42:53 +0200

    Ticket #14894 - get_keyset_by_handle reworked with fixtures_utils

--- tests/interfaces/whois/test_whois_get_keyset_by_handle.cc
+++ tests/interfaces/whois/test_whois_get_keyset_by_handle.cc
@@ -1,54 +1,66 @@
-//registrar!
-//contact could be empty
+#include "tests/interfaces/whois/fixture_common.h"
+#include "src/fredlib/domain/create_domain.h"
+#include "src/fredlib/object_state/perform_object_state_request.h"
+#include "boost/date_time/posix_time/posix_time_types.hpp"
+#include "tests/setup/fixtures_utils.h"
+
+BOOST_AUTO_TEST_SUITE(TestWhois)
 BOOST_AUTO_TEST_SUITE(get_keyset_by_handle)
 
 struct get_keyset_by_handle_fixture
-: test_registrar_fixture, test_contact_fixture
+: whois_impl_instance_fixture
 {
-    std::string test_keyset_handle;
-    std::string no_keyset_handle;
-    std::string wrong_keyset_handle;
+    Fred::OperationContext ctx;
+    const Fred::InfoRegistrarData registrar;
+    const Fred::InfoKeysetData keyset;
+    const boost::posix_time::ptime now_utc;
+    const boost::posix_time::ptime now_prague;
 
     get_keyset_by_handle_fixture()
-    : test_registrar_fixture(),
-      test_contact_fixture(),
-      test_keyset_handle(std::string("TEST-KEYSET-HANDLE")+xmark),
-      no_keyset_handle("fine-keyset-handle"),
-      wrong_keyset_handle("")
+    : registrar(
+          Test::exec(Test::CreateX_factory<Fred::CreateRegistrar>().make(), ctx)), //Test::registrar::make
+      keyset(
+          Test::exec(
+              Test::CreateX_factory<Fred::CreateKeyset>().make(registrar.handle)
+                  .set_dns_keys(Util::vector_of<Fred::DnsKey>(Fred::DnsKey(42, 777, 13, "any-key")))//what key has to be here?
+                  .set_tech_contacts(Util::vector_of<std::string>(Test::contact::make(ctx).handle)),//(Test::contact::make(ctx).handle))
+              ctx)),
+      now_utc(boost::posix_time::time_from_string(
+                  static_cast<std::string>(ctx.get_conn()
+                  .exec("SELECT now()::timestamp")[0][0]))),
+      now_prague(boost::posix_time::time_from_string(
+                  static_cast<std::string>(ctx.get_conn()
+                  .exec("SELECT now() AT TIME ZONE 'Europe/Prague'")[0][0])))
     {
-        Fred::OperationContext ctx;
-        Fred::CreateKeyset(test_keyset_handle, test_registrar_handle)
-            .set_dns_keys(Util::vector_of<Fred::DnsKey>(Fred::DnsKey(42, 777, 13, "any-key")))//what key has to be here?
-            .set_tech_contacts(Util::vector_of<std::string>(test_admin)(test_contact))
-            .exec(ctx);
+//        Fred::CreateKeyset(test_keyset_handle, test_registrar_handle)
+//            .set_dns_keys(Util::vector_of<Fred::DnsKey>(Fred::DnsKey(42, 777, 13, "any-key")))//what key has to be here?
+//            .set_tech_contacts(Util::vector_of<std::string>(test_admin)(test_contact))
+//            .exec(ctx);
         ctx.commit_transaction();
-        BOOST_MESSAGE(test_keyset_handle);
     }
 };
 
 BOOST_FIXTURE_TEST_CASE(get_keyset_by_handle, get_keyset_by_handle_fixture)
 {
-    Fred::OperationContext ctx;
-    Fred::InfoKeysetData ikd = Fred::InfoKeysetByHandle(test_keyset_handle).exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone).info_keyset_data;
-    Registry::WhoisImpl::KeySet ks = impl.get_keyset_by_handle(test_keyset_handle);
+    Registry::WhoisImpl::KeySet ks = impl.get_keyset_by_handle(keyset.handle);
 
-    BOOST_CHECK(ks.changed.isnull());//new has to be unchanged
-    BOOST_CHECK(ks.created == ikd.creation_time);
-    BOOST_CHECK(ks.dns_keys.at(0).alg == ikd.dns_keys.at(0).get_alg());
-    BOOST_CHECK(ks.dns_keys.at(0).flags == ikd.dns_keys.at(0).get_flags());
-    BOOST_CHECK(ks.dns_keys.at(0).protocol == ikd.dns_keys.at(0).get_protocol());
-    BOOST_CHECK(ks.dns_keys.at(0).public_key == ikd.dns_keys.at(0).get_key());
-    BOOST_CHECK(ks.handle == ikd.handle);
-    BOOST_CHECK(ks.last_transfer.isnull());//new has to have no transfer
-    BOOST_CHECK(ks.registrar_handle == ikd.create_registrar_handle);
-    BOOST_CHECK(ks.tech_contact_handles.at(0) == ikd.tech_contacts.at(0).handle);
+    BOOST_CHECK(ks.changed.get_value() == ptime(not_a_date_time));
+    BOOST_CHECK(ks.created == now_utc);
+    BOOST_CHECK(ks.dns_keys.at(0).alg        == keyset.dns_keys.at(0).get_alg());
+    BOOST_CHECK(ks.dns_keys.at(0).flags      == keyset.dns_keys.at(0).get_flags());
+    BOOST_CHECK(ks.dns_keys.at(0).protocol   == keyset.dns_keys.at(0).get_protocol());
+    BOOST_CHECK(ks.dns_keys.at(0).public_key == keyset.dns_keys.at(0).get_key());
+    BOOST_CHECK(ks.handle == keyset.handle);
+    BOOST_CHECK(ks.last_transfer.get_value_or_default() == ptime(not_a_date_time));
+    BOOST_CHECK(ks.registrar_handle == keyset.create_registrar_handle);
+    BOOST_CHECK(ks.tech_contact_handles.at(0) == keyset.tech_contacts.at(0).handle);
 }
 
 BOOST_FIXTURE_TEST_CASE(get_keyset_by_handle_no_keyset, get_keyset_by_handle_fixture)
 {
     try
     {
-        Registry::WhoisImpl::KeySet ks = impl.get_keyset_by_handle(no_keyset_handle);
+        Registry::WhoisImpl::KeySet ks = impl.get_keyset_by_handle("fine-keyset-handle");
         BOOST_ERROR("unreported dangling keyset");
     }
     catch(const Registry::WhoisImpl::ObjectNotExists& ex)
@@ -62,7 +74,7 @@ BOOST_FIXTURE_TEST_CASE(get_keyset_by_handle_wrong_nsset, get_keyset_by_handle_f
 {
     try
     {
-        Registry::WhoisImpl::KeySet ks = impl.get_keyset_by_handle(wrong_keyset_handle);
+        Registry::WhoisImpl::KeySet ks = impl.get_keyset_by_handle("");
         BOOST_ERROR("keyset handle rule is wrong");
     }
     catch(const Registry::WhoisImpl::InvalidHandle& ex)
@@ -73,3 +85,4 @@ BOOST_FIXTURE_TEST_CASE(get_keyset_by_handle_wrong_nsset, get_keyset_by_handle_f
 }
 
 BOOST_AUTO_TEST_SUITE_END()//get_keyset_by_handle
+BOOST_AUTO_TEST_SUITE_END()//TestWhois
