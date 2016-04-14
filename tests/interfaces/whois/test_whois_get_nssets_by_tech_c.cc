1762f78ab9fc377c48be10c217317acc3de4e86f
Mykyta Viazovskyi <mykyta.viazovskyi@nic.cz>
2016-04-14 19:15:51 +0200

    Ticket #14894 - get_nssets_by_tech_c reworked with fixtures_utils

--- tests/interfaces/whois/test_whois_get_nssets_by_tech_c.cc
+++ tests/interfaces/whois/test_whois_get_nssets_by_tech_c.cc
@@ -1,37 +1,57 @@
-//many registrars!
-//many contacts!
+#include "tests/interfaces/whois/fixture_common.h"
+#include "tests/setup/fixtures_utils.h"
+
+BOOST_AUTO_TEST_SUITE(TestWhois)
 BOOST_AUTO_TEST_SUITE(get_nssets_by_tech_c)
 
 struct get_nssets_by_tech_c_fixture
-: test_registrar_fixture
+: whois_impl_instance_fixture
 {
-    std::string test_contact_handle;
-    std::string test_no_handle;//better name
-    std::string test_wrong_handle;
-    unsigned long test_limit;
+    Fred::OperationContext ctx;
+    std::string test_fqdn;
+    unsigned int test_limit;
+    const Fred::InfoRegistrarData registrar;
+    const Fred::InfoContactData contact;
+    const Fred::InfoNssetData nsset;
+    const boost::posix_time::ptime now_utc;
+    const boost::posix_time::ptime now_prague;
+    std::map<std::string, Fred::InfoNssetData> nsset_info;
 
     get_nssets_by_tech_c_fixture()
-    : test_registrar_fixture(),
-      test_contact_handle(std::string("TEST-CONTACT-HANDLE") + xmark),
-      test_no_handle("fine-tech-c-handle"),
-      test_wrong_handle(""),
-      test_limit(10)
+    : test_limit(10),
+      registrar(Test::registrar::make(ctx)),     
+      contact(Test::contact::make(ctx)),
+      now_utc(boost::posix_time::time_from_string(
+                  static_cast<std::string>(ctx.get_conn()
+                  .exec("SELECT now()::timestamp")[0][0]))),
+      now_prague(boost::posix_time::time_from_string(
+                  static_cast<std::string>(ctx.get_conn()
+                  .exec("SELECT now() AT TIME ZONE 'Europe/Prague'")[0][0])))
     {
-        Fred::OperationContext ctx;
-        for(unsigned long i = 0; i < test_limit; ++i)
+        Util::vector_of<Fred::DnsHost> dns_hosts(
+            Fred::DnsHost(test_fqdn,
+                          Util::vector_of<boost::asio::ip::address>(
+                              boost::asio::ip::address())));
+        for(unsigned int i = 0; i < test_limit; ++i)
         {
-            std::ostringstream test_handles;
-            test_handles << "n" << i;
-            std::vector<Fred::DnsHost> v_dns;
-            v_dns.push_back(Fred::DnsHost(test_contact_handle,
-                            Util::vector_of<boost::asio::ip::address>(boost::asio::ip::address())));
-            std::vector<std::string> tech_contacts;
-            tech_contacts.push_back("TEST-TECH-CONTACT" + xmark);
-
-            Fred::CreateNsset(test_handles.str(), test_registrar_handle + boost::lexical_cast<std::string>(i),
-                              Optional<std::string>(), Optional<short>(), v_dns,
-                              tech_contacts, Optional<unsigned long long>())
-                .exec(ctx);
+            const Fred::InfoNssetData& ind = Test::exec(
+                Test::CreateX_factory<Fred::CreateNsset>()
+                    .make(registrar.handle)
+                    .set_dns_hosts(dns_hosts)
+                    .set_tech_contacts(Util::vector_of<std::string>(contact.handle)),
+                ctx);
+            nsset_info[ind.handle] = ind;
+        }
+        //different contact's nssets
+        for(unsigned int i = 0; i < 3; ++i)
+        {
+            Test::exec(
+                Test::CreateX_factory<Fred::CreateNsset>()
+                    .make(registrar.handle)
+                    .set_dns_hosts(dns_hosts)
+                    .set_tech_contacts(Util::vector_of<std::string>(
+                        Test::contact::make(ctx).handle)),
+                ctx);
         }
         ctx.commit_transaction();
     }
@@ -39,35 +59,66 @@ struct get_nssets_by_tech_c_fixture
 
 BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c, get_nssets_by_tech_c_fixture)
 {
-    Fred::OperationContext ctx;
-    std::vector<Fred::InfoNssetOutput> v_ino = Fred::InfoNssetByDNSFqdn(test_contact_handle).exec(ctx, Registry::WhoisImpl::Server_impl::output_timezone);
-    Registry::WhoisImpl::NSSetSeq nss_s = impl.get_nssets_by_tech_c(test_contact_handle, test_limit);
-    for(unsigned long i = 0; i < test_limit; ++i)
+    Registry::WhoisImpl::NSSetSeq nss_s =
+        impl.get_nssets_by_tech_c(contact.handle, test_limit);
+    
+    BOOST_CHECK(!nss_s.limit_exceeded);
+    BOOST_CHECK(nss_s.content.size() == test_limit);
+    std::map<std::string, Fred::InfoNssetData>::iterator found;
+    for(std::vector<Registry::WhoisImpl::NSSet>::iterator it = nss_s.content.begin();
+        it != nss_s.content.end();
+        ++it)
     {
-        std::vector<Fred::InfoNssetOutput>::iterator it = v_ino.begin(), end = v_ino.end();
-        while(it != end)
-        {
-            if(it->info_nsset_data.handle == nss_s.content.at(i).handle) break;
-            ++it;
-        }
-        BOOST_CHECK(nss_s.content.at(i).handle == it->info_nsset_data.handle);
-        Fred::InfoNssetData found = it->info_nsset_data;
-        BOOST_CHECK(nss_s.content.at(i).changed.isnull());
-        BOOST_CHECK(nss_s.content.at(i).last_transfer.isnull());
-        BOOST_CHECK(nss_s.content.at(i).created == found.creation_time);//as that or greater than __
-        BOOST_CHECK(nss_s.content.at(i).handle == found.handle);
-        BOOST_CHECK(nss_s.content.at(i).nservers.at(0).fqdn == found.dns_hosts.at(0).get_fqdn());
-        BOOST_CHECK(nss_s.content.at(i).nservers.at(0).ip_addresses.at(0) == found.dns_hosts.at(0).get_inet_addr().at(0)); //comparing two boost::address'es
-        BOOST_CHECK(nss_s.content.at(i).registrar_handle == found.create_registrar_handle);
-        BOOST_CHECK(nss_s.content.at(i).tech_contact_handles.at(0) == found.tech_contacts.at(0).handle);
+        found = nsset_info.find(it->handle);
+        BOOST_REQUIRE(it->handle == found->second.handle);
+        BOOST_CHECK(it->changed.isnull());
+        BOOST_CHECK(it->last_transfer.isnull());
+        BOOST_CHECK(it->created == now_utc);
+        BOOST_CHECK(it->handle == found->second.handle);
+        BOOST_CHECK(it->nservers.at(0).fqdn ==
+                found->second.dns_hosts.at(0).get_fqdn());
+        BOOST_CHECK(it->nservers.at(0).ip_addresses.at(0) ==
+                found->second.dns_hosts.at(0).get_inet_addr().at(0));
+        BOOST_CHECK(it->registrar_handle == found->second.create_registrar_handle);
+        BOOST_CHECK(it->tech_contact_handles.at(0) ==
+                found->second.tech_contacts.at(0).handle);
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_no_ns, get_nssets_by_tech_c_fixture)
+BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_limit_exceeded, get_nssets_by_tech_c_fixture)
+{
+    Registry::WhoisImpl::NSSetSeq nss_s =
+        impl.get_nssets_by_tech_c(contact.handle, test_limit - 1);
+    
+    BOOST_CHECK(nss_s.limit_exceeded);
+    BOOST_CHECK(nss_s.content.size() == test_limit - 1);
+    std::map<std::string, Fred::InfoNssetData>::iterator found;
+    for(std::vector<Registry::WhoisImpl::NSSet>::iterator it = nss_s.content.begin();
+        it != nss_s.content.end();
+        ++it)
+    {
+        found = nsset_info.find(it->handle);
+        BOOST_REQUIRE(it->handle == found->second.handle);
+        BOOST_CHECK(it->changed.isnull());
+        BOOST_CHECK(it->last_transfer.isnull());
+        BOOST_CHECK(it->created == now_utc);
+        BOOST_CHECK(it->handle == found->second.handle);
+        BOOST_CHECK(it->nservers.at(0).fqdn ==
+                found->second.dns_hosts.at(0).get_fqdn());
+        BOOST_CHECK(it->nservers.at(0).ip_addresses.at(0) ==
+                found->second.dns_hosts.at(0).get_inet_addr().at(0));
+        BOOST_CHECK(it->registrar_handle == found->second.create_registrar_handle);
+        BOOST_CHECK(it->tech_contact_handles.at(0) ==
+                found->second.tech_contacts.at(0).handle);
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_no_ns, whois_impl_instance_fixture)
 {
     try
     {
-        Registry::WhoisImpl::NSSetSeq nss_s = impl.get_nssets_by_tech_c(test_no_handle, test_limit);
+        Registry::WhoisImpl::NSSetSeq nss_s =
+            impl.get_nssets_by_tech_c("absent-contact", 0);
         BOOST_ERROR("unreported dangling NSSets");
     }
     catch(const Registry::WhoisImpl::ObjectNotExists& ex)
@@ -77,11 +128,12 @@ BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_no_ns, get_nssets_by_tech_c_fixture
     }
 }
 
-BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_wrong_ns, get_nssets_by_tech_c_fixture)
+BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_wrong_ns, whois_impl_instance_fixture)
 {
     try
     {
-        Registry::WhoisImpl::NSSetSeq nss_s = impl.get_nssets_by_tech_c(test_wrong_handle, test_limit);
+        Registry::WhoisImpl::NSSetSeq nss_s =
+            impl.get_nssets_by_tech_c("", 0);
         BOOST_ERROR("domain handle rule is wrong");
     }
     catch(const Registry::WhoisImpl::InvalidHandle& ex)
@@ -92,3 +144,4 @@ BOOST_FIXTURE_TEST_CASE(get_nssets_by_tech_c_wrong_ns, get_nssets_by_tech_c_fixt
 }
 
 BOOST_AUTO_TEST_SUITE_END()//get_nssets_by_tech_c
+BOOST_AUTO_TEST_SUITE_END()//TestWhois
