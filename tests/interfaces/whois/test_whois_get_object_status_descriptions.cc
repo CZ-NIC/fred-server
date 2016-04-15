0ecdf3c7f08a01a06a11194b2088b92a5a90d3d4
Mykyta Viazovskyi <mykyta.viazovskyi@nic.cz>
2016-04-15 17:34:59 +0200

    Ticket #14894 - get_object_status_descriptions reworked with fixtures_utils

--- tests/interfaces/whois/test_whois_get_object_status_descriptions.cc
+++ tests/interfaces/whois/test_whois_get_object_status_descriptions.cc
@@ -1,5 +1,9 @@
-//-
-BOOST_AUTO_TEST_SUITE(get_domain_status_descriptions)
+#include "tests/interfaces/whois/fixture_common.h"
+#include "tests/setup/fixtures_utils.h"
+#include "src/fredlib/object_state/get_object_state_descriptions.h"
+
+BOOST_AUTO_TEST_SUITE(TestWhois)
+BOOST_AUTO_TEST_SUITE(gdsd)//get_domain_status_descriptions)
 
 struct object_status_descriptions_fixture
 : whois_impl_instance_fixture
@@ -7,15 +11,12 @@ struct object_status_descriptions_fixture
     typedef std::map<std::string, std::string> map_type;
 
     std::string test_lang;
-    std::string no_lang;
-    std::string other_lang;
     std::string object_name;
     map_type statuses;
+    unsigned int status_number;
 
     object_status_descriptions_fixture()
-    : test_lang("EN"),
-      no_lang(""),
-      other_lang("XX")
+    : test_lang("EN")
     {
         statuses["expired"] = "description of expired";
         statuses["unguarded"] = "description of unguarded";
@@ -28,9 +29,10 @@ struct object_status_descriptions_fixture
                 "VALUES ((SELECT id FROM enum_object_states WHERE name = $1::text),"
                 " $2::text,"
                 " $3::text)",
-                    Database::query_param_list(p.first)(other_lang)(p.second)
+                    Database::query_param_list(p.first)("XX")(p.second)
             );
         }
+        ctx.commit_transaction();
     }
 };
 
@@ -40,6 +42,7 @@ struct domain_type
     domain_type()
     : object_status_descriptions_fixture()
     {
+      status_number = 2;
       object_name = ("domain");
     }
 
@@ -55,6 +58,7 @@ struct contact_type
     contact_type()
     : object_status_descriptions_fixture()
     {
+      status_number = 1;
       object_name = ("contact");
     }
 
@@ -70,6 +74,7 @@ struct nsset_type
     nsset_type()
     : object_status_descriptions_fixture()
     {
+      status_number = 1;
       object_name = ("nsset");
     }
 
@@ -85,6 +90,7 @@ struct keyset_type
     keyset_type()
     : object_status_descriptions_fixture()
     {
+      status_number = 1;
       object_name = ("keyset");
     }
 
@@ -102,16 +108,15 @@ bool private_sort(T o1, T o2)
 
 typedef boost::mpl::list<domain_type, contact_type, nsset_type, keyset_type> test_types;
 
-BOOST_FIXTURE_TEST_CASE_TEMPLATE(get_domain_status_descriptions, T, test_types, T)
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(gdsd/*get_domain_status_descriptions*/, T, test_types, T)
 {
-    T fix;
     Fred::OperationContext ctx;
     std::vector<Fred::ObjectStateDescription> states =
-                        Fred::GetObjectStateDescriptions(fix.test_lang)
-                        .set_object_type(fix.object_name)
+                        Fred::GetObjectStateDescriptions(T::test_lang)
+                        .set_object_type(T::object_name)
                         .set_external()
                         .exec(ctx);
-    std::vector<Registry::WhoisImpl::ObjectStatusDesc> vec_osd = fix.get_description(fix.test_lang);
+    std::vector<Registry::WhoisImpl::ObjectStatusDesc> vec_osd = T::get_description(T::test_lang);
     BOOST_CHECK(states.size() == vec_osd.size());
     std::sort(states.begin(), states.end(), private_sort<Fred::ObjectStateDescription>);
     std::sort(vec_osd.begin(), vec_osd.end(), private_sort<Registry::WhoisImpl::ObjectStatusDesc>);
@@ -124,13 +129,12 @@ BOOST_FIXTURE_TEST_CASE_TEMPLATE(get_domain_status_descriptions, T, test_types, 
     }
 }
 
-BOOST_FIXTURE_TEST_CASE_TEMPLATE(get_domain_status_descriptions_missing, T, test_types, T)
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(gdsdm/*get_domain_status_descriptions_missing*/, T, test_types, T)
 {
-    T fix;
     try
     {
-        std::vector<Registry::WhoisImpl::ObjectStatusDesc> vec_osd = fix.get_description(fix.no_lang);
-        BOOST_ERROR(std::string("this ") + fix.object_name + " must not have a localization");
+        std::vector<Registry::WhoisImpl::ObjectStatusDesc> vec_osd = T::get_description("");
+        BOOST_ERROR(std::string("this ") + T::object_name + " must not have a localization");
     }
     catch(const Registry::WhoisImpl::MissingLocalization& ex)
     {
@@ -139,16 +143,16 @@ BOOST_FIXTURE_TEST_CASE_TEMPLATE(get_domain_status_descriptions_missing, T, test
     }
 }
 
-BOOST_FIXTURE_TEST_CASE_TEMPLATE(get_domain_status_descriptions_other_lang, T, test_types, T)
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(gdsdol/*get_domain_status_descriptions_other_lang*/, T, test_types, T)
 {
-    T fix;
-    std::vector<Registry::WhoisImpl::ObjectStatusDesc> vec_osd = fix.get_description(fix.other_lang);
-    BOOST_CHECK(fix.statuses.size() == vec_osd.size());
+    std::vector<Registry::WhoisImpl::ObjectStatusDesc> vec_osd = T::get_description("XX");
+    BOOST_CHECK(T::status_number == vec_osd.size());
     for(std::vector<Registry::WhoisImpl::ObjectStatusDesc>::iterator it = vec_osd.begin(); it != vec_osd.end(); ++it)
     {
         //if not present - at() throws
-        BOOST_CHECK(fix.statuses.at(it->handle) == it->name);
+        BOOST_CHECK(T::statuses.at(it->handle) == it->name);
     }
 }
 
-BOOST_AUTO_TEST_SUITE_END();//get_domain_status_descriptions
+BOOST_AUTO_TEST_SUITE_END()//get_domain_status_descriptions
+BOOST_AUTO_TEST_SUITE_END()//TestWhois
