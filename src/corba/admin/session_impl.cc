bc699cbb3f0c370320ed8e31cc383b245d4202c7
Jiri Sadek <jiri.sadek@nic.cz>
2016-05-10 00:22:30 +0200

    Ticket #15756 - daphne backend mojeid prevalidated transfer public request corba type translation

--- src/corba/admin/session_impl.cc
+++ src/corba/admin/session_impl.cc
@@ -1549,6 +1549,12 @@ Registry::PublicRequest::Detail* ccReg_Session_i::createPublicRequestDetail(Fred
   else if (_request->getType() == "mojeid_contact_reidentification") {
       detail->type = Registry::PublicRequest::PRT_MOJEID_CONTACT_REIDENTIFICATION;
   }
+  else if (_request->getType() == "mojeid_prevalidated_contact_transfer") {
+      detail->type = Registry::PublicRequest::PRT_MOJEID_PREVALIDATED_CONTACT_TRANSFER;
+  }
+  else if (_request->getType() == "mojeid_prevalidated_unidentified_contact_transfer") {
+      detail->type = Registry::PublicRequest::PRT_MOJEID_PREVALIDATED_UNIDENTIFIED_CONTACT_TRANSFER;
+  }
   else {
       throw std::runtime_error("unknown public request type");
   }
