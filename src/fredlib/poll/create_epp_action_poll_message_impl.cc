2362907539a8275d2b69cd45ed9e6a92615841a4
Michal Strnad <michal.strnad@nic.cz>
2016-05-09 15:36:16 +0200

    Ticket #15726 - Change result value names.

--- src/fredlib/poll/create_epp_action_poll_message_impl.cc
+++ src/fredlib/poll/create_epp_action_poll_message_impl.cc
@@ -29,8 +29,8 @@ unsigned long long CreateEppActionPollMessage::exec(Fred::OperationContext &_ctx
     Database::Result registrar_and_type_res =
         message_to_previous_registrar
         ? _ctx.get_conn().exec_params(
-              "SELECT eot.name AS \"object type\","
-                     "r.handle AS \"registrar handle\" "
+              "SELECT eot.name AS object_type_,"
+                     "r.handle AS registrar_handle_ "
               "FROM history h "
               "JOIN object_history oh ON oh.historyid=h.id "
               "JOIN object_registry oreg ON oreg.id=oh.id "
@@ -38,8 +38,8 @@ unsigned long long CreateEppActionPollMessage::exec(Fred::OperationContext &_ctx
               "JOIN registrar r ON r.id=oh.clid "
               "WHERE h.next=$1::BIGINT", Database::query_param_list(history_id_))
         : _ctx.get_conn().exec_params(
-              "SELECT eot.name AS \"object type\","
-                     "r.handle AS \"registrar handle\" "
+              "SELECT eot.name AS object_type_,"
+                     "r.handle AS registrar_handle_ "
               "FROM object_registry oreg "
               "JOIN object_history oh ON oh.id=oreg.id "
               "JOIN enum_object_type eot ON eot.id=oreg.type "
@@ -50,7 +50,7 @@ unsigned long long CreateEppActionPollMessage::exec(Fred::OperationContext &_ctx
         BOOST_THROW_EXCEPTION(Exception().set_object_history_not_found(history_id_));
     }
 
-    if (static_cast<std::string>(registrar_and_type_res[0]["object type"])
+    if (static_cast<std::string>(registrar_and_type_res[0]["object_type_"])
         !=
         to_registry_handle(object_type_)
     ) {
@@ -65,7 +65,7 @@ unsigned long long CreateEppActionPollMessage::exec(Fred::OperationContext &_ctx
     }
 
     const unsigned long long poll_msg_id = CreatePollMessage(
-        static_cast<std::string>(registrar_and_type_res[0]["registrar handle"]),
+        static_cast<std::string>(registrar_and_type_res[0]["registrar_handle_"]),
         message_type_handle_
     ).exec(_ctx);
 
