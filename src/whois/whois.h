49ca59f329a2b551407754051957b36f58b04676
Mykyta Viazovskyi <mykyta.viazovskyi@nic.cz>
2016-06-21 18:28:47 +0200

    Ticket #14894 - remove trailing whitespace

--- src/whois/whois.h
+++ src/whois/whois.h
@@ -41,25 +41,25 @@ namespace WhoisImpl {
 
 struct PlaceAddress
 {
-    std::string street1; 
-    std::string street2; 
-    std::string street3; 
-    std::string city; 
-    std::string stateorprovince; 
-    std::string postal_code; 
-    std::string country_code; 
+    std::string street1;
+    std::string street2;
+    std::string street3;
+    std::string city;
+    std::string stateorprovince;
+    std::string postal_code;
+    std::string country_code;
 
     PlaceAddress () {}
 
     PlaceAddress (
-        const std::string& _street1, 
-        const std::string& _street2, 
-        const std::string& _street3, 
-        const std::string& _city, 
-        const std::string& _stateorprovince, 
-        const std::string& _postal_code, 
+        const std::string& _street1,
+        const std::string& _street2,
+        const std::string& _street3,
+        const std::string& _city,
+        const std::string& _stateorprovince,
+        const std::string& _postal_code,
         const std::string& _country_code
-    ) : 
+    ) :
         street1(_street1),
         street2(_street2),
         street3(_street3),
@@ -69,7 +69,7 @@ struct PlaceAddress
         country_code(_country_code)
     {}
 
-    PlaceAddress (const PlaceAddress& other) 
+    PlaceAddress (const PlaceAddress& other)
     : street1(other.street1),
       street2(other.street2),
       street3(other.street3),
@@ -83,32 +83,32 @@ struct PlaceAddress
 struct Registrar
 {
     unsigned long long id; /**< database id of the registrar */
-    std::string handle; 
-    std::string name; 
-    std::string organization; 
-    std::string url; 
-    std::string phone; 
-    std::string fax; 
-    PlaceAddress address; 
+    std::string handle;
+    std::string name;
+    std::string organization;
+    std::string url;
+    std::string phone;
+    std::string fax;
+    PlaceAddress address;
 
     Registrar() {}
 
     Registrar(
-        const std::string& _handle, 
-        const std::string& _name, 
-        const std::string& _organization, 
-        const std::string& _url, 
-        const std::string& _phone, 
-        const std::string& _fax, 
+        const std::string& _handle,
+        const std::string& _name,
+        const std::string& _organization,
+        const std::string& _url,
+        const std::string& _phone,
+        const std::string& _fax,
         const PlaceAddress& _address
-    ) : 
+    ) :
       id(0),
-      handle(_handle), 
-      name(_name), 
-      organization(_organization), 
-      url(_url), 
-      phone(_phone), 
-      fax(_fax), 
+      handle(_handle),
+      name(_name),
+      organization(_organization),
+      url(_url),
+      phone(_phone),
+      fax(_fax),
       address(_address)
     {}
 };
@@ -134,99 +134,99 @@ struct ContactIdentification
  */
 struct Contact
 {
-    std::string handle; 
-    std::string   organization; 
-    bool disclose_organization; 
-    std::string   name; 
-    bool disclose_name; 
-    PlaceAddress  address; 
-    bool disclose_address; 
-    std::string   phone; 
-    bool disclose_phone; 
-    std::string   fax; 
-    bool disclose_fax; 
-    std::string   email; 
-    bool disclose_email; 
-    std::string   notify_email; 
-    bool disclose_notify_email; 
-    ContactIdentification identification; 
-    bool         disclose_identification; 
-    std::string   vat_number; 
-    bool disclose_vat_number; 
-    std::string creating_registrar; 
-    std::string sponsoring_registrar; 
-    boost::posix_time::ptime created; 
-    Nullable<boost::posix_time::ptime> changed; 
-    Nullable<boost::posix_time::ptime> last_transfer; 
-    std::vector<std::string> statuses; 
+    std::string handle;
+    std::string   organization;
+    bool disclose_organization;
+    std::string   name;
+    bool disclose_name;
+    PlaceAddress  address;
+    bool disclose_address;
+    std::string   phone;
+    bool disclose_phone;
+    std::string   fax;
+    bool disclose_fax;
+    std::string   email;
+    bool disclose_email;
+    std::string   notify_email;
+    bool disclose_notify_email;
+    ContactIdentification identification;
+    bool         disclose_identification;
+    std::string   vat_number;
+    bool disclose_vat_number;
+    std::string creating_registrar;
+    std::string sponsoring_registrar;
+    boost::posix_time::ptime created;
+    Nullable<boost::posix_time::ptime> changed;
+    Nullable<boost::posix_time::ptime> last_transfer;
+    std::vector<std::string> statuses;
 
-    Contact() {} 
+    Contact() {}
 
     Contact(
-        const std::string& _handle, 
-        const std::string& _organization, 
-        const std::string& _name, 
-        const PlaceAddress& _address, 
-        const std::string& _phone, 
-        const std::string& _fax, 
-        const std::string& _email, 
-        const std::string& _notify_email, 
-        const ContactIdentification& _identification, 
-        const std::string& _vat_number, 
-        const std::string& _creating_registrar, 
-        const std::string& _sponsoring_registrar, 
-        const boost::posix_time::ptime& _created, 
-        const Nullable<boost::posix_time::ptime>& _changed, 
-        const Nullable<boost::posix_time::ptime>& _last_transfer, 
-        const std::vector<std::string>& _statuses, 
-        bool _disclose_organization = false, 
-        bool _disclose_name = false, 
-        bool _disclose_address = false, 
-        bool _disclose_phone = false, 
-        bool _disclose_fax = false, 
-        bool _disclose_email = false, 
-        bool _disclose_notify_email = false, 
-        bool _disclose_identification = false, 
+        const std::string& _handle,
+        const std::string& _organization,
+        const std::string& _name,
+        const PlaceAddress& _address,
+        const std::string& _phone,
+        const std::string& _fax,
+        const std::string& _email,
+        const std::string& _notify_email,
+        const ContactIdentification& _identification,
+        const std::string& _vat_number,
+        const std::string& _creating_registrar,
+        const std::string& _sponsoring_registrar,
+        const boost::posix_time::ptime& _created,
+        const Nullable<boost::posix_time::ptime>& _changed,
+        const Nullable<boost::posix_time::ptime>& _last_transfer,
+        const std::vector<std::string>& _statuses,
+        bool _disclose_organization = false,
+        bool _disclose_name = false,
+        bool _disclose_address = false,
+        bool _disclose_phone = false,
+        bool _disclose_fax = false,
+        bool _disclose_email = false,
+        bool _disclose_notify_email = false,
+        bool _disclose_identification = false,
         bool _disclose_vat_number = false
-    ) : 
-        handle(_handle), 
-        organization(_organization), 
-        name(_name), 
-        address(_address), 
-        phone(_phone), 
-        fax(_fax), 
-        email(_email), 
-        notify_email(_notify_email), 
-        identification(_identification), 
-        vat_number(_vat_number), 
-        creating_registrar(_creating_registrar), 
-        sponsoring_registrar(_sponsoring_registrar), 
-        created(_created), 
-        changed(_changed), 
-        last_transfer(_last_transfer), 
-        statuses(_statuses), 
-        disclose_organization(_disclose_organization), 
-        disclose_name(_disclose_name), 
-        disclose_address(_disclose_address), 
-        disclose_phone(_disclose_phone), 
-        disclose_fax(_disclose_fax), 
-        disclose_email(_disclose_email), 
-        disclose_notify_email(_disclose_notify_email), 
-        disclose_identification(_disclose_identification), 
+    ) :
+        handle(_handle),
+        organization(_organization),
+        name(_name),
+        address(_address),
+        phone(_phone),
+        fax(_fax),
+        email(_email),
+        notify_email(_notify_email),
+        identification(_identification),
+        vat_number(_vat_number),
+        creating_registrar(_creating_registrar),
+        sponsoring_registrar(_sponsoring_registrar),
+        created(_created),
+        changed(_changed),
+        last_transfer(_last_transfer),
+        statuses(_statuses),
+        disclose_organization(_disclose_organization),
+        disclose_name(_disclose_name),
+        disclose_address(_disclose_address),
+        disclose_phone(_disclose_phone),
+        disclose_fax(_disclose_fax),
+        disclose_email(_disclose_email),
+        disclose_notify_email(_disclose_notify_email),
+        disclose_identification(_disclose_identification),
         disclose_vat_number(_disclose_vat_number)
     {}
 };
 
 struct NameServer
 {
-    std::string fqdn; 
-    std::vector<boost::asio::ip::address> ip_addresses; 
+    std::string fqdn;
+    std::vector<boost::asio::ip::address> ip_addresses;
 
     NameServer() {}
 
     NameServer(
-        const std::string& _fqdn, 
-        const std::vector<boost::asio::ip::address>& _ip_addresses 
+        const std::string& _fqdn,
+        const std::vector<boost::asio::ip::address>& _ip_addresses
     ) :
         fqdn(_fqdn),
         ip_addresses(_ip_addresses)
@@ -235,42 +235,42 @@ struct NameServer
 
 struct NSSet
 {
-    std::string handle; 
-    std::vector<NameServer> nservers; 
-    std::vector<std::string> tech_contacts; 
-    std::string creating_registrar; 
-    boost::posix_time::ptime created; 
-    Nullable<boost::posix_time::ptime> changed; 
-    Nullable<boost::posix_time::ptime> last_transfer; 
-    std::vector<std::string> statuses; 
+    std::string handle;
+    std::vector<NameServer> nservers;
+    std::vector<std::string> tech_contacts;
+    std::string sponsoring_registrar;
+    boost::posix_time::ptime created;
+    Nullable<boost::posix_time::ptime> changed;
+    Nullable<boost::posix_time::ptime> last_transfer;
+    std::vector<std::string> statuses;
 
     NSSet() {}
 
     NSSet(
-        const std::string& _handle, 
-        const std::vector<NameServer>& _nservers, 
-        const std::vector<std::string>& _tech_contacts, 
-        const std::string& _creating_registrar, 
-        const boost::posix_time::ptime& _created, 
-        const Nullable<boost::posix_time::ptime>& _changed, 
-        const Nullable<boost::posix_time::ptime>& _last_transfer, 
+        const std::string& _handle,
+        const std::vector<NameServer>& _nservers,
+        const std::vector<std::string>& _tech_contacts,
+        const std::string& _sponsoring_registrar,
+        const boost::posix_time::ptime& _created,
+        const Nullable<boost::posix_time::ptime>& _changed,
+        const Nullable<boost::posix_time::ptime>& _last_transfer,
         const std::vector<std::string>& _statuses
     ) :
-        handle(_handle), 
-        nservers(_nservers), 
-        tech_contacts(_tech_contacts), 
-        creating_registrar(_creating_registrar), 
-        created(_created), 
-        changed(_changed), 
-        last_transfer(_last_transfer), 
+        handle(_handle),
+        nservers(_nservers),
+        tech_contacts(_tech_contacts),
+        sponsoring_registrar(_sponsoring_registrar),
+        created(_created),
+        changed(_changed),
+        last_transfer(_last_transfer),
         statuses(_statuses)
     {}
 };
 
 struct NSSetSeq
 {
-    std::vector<NSSet> content; 
-    bool limit_exceeded; 
+    std::vector<NSSet> content;
+    bool limit_exceeded;
 
     NSSetSeq()
     : limit_exceeded(false)
@@ -288,72 +288,72 @@ struct NSSetSeq
  */
 struct DNSKey
 {
-    short flags; 
-    short protocol; 
-    short alg; 
-    std::string public_key; 
+    short flags;
+    short protocol;
+    short alg;
+    std::string public_key;
 
     DNSKey() {}
 
     DNSKey(
-        const short& _flags, 
-        const short& _protocol, 
-        const short& _alg, 
-        const std::string& _public_key 
+        const short& _flags,
+        const short& _protocol,
+        const short& _alg,
+        const std::string& _public_key
     ) :
-        flags(_flags), 
-        protocol(_protocol), 
-        alg(_alg), 
+        flags(_flags),
+        protocol(_protocol),
+        alg(_alg),
         public_key(_public_key)
     {}
 };
 
 struct KeySet
 {
-    std::string handle; 
-    std::vector<DNSKey> dns_keys; 
-    std::vector<std::string> tech_contacts; 
-    std::string creating_registrar; 
-    boost::posix_time::ptime created; 
-    Nullable<boost::posix_time::ptime> changed; 
-    Nullable<boost::posix_time::ptime> last_transfer; 
-    std::vector<std::string> statuses; 
+    std::string handle;
+    std::vector<DNSKey> dns_keys;
+    std::vector<std::string> tech_contacts;
+    std::string sponsoring_registrar;
+    boost::posix_time::ptime created;
+    Nullable<boost::posix_time::ptime> changed;
+    Nullable<boost::posix_time::ptime> last_transfer;
+    std::vector<std::string> statuses;
 
     KeySet() {}
 
     KeySet(
-        const std::string& _handle, 
-        const std::vector<DNSKey>& _dns_keys, 
-        const std::vector<std::string>& _tech_contacts, 
-        const std::string& _creating_registrar, 
-        const boost::posix_time::ptime& _created, 
-        const Nullable<boost::posix_time::ptime>& _changed, 
-        const Nullable<boost::posix_time::ptime>& _last_transfer, 
-        const std::vector<std::string>& _statuses 
+        const std::string& _handle,
+        const std::vector<DNSKey>& _dns_keys,
+        const std::vector<std::string>& _tech_contacts,
+        const std::string& _sponsoring_registrar,
+        const boost::posix_time::ptime& _created,
+        const Nullable<boost::posix_time::ptime>& _changed,
+        const Nullable<boost::posix_time::ptime>& _last_transfer,
+        const std::vector<std::string>& _statuses
     ) :
-        handle(_handle), 
-        dns_keys(_dns_keys), 
-        tech_contacts(_tech_contacts), 
-        creating_registrar(_creating_registrar), 
-        created(_created), 
-        changed(_changed), 
-        last_transfer(_last_transfer), 
+        handle(_handle),
+        dns_keys(_dns_keys),
+        tech_contacts(_tech_contacts),
+        sponsoring_registrar(_sponsoring_registrar),
+        created(_created),
+        changed(_changed),
+        last_transfer(_last_transfer),
         statuses(_statuses)
     {}
 };
 
 struct KeySetSeq
 {
-    std::vector<KeySet> content; 
-    bool limit_exceeded; 
+    std::vector<KeySet> content;
+    bool limit_exceeded;
 
     KeySetSeq()
     : limit_exceeded(false)
     {}
 
     KeySetSeq(
-        const std::vector<KeySet>& _content, 
-        bool _limit_exceeded = false 
+        const std::vector<KeySet>& _content,
+        bool _limit_exceeded = false
     ) :
         content(_content),
         limit_exceeded(_limit_exceeded)
@@ -367,7 +367,7 @@ struct Domain
     std::vector<std::string> admin_contacts; 
     std::string nsset; 
     std::string keyset; 
-    std::string creating_registrar; 
+    std::string sponsoring_registrar; 
     std::vector<std::string> statuses; 
     boost::posix_time::ptime registered; 
     Nullable<boost::posix_time::ptime> changed; 
@@ -383,7 +383,7 @@ struct Domain
         const std::vector<std::string>& _admin_contacts, 
         const std::string& _nsset, 
         const std::string& _keyset, 
-        const std::string& _creating_registrar, 
+        const std::string& _sponsoring_registrar, 
         const std::vector<std::string>& _statuses, 
         const boost::posix_time::ptime& _registered, 
         const Nullable<boost::posix_time::ptime>& _changed, 
@@ -396,7 +396,7 @@ struct Domain
         admin_contacts(_admin_contacts), 
         nsset(_nsset), 
         keyset(_keyset), 
-        creating_registrar(_creating_registrar), 
+        sponsoring_registrar(_sponsoring_registrar), 
         statuses(_statuses), 
         registered(_registered), 
         changed(_changed), 
@@ -408,15 +408,15 @@ struct Domain
 
 struct DomainSeq
 {
-    std::vector<Domain> content; 
-    bool limit_exceeded; 
+    std::vector<Domain> content;
+    bool limit_exceeded;
 
     DomainSeq()
     : limit_exceeded(false)
     {}
 
     DomainSeq(
-        const std::vector<Domain>& _content, 
+        const std::vector<Domain>& _content,
         bool _limit_exceeded = false
     )
     : content(_content),
@@ -426,52 +426,52 @@ struct DomainSeq
 
 struct RegistrarGroup
 {
-    std::string name; 
-    std::vector<std::string> members; 
+    std::string name;
+    std::vector<std::string> members;
 
     RegistrarGroup() {}
 
     RegistrarGroup(
-        const std::string& _name, 
-        const std::vector<std::string>& _members 
+        const std::string& _name,
+        const std::vector<std::string>& _members
     ) :
-        name(_name), 
+        name(_name),
         members(_members)
     {}
 };
 
 struct RegistrarCertification
 {
-    std::string registrar; 
-    short score; 
-    unsigned long long evaluation_file_id; 
+    std::string registrar;
+    short score;
+    unsigned long long evaluation_file_id;
 
     RegistrarCertification() {}
 
     RegistrarCertification(
-        const std::string& _registrar, 
-        short _score, 
-        unsigned long long _evaluation_file_id 
+        const std::string& _registrar,
+        short _score,
+        unsigned long long _evaluation_file_id
     ) :
-        registrar(_registrar), 
-        score(_score), 
-        evaluation_file_id(_evaluation_file_id) 
+        registrar(_registrar),
+        score(_score),
+        evaluation_file_id(_evaluation_file_id)
     {}
-};                               
+};
 
 struct ObjectStatusDesc
 {
-    std::string handle; 
-    std::string name; 
+    std::string handle;
+    std::string name;
 
     ObjectStatusDesc() {}
 
     ObjectStatusDesc(
-        const std::string& _handle, 
-        const std::string& _name 
+        const std::string& _handle,
+        const std::string& _name
     ) :
-        handle(_handle), 
-        name(_name) 
+        handle(_handle),
+        name(_name)
     {}
 };
 
@@ -547,7 +547,7 @@ struct MissingLocalization
 class Server_impl
 {
 public:
-    
+
     virtual ~Server_impl() {}
 
     /**
