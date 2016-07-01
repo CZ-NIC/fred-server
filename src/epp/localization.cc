e945b2384e1d504c65d5bf4ea3ea6f72a02f166f
Michal Strnad <michal.strnad@nic.cz>
2016-07-01 11:16:43 +0200

    Ticket #13992 - Simplify localization.cc file.

--- src/epp/localization.cc
+++ src/epp/localization.cc
@@ -17,85 +17,79 @@
 
 namespace Epp {
 
-static std::string get_reason(
-    Fred::OperationContext& _ctx,
-    const Reason::Enum _reason,
-    const SessionLang::Enum _lang
-) {
+namespace {
+
+std::string get_reason(
+    Fred::OperationContext &_ctx,
+    Reason::Enum _reason,
+    SessionLang::Enum _lang)
+{
+    const std::string column_name = (_lang == SessionLang::cs) ? "reason_cs"
+                                                               : "reason";
     const Database::Result res = _ctx.get_conn().exec_params(
-        "SELECT "
-            "reason AS reason_en_, "
-            "reason_cs AS reason_cz_ "
+        "SELECT " + column_name + " "
         "FROM enum_reason "
-        "WHERE id = $1::integer ",
-        Database::query_param_list( to_description_db_id(_reason) )
-    );
+        "WHERE id=$1::integer",
+        Database::query_param_list(to_description_db_id(_reason)));
 
-    if(res.size() != 1) {
+    if (res.size() < 1) {
         throw MissingLocalizedDescription();
     }
 
-    if(_lang == SessionLang::en) {
-        return static_cast<std::string>(res[0]["reason_en_"]);
-    }
-    if(_lang == SessionLang::cs) {
-        return static_cast<std::string>(res[0]["reason_cz_"]);
+    switch (_lang)
+    {
+        case SessionLang::en:
+        case SessionLang::cs:
+            return static_cast< std::string >(res[0][0]);
     }
 
     throw UnknownLocalizationLanguage();
 }
 
-static std::string get_response_msg(
-    Fred::OperationContext& _ctx,
-    const Response::Enum _response,
-    const SessionLang::Enum _lang
-) {
+std::string get_response_msg(
+    Fred::OperationContext &_ctx,
+    Response::Enum _response,
+    SessionLang::Enum _lang)
+{
+    const std::string column_name = (_lang == SessionLang::cs) ? "status_cs"
+                                                               : "status";
     const Database::Result res = _ctx.get_conn().exec_params(
-        "SELECT "
-            "status AS status_en_, "
-            "status_cs AS status_cz_ "
+        "SELECT " + column_name + " "
         "FROM enum_error "
-        "WHERE id = $1::integer ",
-        Database::query_param_list( to_description_db_id(_response) )
-    );
+        "WHERE id=$1::integer",
+        Database::query_param_list(to_description_db_id(_response)));
 
-    if(res.size() != 1) {
+    if (res.size() < 1) {
         throw MissingLocalizedDescription();
     }
 
-    if(_lang == SessionLang::en) {
-        return static_cast<std::string>(res[0]["status_en_"]);
-    }
-    if(_lang == SessionLang::cs) {
-        return static_cast<std::string>(res[0]["status_cz_"]);
+    switch (_lang)
+    {
+        case SessionLang::en:
+        case SessionLang::cs:
+            return static_cast< std::string >(res[0][0]);
     }
 
     throw UnknownLocalizationLanguage();
 }
 
-static std::set<LocalizedError> create_localized_errors(
-    Fred::OperationContext& _ctx,
-    const std::set<Error>& _errors,
-    const SessionLang::Enum _lang
-) {
-    std::set<LocalizedError> result;
+std::set< LocalizedError > create_localized_errors(
+    Fred::OperationContext &_ctx,
+    const std::set< Error > &_errors,
+    SessionLang::Enum _lang)
+{
+    std::set< LocalizedError > result;
 
-    BOOST_FOREACH(const Error& e, _errors) {
-        result.insert(
-            LocalizedError(
-                e.param,
-                e.position,
-                get_reason(
-                    _ctx,
-                    e.reason,
-                    _lang
-                )
-            )
-        );
+    for (std::set< Error >::const_iterator error_ptr = _errors.begin(); error_ptr != _errors.end(); ++error_ptr)
+    {
+        const std::string reason_description = get_reason(_ctx, error_ptr->reason, _lang);
+        result.insert(LocalizedError(error_ptr->param, error_ptr->position, reason_description));
     }
 
     return result;
 }
+
+}//namespace Epp::{anonymous}
 
 LocalizedFailResponse create_localized_fail_response(
     Fred::OperationContext& _ctx,
