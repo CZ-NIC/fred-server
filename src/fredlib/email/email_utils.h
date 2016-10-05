56d24a5dc759f8ee74ac92b79d4745e7942329ca
Mykyta Viazovskyi <mykyta.viazovskyi@nic.cz>
2016-10-05 13:26:47 +0200

    Ticket #16473 - add function to send emails to many addessees joined

--- src/fredlib/email/email_utils.h
+++ src/fredlib/email/email_utils.h
@@ -26,22 +26,21 @@ struct EmailData
     {}
 };
 
-struct FailedToSendMailToRecipient
+struct FailedToSendMailToRecipient : std::exception
 {
-    const std::string                failed_recipient;
-    const std::set<std::string>      skipped_recipients;
+    const std::string                  failed_recipient;
+    const std::set<std::string>        skipped_recipients;
 
     FailedToSendMailToRecipient(
-        const std::string&           _failed_recipient,
-        const std::set<std::string>& _skipped_recipients)
+        const std::string&             _failed_recipient,
+        const std::set<std::string>&   _skipped_recipients)
     : failed_recipient(_failed_recipient),
       skipped_recipients(_skipped_recipients)
     {}
+
+    virtual ~FailedToSendMailToRecipient() throw() {}
 };
 
-/**
- * @throws FailedToSendMailToRecipient
- */
 void send_email(boost::shared_ptr<Fred::Mailer::Manager> _mailer, const EmailData& _data)
 {
     std::set<std::string> trimmed_recipient_email_addresses;
@@ -72,4 +71,38 @@ void send_email(boost::shared_ptr<Fred::Mailer::Manager> _mailer, const EmailDat
         }
     }
 }
+
+unsigned long long send_joined_addresses_email(boost::shared_ptr<Fred::Mailer::Manager> _mailer, const EmailData& _data)
+{
+    std::set<std::string> trimmed_recipient_email_addresses;
+    BOOST_FOREACH(const std::string& email, _data.recipient_email_addresses)
+    {
+        trimmed_recipient_email_addresses.insert( boost::trim_copy(email) );
+    }
+
+    std::ostringstream oss;
+    for (std::set<std::string>::const_iterator it = trimmed_recipient_email_addresses.begin();
+            it != trimmed_recipient_email_addresses.end(); ++it)
+    {
+        oss << *it;
+    }
+    try
+    {
+        return _mailer->sendEmail(
+                "",
+                oss.str(),
+                "",
+                _data.template_name,
+                _data.template_parameters,
+                Fred::Mailer::Handles(),
+                Fred::Mailer::Attachments());
+    }
+    catch(const Fred::Mailer::NOT_SEND& e)
+    {
+        throw FailedToSendMailToRecipient(
+                oss.str(),
+                std::set<std::string>());
+    }
+}
+
 } // namespace Fred
