9aafe83a9749fa7db3669f2cdc0c3e7460253696
Jan Zima <jan.zima@nic.cz>
2016-11-03 13:08:02 +0100

    Ticket #15031 - billing failed exception fix

--- src/epp/domain/domain_billing.cc
+++ src/epp/domain/domain_billing.cc
@@ -36,95 +36,94 @@ namespace Epp
     {
         unsigned long long zone_id = Fred::Zone::find_zone_in_fqdn(
                 ctx, Fred::Zone::rem_trailing_dot(fqdn)).id;
-        try
+
+        //get_operation_payment_settings
+        Database::Result operation_price_list_result
+            = ctx.get_conn().exec_params(
+                "SELECT enable_postpaid_operation, operation_id, price, quantity"
+                " FROM price_list pl "
+                    " JOIN enum_operation eo ON pl.operation_id = eo.id "
+                    " JOIN zone z ON z.id = pl.zone_id "
+                " WHERE pl.valid_from <= $1::timestamp "
+                      " AND (pl.valid_to is NULL OR pl.valid_to > $1::timestamp ) "
+                " AND pl.zone_id = $2::bigint AND eo.operation = $3::text "
+            , Database::query_param_list(domain_create_timestamp_utc)(zone_id)("CreateDomain"));
+
+        if(operation_price_list_result.size() != 1)
         {
-            //get_operation_payment_settings
-            Database::Result operation_price_list_result
-                = ctx.get_conn().exec_params(
-                    "SELECT enable_postpaid_operation, operation_id, price, quantity"
-                    " FROM price_list pl "
-                        " JOIN enum_operation eo ON pl.operation_id = eo.id "
-                        " JOIN zone z ON z.id = pl.zone_id "
-                    " WHERE pl.valid_from <= $1::timestamp "
-                          " AND (pl.valid_to is NULL OR pl.valid_to > $1::timestamp ) "
-                    " AND pl.zone_id = $2::bigint AND eo.operation = $3::text "
-                , Database::query_param_list(domain_create_timestamp_utc)(zone_id)("CreateDomain"));
-
-            if(operation_price_list_result.size() != 1)
-            {
-                throw std::runtime_error("price_list result");
-            }
-
-            bool enable_postpaid_operation = operation_price_list_result[0][0];
-            unsigned long long operation_id = operation_price_list_result[0][1];
-            Decimal price_list_price = std::string(operation_price_list_result[0][2]);
-            Decimal  price_list_quantity = std::string(operation_price_list_result[0][3]);
-
-            if(price_list_quantity == Decimal("0"))
-            {
-                throw std::runtime_error("price_list_quantity == 0");
-            }
-
-            Decimal price =  price_list_price * Decimal("1") / price_list_quantity;//count_price
-
-            //get_registrar_credit - lock record in registrar_credit table for registrar and zone
-            Database::Result locked_registrar_credit_result
-                = ctx.get_conn().exec_params(
-                    "SELECT id, credit "
-                        " FROM registrar_credit "
-                        " WHERE registrar_id = $1::bigint "
-                            " AND zone_id = $2::bigint "
-                    " FOR UPDATE "
-                , Database::query_param_list(sponsoring_registrar_id)(zone_id));
-
-            if(locked_registrar_credit_result.size() != 1)
-            {
-                throw std::runtime_error("unable to get registrar_credit");
-            }
-
-            unsigned long long registrar_credit_id = locked_registrar_credit_result[0][0];
-            Decimal registrar_credit_balance = std::string(locked_registrar_credit_result[0][1]);
-
-            if(price != Decimal("0") && registrar_credit_balance < price && !enable_postpaid_operation)
-            {
-                throw std::runtime_error("insufficient balance");
-            }
-
-            // save info about debt into credit
-            Database::Result registrar_credit_transaction_result
-                = ctx.get_conn().exec_params(
-                    "INSERT INTO registrar_credit_transaction "
-                        " (id, balance_change, registrar_credit_id) "
-                        " VALUES (DEFAULT, $1::numeric , $2::bigint) "
-                    " RETURNING id "
-                , Database::query_param_list(Decimal("0") - price)(registrar_credit_id));
-
-            if(registrar_credit_transaction_result.size() != 1)
-            {
-                throw std::runtime_error("charge_operation: registrar_credit_transaction failed");
-            }
-
-            unsigned long long registrar_credit_transaction_id = registrar_credit_transaction_result[0][0];
-
-            // new record to invoice_operation
-            ctx.get_conn().exec_params(
-                "INSERT INTO invoice_operation "
-                " (id, object_id, registrar_id, operation_id, zone_id" //4
-                " , crdate, quantity, date_from,  date_to "
-                " , registrar_credit_transaction_id) "
-                "  VALUES (DEFAULT, $1::bigint, $2::bigint, $3::bigint, $4::bigint "
-                " , CURRENT_TIMESTAMP::timestamp, $5::integer, $6::date, NULL::date "
-                " , $7::bigint) "
-                //" RETURNING id "
-                , Database::query_param_list(created_domain_id)
-                (sponsoring_registrar_id)(operation_id)(zone_id)
-                ("1")(boost::date_time::c_local_adjustor<ptime>::utc_to_local(domain_create_timestamp_utc).date())
-                (registrar_credit_transaction_id)
-                );
+            ctx.get_log().error("price_list result");
+            throw std::runtime_error("price_list result");
         }
-        catch (...) {
+
+        bool enable_postpaid_operation = operation_price_list_result[0][0];
+        unsigned long long operation_id = operation_price_list_result[0][1];
+        Decimal price_list_price = std::string(operation_price_list_result[0][2]);
+        Decimal  price_list_quantity = std::string(operation_price_list_result[0][3]);
+
+        if(price_list_quantity == Decimal("0"))
+        {
+            ctx.get_log().error("price_list_quantity == 0");
+            throw std::runtime_error("price_list_quantity == 0");
+        }
+
+        Decimal price =  price_list_price * Decimal("1") / price_list_quantity;//count_price
+
+        //get_registrar_credit - lock record in registrar_credit table for registrar and zone
+        Database::Result locked_registrar_credit_result
+            = ctx.get_conn().exec_params(
+                "SELECT id, credit "
+                    " FROM registrar_credit "
+                    " WHERE registrar_id = $1::bigint "
+                        " AND zone_id = $2::bigint "
+                " FOR UPDATE "
+            , Database::query_param_list(sponsoring_registrar_id)(zone_id));
+
+        if(locked_registrar_credit_result.size() != 1)
+        {
+            ctx.get_log().error("unable to get registrar_credit");
+            throw std::runtime_error("unable to get registrar_credit");
+        }
+
+        unsigned long long registrar_credit_id = locked_registrar_credit_result[0][0];
+        Decimal registrar_credit_balance = std::string(locked_registrar_credit_result[0][1]);
+
+        if(price != Decimal("0") && registrar_credit_balance < price && !enable_postpaid_operation)
+        {
             throw BillingFailure();
         }
+
+        // save info about debt into credit
+        Database::Result registrar_credit_transaction_result
+            = ctx.get_conn().exec_params(
+                "INSERT INTO registrar_credit_transaction "
+                    " (id, balance_change, registrar_credit_id) "
+                    " VALUES (DEFAULT, $1::numeric , $2::bigint) "
+                " RETURNING id "
+            , Database::query_param_list(Decimal("0") - price)(registrar_credit_id));
+
+        if(registrar_credit_transaction_result.size() != 1)
+        {
+            ctx.get_log().error("charge_operation: registrar_credit_transaction failed");
+            throw std::runtime_error("charge_operation: registrar_credit_transaction failed");
+        }
+
+        unsigned long long registrar_credit_transaction_id = registrar_credit_transaction_result[0][0];
+
+        // new record to invoice_operation
+        ctx.get_conn().exec_params(
+            "INSERT INTO invoice_operation "
+            " (id, object_id, registrar_id, operation_id, zone_id" //4
+            " , crdate, quantity, date_from,  date_to "
+            " , registrar_credit_transaction_id) "
+            "  VALUES (DEFAULT, $1::bigint, $2::bigint, $3::bigint, $4::bigint "
+            " , CURRENT_TIMESTAMP::timestamp, $5::integer, $6::date, NULL::date "
+            " , $7::bigint) "
+            //" RETURNING id "
+            , Database::query_param_list(created_domain_id)
+            (sponsoring_registrar_id)(operation_id)(zone_id)
+            ("1")(boost::date_time::c_local_adjustor<ptime>::utc_to_local(domain_create_timestamp_utc).date())
+            (registrar_credit_transaction_id)
+            );
     }
 
     void renew_domain_bill_item(
@@ -139,102 +138,101 @@ namespace Epp
         //exception in find_zone_in_fqdn is not BillingFailure
         unsigned long long zone_id = Fred::Zone::find_zone_in_fqdn(
                 ctx, Fred::Zone::rem_trailing_dot(fqdn)).id;
-        try
+
+        //get_operation_payment_settings
+        Database::Result operation_price_list_result
+            = ctx.get_conn().exec_params(
+                "SELECT enable_postpaid_operation, operation_id, price, quantity"
+                " FROM price_list pl "
+                    " JOIN enum_operation eo ON pl.operation_id = eo.id "
+                    " JOIN zone z ON z.id = pl.zone_id "
+                " WHERE pl.valid_from <= $1::timestamp "
+                    " AND (pl.valid_to is NULL OR pl.valid_to > $1::timestamp ) "
+                " AND pl.zone_id = $2::bigint AND eo.operation = $3::text "
+            , Database::query_param_list(domain_renew_timestamp_utc)(zone_id)("RenewDomain"));
+
+        if(operation_price_list_result.size() != 1)
         {
-            //get_operation_payment_settings
-            Database::Result operation_price_list_result
-                = ctx.get_conn().exec_params(
-                    "SELECT enable_postpaid_operation, operation_id, price, quantity"
-                    " FROM price_list pl "
-                        " JOIN enum_operation eo ON pl.operation_id = eo.id "
-                        " JOIN zone z ON z.id = pl.zone_id "
-                    " WHERE pl.valid_from <= $1::timestamp "
-                        " AND (pl.valid_to is NULL OR pl.valid_to > $1::timestamp ) "
-                    " AND pl.zone_id = $2::bigint AND eo.operation = $3::text "
-                , Database::query_param_list(domain_renew_timestamp_utc)(zone_id)("RenewDomain"));
-
-            if(operation_price_list_result.size() != 1)
-            {
-                throw std::runtime_error("price_list result");
-            }
-
-            bool enable_postpaid_operation = operation_price_list_result[0][0];
-            unsigned long long operation_id = operation_price_list_result[0][1];
-            Decimal price_list_price = std::string(operation_price_list_result[0][2]);
-            Decimal  price_list_quantity = std::string(operation_price_list_result[0][3]);
-
-            if(price_list_quantity == Decimal("0"))
-            {
-                throw std::runtime_error("price_list_quantity == 0");
-            }
-
-            Decimal price =  price_list_price
-                * Decimal(boost::lexical_cast<std::string>(length_of_domain_registration_in_years))
-                / price_list_quantity;//count_price
-
-            ctx.get_log().debug(boost::format("price_list_price: %1% price_list_quantity: %2% price: %3%")
-            % price_list_price.get_string() % price_list_quantity.get_string() % price.get_string());
-
-            //get_registrar_credit - lock record in registrar_credit table for registrar and zone
-            Database::Result locked_registrar_credit_result
-                = ctx.get_conn().exec_params(
-                    "SELECT id, credit "
-                        " FROM registrar_credit "
-                        " WHERE registrar_id = $1::bigint "
-                            " AND zone_id = $2::bigint "
-                    " FOR UPDATE "
-                , Database::query_param_list(sponsoring_registrar_id)(zone_id));
-
-            if(locked_registrar_credit_result.size() != 1)
-            {
-                throw std::runtime_error("unable to get registrar_credit");
-            }
-
-            unsigned long long registrar_credit_id = locked_registrar_credit_result[0][0];
-            Decimal registrar_credit_balance = std::string(locked_registrar_credit_result[0][1]);
-
-            if(price != Decimal("0") && registrar_credit_balance < price && !enable_postpaid_operation)
-            {
-                throw std::runtime_error("insufficient balance");
-            }
-
-            // save info about debt into credit
-            Database::Result registrar_credit_transaction_result
-                = ctx.get_conn().exec_params(
-                    "INSERT INTO registrar_credit_transaction "
-                        " (id, balance_change, registrar_credit_id) "
-                        " VALUES (DEFAULT, $1::numeric , $2::bigint) "
-                    " RETURNING id "
-                , Database::query_param_list(Decimal("0") - price)(registrar_credit_id));
-
-            if(registrar_credit_transaction_result.size() != 1)
-            {
-                throw std::runtime_error("charge_operation: registrar_credit_transaction failed");
-            }
-
-            unsigned long long registrar_credit_transaction_id = registrar_credit_transaction_result[0][0];
-
-            // new record to invoice_operation
-            ctx.get_conn().exec_params(
-                "INSERT INTO invoice_operation "
-                " (id, object_id, registrar_id, operation_id, zone_id" //4
-                " , crdate, quantity, date_from,  date_to "
-                " , registrar_credit_transaction_id) "
-                "  VALUES (DEFAULT, $1::bigint, $2::bigint, $3::bigint, $4::bigint "
-                " , CURRENT_TIMESTAMP::timestamp, $5::integer, $6::date, $7::date "
-                " , $8::bigint) "
-                //" RETURNING id "
-                , Database::query_param_list(renewed_domain_id)
-                (sponsoring_registrar_id)(operation_id)(zone_id)
-                (length_of_domain_registration_in_years)
-                (boost::date_time::c_local_adjustor<ptime>::utc_to_local(domain_renew_timestamp_utc).date())
-                (domain_expiration_date_local)
-                (registrar_credit_transaction_id)
-                );
+            ctx.get_log().error("price_list result");
+            throw std::runtime_error("price_list result");
         }
-        catch (...) {
+
+        bool enable_postpaid_operation = operation_price_list_result[0][0];
+        unsigned long long operation_id = operation_price_list_result[0][1];
+        Decimal price_list_price = std::string(operation_price_list_result[0][2]);
+        Decimal  price_list_quantity = std::string(operation_price_list_result[0][3]);
+
+        if(price_list_quantity == Decimal("0"))
+        {
+            ctx.get_log().error("price_list_quantity == 0");
+            throw std::runtime_error("price_list_quantity == 0");
+        }
+
+        Decimal price =  price_list_price
+            * Decimal(boost::lexical_cast<std::string>(length_of_domain_registration_in_years))
+            / price_list_quantity;//count_price
+
+        ctx.get_log().debug(boost::format("price_list_price: %1% price_list_quantity: %2% price: %3%")
+        % price_list_price.get_string() % price_list_quantity.get_string() % price.get_string());
+
+        //get_registrar_credit - lock record in registrar_credit table for registrar and zone
+        Database::Result locked_registrar_credit_result
+            = ctx.get_conn().exec_params(
+                "SELECT id, credit "
+                    " FROM registrar_credit "
+                    " WHERE registrar_id = $1::bigint "
+                        " AND zone_id = $2::bigint "
+                " FOR UPDATE "
+            , Database::query_param_list(sponsoring_registrar_id)(zone_id));
+
+        if(locked_registrar_credit_result.size() != 1)
+        {
+            ctx.get_log().error("unable to get registrar_credit");
+            throw std::runtime_error("unable to get registrar_credit");
+        }
+
+        unsigned long long registrar_credit_id = locked_registrar_credit_result[0][0];
+        Decimal registrar_credit_balance = std::string(locked_registrar_credit_result[0][1]);
+
+        if(price != Decimal("0") && registrar_credit_balance < price && !enable_postpaid_operation)
+        {
             throw BillingFailure();
         }
+
+        // save info about debt into credit
+        Database::Result registrar_credit_transaction_result
+            = ctx.get_conn().exec_params(
+                "INSERT INTO registrar_credit_transaction "
+                    " (id, balance_change, registrar_credit_id) "
+                    " VALUES (DEFAULT, $1::numeric , $2::bigint) "
+                " RETURNING id "
+            , Database::query_param_list(Decimal("0") - price)(registrar_credit_id));
+
+        if(registrar_credit_transaction_result.size() != 1)
+        {
+            ctx.get_log().error("charge_operation: registrar_credit_transaction failed");
+            throw std::runtime_error("charge_operation: registrar_credit_transaction failed");
+        }
+
+        unsigned long long registrar_credit_transaction_id = registrar_credit_transaction_result[0][0];
+
+        // new record to invoice_operation
+        ctx.get_conn().exec_params(
+            "INSERT INTO invoice_operation "
+            " (id, object_id, registrar_id, operation_id, zone_id" //4
+            " , crdate, quantity, date_from,  date_to "
+            " , registrar_credit_transaction_id) "
+            "  VALUES (DEFAULT, $1::bigint, $2::bigint, $3::bigint, $4::bigint "
+            " , CURRENT_TIMESTAMP::timestamp, $5::integer, $6::date, $7::date "
+            " , $8::bigint) "
+            //" RETURNING id "
+            , Database::query_param_list(renewed_domain_id)
+            (sponsoring_registrar_id)(operation_id)(zone_id)
+            (length_of_domain_registration_in_years)
+            (boost::date_time::c_local_adjustor<ptime>::utc_to_local(domain_renew_timestamp_utc).date())
+            (domain_expiration_date_local)
+            (registrar_credit_transaction_id)
+            );
     }
 
 }//namespace Epp
