d6da3128174f39220437893eaddda682572d3a9e
Jan Zima <jan.zima@nic.cz>
2017-01-13 12:02:02 +0100

    Ticket #15031 - renew period policy test fix

--- tests/interfaces/epp/domain/test_domain_renew_impl.cc
+++ tests/interfaces/epp/domain/test_domain_renew_impl.cc
@@ -174,7 +174,7 @@ BOOST_FIXTURE_TEST_CASE(renew_small_period, HasDomainData)
         BOOST_CHECK(ex.get().size() == 1);
         BOOST_CHECK(ex.get().rbegin()->param == Epp::Param::domain_period);
         BOOST_CHECK(ex.get().rbegin()->position == 0);
-        BOOST_CHECK(ex.get().rbegin()->reason == Epp::Reason::period_range);
+        BOOST_CHECK(ex.get().rbegin()->reason == Epp::Reason::period_policy);
     }
     catch(...)
     {
