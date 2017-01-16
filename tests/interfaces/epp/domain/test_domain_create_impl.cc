8383f0ae69dbd08708e45fe18317699497d6d134
Jan Zima <jan.zima@nic.cz>
2017-01-16 13:58:58 +0100

    Ticket #15031 - create domain tests

--- tests/interfaces/epp/domain/test_domain_create_impl.cc
+++ tests/interfaces/epp/domain/test_domain_create_impl.cc
@@ -86,6 +86,66 @@ BOOST_FIXTURE_TEST_CASE(create_invalid_fqdn_zone, HasDomainData)
     }
 }
 
+BOOST_FIXTURE_TEST_CASE(create_invalid_cz_zone, HasDomainData)
+{
+    domain1_create_input_data.fqdn = std::string("cz");
+
+    BOOST_TEST_MESSAGE(std::string("domain1_create_input_data.fqdn ") << domain1_create_input_data.fqdn);
+    BOOST_TEST_MESSAGE(std::string("info_registrar_data_.id ") << info_registrar_data_.id);
+
+    try{
+        Epp::domain_create_impl(
+            ctx,
+            domain1_create_input_data,
+            info_registrar_data_.id,
+            42
+        );
+        BOOST_ERROR("exception expected");
+    }
+    catch(const Epp::ParameterValuePolicyError& ex)
+    {
+        BOOST_TEST_MESSAGE("Epp::ParameterValuePolicyError");
+        BOOST_CHECK(ex.get().size() == 1);
+        BOOST_CHECK(ex.get().rbegin()->param == Epp::Param::domain_fqdn);
+        BOOST_CHECK(ex.get().rbegin()->position == 0);
+        BOOST_CHECK(ex.get().rbegin()->reason == Epp::Reason::not_applicable_domain);
+    }
+    catch(...)
+    {
+        BOOST_ERROR("unexpected exception type");
+    }
+}
+
+BOOST_FIXTURE_TEST_CASE(fqdn_syntax_dot_cz, HasDomainData)
+{
+    domain1_create_input_data.fqdn = std::string(".cz");
+
+    BOOST_TEST_MESSAGE(std::string("domain1_create_input_data.fqdn ") << domain1_create_input_data.fqdn);
+    BOOST_TEST_MESSAGE(std::string("info_registrar_data_.id ") << info_registrar_data_.id);
+
+    try{
+        Epp::domain_create_impl(
+            ctx,
+            domain1_create_input_data,
+            info_registrar_data_.id,
+            42
+        );
+        BOOST_ERROR("exception expected");
+    }
+    catch(const Epp::ParameterValueSyntaxError& ex)
+    {
+        BOOST_TEST_MESSAGE("Epp::ParameterValueSyntaxError");
+        BOOST_CHECK(ex.get().size() == 1);
+        BOOST_CHECK(ex.get().rbegin()->param == Epp::Param::domain_fqdn);
+        BOOST_CHECK(ex.get().rbegin()->position == 0);
+        BOOST_CHECK(ex.get().rbegin()->reason == Epp::Reason::bad_format_fqdn);
+    }
+    catch(...)
+    {
+        BOOST_ERROR("unexpected exception type");
+    }
+}
+
 BOOST_FIXTURE_TEST_CASE(fqdn_syntax_front_hyphen, HasDomainData)
 {
     domain1_create_input_data.fqdn = std::string("-") + domain1_create_input_data.fqdn;
