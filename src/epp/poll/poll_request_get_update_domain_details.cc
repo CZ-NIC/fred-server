209ff426303d01e6f8d53b1ecd69d69c37878c8d
Miroslav Franc <miroslav.franc@nic.cz>
2017-03-06 13:46:55 +0100

    Ticket #17726 - return its own struct to the localization layer & rename some badly named macros

--- src/epp/poll/poll_request_get_update_domain_details.cc
+++ src/epp/poll/poll_request_get_update_domain_details.cc
@@ -24,8 +24,84 @@
 #include "src/epp/impl/epp_result_success.h"
 #include "util/db/param_query_composition.h"
 
+#include "src/fredlib/object_state/get_object_states.h"
+#include "src/fredlib/registrar/info_registrar.h"
+
 namespace Epp {
 namespace Poll {
+
+namespace {
+
+// could be merged with code from info_domain?
+InfoDomainOutputData get_info_domain_output_data(
+    Fred::OperationContext& _ctx,
+    unsigned long long _session_registrar_id,
+    unsigned long long _history_id)
+{
+    const Fred::InfoDomainData info_domain_data =
+        Fred::InfoDomainHistoryByHistoryid(_history_id).exec(_ctx).info_domain_data;
+
+    InfoDomainOutputData ret;
+
+    ret.roid = info_domain_data.roid;
+    ret.fqdn = info_domain_data.fqdn;
+    ret.registrant = info_domain_data.registrant.handle;
+    ret.nsset = info_domain_data.nsset.isnull()
+        ? Nullable<std::string>()
+        : Nullable<std::string>(info_domain_data.nsset.get_value().handle);
+    ret.keyset = info_domain_data.keyset.isnull()
+        ? Nullable<std::string>()
+        : Nullable<std::string>(info_domain_data.keyset.get_value().handle);
+
+    {
+        typedef std::vector<Fred::ObjectStateData> ObjectStatesData;
+        ObjectStatesData domain_states_data = Fred::GetObjectStates(info_domain_data.id).exec(_ctx);
+        for (ObjectStatesData::const_iterator object_state_ptr = domain_states_data.begin();
+             object_state_ptr != domain_states_data.end(); ++object_state_ptr)
+        {
+            if (object_state_ptr->is_external) {
+                ret.states.insert(
+                    Conversion::Enums::from_db_handle<Fred::Object_State>(object_state_ptr->state_name));
+            }
+        }
+    }
+
+    ret.sponsoring_registrar_handle = info_domain_data.sponsoring_registrar_handle;
+    ret.creating_registrar_handle = info_domain_data.create_registrar_handle;
+    ret.last_update_registrar_handle = info_domain_data.update_registrar_handle;
+
+    ret.crdate = info_domain_data.creation_time;
+    ret.last_update = info_domain_data.update_time;
+    ret.last_transfer = info_domain_data.transfer_time;
+    ret.exdate = info_domain_data.expiration_date;
+
+    const std::string callers_registrar_handle =
+        Fred::InfoRegistrarById(_session_registrar_id).exec(_ctx).info_registrar_data.handle;
+    const bool callers_is_sponsoring_registrar = info_domain_data.sponsoring_registrar_handle == callers_registrar_handle;
+    const bool authinfopw_has_to_be_hidden = !callers_is_sponsoring_registrar;
+    ret.authinfopw = authinfopw_has_to_be_hidden
+        ? boost::optional<std::string>()
+        : info_domain_data.authinfopw;
+
+    for (
+        std::vector<Fred::ObjectIdHandlePair>::const_iterator object_id_handle_pair = info_domain_data.admin_contacts.begin();
+        object_id_handle_pair != info_domain_data.admin_contacts.end();
+        ++object_id_handle_pair)
+    {
+        ret.admin.insert(object_id_handle_pair->handle);
+    }
+
+    ret.ext_enum_domain_validation =
+        info_domain_data.enum_domain_validation.isnull() ? Nullable<Epp::Domain::EnumValidationExtension>()
+        : Epp::Domain::EnumValidationExtension(
+            info_domain_data.enum_domain_validation.get_value().validation_expiration,
+            info_domain_data.enum_domain_validation.get_value().publish
+            );
+
+    return ret;
+}
+
+}
 
 PollRequestUpdateDomainOutputData poll_request_get_update_domain_details(
     Fred::OperationContext& _ctx,
@@ -62,8 +138,8 @@ PollRequestUpdateDomainOutputData poll_request_get_update_domain_details(
 
     PollRequestUpdateDomainOutputData ret;
     try {
-        ret.old_data = Fred::InfoDomainHistoryByHistoryid(old_history_id).exec(_ctx);
-        ret.new_data = Fred::InfoDomainHistoryByHistoryid(new_history_id).exec(_ctx);
+        ret.old_data = get_info_domain_output_data(_ctx, _registrar_id, old_history_id);
+        ret.new_data = get_info_domain_output_data(_ctx, _registrar_id, new_history_id);
     }
     catch(const Fred::InfoDomainHistoryByHistoryid::Exception&) {
         throw EppResponseFailure(EppResultFailure(EppResultCode::command_failed));
--- src/epp/poll/poll_request_get_update_domain_details.h
+++ src/epp/poll/poll_request_get_update_domain_details.h
@@ -16,19 +16,55 @@
  * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef POLL_POLL_REQUEST_GET_UPDATE_DOMAIN_DETAILS_H_EE5157F1F74A49D28A6B8F4CC22BB363
-#define POLL_POLL_REQUEST_GET_UPDATE_DOMAIN_DETAILS_H_EE5157F1F74A49D28A6B8F4CC22BB363
+#ifndef POLL_REQUEST_GET_UPDATE_DOMAIN_DETAILS_H_EE5157F1F74A49D28A6B8F4CC22BB363
+#define POLL_REQUEST_GET_UPDATE_DOMAIN_DETAILS_H_EE5157F1F74A49D28A6B8F4CC22BB363
 
 #include "src/fredlib/opcontext.h"
 #include "src/fredlib/domain/info_domain.h"
 
+#include "src/epp/domain/impl/domain_enum_validation.h"
+#include "src/fredlib/domain/enum_validation_extension.h"
+#include "src/fredlib/object/object_state.h"
+#include "src/fredlib/opcontext.h"
+#include "util/db/nullable.h"
+
+#include <boost/date_time/gregorian/greg_date.hpp>
+#include <boost/date_time/posix_time/ptime.hpp>
+#include <boost/optional.hpp>
+
+#include <set>
+#include <string>
+
 namespace Epp {
 namespace Poll {
 
+// could be merged with code from info_domain?
+struct InfoDomainOutputData {
+    typedef std::set<Fred::Object_State::Enum> States;
+
+    std::string roid;
+    std::string fqdn;
+    std::string registrant;
+    Nullable<std::string> nsset;
+    Nullable<std::string> keyset;
+    States states;
+    std::string sponsoring_registrar_handle;
+    std::string creating_registrar_handle;
+    Nullable<std::string> last_update_registrar_handle;
+    boost::posix_time::ptime crdate;
+    Nullable<boost::posix_time::ptime> last_update;
+    Nullable<boost::posix_time::ptime> last_transfer;
+    boost::gregorian::date exdate;
+    boost::optional<std::string> authinfopw;
+    std::set<std::string> admin;
+    Nullable<Epp::Domain::EnumValidationExtension> ext_enum_domain_validation;
+    std::set<std::string> tmpcontact;
+};
+
 struct PollRequestUpdateDomainOutputData
 {
-    Fred::InfoDomainOutput old_data;
-    Fred::InfoDomainOutput new_data;
+    InfoDomainOutputData old_data;
+    InfoDomainOutputData new_data;
 };
 
 PollRequestUpdateDomainOutputData poll_request_get_update_domain_details(
