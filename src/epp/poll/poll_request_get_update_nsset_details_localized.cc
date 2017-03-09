778ddcb99edf9fb3076160713de71dc823b16b73
Miroslav Franc <miroslav.franc@nic.cz>
2017-03-09 19:13:29 +0100

    Ticket #17726 - attempt at unifying poll update with info messages at the localization layer

---
+++ src/epp/poll/poll_request_get_update_nsset_details_localized.cc
@@ -0,0 +1,166 @@
+/*
+ * Copyright (C) 2017  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "src/epp/poll/poll_request_get_update_nsset_details_localized.h"
+#include "src/epp/poll/poll_request_get_update_nsset_details.h"
+#include "src/epp/nsset/info_nsset.h"
+#include "src/fredlib/object_state/get_object_states.h"
+#include "src/epp/impl/localization.h"
+#include "src/epp/impl/action.h"
+#include "src/epp/impl/epp_response_failure.h"
+#include "src/epp/impl/epp_response_failure_localized.h"
+#include "src/epp/impl/epp_response_success.h"
+#include "src/epp/impl/epp_response_success_localized.h"
+#include "src/epp/impl/epp_result_code.h"
+#include "src/epp/impl/epp_result_failure.h"
+#include "src/epp/impl/epp_result_success.h"
+#include "src/epp/impl/localization.h"
+#include "util/log/context.h"
+
+#include <boost/format.hpp>
+#include <boost/format/free_funcs.hpp>
+
+#include <stdexcept>
+
+namespace Epp {
+namespace Poll {
+
+namespace {
+
+// a bunch of workarounds -- taken from info_nsset_localized.cc
+// TODO udelat pres ziskani externich stavu, zatim na to neni ve fredlibu rozhrani
+// TODO Fred::GetObjectStates pro "handle"
+void filter_states(
+    Fred::OperationContext& _ctx,
+    Epp::Nsset::InfoNssetOutputData& _output_data)
+{
+    std::set<std::string> filtered_states;
+
+    const std::vector<Fred::ObjectStateData> state_definitions =
+        Fred::GetObjectStates(
+            Fred::InfoNssetByHandle(_output_data.handle).exec(_ctx).info_nsset_data.id).exec(_ctx);
+
+    for(std::set<std::string>::const_iterator state_it = _output_data.states.begin();
+        state_it != _output_data.states.end();
+        ++state_it)
+    {
+        for(std::vector<Fred::ObjectStateData>::const_iterator state_def_it = state_definitions.begin();
+            state_def_it != state_definitions.end();
+            ++state_def_it)
+        {
+            if (state_def_it->is_external)
+            {
+                filtered_states.insert(*state_it);
+            }
+        }
+    }
+    _output_data.states = filtered_states;
+
+    if (_output_data.states.empty())
+    {
+        _output_data.states.insert("ok");
+    }
+}
+
+} // namespace Epp::Poll::{anonymous}
+
+PollRequestUpdateNssetLocalizedResponse poll_request_get_update_nsset_details_localized(
+    unsigned long long _message_id,
+    const SessionData& _session_data,
+    const std::string& _server_transaction_handle)
+{
+    try {
+        Logging::Context logging_ctx("rifd");
+        Logging::Context logging_ctx2(boost::str(boost::format("clid-%1%") % _session_data.registrar_id));
+        Logging::Context logging_ctx3(_server_transaction_handle);
+        Logging::Context logging_ctx4(boost::str(boost::format("action-%1%") % static_cast<unsigned>(Action::PollResponse)));
+
+        Fred::OperationContextCreator ctx;
+
+        PollRequestUpdateNssetOutputData output_data =
+            poll_request_get_update_nsset_details(ctx, _message_id, _session_data.registrar_id);
+
+        filter_states(ctx, output_data.old_data);
+        filter_states(ctx, output_data.new_data);
+
+        const PollRequestUpdateNssetLocalizedOutputData localized_output_data =
+            PollRequestUpdateNssetLocalizedOutputData(
+                Epp::Nsset::InfoNssetLocalizedOutputData(
+                    output_data.old_data.handle,
+                    output_data.old_data.roid,
+                    output_data.old_data.sponsoring_registrar_handle,
+                    output_data.old_data.creating_registrar_handle,
+                    output_data.old_data.last_update_registrar_handle,
+                    localize_object_states_deprecated(ctx, output_data.old_data.states, _session_data.lang),
+                    output_data.old_data.crdate,
+                    output_data.old_data.last_update,
+                    output_data.old_data.last_transfer,
+                    output_data.old_data.authinfopw,
+                    output_data.old_data.dns_hosts,
+                    output_data.old_data.tech_contacts,
+                    output_data.old_data.tech_check_level),
+                Epp::Nsset::InfoNssetLocalizedOutputData(
+                    output_data.new_data.handle,
+                    output_data.new_data.roid,
+                    output_data.new_data.sponsoring_registrar_handle,
+                    output_data.new_data.creating_registrar_handle,
+                    output_data.new_data.last_update_registrar_handle,
+                    localize_object_states_deprecated(ctx, output_data.new_data.states, _session_data.lang),
+                    output_data.new_data.crdate,
+                    output_data.new_data.last_update,
+                    output_data.new_data.last_transfer,
+                    output_data.new_data.authinfopw,
+                    output_data.new_data.dns_hosts,
+                    output_data.new_data.tech_contacts,
+                    output_data.new_data.tech_check_level));
+
+        return PollRequestUpdateNssetLocalizedResponse(
+            EppResponseSuccessLocalized(
+                ctx,
+                EppResponseSuccess(EppResultSuccess(EppResultCode::command_completed_successfully)),
+                _session_data.lang),
+            localized_output_data);
+    }
+    catch (const EppResponseFailure& e) {
+        Fred::OperationContextCreator ctx;
+        ctx.get_log().info(std::string("poll_request_nsset_update_details_localized: ") + e.what());
+        throw EppResponseFailureLocalized(
+            ctx,
+            e,
+            _session_data.lang);
+    }
+    catch (const std::exception& e) {
+        Fred::OperationContextCreator ctx;
+        ctx.get_log().info(std::string("poll_request_nsset_update_details_localized: ") + e.what());
+        throw EppResponseFailureLocalized(
+            ctx,
+            EppResponseFailure(EppResultFailure(EppResultCode::command_failed)),
+            _session_data.lang);
+    }
+    catch (...) {
+        Fred::OperationContextCreator ctx;
+        ctx.get_log().info(std::string("unexpected exception in poll_request_nsset_update_details_localized function"));
+        throw EppResponseFailureLocalized(
+            ctx,
+            EppResponseFailure(EppResultFailure(EppResultCode::command_failed)),
+            _session_data.lang);
+    }
+}
+
+} // namespace Epp::Poll
+} // namespace Epp
---
+++ src/epp/poll/poll_request_get_update_nsset_details_localized.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2017  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef POLL_REQUEST_GET_UPDATE_NSSET_DETAILS_LOCALIZED_H_A84E5561C46F4690A026D028B3DA45D5
+#define POLL_REQUEST_GET_UPDATE_NSSET_DETAILS_LOCALIZED_H_A84E5561C46F4690A026D028B3DA45D5
+
+#include "src/epp/nsset/impl/info_nsset_localized_output_data.h"
+#include "src/epp/impl/epp_response_success_localized.h"
+#include "src/epp/impl/session_data.h"
+
+#include <string>
+
+namespace Epp {
+namespace Poll {
+
+struct PollRequestUpdateNssetLocalizedOutputData
+{
+    Epp::Nsset::InfoNssetLocalizedOutputData old_data;
+    Epp::Nsset::InfoNssetLocalizedOutputData new_data;
+
+    PollRequestUpdateNssetLocalizedOutputData(
+        const Epp::Nsset::InfoNssetLocalizedOutputData& _old_data,
+        const Epp::Nsset::InfoNssetLocalizedOutputData& _new_data)
+    :
+        old_data(_old_data),
+        new_data(_new_data)
+    {}
+};
+
+struct PollRequestUpdateNssetLocalizedResponse
+{
+    EppResponseSuccessLocalized epp_response;
+    PollRequestUpdateNssetLocalizedOutputData data;
+
+    PollRequestUpdateNssetLocalizedResponse(
+        const EppResponseSuccessLocalized& _epp_response,
+        const PollRequestUpdateNssetLocalizedOutputData& _data)
+    :
+        epp_response(_epp_response),
+        data(_data)
+    {}
+};
+
+PollRequestUpdateNssetLocalizedResponse poll_request_get_update_nsset_details_localized(
+    unsigned long long _message_id,
+    const SessionData& _session_data,
+    const std::string& _server_transaction_handle);
+
+} // namespace Epp::Poll
+} // namespace Epp
+
+#endif
