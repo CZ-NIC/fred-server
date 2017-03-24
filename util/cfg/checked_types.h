1f5595818569000c6fab26521c94df8cd576fe01
Jan Zima <jan.zima@nic.cz>
2017-03-24 13:22:48 +0100

    Ticket #18515 - checked types header

---
+++ util/cfg/checked_types.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2017  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ *  @file
+ *  checked config option types
+ */
+
+#ifndef CHECKED_TYPES_H_f12642247ed944ddaee5a054d16eead9
+#define CHECKED_TYPES_H_f12642247ed944ddaee5a054d16eead9
+
+#include <string>
+
+// types checked by custom validator
+struct Checked
+{
+    typedef std::string string;
+    typedef unsigned long long ulonglong;
+    typedef unsigned long ulong;
+    typedef ulonglong id;
+    typedef double fpnumber;
+    typedef boost::gregorian::date date;
+    typedef boost::posix_time::ptime ptime;
+    typedef unsigned short ushort;
+
+    class string_fpnumber
+    {
+        std::string s_;
+    public:
+        std::string to_string() const
+        {
+            return s_;
+        }
+
+        explicit string_fpnumber(const std::string _s)
+        : s_(_s)
+        {}
+
+        string_fpnumber()
+        {}
+
+        string_fpnumber(const string_fpnumber & _s)
+        : s_(_s.to_string())
+        {}
+
+        string_fpnumber & operator=(const string_fpnumber & rhs)
+        {
+            s_ = rhs.s_;
+            return *this;
+        }
+
+        string_fpnumber & operator=(const std::string & rhs)
+        {
+            s_ = rhs;
+            return *this;
+        }
+
+        bool operator==(const string_fpnumber & rhs) const
+        {
+            return s_ == rhs.s_;
+        }
+
+        bool operator<(const string_fpnumber & rhs) const
+        {
+            return s_ < rhs.s_;
+        }
+    };
+
+};
+
+
+#endif
