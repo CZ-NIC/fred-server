d3243473a5228dc9baeae300d8758f0a9adedf76
Ales Friedl <ales.friedl@nic.cz>
2017-04-04 10:13:30 +0200

    Ticket #17427 - remove code already commented out

--- src/epp/epp_response_failure.h
+++ src/epp/epp_response_failure.h
@@ -69,22 +69,9 @@ public:
         return epp_results_.front();
     }
 
-    //EppResultFailure& first_epp_result_with_code(EppResultCode::Failure _epp_result_code) {
-    //    for (std::vector<EppResultFailure>::iterator epp_result = epp_results_.begin();
-    //         epp_result != epp_results_.end();
-    //         ++epp_result)
-    //    {
-    //        if (epp_result->epp_result_code() == _epp_result_code) {
-    //            return *epp_result;
-    //        }
-    //    }
-    //    epp_results_.push_back(EppResultFailure(_epp_result_code));
-    //    return epp_results_.back();
-    //}
-    //
     /**
-     * For now, we have only one epp_result,
-     * so its description is used to describe this exception.
+     * As for now, we have only one epp_result, so its
+     * description is used to describe this exception.
      */
     const char* what() const throw() {
         return epp_result().c_str();
