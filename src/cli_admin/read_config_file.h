585f70ae9f2ded79c6950c786dc11c0faf2dfe25
Jan Zima <jan.zima@nic.cz>
2017-04-05 17:22:10 +0200

    Ticket #18515 - fred-admin config dump from additional config files fix

--- src/cli_admin/read_config_file.h
+++ src/cli_admin/read_config_file.h
@@ -26,60 +26,60 @@
 
 #include "util/cfg/faked_args.h"
 #include "util/cfg/handle_args.h"
-#include "util/log/logger.h"
 #include "util/log/context.h"
 
 #include "cfg/config_handler_decl.h"
-#include "util/cfg/handle_general_args.h"
 #include "cfg/handle_logging_args.h"
+#include "util/cfg/handle_general_args.h"
+#include "util/log/logger.h"
 
-namespace Admin
+namespace Admin {
+
+/**
+ * read extra config file into key-value config map
+ */
+template <class HANDLE_ARGS>
+std::map<std::string, std::string> readConfigFile(
+        const std::string& conf_file)
 {
+    boost::shared_ptr<HANDLE_ARGS> handle_args_ptr(new HANDLE_ARGS);
 
-    /**
-     * read extra config file into key-value config map
-     */
-    template<class HANDLE_ARGS> std::map<std::string, std::string> readConfigFile(
-            const std::string &conf_file)
+    HandlerPtrVector hpv = boost::assign::list_of(
+            HandleArgsPtr(new HandleConfigFileArgs(conf_file)))(
+            HandleArgsPtr(handle_args_ptr));
+
+    // it always needs some argv vector, argc cannot be 0
+    FakedArgs fa;
+    fa.add_argv(std::string(""));
+
+    //handle
+    for (HandlerPtrVector::const_iterator i = hpv.begin(); i != hpv.end();
+            ++i)
     {
-        boost::shared_ptr<HANDLE_ARGS> handle_args_ptr(new HANDLE_ARGS);
-
-        HandlerPtrVector hpv = boost::assign::list_of(
-                HandleArgsPtr(new HandleConfigFileArgs(conf_file)))(
-                HandleArgsPtr(handle_args_ptr));
-
-        // it always needs some argv vector, argc cannot be 0
-        FakedArgs fa;
-        fa.add_argv(std::string(""));
-
-        //handle
-        for (HandlerPtrVector::const_iterator i = hpv.begin(); i != hpv.end();
-                ++i)
-        {
-            FakedArgs fa_out;
-            (*i)->handle(fa.get_argc(), fa.get_argv(), fa_out);
-            fa = fa_out; //last output to next input
-        }
-
-        std::map<std::string, std::string> set_cfg = handle_args_ptr->get_map();
-
-        //config dump
-        if (CfgArgs::instance()->get_handler_ptr_by_type<HandleLoggingArgs>()->log_config_dump)
-        {
-            for (std::string config_item =
-                    AccumulatedConfig::get_instance().pop_front();
-                    !config_item.empty(); config_item =
-                            AccumulatedConfig::get_instance().pop_front())
-            {
-                Logging::Manager::instance_ref().get(PACKAGE).debug(
-                        config_item);
-            }
-        }
-
-        return set_cfg;
+        FakedArgs fa_out;
+        (*i)->handle(fa.get_argc(), fa.get_argv(), fa_out);
+        fa = fa_out; //last output to next input
     }
+
+    std::map<std::string, std::string> set_cfg = handle_args_ptr->get_map();
+
+    //config dump
+    if (CfgArgGroups::instance()->get_handler_ptr_by_type<HandleLoggingArgsGrp>()->get_log_config_dump())
+    {
+        for (std::string config_item =
+                        AccumulatedConfig::get_instance().pop_front();
+                !config_item.empty();
+                config_item =
+                        AccumulatedConfig::get_instance().pop_front())
+        {
+            Logging::Manager::instance_ref().get(PACKAGE).debug(
+                    config_item);
+        }
+    }
+
+    return set_cfg;
+}
 
 } // namespace Admin
 
 #endif // READ_CONFIG_FILE_H_9777d979c2ee4b83a0447019af4a7267
-
