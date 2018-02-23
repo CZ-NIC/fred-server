b041f577694218750df117b5f2c5d1d378d94da0
Ales Friedl <ales.friedl@nic.cz>
2018-02-23 07:55:07 +0100

    Ticket #17312 - implement new IDL types (Nullable)IsoDate/Time and Buffer + wrappers

---
+++ src/backend/buffer.cc
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "buffer.hh"
+
+#include <string>
+
+namespace Fred {
+namespace Backend {
+
+Buffer::Buffer(const std::string& _data)
+    : data(_data)
+{
+}
+
+} // namespace Fred::Backend
+} // namespace Fred
---
+++ src/bin/corba/util/corba_conversions_isodatetime.hh
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CORBA_CONVERSIONS_ISODATETIME_HH_6C81DCD1D79F45E2AC3190723E2F6DE3
+#define CORBA_CONVERSIONS_ISODATETIME_HH_6C81DCD1D79F45E2AC3190723E2F6DE3
+
+#include "src/bin/corba/IsoDateTime.hh"
+#include "src/bin/corba/NullableIsoDateTime.hh"
+#include "src/util/db/nullable.hh"
+
+#include <boost/date_time/posix_time/posix_time_types.hpp>
+
+namespace CorbaConversion {
+namespace Util {
+
+boost::posix_time::ptime
+unwrap_IsoDateTime_to_boost_posix_time_ptime(
+        const Registry::IsoDateTime src);
+
+void
+unwrap_IsoDateTime_to_boost_posix_time_ptime(
+        const Registry::IsoDateTime src,
+        boost::posix_time::ptime& dst);
+
+Nullable<boost::posix_time::ptime>
+unwrap_NullableIsoDateTime_to_Nullable_boost_posix_time_ptime(
+        const Registry::NullableIsoDateTime* src_ptr);
+
+void
+unwrap_NullableIsoDateTime_to_Nullable_boost_posix_time_ptime(
+        const Registry::NullableIsoDateTime* src_ptr,
+        Nullable<boost::posix_time::ptime>& dst);
+
+Registry::IsoDateTime
+wrap_boost_posix_time_ptime_to_IsoDateTime(
+        const boost::posix_time::ptime& src);
+
+void
+wrap_boost_posix_time_ptime_to_IsoDateTime(
+        const boost::posix_time::ptime& src,
+        Registry::IsoDateTime& dst);
+
+Registry::NullableIsoDateTime_var
+        wrap_Nullable_boost_posix_time_ptime_to_NullableIsoDateTime(
+        const Nullable<boost::posix_time::ptime>& src);
+
+} // namespace CorbaConversion::Util
+} // namespace CorbaConversion
+
+#endif
