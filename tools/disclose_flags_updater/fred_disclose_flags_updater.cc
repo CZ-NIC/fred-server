19cdbc6bdcea88be976864c7b1fc8ab5cee04dd3
Jiri Sadek <jiri.sadek@nic.cz>
2018-04-20 13:30:24 +0200

    Ticket #21771 - [CR] Fix exit status on error

--- tools/disclose_flags_updater/fred_disclose_flags_updater.cc
+++ tools/disclose_flags_updater/fred_disclose_flags_updater.cc
@@ -190,6 +190,7 @@ int main(int argc, char *argv[])
     catch (const std::exception& ex)
     {
         std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
     }
     return 0;
 }
