0a5e3215e0b5f01a4929ec3e91a6c57c3f18b6ea
Radka Karvankova <radka.karvankova@nic.cz>
2018-06-20 16:23:43 +0200

    Ticket #22137 - Create zone ns

---
+++ src/libfred/zone_ns/create_zone_ns.cc
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "src/libfred/zone_ns/create_zone_ns.hh"
+#include "src/libfred/zone_ns/util.hh"
+#include "src/libfred/zone/info_zone.hh"
+
+#include <algorithm>
+#include <sstream>
+
+namespace LibFred {
+namespace ZoneNs {
+
+CreateZoneNs& CreateZoneNs::set_nameserver_fqdn(const std::string& _nameserver_fqdn)
+{
+    nameserver_fqdn_ = _nameserver_fqdn;
+    return *this;
+}
+
+CreateZoneNs& CreateZoneNs::set_nameserver_ip_addresses(
+        const std::vector<boost::asio::ip::address>& _nameserver_ip_addresses)
+{
+    nameserver_ip_addresses_ = _nameserver_ip_addresses;
+    return *this;
+}
+
+unsigned long long CreateZoneNs::exec(OperationContext& _ctx) const
+{
+    const LibFred::Zone::InfoZoneData zone_info = LibFred::Zone::InfoZone(zone_fqdn_).exec(_ctx);
+    const unsigned long long zone_id = LibFred::Zone::get_zone_id(zone_info);
+
+    try
+    {
+        const Database::Result insert_result = _ctx.get_conn().exec_params(
+                // clang-format off
+                "INSERT INTO zone_ns (zone, fqdn, addrs) "
+                "VALUES ($1::bigint, LOWER($2::text), $3::inet[]) "
+                "RETURNING id",
+                // clang-format on
+                Database::query_param_list(zone_id)
+                                          (nameserver_fqdn_)
+                                          (ip_addresses_to_string(nameserver_ip_addresses_)));
+
+        if (insert_result.size() == 1)
+        {
+            const unsigned long long id = static_cast<unsigned long long>(insert_result[0][0]);
+            return id;
+        }
+    }
+    catch (const std::exception&)
+    {
+        throw CreateZoneNsException();
+    }
+    throw CreateZoneNsException();
+}
+
+} // namespace LibFred::ZoneNs
+} // namespace LibFred
---
+++ src/libfred/zone_ns/create_zone_ns.hh
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef CREATE_ZONE_NS_HH_2D413215C56D4D7F9D5233D84B5915B8
+#define CREATE_ZONE_NS_HH_2D413215C56D4D7F9D5233D84B5915B8
+
+#include "src/libfred/opcontext.hh"
+
+#include <boost/asio/ip/address.hpp>
+#include <string>
+#include <vector>
+
+namespace LibFred {
+namespace ZoneNs {
+
+class CreateZoneNs
+{
+public:
+    explicit CreateZoneNs(const std::string& _zone_fqdn)
+        : zone_fqdn_(_zone_fqdn),
+          nameserver_fqdn_("localhost")
+    {
+    }
+
+    CreateZoneNs& set_nameserver_fqdn(const std::string& _nameserver_fqdn);
+    CreateZoneNs& set_nameserver_ip_addresses(
+            const std::vector<boost::asio::ip::address>& _nameserver_ip_addresses);
+
+    unsigned long long exec(OperationContext& _ctx) const;
+private:
+    std::string zone_fqdn_;
+    std::string nameserver_fqdn_;
+    std::vector<boost::asio::ip::address> nameserver_ip_addresses_;
+};
+
+} // namespace LibFred::ZoneNs
+} // namespace LibFred
+
+#endif
