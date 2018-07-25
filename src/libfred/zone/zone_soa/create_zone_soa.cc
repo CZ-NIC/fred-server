1082b780b8e9abfb6b82db6c68313022fad48e04
Radka Karvankova <radka.karvankova@nic.cz>
2018-07-25 10:14:02 +0200

    Ticket #22368 - Move zone implementation and update namespaces

---
+++ src/libfred/zone/zone_soa/create_zone_soa.cc
@@ -0,0 +1,140 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "src/libfred/zone/info_zone.hh"
+#include "src/libfred/zone/zone_soa/create_zone_soa.hh"
+#include "src/libfred/zone/zone_soa/exceptions.hh"
+
+namespace LibFred {
+namespace Zone {
+
+namespace {
+constexpr int seconds_per_hour = 60 * 60;
+
+constexpr int default_ttl_in_seconds = 5 * seconds_per_hour;
+constexpr char default_hostmaster[] = "hostmaster@localhost";
+constexpr int default_refresh_in_seconds  = 3 * seconds_per_hour;
+constexpr int default_update_retr_in_seconds = seconds_per_hour;
+constexpr int default_expiry_in_seconds = 2 * 7 * 24 * seconds_per_hour;
+constexpr int default_minimum_in_seconds = 2 * seconds_per_hour;
+constexpr char default_ns_fqdn[] = "localhost";
+}
+
+CreateZoneSoa::CreateZoneSoa(const std::string& _fqdn)
+        : fqdn_(_fqdn),
+          ttl_(default_ttl_in_seconds),
+          hostmaster_(default_hostmaster),
+          refresh_(default_refresh_in_seconds),
+          update_retr_(default_update_retr_in_seconds),
+          expiry_(default_expiry_in_seconds),
+          minimum_(default_minimum_in_seconds),
+          ns_fqdn_(default_ns_fqdn)
+{
+}
+
+CreateZoneSoa& CreateZoneSoa::set_ttl(const int _ttl)
+{
+    ttl_ = _ttl;
+    return *this;
+}
+
+CreateZoneSoa& CreateZoneSoa::set_hostmaster(const std::string& _hostmaster)
+{
+    hostmaster_ = _hostmaster;
+    return *this;
+}
+
+CreateZoneSoa& CreateZoneSoa::set_refresh(const int _refresh)
+{
+    refresh_ = _refresh;
+    return *this;
+}
+
+CreateZoneSoa& CreateZoneSoa::set_update_retr(const int _update_retr)
+{
+    update_retr_ = _update_retr;
+    return *this;
+}
+
+CreateZoneSoa& CreateZoneSoa::set_expiry(const int _expiry)
+{
+    expiry_ = _expiry;
+    return *this;
+}
+
+CreateZoneSoa& CreateZoneSoa::set_minimum(const int _minimum)
+{
+    minimum_ = _minimum;
+    return *this;
+}
+
+CreateZoneSoa& CreateZoneSoa::set_ns_fqdn(const std::string& _ns_fqdn)
+{
+    ns_fqdn_ = _ns_fqdn;
+    return *this;
+}
+
+unsigned long long CreateZoneSoa::exec(OperationContext& _ctx) const
+{
+    const LibFred::Zone::InfoZoneData zone_info = LibFred::Zone::InfoZone(fqdn_).exec(_ctx);
+
+    const unsigned long long zone_id = LibFred::Zone::get_zone_id(zone_info);
+
+    const Database::Result select_result = _ctx.get_conn().exec_params(
+            // clang-format off
+            "SELECT zone FROM zone_soa WHERE zone=$1::bigint",
+            // clang-format on
+            Database::query_param_list(zone_id));
+    if (select_result.size() > 0)
+    {
+        throw AlreadyExistingZoneSoa();
+    }
+
+    try
+    {
+        const Database::Result insert_result = _ctx.get_conn().exec_params(
+                // clang-format off
+                "INSERT INTO zone_soa (zone, ttl, hostmaster, refresh, update_retr, expiry, minimum, ns_fqdn) "
+                "VALUES ($1::bigint, $2::integer, $3::text, $4::integer, $5::integer, $6::integer, $7::integer, $8::text) "
+                "RETURNING zone",
+                // clang-format on
+                Database::query_param_list(zone_id)
+                                        (ttl_)
+                                        (hostmaster_)
+                                        (refresh_)
+                                        (update_retr_)
+                                        (expiry_)
+                                        (minimum_)
+                                        (ns_fqdn_)
+                );
+
+        if (insert_result.size() == 1)
+        {
+            const unsigned long long id = static_cast<unsigned long long>(insert_result[0][0]);
+            return id;
+        }
+    }
+    catch (const std::exception&)
+    {
+        throw CreateZoneSoaException();
+    }
+    throw CreateZoneSoaException();
+}
+
+} // namespace LibFred::Zone
+} // namespace LibFred
---
+++ test/libfred/zone/zone_soa/test_update_zone_soa.cc
@@ -0,0 +1,127 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "src/libfred/opcontext.hh"
+#include "src/libfred/opexception.hh"
+#include "src/libfred/zone/create_zone.hh"
+#include "src/libfred/zone/exceptions.hh"
+#include "src/libfred/zone/zone_soa/create_zone_soa.hh"
+#include "src/libfred/zone/zone_soa/info_zone_soa.hh"
+#include "src/libfred/zone/zone_soa/update_zone_soa.hh"
+#include "src/libfred/zone/zone_soa/exceptions.hh"
+#include "src/util/random_data_generator.hh"
+#include "test/libfred/zone/zone_soa/util.hh"
+#include "test/libfred/zone/util.hh"
+#include "test/setup/fixtures.hh"
+
+#include <boost/test/test_tools.hpp>
+#include <boost/test/unit_test.hpp>
+#include <string>
+
+namespace Test {
+
+struct UpdateZoneSoaFixture
+{
+    std::string fqdn;
+    ::LibFred::Zone::InfoZoneSoaData zone_soa;
+
+    UpdateZoneSoaFixture(::LibFred::OperationContext& _ctx)
+        : fqdn(RandomDataGenerator().xstring(3))
+    {
+        ::LibFred::Zone::CreateZone(fqdn, 5, 6).exec(_ctx);
+
+        zone_soa.ttl = 5 * 60 * 60;
+        zone_soa.hostmaster = "hostmaster@localhost";
+        zone_soa.refresh = 3 * 60 * 60;
+        zone_soa.update_retr = 60 * 60;
+        zone_soa.expiry = 2 * 7 * 24 * 60 * 60;
+        zone_soa.minimum = 2 * 60 * 60;
+        zone_soa.ns_fqdn = "localhost";
+    }
+
+    ~UpdateZoneSoaFixture()
+    {}
+};
+
+BOOST_FIXTURE_TEST_SUITE(TestUpdateZoneSoa, SupplyFixtureCtx<UpdateZoneSoaFixture>)
+
+BOOST_AUTO_TEST_CASE(set_nonexistent_zone)
+{
+    zone_soa.ttl = new_ttl_in_seconds;
+    BOOST_CHECK_THROW(::LibFred::Zone::UpdateZoneSoa(RandomDataGenerator().xstring(3))
+                .set_ttl(zone_soa.ttl)
+                .exec(ctx),
+           ::LibFred::Zone::NonExistentZone);
+}
+
+BOOST_AUTO_TEST_CASE(set_nonexistent_zone_soa)
+{
+    zone_soa.ttl = new_ttl_in_seconds;
+    BOOST_CHECK_THROW(::LibFred::Zone::UpdateZoneSoa(fqdn)
+                .set_ttl(zone_soa.ttl)
+                .exec(ctx),
+           ::LibFred::Zone::NonExistentZoneSoa);
+}
+
+BOOST_AUTO_TEST_CASE(set_no_update_zone_soa)
+{
+    BOOST_CHECK_THROW(::LibFred::Zone::UpdateZoneSoa(fqdn)
+                .exec(ctx),
+           ::LibFred::Zone::NoZoneSoaData);
+}
+
+BOOST_AUTO_TEST_CASE(set_min_update_zone_soa)
+{
+    zone_soa.zone = ::LibFred::Zone::CreateZoneSoa(fqdn).exec(ctx);
+    zone_soa.minimum = new_minimum_in_seconds;
+    ::LibFred::Zone::UpdateZoneSoa(fqdn)
+           .set_minimum(zone_soa.minimum)
+           .exec(ctx);
+    BOOST_CHECK(zone_soa == ::LibFred::Zone::InfoZoneSoa(fqdn).exec(ctx));
+}
+
+BOOST_AUTO_TEST_CASE(set_max_update_zone_soa)
+{
+    ::LibFred::Zone::InfoZoneSoaData zone_soa;
+
+    zone_soa.zone = ::LibFred::Zone::CreateZoneSoa(fqdn).exec(ctx);
+    zone_soa.ttl = new_ttl_in_seconds;
+    zone_soa.hostmaster = new_hostmaster;
+    zone_soa.refresh = new_refresh_in_seconds;
+    zone_soa.update_retr = new_update_retr_in_seconds;
+    zone_soa.expiry = new_expiry_in_seconds;
+    zone_soa.minimum = new_minimum_in_seconds;
+    zone_soa.ns_fqdn = new_ns_fqdn;
+
+    ::LibFred::Zone::UpdateZoneSoa(fqdn)
+            .set_ttl(zone_soa.ttl)
+            .set_hostmaster(zone_soa.hostmaster)
+            .set_refresh(zone_soa.refresh)
+            .set_update_retr(zone_soa.update_retr)
+            .set_expiry(zone_soa.expiry)
+            .set_minimum(zone_soa.minimum)
+            .set_ns_fqdn(zone_soa.ns_fqdn)
+            .exec(ctx);
+
+    BOOST_CHECK(zone_soa == ::LibFred::Zone::InfoZoneSoa(fqdn).exec(ctx));
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+
+} // namespace Test
+
