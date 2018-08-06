d436a9e6d46a585edddb7ffb07498cb0cee9079e
Radka Karvankova <radka.karvankova@nic.cz>
2018-08-06 16:17:39 +0200

    Ticket #22368 - Tests of backend

---
+++ test/backend/admin/zone/test_add_zone.cc
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "src/backend/admin/zone/zone.hh"
+#include "src/libfred/zone/exceptions.hh"
+#include "test/backend/admin/zone/fixtures.hh"
+#include "test/setup/fixtures.hh"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+
+namespace Test {
+
+BOOST_AUTO_TEST_SUITE(Backend)
+BOOST_AUTO_TEST_SUITE(Admin)
+BOOST_AUTO_TEST_SUITE(Zone)
+BOOST_AUTO_TEST_SUITE(TestAdminAddZone)
+
+unsigned int exists_zone(const std::string& _fqdn)
+{
+    ::LibFred::OperationContextCreator ctx;
+    const Database::Result db_result = ctx.get_conn().exec_params(
+            // clang-format off
+            "SELECT 1 FROM zone AS z "
+            "INNER JOIN zone_soa AS zs ON (zs.zone = z.id) "
+            "WHERE z.fqdn = $1::text ",
+            // clang-format on
+            Database::query_param_list(_fqdn));
+    return db_result.size();
+}
+
+BOOST_FIXTURE_TEST_CASE(set_already_existing_zone, SupplyFixtureCtx<HasExistingZone>)
+{
+    BOOST_CHECK_THROW(
+            ::Admin::Zone::add_zone(
+                    zone.fqdn,
+                    expiration_period_min,
+                    expiration_period_max,
+                    ttl,
+                    hostmaster,
+                    refresh,
+                    update_retr,
+                    expiry,
+                    minimum,
+                    ns_fqdn),
+            ::LibFred::Zone::DuplicateZone);
+}
+
+BOOST_FIXTURE_TEST_CASE(set_enum_zone, SupplyFixtureCtx<HasEnumZone>)
+{
+    ::Admin::Zone::add_zone(
+            zone.fqdn,
+            expiration_period_min,
+            expiration_period_max,
+            ttl,
+            hostmaster,
+            refresh,
+            update_retr,
+            expiry,
+            minimum,
+            ns_fqdn);
+    BOOST_CHECK_EQUAL(exists_zone(zone.fqdn), 1);
+}
+
+BOOST_FIXTURE_TEST_CASE(set_non_enum_zone, SupplyFixtureCtx<HasNonEnumZone>)
+{
+    ::Admin::Zone::add_zone(
+            zone.fqdn,
+            expiration_period_min,
+            expiration_period_max,
+            ttl,
+            hostmaster,
+            refresh,
+            update_retr,
+            expiry,
+            minimum,
+            ns_fqdn);
+    BOOST_CHECK_EQUAL(exists_zone(zone.fqdn), 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
+BOOST_AUTO_TEST_SUITE_END();
+BOOST_AUTO_TEST_SUITE_END();
+BOOST_AUTO_TEST_SUITE_END();
+
+} // namespace Test
---
+++ test/backend/admin/zone/test_add_zone_ns.cc
@@ -0,0 +1,92 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "src/backend/admin/zone/zone.hh"
+#include "src/libfred/zone/exceptions.hh"
+#include "test/backend/admin/zone/fixtures.hh"
+#include "test/setup/fixtures.hh"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/test_tools.hpp>
+
+namespace Test {
+
+BOOST_AUTO_TEST_SUITE(Backend)
+BOOST_AUTO_TEST_SUITE(Admin)
+BOOST_AUTO_TEST_SUITE(Zone)
+BOOST_AUTO_TEST_SUITE(TestAdminAddZoneNs)
+
+unsigned int exists_zone_ns(
+        const unsigned long long _zone_id,
+        const std::string& _fqdn,
+        const std::string& _addrs)
+{
+    ::LibFred::OperationContextCreator ctx;
+    const std::string ip_addresses = "{" + _addrs + "}";
+    const Database::Result db_result = ctx.get_conn().exec_params(
+            // clang-format off
+            "SELECT 1 FROM zone_ns AS zn "
+            "WHERE zn.zone = $1::bigint "
+            "AND zn.fqdn = $2::text "
+            "AND zn.addrs = $3::inet[] ",
+            // clang-format on
+            Database::query_param_list(_zone_id)(_fqdn)(ip_addresses));
+    return db_result.size();
+}
+
+BOOST_FIXTURE_TEST_CASE(set_nonexistent_zone, SupplyFixtureCtx<HasNonExistentZone>)
+{
+    BOOST_CHECK_THROW(
+            ::Admin::Zone::add_zone_ns(zone.fqdn, nameserver_fqdn, nameserver_ip_addresses),
+            ::LibFred::Zone::NonExistentZone);
+}
+
+BOOST_FIXTURE_TEST_CASE(set_more_zone_ns_records, SupplyFixtureCtx<HasMoreRecords>)
+{
+    ::Admin::Zone::add_zone_ns(zone.fqdn, nameserver_fqdn_0, nameserver_ip_addresses);
+    ::Admin::Zone::add_zone_ns(zone.fqdn, nameserver_fqdn_1, nameserver_ip_addresses);
+    ::Admin::Zone::add_zone_ns(zone.fqdn, nameserver_fqdn_2, nameserver_ip_addresses);
+    BOOST_CHECK_EQUAL(exists_zone_ns(zone_id, nameserver_fqdn_0, nameserver_ip_addresses), 1);
+    BOOST_CHECK_EQUAL(exists_zone_ns(zone_id, nameserver_fqdn_1, nameserver_ip_addresses), 1);
+    BOOST_CHECK_EQUAL(exists_zone_ns(zone_id, nameserver_fqdn_2, nameserver_ip_addresses), 1);
+}
+
+BOOST_FIXTURE_TEST_CASE(set_empty_ip_address, SupplyFixtureCtx<HasEmptyIpAddress>)
+{
+    ::Admin::Zone::add_zone_ns(zone.fqdn, nameserver_fqdn, nameserver_ip_addresses);
+    BOOST_CHECK_EQUAL(exists_zone_ns(zone_id, nameserver_fqdn, nameserver_ip_addresses), 1);
+}
+BOOST_FIXTURE_TEST_CASE(set_one_ip_address, SupplyFixtureCtx<HasOneIpAddress>)
+{
+    ::Admin::Zone::add_zone_ns(zone.fqdn, nameserver_fqdn, nameserver_ip_addresses);
+    BOOST_CHECK_EQUAL(exists_zone_ns(zone_id, nameserver_fqdn, nameserver_ip_addresses), 1);
+}
+
+BOOST_FIXTURE_TEST_CASE(set_more_ip_addresses, SupplyFixtureCtx<HasMoreIpAddresses>)
+{
+    ::Admin::Zone::add_zone_ns(zone.fqdn, nameserver_fqdn, nameserver_ip_addresses);
+    BOOST_CHECK_EQUAL(exists_zone_ns(zone_id, nameserver_fqdn, nameserver_ip_addresses), 1);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END();
+BOOST_AUTO_TEST_SUITE_END();
+BOOST_AUTO_TEST_SUITE_END();
+BOOST_AUTO_TEST_SUITE_END();
+
+} // namespace Test
