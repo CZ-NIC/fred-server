31d8ad4b61736e348c2b708b5a8a2dbf926199d5
Michal Strnad <michal.strnad@nic.cz>
2018-08-07 12:32:18 +0200

    Ticket #21961 - Fix RandomDataGenerator::msseed

--- src/util/random_data_generator.hh
+++ src/util/random_data_generator.hh
@@ -21,7 +21,6 @@
  *  RandomDataGenerator class using boost
  */
 
-
 #ifndef RANDOM_DATA_GENERATOR_HH_94BF03B65AC44513BB65FE985FA1311A
 #define RANDOM_DATA_GENERATOR_HH_94BF03B65AC44513BB65FE985FA1311A
 
@@ -38,14 +37,13 @@
 #include <sys/time.h>
 #include <time.h>
 
-
 /**
  * \class RandomDataGenerator
  * \brief pseudo-random data generator
  */
-
 class RandomDataGenerator //data generator is actually pseudo-random
 {
+private:
     //typedef for a random number generator
     //boost::mt19937, boost::ecuyer1988, boost::minstd_rand
     typedef boost::mt19937 base_generator_t;
@@ -72,33 +70,30 @@ class RandomDataGenerator //data generator is actually pseudo-random
     boost::variate_generator<base_generator_t, int_distribution_t > gint;
     boost::variate_generator<base_generator_t, real_distribution_t > greal;
     boost::variate_generator<base_generator_t, int_distribution_t > gtime;
-
 public:
-    //ctor
-    RandomDataGenerator(unsigned seed = 0)
-        : seed_(seed ? seed : msseed())
-          , rng(seed_)
+    explicit RandomDataGenerator(unsigned seed = 0)
+        : seed_(seed != 0 ? seed : msseed()),
+          rng(seed_),
 
           //ranges definitions
-        , gen_letter52(0,51)
-        , gen_numletter10(0,9)
-        , gen_letter62(0,62)
-        , gen_num1_5(1,5)
-        , gen_num1_6(1,6)
-        , gen_int(std::numeric_limits<int>::min(), std::numeric_limits<int>::max())
-        , gen_real(std::numeric_limits<double>::min(), std::numeric_limits<double>::max())
-        , gen_time(631148400, 2147483647)
-
+          gen_letter52(0, 51),
+          gen_numletter10(0, 9),
+          gen_letter62(0, 61),
+          gen_num1_5(1, 5),
+          gen_num1_6(1, 6),
+          gen_int(std::numeric_limits<int>::min(), std::numeric_limits<int>::max()),
+          gen_real(std::numeric_limits<double>::min(), std::numeric_limits<double>::max()),
+          gen_time(631148400, 2147483647),
 
           //generator instances
-        , letter52(rng, gen_letter52)
-        , numletter10(rng, gen_numletter10)
-        , letter62(rng,gen_letter62)
-        , num1_5(rng, gen_num1_5)
-        , num1_6(rng, gen_num1_6)
-        , gint(rng, gen_int)
-        , greal(rng, gen_real)
-        , gtime(rng, gen_time)
+          letter52(rng, gen_letter52),
+          numletter10(rng, gen_numletter10),
+          letter62(rng,gen_letter62),
+          num1_5(rng, gen_num1_5),
+          num1_6(rng, gen_num1_6),
+          gint(rng, gen_int),
+          greal(rng, gen_real),
+          gtime(rng, gen_time)
     {
         //std::cout << "RandomDataGenerator using seed: " << seed_ << std::endl;
     }
@@ -112,30 +107,52 @@ public:
     ///generate some letter A-Z a-z
     char xletter()
     {
-        unsigned rnumber = letter52();
-        char ret = rnumber < 26
-                ? static_cast<char>(rnumber + 65) //A-Z
-                : static_cast<char>(rnumber + 71) ; //a-z
-        return ret;
+        const char idx = static_cast<char>(letter52());
+        constexpr auto middle_idx = static_cast<char>(52 / 2);
+        if (idx < middle_idx)
+        {
+            const char upper_case_letters_idx = idx;
+            return 'A' + upper_case_letters_idx;
+        }
+        const char lower_case_letters_idx = idx - middle_idx;
+        return 'a' + lower_case_letters_idx;
     }
 
     ///generate some letter 0-9
     char xnumletter()
     {
-        return static_cast<char>(numletter10() + 48);
+        return static_cast<char>(numletter10()) + '0';
     }
 
     ///generate some letter A-Z a-z 0-9
     char xnletter()
     {
-        unsigned rnumber = letter62();
-        char ret = rnumber < 26
-            ? static_cast<char>(rnumber + 65) //A-Z
-            : ( rnumber < 52
-                ? static_cast<char>(rnumber + 71) //a-z
-                : static_cast<char>(rnumber + 48) //0-9
-              );
-        return ret;
+        constexpr auto number_of_letters = static_cast<char>(26);
+
+        constexpr auto upper_case_idx_start = static_cast<char>(0);
+        constexpr auto upper_case_idx_end = static_cast<char>(upper_case_idx_start + number_of_letters);
+
+        constexpr auto lower_case_idx_start = upper_case_idx_end;
+        constexpr auto lower_case_idx_end = static_cast<char>(lower_case_idx_start + number_of_letters);
+
+        constexpr auto digit_idx_start = lower_case_idx_end;
+
+        const auto idx = static_cast<char>(letter62());
+
+        if ((upper_case_idx_start <= idx) && (idx < upper_case_idx_end))
+        {
+            const auto upper_case_idx = static_cast<char>(idx - upper_case_idx_start);
+            return 'A' + upper_case_idx;
+        }
+
+        if ((lower_case_idx_start <= idx) && (idx < lower_case_idx_end))
+        {
+            const auto lower_case_idx = static_cast<char>(idx - lower_case_idx_start);
+            return 'a' + lower_case_idx;
+        }
+
+        const auto digit_idx = static_cast<char>(idx - digit_idx_start);
+        return '0' + digit_idx;
     }
 
 
@@ -145,7 +162,9 @@ public:
         std::string ret;
         ret.reserve(length);
         for(std::size_t i = 0; i < length; ++i)
+        {
             ret.push_back(xletter());
+        }
         return ret;
     }
 
@@ -155,7 +174,9 @@ public:
         std::string ret;
         ret.reserve(length);
         for(std::size_t i = 0; i < length; ++i)
+        {
             ret.push_back(xnumletter());
+        }
         return ret;
     }
 
@@ -165,7 +186,9 @@ public:
         std::string ret;
         ret.reserve(length);
         for(std::size_t i = 0; i < length; ++i)
+        {
             ret.push_back(xnletter());
+        }
         return ret;
     }
 
@@ -194,35 +217,36 @@ public:
         return (1U << std::numeric_limits<int>::digits) + gint();
     }
 
-    unsigned long msseed()
+    ///generate some signed real number
+    double xreal()
+    {
+        return greal();
+    }
+
+    ///generate some unix time from 1.1.1990 to the end of unix time (19.01.2038 04:14:07 (CET))
+    time_t xtime()
+    {
+        return gtime();
+    }
+
+    ///generate some posix time from 1.1.1990 to the end of unix time (19.01.2038 04:14:07 (CET))
+    boost::posix_time::ptime xptime()
+    {
+        return boost::posix_time::from_time_t(gtime());
+    }
+
+    ///generate some gregorian date from 1.1.1990 to the end of unix time (19.01.2038)
+    boost::gregorian::date xdate()
+    {
+        return xptime().date();
+    }
+private:
+    static unsigned long msseed()
     {
         struct timeval tv;
         gettimeofday(&tv, NULL);
-        return tv.tv_usec + tv.tv_sec;
+        return 1000 * 1000 * static_cast<unsigned long>(tv.tv_sec) + tv.tv_usec;
     }
-   ///generate some signed real number
-   double xreal()
-   {
-       return greal();
-   }
-   ///generate some unix time from 1.1.1990 to the end of unix time (19.01.2038 04:14:07 (CET))
-   time_t xtime()
-   {
-       return gtime();
-   }
-   ///generate some posix time from 1.1.1990 to the end of unix time (19.01.2038 04:14:07 (CET))
-   boost::posix_time::ptime xptime()
-   {
-       return boost::posix_time::from_time_t(gtime());
-   }
-
-   ///generate some gregorian date from 1.1.1990 to the end of unix time (19.01.2038)
-   boost::gregorian::date xdate()
-   {
-       return xptime().date();
-   }
-
-};//class RandomDataGenerator
-
+};
 
 #endif
