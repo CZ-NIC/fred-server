fbb9a3f1b49e1016cc81146b413a85d1ece27df3
Michal Strnad <michal.strnad@nic.cz>
2018-08-07 07:20:32 +0200

    Ticket #20540 - Fix random number generator

--- src/util/random.cc
+++ src/util/random.cc
@@ -1,93 +1,93 @@
 #include "src/util/random.hh"
-#include <iostream>
+
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int.hpp>
 #include <boost/random/variate_generator.hpp>
-#include <string>
-#include <vector>
-#include <time.h>
-#include <sys/time.h>
+
 #include <atomic>
+
+#include <sys/time.h>
+#include <time.h>
 
 
 namespace Random {
 
-namespace
-{
+namespace {
 
-class RandomSequenceNumber
+boost::mt19937& get_engine()
 {
-public:
-    RandomSequenceNumber()
+    class RandomSequenceNumber
     {
-        struct timeval tv;
-        gettimeofday(&tv, NULL);
-        id_.store(1000 * 1000 * static_cast<unsigned long>(tv.tv_sec) + tv.tv_usec);
-    }
+    public:
+        static RandomSequenceNumber& get_singleton()
+        {
+            static RandomSequenceNumber singleton;
+            return singleton;
+        }
+        unsigned long next()
+        {
+            return static_cast<unsigned long>(++id_);
+        }
 
-    unsigned long next()
-    {
-        return static_cast<unsigned long>(++id_);
-    }
+    private:
+        RandomSequenceNumber()
+            : id_(pseudorandom_value())
+        {
+        }
+        static unsigned long pseudorandom_value()
+        {
+            struct timeval tv;
+            gettimeofday(&tv, NULL);
+            return 1000 * 1000 * static_cast<unsigned long>(tv.tv_sec) + tv.tv_usec;
+        }
+        std::atomic<unsigned long> id_;
+    };
 
-private:
-    std::atomic<unsigned long> id_;
-};
-
-RandomSequenceNumber& get_random_sequence_number()
-{
-    static RandomSequenceNumber global_seed;
-    return global_seed;
+    thread_local boost::mt19937 engine(RandomSequenceNumber::get_singleton().next());
+    return engine;
 }
 
 } // namespace Random::{anonymous}
 
-
-int integer(const int &_min, const int &_max)
+int integer(int _min, int _max)
 {
-    thread_local boost::mt19937 rng(get_random_sequence_number().next());
     boost::uniform_int<> range(_min, _max);
-    boost::variate_generator<boost::mt19937&, boost::uniform_int<> > gen(rng, range);
+    boost::variate_generator<decltype(get_engine()), boost::uniform_int<>> gen(get_engine(), range);
     return gen();
 }
 
-
-std::string string_from(const unsigned int &_len, const std::string &_allowed)
+std::string string_from(unsigned _len, const std::string& _allowed)
 {
     std::string out;
     out.reserve(_len);
-    for (unsigned int i = 0; i < _len; i++) {
-        out += choice<std::string, char>(_allowed);
+    for (unsigned int i = 0; i < _len; ++i)
+    {
+        out += _allowed[integer(0, static_cast<int>(_allowed.size() - 1))];
     }
     return out;
 }
 
-
-std::string string_lower_alpha(const unsigned int &_len)
+std::string string_lower_alpha(unsigned _len)
 {
     return string_from(_len, "abcdefghijklmnopqrstuvwxyz");
 }
 
-
-std::string string_upper_alpha(const unsigned int &_len)
+std::string string_upper_alpha(unsigned _len)
 {
     return string_from(_len, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 }
 
-
-std::string string_alpha(const unsigned int &_len)
+std::string string_alpha(unsigned _len)
 {
     return string_from(_len, "abcdefghijklmnopqrstuvwxyz"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+                            "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 }
 
-
-std::string string_alphanum(const unsigned int &_len)
+std::string string_alphanum(unsigned _len)
 {
     return string_from(_len, "abcdefghijklmnopqrstuvwxyz"
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890");
+                            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                            "1234567890");
 }
 
-
-}
-
+} // namespace Random
--- src/util/random.hh
+++ src/util/random.hh
@@ -3,30 +3,20 @@
 
 #include <string>
 
-
 namespace Random {
 
+int integer(int _min, int _max);
 
-int integer(const int &_min, const int &_max);
+std::string string_from(unsigned _len, const std::string& _allowed);
 
-std::string string_from(const unsigned int &_len, const std::string &_allowed);
+std::string string_lower_alpha(unsigned _len);
 
-std::string string_lower_alpha(const unsigned int &_len);
+std::string string_upper_alpha(unsigned _len);
 
-std::string string_upper_alpha(const unsigned int &_len);
+std::string string_alpha(unsigned _len);
 
-std::string string_alpha(const unsigned int &_len);
+std::string string_alphanum(unsigned _len);
 
-std::string string_alphanum(const unsigned int &_len);
-
-template<typename T1, typename T2>
-T2 choice(const T1 &_from)
-{
-    return _from[integer(0, static_cast<int>(_from.size() - 1))];
-}
-
-
-}
+} // namespace Random
 
 #endif /*RANDOM_UTIL_H_*/
-
