d7a417dff11c8e14ab30b4f5252e0f62d2aeba4c
Radka Karvankova <radka.karvankova@nic.cz>
2018-08-17 10:01:15 +0200

    Ticket #21870 - [CR] get_value_or()

--- src/libfred/zone/zone_soa/create_zone_soa.cc
+++ src/libfred/zone/zone_soa/create_zone_soa.cc
@@ -36,77 +36,49 @@ const std::string default_ns_fqdn = "localhost";
 }
 
 CreateZoneSoa::CreateZoneSoa(const std::string& _fqdn)
-        : fqdn_(_fqdn),
-          ttl_(default_ttl_in_seconds),
-          hostmaster_(default_hostmaster),
-          refresh_(default_refresh_in_seconds),
-          update_retr_(default_update_retr_in_seconds),
-          expiry_(default_expiry_in_seconds),
-          minimum_(default_minimum_in_seconds),
-          ns_fqdn_(default_ns_fqdn)
+        : fqdn_(_fqdn)
 {
 }
 
 CreateZoneSoa& CreateZoneSoa::set_ttl(const boost::optional<int> _ttl)
 {
-    if (_ttl != boost::none)
-    {
-        ttl_ = _ttl;
-    }
+    ttl_ = _ttl;
     return *this;
 }
 
 CreateZoneSoa& CreateZoneSoa::set_hostmaster(const boost::optional<std::string>& _hostmaster)
 {
-    if (_hostmaster != boost::none)
-    {
-        hostmaster_ = _hostmaster;
-    }
+    hostmaster_ = _hostmaster;
     return *this;
 }
 
 CreateZoneSoa& CreateZoneSoa::set_refresh(const boost::optional<int> _refresh)
 {
-    if (_refresh != boost::none)
-    {
-        refresh_ = _refresh;
-    }
+    refresh_ = _refresh;
     return *this;
 }
 
 CreateZoneSoa& CreateZoneSoa::set_update_retr(const boost::optional<int> _update_retr)
 {
-    if (_update_retr != boost::none)
-    {
-        update_retr_ = _update_retr;
-    }
+    update_retr_ = _update_retr;
     return *this;
 }
 
 CreateZoneSoa& CreateZoneSoa::set_expiry(const boost::optional<int> _expiry)
 {
-    if (_expiry != boost::none)
-    {
-        expiry_ = _expiry;
-    }
+    expiry_ = _expiry;
     return *this;
 }
 
 CreateZoneSoa& CreateZoneSoa::set_minimum(const boost::optional<int> _minimum)
 {
-    if (_minimum != boost::none)
-    {
-        minimum_ = _minimum;
-    }
+    minimum_ = _minimum;
     return *this;
 }
 
 CreateZoneSoa& CreateZoneSoa::set_ns_fqdn(const boost::optional<std::string>& _ns_fqdn)
 {
-    if (_ns_fqdn != boost::none)
-    {
-        ns_fqdn_ = _ns_fqdn;
-    }
+    ns_fqdn_ = _ns_fqdn;
     return *this;
 }
 
@@ -135,13 +107,13 @@ unsigned long long CreateZoneSoa::exec(OperationContext& _ctx) const
                 "RETURNING zone",
                 // clang-format on
                 Database::query_param_list(zone_id)
-                                        (ttl_.get())
-                                        (hostmaster_.get())
-                                        (refresh_.get())
-                                        (update_retr_.get())
-                                        (expiry_.get())
-                                        (minimum_.get())
-                                        (ns_fqdn_.get())
+                                        (ttl_.get_value_or(default_ttl_in_seconds))
+                                        (hostmaster_.get_value_or(default_hostmaster))
+                                        (refresh_.get_value_or(default_refresh_in_seconds))
+                                        (update_retr_.get_value_or(default_update_retr_in_seconds))
+                                        (expiry_.get_value_or(default_expiry_in_seconds))
+                                        (minimum_.get_value_or(default_minimum_in_seconds))
+                                        (ns_fqdn_.get_value_or(default_ns_fqdn))
                 );
 
         if (insert_result.size() == 1)
