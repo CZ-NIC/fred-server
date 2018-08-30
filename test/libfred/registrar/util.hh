7e7136716e651905a5d0d8df0f162854c02deace
Radka Karvankova <radka.karvankova@nic.cz>
2018-08-30 14:54:20 +0200

    Ticket #22601 - Test

---
+++ test/libfred/registrar/util.hh
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2018  CZ.NIC, z.s.p.o.
+ *
+ * This file is part of FRED.
+ *
+ * FRED is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * FRED is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FRED.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTIL_HH_9B225D49F9484E74BB868967CEB6AA63
+#define UTIL_HH_9B225D49F9484E74BB868967CEB6AA63
+
+#include "src/libfred/opcontext.hh"
+#include "test/setup/fixtures.hh"
+
+namespace Test {
+
+struct ContextHolder
+    : virtual instantiate_db_template
+{
+    LibFred::OperationContextCreator ctx;
+};
+
+template <class T>
+struct SupplyFixtureCtx : ContextHolder, T
+{
+    SupplyFixtureCtx()
+        : ContextHolder(),
+          T(ctx)
+    {
+    }
+};
+
+} // namespace Test
+
+
+#endif
