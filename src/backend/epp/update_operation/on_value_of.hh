eae7ba502cb700cb101ac2836712a369e77dc786
Michal Strnad <michal.strnad@nic.cz>
2020-08-06 08:11:56 +0200

    Ticket #27008 - Fix missing header

--- src/backend/epp/update_operation/on_value_of.hh
+++ src/backend/epp/update_operation/on_value_of.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2019  CZ.NIC, z. s. p. o.
+ * Copyright (C) 2018-2020  CZ.NIC, z. s. p. o.
  *
  * This file is part of FRED.
  *
@@ -23,7 +23,7 @@
 
 #include <boost/variant.hpp>
 
-#include <iosfwd>
+#include <iostream>
 #include <stdexcept>
 #include <utility>
 
