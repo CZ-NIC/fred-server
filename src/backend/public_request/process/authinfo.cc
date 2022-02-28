be235dcde33f9a04e5da9811f895f754ada38a25
Ales Friedl <ales.friedl@nic.cz>
2022-02-28 12:42:39 +0100

    Ticket #31493 - update licenses

--- src/backend/public_request/process/authinfo.cc
+++ src/backend/public_request/process/authinfo.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018-2021  CZ.NIC, z. s. p. o.
+ * Copyright (C) 2018-2022  CZ.NIC, z. s. p. o.
  *
  * This file is part of FRED.
  *
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with FRED.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 #include "src/backend/public_request/process/authinfo.hh"
 
 #include "src/backend/public_request/exceptions.hh"
